4cc966c336d905e91e4cb50fa23f236db20cc262
Jasper St. Pierre <jstpierre@mecheye.net>
2020-11-29 16:59:31 -0800

    f_pc_searcher

---
+++ include/f/f_pc/f_pc_searcher.h
@@ -0,0 +1,16 @@
+
+#ifndef F_PC_SEARCHER_H
+#define F_PC_SEARCHER_H
+
+#include "global.h"
+
+struct base_process_class;
+
+extern "C" {
+
+void * fpcSch_JudgeForPName(base_process_class *pProc, void *pUserData);
+void * fpcSch_JudgeByID(base_process_class *pProc, void *pUserData);
+
+};
+
+#endif
