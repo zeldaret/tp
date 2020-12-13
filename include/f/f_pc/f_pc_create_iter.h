58cd060a52577b7e8c2fa4578d18626e6870dfb3
lepelog <lepelog@users.noreply.github.com>
2020-12-13 02:28:24 +0100

    most of f_pc_create_iter

--- include/f/f_pc/f_pc_create_iter.h
+++ include/f/f_pc/f_pc_create_iter.h
@@ -4,10 +4,29 @@
 
 #include "SComponent/c_node_iter.h"
 
+struct node_method_data {
+    cNdIt_MethodFunc mFunc;
+    void* mpUserData;
+};
+
+struct node_judge_data {
+    cNdIt_JudgeFunc mFunc;
+    void* mpUserData;
+};
+
+struct fpcCtIt_jilprm_c {
+    u32 mUnk0;
+    cNdIt_JudgeFunc mFunc;
+    void* mpUserData;
+};
+
+
 extern "C" {
 
 int fpcCtIt_Method(cNdIt_MethodFunc pJudge, void *pUserData);
 void * fpcCtIt_Judge(cNdIt_JudgeFunc pJudge, void *pUserData);
+void *fpcCtIt_filter_JudgeInLayer(create_tag*,fpcCtIt_jilprm_c*);
+void *fpcCtIt_JudgeInLayer(u32 pUnk0, cNdIt_JudgeFunc pFunc, void* pUserData);
 
 };
 
--- src/f/f_pc/f_pc_create_iter.cpp
+++ src/f/f_pc/f_pc_create_iter.cpp
@@ -1,1 +1,44 @@
-// ok
+#include "global.h"
+#include "f/f_pc/f_pc_create_tag.h"
+#include "f/f_pc/f_pc_create_iter.h"
+#include "SComponent/c_tag_iter.h"
+#include "SComponent/c_list_iter.h"
+
+// g_fpcCtTg_Queue
+extern node_list_class lbl_803A3990;
+
+extern "C" {
+
+int fpcCtIt_Method(cNdIt_MethodFunc pJudge, void *pUserData) {
+    node_method_data iter;
+    iter.mFunc = pJudge;
+    iter.mpUserData = pUserData;
+    return cLsIt_Method(&lbl_803A3990, (cNdIt_MethodFunc)cTgIt_MethodCall, &iter);
+}
+
+void * fpcCtIt_Judge(cNdIt_JudgeFunc pJudge, void *pUserData) {
+    node_judge_data iter;
+    iter.mFunc = pJudge;
+    iter.mpUserData = pUserData;
+    return cLsIt_Judge(&lbl_803A3990, (cNdIt_JudgeFunc)cTgIt_JudgeFilter, &iter);
+}
+asm void *fpcCtIt_filter_JudgeInLayer(create_tag*,fpcCtIt_jilprm_c*) {
+nofralloc
+#include "asm/80020A40.s"
+}
+#ifndef NON_MATCHING
+void *fpcCtIt_JudgeInLayer(u32 pUnk0, cNdIt_JudgeFunc pFunc, void* pUserData) {
+    fpcCtIt_jilprm_c data;
+    data.mUnk0 = pUnk0;
+    data.mFunc = pFunc;
+    data.mpUserData = pUserData;
+    return fpcCtIt_Judge((cNdIt_JudgeFunc)fpcCtIt_filter_JudgeInLayer, &data);
+}
+#else
+asm void *fpcCtIt_JudgeInLayer(u16 pUnk0, cNdIt_JudgeFunc pFunc, void* pUserData) {
+nofralloc
+#include "asm/80020A94.s"
+}
+#endif
+
+}
