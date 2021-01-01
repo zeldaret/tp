2db48bc37ca8344bb3bbfad187e42bfa08da0579
Erin Moon <erin@hecke.rs>
2021-01-01 16:14:06 -0600

    d_kankyo, d_kankyo_*: autosplit
    - d_kankyo
    - d_kankyo_rain
    - d_kankyo_wether
    - d_kankyo_rain

--- include/SComponent/c_xyz.h
+++ include/SComponent/c_xyz.h
@@ -5,4 +5,8 @@
 
 class cXyz : public Vec {};
 
+extern cXyz lbl_80430CF4;  // SComponent::cXyz::Zero
+extern cXyz lbl_80430D0C;  // SComponent::cXyz::BaseX
+extern cXyz lbl_80430D24;  // SComponent::cXyz::BaseY
+
 #endif
