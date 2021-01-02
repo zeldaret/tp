b3f483bfce857f002ffeeefc4dc417655620ccdf
Erin Moon <erin@hecke.rs>
2021-01-02 02:02:03 -0600

    clang-format

--- src/d/d_meter/d_meter_HIO.cpp
+++ src/d/d_meter/d_meter_HIO.cpp
@@ -7,238 +7,237 @@ extern "C" {
 // dMeter_menuHIO_c::dMeter_menuHIO_c(void)
 asm void dMeter_menuHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_801FE330.s"
+#include "d/d_meter/d_meter_HIO/asm/func_801FE330.s"
 }
 
 // __ct__23dMeter_drawCollectHIO_cFv
 // dMeter_drawCollectHIO_c::dMeter_drawCollectHIO_c(void)
 asm void dMeter_drawCollectHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_801FE348.s"
+#include "d/d_meter/d_meter_HIO/asm/func_801FE348.s"
 }
 
 // __ct__22dMeter_drawOptionHIO_cFv
 // dMeter_drawOptionHIO_c::dMeter_drawOptionHIO_c(void)
 asm void dMeter_drawOptionHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_801FE4EC.s"
+#include "d/d_meter/d_meter_HIO/asm/func_801FE4EC.s"
 }
 
 // __ct__22dMeter_drawLetterHIO_cFv
 // dMeter_drawLetterHIO_c::dMeter_drawLetterHIO_c(void)
 asm void dMeter_drawLetterHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_801FE5CC.s"
+#include "d/d_meter/d_meter_HIO/asm/func_801FE5CC.s"
 }
 
 // __ct__23dMeter_drawFishingHIO_cFv
 // dMeter_drawFishingHIO_c::dMeter_drawFishingHIO_c(void)
 asm void dMeter_drawFishingHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_801FE730.s"
+#include "d/d_meter/d_meter_HIO/asm/func_801FE730.s"
 }
 
 // __ct__22dMeter_drawInsectHIO_cFv
 // dMeter_drawInsectHIO_c::dMeter_drawInsectHIO_c(void)
 asm void dMeter_drawInsectHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_801FE8DC.s"
+#include "d/d_meter/d_meter_HIO/asm/func_801FE8DC.s"
 }
 
 // __ct__27dMeter_drawCalibrationHIO_cFv
 // dMeter_drawCalibrationHIO_c::dMeter_drawCalibrationHIO_c(void)
 asm void dMeter_drawCalibrationHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_801FE954.s"
+#include "d/d_meter/d_meter_HIO/asm/func_801FE954.s"
 }
 
 // __ct__21dMeter_drawSkillHIO_cFv
 // dMeter_drawSkillHIO_c::dMeter_drawSkillHIO_c(void)
 asm void dMeter_drawSkillHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_801FE990.s"
+#include "d/d_meter/d_meter_HIO/asm/func_801FE990.s"
 }
 
 // __ct__25dMeter_drawEmpButtonHIO_cFv
 // dMeter_drawEmpButtonHIO_c::dMeter_drawEmpButtonHIO_c(void)
 asm void dMeter_drawEmpButtonHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_801FEAF0.s"
+#include "d/d_meter/d_meter_HIO/asm/func_801FEAF0.s"
 }
 
 // __ct__24dMeter_drawMiniGameHIO_cFv
 // dMeter_drawMiniGameHIO_c::dMeter_drawMiniGameHIO_c(void)
 asm void dMeter_drawMiniGameHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_801FEFF0.s"
+#include "d/d_meter/d_meter_HIO/asm/func_801FEFF0.s"
 }
 
 // __ct__25dMeter_drawLightDropHIO_cFv
 // dMeter_drawLightDropHIO_c::dMeter_drawLightDropHIO_c(void)
 asm void dMeter_drawLightDropHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_801FF31C.s"
+#include "d/d_meter/d_meter_HIO/asm/func_801FF31C.s"
 }
 
 // __ct__16dMeter_drawHIO_cFv
 // dMeter_drawHIO_c::dMeter_drawHIO_c(void)
 asm void dMeter_drawHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_801FF5B8.s"
+#include "d/d_meter/d_meter_HIO/asm/func_801FF5B8.s"
 }
 
 // __ct__16dMeter_ringHIO_cFv
 // dMeter_ringHIO_c::dMeter_ringHIO_c(void)
 asm void dMeter_ringHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_802001AC.s"
+#include "d/d_meter/d_meter_HIO/asm/func_802001AC.s"
 }
 
 // __ct__19dMeter_mapIconHIO_cFv
 // dMeter_mapIconHIO_c::dMeter_mapIconHIO_c(void)
 asm void dMeter_mapIconHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_802004B4.s"
+#include "d/d_meter/d_meter_HIO/asm/func_802004B4.s"
 }
 
 // __ct__16dMeter_fmapHIO_cFv
 // dMeter_fmapHIO_c::dMeter_fmapHIO_c(void)
 asm void dMeter_fmapHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_802006C8.s"
+#include "d/d_meter/d_meter_HIO/asm/func_802006C8.s"
 }
 
 // __dt__16dMeter_fmapHIO_cFv
 // dMeter_fmapHIO_c::~dMeter_fmapHIO_c(void)
 asm void dMeter_fmapHIO_c_NS_dtor(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80200BCC.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80200BCC.s"
 }
 
 // __ct__18dMeter_cursorHIO_cFv
 // dMeter_cursorHIO_c::dMeter_cursorHIO_c(void)
 asm void dMeter_cursorHIO_c(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80200CD4.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80200CD4.s"
 }
 
 // __dt__25dMeter_drawLightDropHIO_cFv
 // dMeter_drawLightDropHIO_c::~dMeter_drawLightDropHIO_c(void)
 asm void func_80200D58(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80200D58.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80200D58.s"
 }
 
 // __dt__25dMeter_drawEmpButtonHIO_cFv
 // dMeter_drawEmpButtonHIO_c::~dMeter_drawEmpButtonHIO_c(void)
 asm void func_80200DA0(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80200DA0.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80200DA0.s"
 }
 
 // __dt__24dMeter_drawMiniGameHIO_cFv
 // dMeter_drawMiniGameHIO_c::~dMeter_drawMiniGameHIO_c(void)
 asm void func_80200DE8(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80200DE8.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80200DE8.s"
 }
 
 // __dt__21dMeter_drawSkillHIO_cFv
 // dMeter_drawSkillHIO_c::~dMeter_drawSkillHIO_c(void)
 asm void func_80200E30(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80200E30.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80200E30.s"
 }
 
 // __dt__27dMeter_drawCalibrationHIO_cFv
 // dMeter_drawCalibrationHIO_c::~dMeter_drawCalibrationHIO_c(void)
 asm void func_80200E78(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80200E78.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80200E78.s"
 }
 
 // __dt__22dMeter_drawInsectHIO_cFv
 // dMeter_drawInsectHIO_c::~dMeter_drawInsectHIO_c(void)
 asm void func_80200EC0(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80200EC0.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80200EC0.s"
 }
 
 // __dt__23dMeter_drawFishingHIO_cFv
 // dMeter_drawFishingHIO_c::~dMeter_drawFishingHIO_c(void)
 asm void func_80200F08(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80200F08.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80200F08.s"
 }
 
 // __dt__22dMeter_drawLetterHIO_cFv
 // dMeter_drawLetterHIO_c::~dMeter_drawLetterHIO_c(void)
 asm void func_80200F50(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80200F50.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80200F50.s"
 }
 
 // __dt__22dMeter_drawOptionHIO_cFv
 // dMeter_drawOptionHIO_c::~dMeter_drawOptionHIO_c(void)
 asm void func_80200F98(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80200F98.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80200F98.s"
 }
 
 // __dt__23dMeter_drawCollectHIO_cFv
 // dMeter_drawCollectHIO_c::~dMeter_drawCollectHIO_c(void)
 asm void func_80200FE0(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80200FE0.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80200FE0.s"
 }
 
 // __dt__19dMeter_mapIconHIO_cFv
 // dMeter_mapIconHIO_c::~dMeter_mapIconHIO_c(void)
 asm void func_80201028(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80201028.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80201028.s"
 }
 
 // __ct__Q216dMeter_fmapHIO_c13MapBlinkAnimeFv
 // dMeter_fmapHIO_c::MapBlinkAnime::dMeter_fmapHIO_c::MapBlinkAnime(void)
 asm void dMeter_fmapHIO_c_NS_MapBlinkAnime(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80201070.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80201070.s"
 }
 
 // __dt__18dMeter_cursorHIO_cFv
 // dMeter_cursorHIO_c::~dMeter_cursorHIO_c(void)
 asm void dMeter_cursorHIO_c_NS_dtor(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80201098.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80201098.s"
 }
 
 // __dt__16dMeter_ringHIO_cFv
 // dMeter_ringHIO_c::~dMeter_ringHIO_c(void)
 asm void dMeter_ringHIO_c_NS_dtor(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_802010E0.s"
+#include "d/d_meter/d_meter_HIO/asm/func_802010E0.s"
 }
 
 // __dt__16dMeter_drawHIO_cFv
 // dMeter_drawHIO_c::~dMeter_drawHIO_c(void)
 asm void dMeter_drawHIO_c_NS_dtor(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80201128.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80201128.s"
 }
 
 // __dt__16dMeter_menuHIO_cFv
 // dMeter_menuHIO_c::~dMeter_menuHIO_c(void)
 asm void dMeter_menuHIO_c_NS_dtor(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_8020123C.s"
+#include "d/d_meter/d_meter_HIO/asm/func_8020123C.s"
 }
 
 // __sinit_d_meter_HIO_cpp
-// 
+//
 asm void func_80201284(void) {
     nofralloc
-    #include "d/d_meter/d_meter_HIO/asm/func_80201284.s"
+#include "d/d_meter/d_meter_HIO/asm/func_80201284.s"
 }
-
 };
