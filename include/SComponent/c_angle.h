c1b8e3791226067a8d3522df66fd7a0b89aa79d7
lepelog <lepelog@users.noreply.github.com>
2021-01-02 20:54:00 +0100

    c_angle

--- include/SComponent/c_angle.h
+++ include/SComponent/c_angle.h
@@ -1,16 +1,20 @@
 #ifndef __C_ANGLE_H_
 #define __C_ANGLE_H_
 
-#include "global.h"
 #include "SComponent/c_xyz.h"
+#include "global.h"
 
 class cSAngle {
 private:
     s16 angle;
+
 public:
+    cSAngle() {}
     cSAngle(const cSAngle&);
     cSAngle(s16);
     cSAngle(float);
+    s16 Get() const { return this->angle; }
+    void Set(s16 angle) { this->angle = angle; }
     void Val(const cSAngle&);
     void Val(s16);
     void Val(float);
@@ -37,9 +41,12 @@ cSAngle operator+(short, const cSAngle&);
 cSAngle operator-(short, const cSAngle&);
 
 class cDegree {
+private:
+    float degree;
+
 public:
     cDegree(float);
-    s16 Formal(void);
+    cDegree& Formal(void);
     void Val(float);
     float Radian(void) const;
     float Sin(void) const;
@@ -48,7 +55,13 @@ public:
 };
 
 class cSPolar {
+private:
+    float radial;
+    cSAngle angle1;
+    cSAngle angle2;
+
 public:
+    cSPolar() {}
     cSPolar(const cXyz&);
     cSPolar& Formal(void);
     void Val(float, short, short);
@@ -58,21 +71,25 @@ public:
 };
 
 class cSGlobe {
+private:
+    float radius;
+    cSAngle azimuth;
+    cSAngle inclination;
+
 public:
     cSGlobe(const cSGlobe&);
     cSGlobe(float, short, short);
     cSGlobe(float, const cSAngle&, const cSAngle&);
     cSGlobe(const cXyz&);
-    void Formal(void);
+    cSGlobe& Formal(void);
     void Val(const cSGlobe&);
     void Val(float, short, short);
     void Val(float, const cSAngle&, const cSAngle&);
     void Val(const cXyz&);
     cXyz Xyz(void) const;
     void Polar(cSPolar*) const;
-    void Norm(void) const;
-    void Invert(void);
+    cXyz Norm(void) const;
+    cSGlobe& Invert(void);
 };
-
 
 #endif
