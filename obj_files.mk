246d2e163bb3ac0f3fbe075a290bbd0266c1ad26
Erin Moon <erin@hecke.rs>
2021-01-02 00:38:16 -0600

    d_kantera_icon_meter: split

--- obj_files.mk
+++ obj_files.mk
@@ -172,7 +172,7 @@ TEXT_O_FILES := 						            \
             $(BUILD_DIR)/asm/d/d_kyeff.o    \
             $(BUILD_DIR)/asm/d/d_kyeff2.o    \
             $(BUILD_DIR)/asm/d/ky/d_ky_thunder.o    \
-            $(BUILD_DIR)/asm/d/kantera/icon/d_kantera_icon_meter.o    \
+            $(BUILD_DIR)/src/d/d_kantera_icon_meter.o    \
             $(BUILD_DIR)/asm/d/menu/d_menu_calibration.o    \
             $(BUILD_DIR)/asm/d/menu/d_menu_collect.o    \
             $(BUILD_DIR)/asm/d/menu/d_menu_dmap.o    \
