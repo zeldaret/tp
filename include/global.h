689d83dd238b5e07cfe513784dd2ad47a378b395
lepelog <lepelog@users.noreply.github.com>
2021-01-05 18:32:20 +0100

    Move and type PSVEC and PSMTX funcs and follow includes in functions.h (#68)
    Co-authored-by: Pheenoh <pheenoh@gmail.com>

--- include/global.h
+++ include/global.h
@@ -8,10 +8,6 @@
 // Align X to the next N bytes (N must be power of two)
 #define ALIGN_NEXT(X, N) ALIGN_PREV(((X) + (N)-1), N)
 
-struct Vec {
-    float x, y, z;
-};
-
 #define JUT_ASSERT(...)
 #define ASSERT(...)
 
