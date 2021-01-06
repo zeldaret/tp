450309d1afebbc884ae08b34d09b7aefb98183af
lepelog <lepelog@users.noreply.github.com>
2021-01-06 13:00:26 +0100

    inline definitions in random.h and format

--- include/JSystem/JMath/random.h
+++ include/JSystem/JMath/random.h
@@ -8,6 +8,23 @@ struct TRandom_fast_ {
     u32 value;
 
     TRandom_fast_(u32 value);
+    u32 get(void) {
+        value = (value * 0x19660d) + 0x3c6ef35f;
+        return value;
+    }
+
+    u32 get_bit32(void) { return this->get(); }
+
+    float get_ufloat_1(void) {
+        union {
+            f32 f;
+            s32 s;
+        } out;
+        out.s = ((this->get() >> 9) & 0x7FFFFF) | 0x3f800000;
+        return out.f - 1;
+    }
+
+    void setSeed(u32 seed) { value = seed; }
 };
 }  // namespace JMath
 
--- libs/Z2AudioLib/Z2Calc.cpp
+++ libs/Z2AudioLib/Z2Calc.cpp
@@ -5,44 +5,42 @@ extern s8 lbl_80451330;
 // Z2AudioLib::oRandom$401
 extern JMath::TRandom_fast_ lbl_80451334;
 
-float Z2Calc::linearTransform(float param1, float param2, float param3, float param4, float param5, bool param6){
+float Z2Calc::linearTransform(float param1, float param2, float param3, float param4, float param5,
+                              bool param6) {
     float temp;
-    
+
     temp = param4 + (param1 - param2) * ((param5 - param4) / (param3 - param2));
 
-    if(param6){
+    if (param6) {
         return temp;
-    }
-    else if(param4 < param5){
-        if(temp > param5){
+    } else if (param4 < param5) {
+        if (temp > param5) {
             return param5;
-        }
-        else if(temp < param4){
+        } else if (temp < param4) {
             return param4;
-        }
-        else{
+        } else {
             return temp;
         }
-    }
-    else if(temp > param4){
+    } else if (temp > param4) {
         return param4;
-    }
-    else if(temp < param5){
+    } else if (temp < param5) {
         return param5;
-    }
-    else{
+    } else {
         return temp;
     }
 }
 
-float Z2Calc::getParamByExp(float f1, float f2, float f3, float f4, float f5, float f6, Z2Calc::CurveSign sign) {
+float Z2Calc::getParamByExp(float f1, float f2, float f3, float f4, float f5, float f6,
+                            Z2Calc::CurveSign sign) {
     float out;
     if (sign == Z2Calc::CURVE_SIGN_1) {
-        float tmp = func_8036C740(Z2Calc::linearTransform(f1, f2, f3, /* 0.0 */lbl_80455828, f4, true));
-        out = Z2Calc::linearTransform(tmp, /* 1.0 */lbl_8045582C, func_8036C740(f4), f5, f6, true);
+        float tmp =
+            func_8036C740(Z2Calc::linearTransform(f1, f2, f3, /* 0.0 */ lbl_80455828, f4, true));
+        out = Z2Calc::linearTransform(tmp, /* 1.0 */ lbl_8045582C, func_8036C740(f4), f5, f6, true);
     } else if (sign == Z2Calc::CURVE_SIGN_0) {
-        float tmp = func_8036C740(Z2Calc::linearTransform(f1, f2, f3, f4, /* 0.0 */lbl_80455828, true));
-        out = Z2Calc::linearTransform(tmp, func_8036C740(f4), /* 1.0 */lbl_8045582C, f5, f6, true);
+        float tmp =
+            func_8036C740(Z2Calc::linearTransform(f1, f2, f3, f4, /* 0.0 */ lbl_80455828, true));
+        out = Z2Calc::linearTransform(tmp, func_8036C740(f4), /* 1.0 */ lbl_8045582C, f5, f6, true);
     } else {
         out = Z2Calc::linearTransform(f1, f2, f3, f5, f6, false);
     }
@@ -56,8 +54,8 @@ float Z2Calc::getParamByExp(float f1, float f2, float f3, float f4, float f5, fl
 }
 
 float Z2Calc::getRandom(float f1, float f2, float f3) {
-    float tmp = /* 2.0 */lbl_80455830 * f3;
-    float tmp2 = (/* 1.0 */lbl_8045582C - f3) * /* -2.0 */lbl_80455834;
+    float tmp = /* 2.0 */ lbl_80455830 * f3;
+    float tmp2 = (/* 1.0 */ lbl_8045582C - f3) * /* -2.0 */ lbl_80455834;
     f1 *= Z2Calc::getRandom_0_1() < f3 ? tmp : tmp2;
     float tmp3 = func_8036C780(Z2Calc::getRandom_0_1(), f2);
     return tmp3 * f1;
@@ -70,7 +68,8 @@ float Z2Calc::getRandom_0_1(void) {
         __ct__Q25JMath13TRandom_fast_FUl(&lbl_80451334, 0);
         lbl_80451330 = true;
     }
-    lbl_80451334.value = (lbl_80451334.value * 0x19660d) + 0x3c6ef35f; 
+    // this is really just lbl_80451334.get_ufloat_1()
+    lbl_80451334.value = (lbl_80451334.value * 0x19660d) + 0x3c6ef35f;
     union {
         f32 f;
         s32 s;
@@ -79,53 +78,50 @@ float Z2Calc::getRandom_0_1(void) {
     return out.f - lbl_8045582C;
 }
 
-
-void Z2Calc::FNoise1f::setParam(float param1, float param2, float param3){
+void Z2Calc::FNoise1f::setParam(float param1, float param2, float param3) {
     this->unk0 = param1;
     this->unk4 = param1;
     this->unk12 = param2;
     this->unk8 = param3;
 }
 
-float Z2Calc::FNoise1f::tau(float param1){
+float Z2Calc::FNoise1f::tau(float param1) {
     float temp1;
     float temp2;
 
     temp1 = param1;
     temp2 = temp1 + (temp1 * temp1);
 
-    if(temp2 > lbl_8045582C){
+    if (temp2 > lbl_8045582C) {
         temp2 = temp2 - lbl_8045582C;
-        
-        if(temp1 < this->unk12){
+
+        if (temp1 < this->unk12) {
             temp2 = temp2 + this->unk12;
         }
 
         return temp2;
-    }
-    else if(temp1 < this->unk12){
+    } else if (temp1 < this->unk12) {
         temp2 = temp2 + this->unk12;
     }
     return temp2;
 }
 
-float Z2Calc::FNoise1f::calcNoise1f(void){
+float Z2Calc::FNoise1f::calcNoise1f(void) {
     float temp1;
     float temp2;
 
     temp1 = this->unk0;
 
-    if(this->unk0 < this->unk4){
+    if (this->unk0 < this->unk4) {
         this->unk0 = temp1 + this->unk8;
 
-        if(this->unk0 < this->unk4){
+        if (this->unk0 < this->unk4) {
             return this->unk0;
         }
-    }
-    else{
+    } else {
         this->unk0 = temp1 - this->unk8;
-        
-        if(this->unk0 > this->unk4){
+
+        if (this->unk0 > this->unk4) {
             return this->unk0;
         }
     }
