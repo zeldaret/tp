87076f9f07cba0d9133eda37d6c069dd0ab08b2c
lepelog <lepelog@users.noreply.github.com>
2021-01-07 16:15:32 +0100

    decompiled some bgs_chk funcs

--- include/SComponent/c_bg_s_poly_info.h
+++ include/SComponent/c_bg_s_poly_info.h
@@ -4,9 +4,14 @@
 #include "global.h"
 
 class cBgS_PolyInfo {
+private:
+    /* 0x00 */ u16 mPolyIndex;
+    /* 0x02 */ u16 unk_0x02;
+    /* 0x04 */ void* unk_0x04;
+    /* 0x08 */ u32 unk_0x08;
 public:
     cBgS_PolyInfo(void);
-    ~cBgS_PolyInfo(void);
+    virtual ~cBgS_PolyInfo(void);
     bool ChkSetInfo(void) const;
     void ClearPi(void);
     void SetPolyInfo(const cBgS_PolyInfo&);
