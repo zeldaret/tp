6d946e8330738df183dd8be4edf4764b477e6e75
lepelog <lepelog@users.noreply.github.com>
2021-01-07 02:39:56 +0100

    match some Z2Calc functions (#72)
    * Z2Calc OK
    
    * inline definitions in random.h and format
    
    * set -enum int compiler flag and fix enum hacks
    
    * note for get_ufloat_1 inline
    
    * PR suggestions and UB note
    
    Co-authored-by: Pheenoh <pheenoh@gmail.com>

--- include/functions.h
+++ include/functions.h
@@ -587,8 +587,10 @@ void Z2SeqMgr_NS_setBattleLastHit(void);
 // Z2Calc
 extern "C" {
 void linearTransform__6Z2CalcFfffffb(void);
-void func_8036C740(void);
-void func_8036C780(void);
+// MSL_C.PPCEABI.bare.H::exp
+double func_8036C740(double);
+// MSL_C.PPCEABI.bare.H::pow
+double func_8036C780(double, double);
 void getRandom_0_1__6Z2CalcFv(void);
 void getParamByExp__6Z2CalcFffffffQ26Z2Calc9CurveSign(void);
 }
@@ -5601,7 +5603,6 @@ void mDoMtx_stack_c_NS_transM(float, float, float);
 
 void setCameraPolygonPos__11Z2StatusMgrFP3Vec(Vec*);
 void setCameraGroupInfo__11Z2StatusMgrFUc(u8);
-
 void Set__8cM3dGSphFRC4cXyzf(void);
 void __pl__FsRC7cSAngle(void);
 void __mi__FsRC7cSAngle(void);
