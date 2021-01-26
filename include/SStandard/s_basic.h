bb23305bfe565a962f302b445a46eca3f277c9ad
lepelog <lepelog@users.noreply.github.com>
2021-01-26 10:01:58 +0100

    decompile s_basic, cleanup mangled names in f_pc

---
+++ include/SStandard/s_basic.h
@@ -0,0 +1,9 @@
+#ifndef S_BASIC_H_
+#define S_BASIC_H_
+
+#include "global.h"
+
+void sBs_FillArea_s(void* pPtr, u32 pNumBytes, s16 pValue);
+void sBs_ClearArea(void* pPtr, u32 pNumBytes);
+
+#endif
