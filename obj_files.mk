6d554264432040969b9392241ac885f9661989ea
Pheenoh <pheenoh@gmail.com>
2021-01-31 16:16:06 -0500

    split m_Do_MemCard

--- obj_files.mk
+++ obj_files.mk
@@ -38,7 +38,7 @@ TEXT_O_FILES := 						            \
             $(BUILD_DIR)/asm/m/Do/dvd/m_Do_dvd_thread.o \
             $(BUILD_DIR)/src/m_Do/m_Do_DVDError.o \
             $(BUILD_DIR)/asm/rodata/rodata_m_Do_MemCard.o \
-            $(BUILD_DIR)/asm/m/Do/m_Do_MemCard.o \
+            $(BUILD_DIR)/src/m_Do/m_Do_MemCard.o \
             $(BUILD_DIR)/asm/rodata/rodata_m_Do_MemCardRWmng.o \
             $(BUILD_DIR)/asm/m/Do/m_Do_MemCardRWmng.o \
             $(BUILD_DIR)/asm/rodata/rodata_m_Do_machine_exception.o \
