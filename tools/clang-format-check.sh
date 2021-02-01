b595f0d0bf2bde1d0e55cc1f6cfc5df71cb07d98
Pheenoh <pheenoh@gmail.com>
2021-01-31 20:55:56 -0500

    fix clang-format check (#110)

---
+++ tools/clang-format-check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+clangFormatTargets=$(find . -type f -regex '.*\.\(cpp\|hpp\|h\|cc\|cxx\)')
+
+for inputFile in $clangFormatTargets
+do
+    clang-format-10 -style=file $inputFile > $inputFile-formatted
+    diff $inputFile $inputFile-formatted
+    if [ $? != 0 ] ; then
+        exit 1
+    else
+        rm $inputFile-formatted
+    fi
+done
