86a69bbb4b443df055bf899c6581089583b03863
Pheenoh <pheenoh@gmail.com>
2021-02-13 15:42:35 -0500

    fapGm_After OK, split f_op_scene_mng, split f_op_camera_mng, split f_op_overlap_mng

---
+++ include/f/f_op/f_op_camera_mng.h
@@ -0,0 +1,6 @@
+#ifndef F_F_OP_CAMERA_MNG_H_
+#define F_F_OP_CAMERA_MNG_H_
+
+void fopCamM_Management(void);
+
+#endif
---
+++ include/f/f_op/f_op_overlap_mng.h
@@ -0,0 +1,6 @@
+#ifndef F_F_OP_OVERLAP_MNG_H_
+#define F_F_OP_OVERLAP_MNG_H_
+
+void fopOvlpM_Management(void);
+
+#endif
