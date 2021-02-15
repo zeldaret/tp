8c8db49733bb6a9961d30e75ae4ef7f28db1977d
Pheenoh <pheenoh@gmail.com>
2021-02-15 15:24:02 -0500

    split f_op_msg_mng

--- obj_files.mk
+++ obj_files.mk
@@ -71,7 +71,7 @@ TEXT_O_FILES := 						            \
             $(BUILD_DIR)/asm/sdata2/f_op_kankyo_mng.o \
             $(BUILD_DIR)/src/f/f_op/f_op_kankyo_mng.o \
             $(BUILD_DIR)/asm/sdata2/f_op_msg_mng.o \
-            $(BUILD_DIR)/asm/f/op/msg/f_op_msg_mng.o \
+            $(BUILD_DIR)/src/f/f_op/f_op_msg_mng.o \
             $(BUILD_DIR)/src/f/f_op/f_op_draw_iter.o \
             $(BUILD_DIR)/src/f/f_op/f_op_draw_tag.o \
             $(BUILD_DIR)/asm/f/op/scene/f_op_scene_pause.o \
