edbf8e600ec40e4565ad785971581c4fbab2c3dc
Pheenoh <pheenoh@gmail.com>
2021-02-15 16:04:27 -0500

    split f_op_kankyo

--- obj_files.mk
+++ obj_files.mk
@@ -66,7 +66,7 @@ TEXT_O_FILES := 						            \
             $(BUILD_DIR)/asm/f/op/scene/f_op_scene_req.o \
             $(BUILD_DIR)/src/f/f_op/f_op_scene_tag.o \
             $(BUILD_DIR)/src/f/f_op/f_op_view.o \
-            $(BUILD_DIR)/asm/f/op/f_op_kankyo.o \
+            $(BUILD_DIR)/src/f/f_op/f_op_kankyo.o \
             $(BUILD_DIR)/asm/f/op/f_op_msg.o \
             $(BUILD_DIR)/asm/sdata2/f_op_kankyo_mng.o \
             $(BUILD_DIR)/src/f/f_op/f_op_kankyo_mng.o \
