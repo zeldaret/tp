1876efc170abf0b2b028be9de2ad1d499fbed672
Pheenoh <pheenoh@gmail.com>
2021-02-16 15:47:00 -0500

    f_op_scene_pause OK

--- include/f/f_op/f_op_scene_pause.h
+++ include/f/f_op/f_op_scene_pause.h
@@ -1,7 +1,13 @@
-#ifndef F_F_FOP_SCENE_PAUSE_H_
-#define F_F_FOP_SCENE_PAUSE_H_
+#ifndef F_F_OP_SCENE_PAUSE_H_
+#define F_F_OP_SCENE_PAUSE_H_
 
-struct scene_class;
+#include "dolphin/types.h"
+
+class scene_class {
+public:
+    /* 0x00 */ u8 field_0x00[0x2c];
+    /* 0x2C */ int* field_0x2C;
+};
 
 int fopScnPause_Enable(scene_class* pScene);
 int fopScnPause_Disable(scene_class* pScene);
