f19f453a807dc128328e6573a77a70f89bc9cfcf
lepelog <lepelog@users.noreply.github.com>
2021-04-27 01:12:40 +0200

    fix format check by not checking tools for clang-format

--- tools/clang-format-check.sh
+++ tools/clang-format-check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-clangFormatTargets=$(find . -type f -regex '.*\.\(cpp\|hpp\|h\|cc\|cxx\)')
+clangFormatTargets=$(find include libs rel src -type f -regex '.*\.\(cpp\|hpp\|h\|cc\|cxx\)')
 
 for inputFile in $clangFormatTargets
 do
