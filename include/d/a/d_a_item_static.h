c3600307eb3f9f71d052a4a34fad292a6ee77dc5
TakaRikka <38417346+TakaRikka@users.noreply.github.com>
2021-04-25 23:55:47 -0700

    move d_a_item_static

--- include/d/a/d_a_item_static.h
+++ include/d/a/d_a_item_static.h
@@ -2,5 +2,23 @@
 #define D_A_D_A_ITEM_STATIC_H
 
 #include "dolphin/types.h"
+#include "SSystem/SComponent/c_lib.h"
+
+class daItem_c {
+public:
+    u32 startCtrl();
+    u32 startControl();
+    u32 endControl();
+
+    void setFlag(u8 pFlag) {
+        cLib_onBit(unk2376, pFlag);
+    }
+
+private:
+    u8 unk0[0x948];
+    u8 unk2376;
+    u8 unk2377;
+    u8 unk2378;
+};
 
 #endif /* D_A_D_A_ITEM_STATIC_H */
