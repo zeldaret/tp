bd636e95753862583123551bdf82ab067ecdfbb1
lepelog <lepelog@users.noreply.github.com>
2021-04-28 22:17:48 +0200

    delete unused asm

--- asm/JSystem/JKernel/JKRHeap/becomeSystemHeap__7JKRHeapFv.s
+++
@@ -1,5 +0,0 @@
-lbl_802CE428:
-/* 802CE428  80 0D 8D F0 */	lwz r0, sSystemHeap__7JKRHeap(r13)
-/* 802CE42C  90 6D 8D F0 */	stw r3, sSystemHeap__7JKRHeap(r13)
-/* 802CE430  7C 03 03 78 */	mr r3, r0
-/* 802CE434  4E 80 00 20 */	blr 
