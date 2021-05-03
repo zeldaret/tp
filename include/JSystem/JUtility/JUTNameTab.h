57d939cf1eae14b920fd1e83c095945c2e1e3c5b
Jasper St. Pierre <jstpierre@mecheye.net>
2021-05-03 07:27:46 -0700

    JUTNameTab

--- include/JSystem/JUtility/JUTNameTab.h
+++ include/JSystem/JUtility/JUTNameTab.h
@@ -3,4 +3,34 @@
 
 #include "dolphin/types.h"
 
+struct ResNTAB {
+    u16 mEntryNum;
+    u16 mPad0;
+    struct Entry {
+        u16 mKeyCode;
+        u16 mOffs;
+    } mEntries[1];
+
+    inline const char* getName(u32 index) const {
+        return ((const char*)mEntries) + mEntries[index].mOffs - 4;
+    }
+};
+
+class JUTNameTab {
+public:
+    JUTNameTab();
+    JUTNameTab(const ResNTAB* pNameTable);
+    virtual ~JUTNameTab() {}
+
+    void setResource(const ResNTAB* pNameTable);
+    s32 getIndex(char const*) const;
+    const char* getName(u16 index) const;
+    u16 calcKeyCode(char const* pName) const;
+
+private:
+    const ResNTAB* mpNameTable;
+    const char* mpStrData;
+    u16 mNameNum;
+};
+
 #endif /* JUTNAMETAB_H */
