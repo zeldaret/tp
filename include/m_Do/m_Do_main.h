d78e7708bfb6d0389b64aa9cd813324059ce114c
TakaRikka <38417346+TakaRikka@users.noreply.github.com>
2021-06-09 04:03:28 -0700

    move m_Do_main

--- include/m_Do/m_Do_main.h
+++ include/m_Do/m_Do_main.h
@@ -2,5 +2,38 @@
 #define M_DO_M_DO_MAIN_H
 
 #include "dolphin/types.h"
+#include "JSystem/JKernel/JKRExpHeap.h"
+
+class HeapCheck {
+public:
+    void CheckHeap1(void);
+    s32 getUsedCount(void) const;
+    void heapDisplay(void) const;
+    u32& getUsedCountRef() { return mUsedCount; }
+    u32& getTotalUsedSizeRef() { return mTotalUsedSize; }
+    JKRExpHeap* getHeap() { return mHeap; }
+    void setHeap(JKRExpHeap* i_heap) { mHeap = i_heap; }
+    void setHeapSize(u32 i_size) { mTargetHeapSize = i_size; }
+    s32 getMaxTotalUsedSize() { return mMaxTotalUsedSize; }
+    s32 getMaxTotalFreeSize() { return mMaxTotalFreeSize; }
+
+private:
+    /* 0x00 */ char* mName;
+    /* 0x04 */ char* mJName;
+    /* 0x08 */ JKRExpHeap* mHeap;
+    /* 0x0C */ s32 mMaxTotalUsedSize;
+    /* 0x10 */ s32 mMaxTotalFreeSize;
+    /* 0x14 */ u8 unk20[8];
+    /* 0x1C */ u32 mTargetHeapSize;
+    /* 0x20 */ u32 mUsedCount;
+    /* 0x24 */ u32 mTotalUsedSize;
+};
+
+struct mDoMain {
+    static u8 COPYDATE_STRING[18 + 2 /* padding */];
+    static u32 memMargin;
+    static u8 sPowerOnTime[4]; // should be u64
+    static u8 sHungUpTime[4]; // should be u64
+};
 
 #endif /* M_DO_M_DO_MAIN_H */
