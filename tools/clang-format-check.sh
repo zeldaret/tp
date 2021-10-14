b88cb8cdc0b7a8af4df22e303ff81e0e617dbc85
icogn <21243684+icogn@users.noreply.github.com>
2021-10-14 17:01:58 -0500

    Update clang-format-check.sh and make executable (#156)
    * Update clang-format-check.sh and make executable
    
    * Make executable
    
    Co-authored-by: Isaac <iico7no2@gmail.com>

--- tools/clang-format-check.sh
+++ tools/clang-format-check.sh
@@ -4,11 +4,8 @@ clangFormatTargets=$(find . -type f -regex '.*\.\(cpp\|hpp\|h\|cc\|cxx\)')
 
 for inputFile in $clangFormatTargets
 do
-    clang-format-10 -style=file $inputFile > $inputFile-formatted
-    diff $inputFile $inputFile-formatted
+    clang-format-10 --dry-run --Werror -style=file $inputFile
     if [ $? != 0 ] ; then
         exit 1
-    else
-        rm $inputFile-formatted
     fi
 done
