zeldaret/tp

commit c4ef30bc95d3e69eb84cb5efa76598bb6e900662

Port JStudio work to new branch (#160)
* build fix

* stb

* functionvalue

* fvb

* jstudio cleanup

* d

rel/d/a/d_a_title/d_a_title.cpp
@@ -4,6 +4,7 @@
 //
 
 #include "rel/d/a/d_a_title/d_a_title.h"
+#include "JSystem/JStudio/JStudio/stb.h"
 #include "dol2asm.h"
 #include "dolphin/types.h"
 

include/JSystem/JGadget/binary.h
@@ -3,4 +3,52 @@
 
 #include "dolphin/types.h"
 
+namespace JGadget {
+namespace binary {
+
+struct TEBit {
+    u32 value;
+};
+
+const void* parseVariableUInt_16_32_following(const void* pu16, u32* pu32First, u32* pu32Second,
+                                              TEBit* tebit);
+
+inline u32 align_roundUp(u32 arg0, u32 uAlign) {
+    return (arg0 + uAlign - 1) & ~(uAlign - 1);
+}
+
+struct TParseData {
+    TParseData(const void* pContent) : raw(pContent) {}
+
+    const void* getRaw() const { return raw; }
+    void setRaw(const void* p) { raw = p; }
+
+    const void* raw;
+};
+
+template <int T>
+struct TParseData_aligned : public TParseData {
+    TParseData_aligned(const void* pContent) : TParseData(pContent) {}
+    void setRaw(const void* p) {
+        if ((u32)p % T != 0) {
+            JUTWarn w;
+            w << "misaligned : " << (u32)p;
+        }
+        static_cast<TParseData*>(this)->setRaw(p);
+    }
+};
+
+// Base for header and/or block parsing
+struct TParse_header_block {
+    virtual ~TParse_header_block();
+
+    virtual bool parseHeader_next(const void** ppData_inout, u32* puBlock_out, u32 arg2) = 0;
+    virtual bool parseBlock_next(const void** ppData_inout, u32* puData_out, u32 arg2) = 0;
+
+    bool parse_next(const void** ppData_inout, u32 a2);
+};
+
+}  // namespace binary
+}  // namespace JGadget
+
 #endif /* BINARY_H */

include/JSystem/JGadget/linklist.h
@@ -22,15 +22,15 @@ struct TNodeLinkList {
         TLinkListNode* node;
     };
 
-    TNodeLinkList() : mListNode() { Initialize_(); }
+    TNodeLinkList() : ocObject_() { Initialize_(); }
     void Initialize_() {
         ptr = NULL;
-        mListNode.mNext = &mListNode;
-        mListNode.mPrev = &mListNode;
+        ocObject_.mNext = &ocObject_;
+        ocObject_.mPrev = &ocObject_;
     }
 
     iterator end() {
-        iterator iter(&mListNode);
+        iterator iter(&ocObject_);
         return iter;
     }
 
@@ -40,11 +40,11 @@ struct TNodeLinkList {
     /* 802DCB08 */ void splice(JGadget::TNodeLinkList::iterator, JGadget::TNodeLinkList&,
                                JGadget::TNodeLinkList::iterator);
     /* 802DCBA8 */ void Insert(JGadget::TNodeLinkList::iterator, JGadget::TLinkListNode*);
-    /* 802DCBD4 */ void Erase(JGadget::TLinkListNode*);
+    /* 802DCBD4 */ iterator Erase(JGadget::TLinkListNode*);
     /* 802DCBF8 */ void Remove(JGadget::TLinkListNode*);
 
     /* 0x00 */ TNodeLinkList* ptr;
-    /* 0x04 */ TLinkListNode mListNode;
+    /* 0x04 */ TLinkListNode ocObject_;
 };  // Size: 0xC
 
 template <typename T, int I>
@@ -55,11 +55,16 @@ struct TLinkList : public TNodeLinkList {
         iterator(TNodeLinkList::iterator iter) : TNodeLinkList::iterator(iter) {}
     };
 
-    TLinkListNode* Element_toNode(T* element) const { return &element->mListNode; }
+    TLinkListNode* Element_toNode(T* element) const { return &element->ocObject_; }
 
     void Insert(TLinkList::iterator iter, T* element) {
         TLinkListNode* node = Element_toNode(element);
         TNodeLinkList::Insert(iter, node);
+    }
+
+    iterator Erase(T* element) {
+        TLinkListNode* node = Element_toNode(element);
+        return ((TNodeLinkList*)this)->Erase(node);
     }
 
     TLinkList::iterator end() {

include/JSystem/JGadget/search.h
@@ -1,0 +1,19 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include "global.h"
+
+namespace JGadget {
+
+//! @todo: mangled name isn't correct, fix this
+//! Current: toValueFromIndex<PFdd_d>__7JGadgetFiPCPFdd_dUlRCPFdd_d
+//!  Target: toValueFromIndex<PFdd_d>__7JGadgetFiPCPFdd_dUlRCPFdd_d_RCPFdd_d
+template <typename T>
+inline const T& toValueFromIndex(int idx, const T* pValue, u32 count, const T& fallback) {
+    ASSERT(pValue != NULL);
+    return (idx >= count) ? fallback : pValue[idx];
+}
+
+}  // namespace JGadget
+
+#endif /* SEARCH_H */

include/JSystem/JGadget/vector.h
@@ -1,0 +1,71 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include "global.h"
+
+extern u8 data_804511E0;
+extern u8 lit_569[];
+
+namespace JGadget {
+
+namespace vector {
+
+u32 extend_default(u32 arg1, u32 arg2, u32 arg3);
+
+typedef u32 (*ExtendFunc)(u32, u32, u32);
+
+}  // namespace vector
+
+template <typename T>
+struct TAllocator {
+    static TAllocator get() {}
+    inline TAllocator() { _0 = lit_569[0]; }
+    /* 0x0 */ u8 _0;
+    /* 0x4 */ u32 _4;
+    /* 0x8 */ u32 _8;
+    /* 0xc */ u32 _c;
+};
+
+template <typename T, template <class> class Allocator>
+struct TVector {
+    TVector(Allocator<T> alloc) {
+        _0 = NULL;
+        pBegin_ = _0;
+        _c = NULL;
+        extend = vector::extend_default;
+    }
+
+    void** begin() { return pBegin_; }
+    void** end() { return pEnd_; }
+    // void erase(void** arg1, void** arg2) {}
+
+    void** _0;
+    void** pBegin_;
+    void** pEnd_;
+    u32 _c;
+    vector::ExtendFunc extend;
+};
+
+struct TVector_pointer_void : TVector<void*, TAllocator> {
+    /* 802DCCD0 */ TVector_pointer_void(JGadget::TAllocator<void*> const&);
+    /* 802DCCFC */ ~TVector_pointer_void();
+    /* 802DCDC4 */ void erase(void**, void**);
+    void insert(void**, void* const&);
+
+    void clear() { erase(begin(), end()); }
+    void push_back(const void*& ref) { insert(end(), (void* const&)ref); }
+};
+
+template <typename T>
+struct TVector_pointer : TVector_pointer_void {
+    // TVector_pointer(const TAllocator<void*>& allocator) : TVector_pointer_void(allocator) {}
+    ~TVector_pointer() {}
+
+    void push_back(const T& ref) {
+        static_cast<TVector_pointer_void*>(this)->push_back((const void*&)ref);
+    }
+};
+
+}  // namespace JGadget
+
+#endif /* VECTOR_H */

include/JSystem/JStudio/JStudio/functionvalue.h
@@ -1,6 +1,392 @@
 #ifndef FUNCTIONVALUE_H
 #define FUNCTIONVALUE_H
 
+#include "JSystem/JGadget/search.h"
+#include "JSystem/JGadget/vector.h"
 #include "dolphin/types.h"
+
+extern u8 lit_652[];
+
+namespace JStudio {
+
+typedef f64 (*ExtrapolateParameter)(f64, f64);
+
+class TFunctionValue;
+class TFunctionValueAttributeSet;
+
+class TFunctionValueAttribute_refer;
+class TFunctionValueAttribute_range;
+class TFunctionValueAttribute_interpolate;
+
+class TFunctionValue {
+public:
+    enum TEProgress { PROG_INIT };
+    enum TEAdjust { ADJ_INIT };
+    enum TEOutside { OUT_INIT };
+    enum TEInterpolate {};
+
+    /* 80281690 */ TFunctionValue();
+    /* 802816A0 */ virtual ~TFunctionValue() = 0;
+
+    virtual u32 getType() const = 0;
+    virtual TFunctionValueAttributeSet getAttributeSet() = 0;
+    virtual void initialize() = 0;
+    virtual void prepare() = 0;
+    virtual f64 getValue(f64 arg1) = 0;
+
+    /* 80281648 */ static ExtrapolateParameter toFunction_outside(int);
+
+    static ExtrapolateParameter toFunction(TFunctionValue::TEOutside outside) {
+        return toFunction_outside(outside);
+    }
+};
+
+class TFunctionValueAttributeSet_const {
+public:
+    TFunctionValueAttributeSet_const(TFunctionValueAttribute_refer* refer,
+                                     TFunctionValueAttribute_range* range,
+                                     TFunctionValueAttribute_interpolate* interp)
+        : refer_(refer), range_(range), interp_(interp) {}
+
+    TFunctionValueAttribute_refer* refer_get() const { return refer_; }
+    TFunctionValueAttribute_range* range_get() const { return range_; }
+    TFunctionValueAttribute_interpolate* interpolate_get() const { return interp_; }
+
+private:
+    /* 0x00 */ TFunctionValueAttribute_refer* refer_;
+    /* 0x04 */ TFunctionValueAttribute_range* range_;
+    /* 0x08 */ TFunctionValueAttribute_interpolate* interp_;
+};
+
+class TFunctionValueAttributeSet : public TFunctionValueAttributeSet_const {
+public:
+    TFunctionValueAttributeSet(TFunctionValueAttribute_refer* refer,
+                               TFunctionValueAttribute_range* range,
+                               TFunctionValueAttribute_interpolate* interp)
+        : TFunctionValueAttributeSet_const(refer, range, interp) {}
+
+    TFunctionValueAttribute_refer* refer_get() const {
+        return static_cast<const TFunctionValueAttributeSet_const*>(this)->refer_get();
+    }
+    TFunctionValueAttribute_range* range_get() const {
+        return static_cast<const TFunctionValueAttributeSet_const*>(this)->range_get();
+    }
+    TFunctionValueAttribute_interpolate* interpolate_get() const {
+        return static_cast<const TFunctionValueAttributeSet_const*>(this)->interpolate_get();
+    }
+};
+
+class TFunctionValueAttribute_refer : public JGadget::TVector_pointer<TFunctionValue*> {
+public:
+    // TFunctionValueAttribute_refer() :
+    // JGadget::TVector_pointer<TFunctionValue*>(JGadget::TAllocator<void*>()) {}
+    ~TFunctionValueAttribute_refer() {}
+
+    /* 802816E8 */ void refer_initialize();
+
+    const TFunctionValueAttribute_refer* refer_getContainer() const { return this; }
+    JGadget::TVector_pointer<TFunctionValue*>& refer_referContainer() { return *this; }
+    bool refer_isReferring(const TFunctionValue* p) const { return false; }  // todo
+};
+
+class TFunctionValueAttribute_range {
+public:
+    /* 80281D18 */ TFunctionValueAttribute_range();
+
+    /* 80281918 */ void range_initialize();
+    /* 8028194C */ void range_prepare();
+    /* 802819F4 */ void range_set(f64, f64);
+    /* 80281A08 */ f64 range_getParameter(f64, f64, f64) const;
+
+    TFunctionValue::TEProgress range_getProgress() const {
+        return (TFunctionValue::TEProgress)mProgress;
+    }
+    void range_setProgress(TFunctionValue::TEProgress progress) { mProgress = progress; }
+    TFunctionValue::TEAdjust range_getAdjust() const { return (TFunctionValue::TEAdjust)mAdjust; }
+    void range_setAdjust(TFunctionValue::TEAdjust adjust) { mAdjust = adjust; }
+    void range_setOutside(TFunctionValue::TEOutside outside) { range_setOutside(outside, outside); }
+    void range_setOutside(TFunctionValue::TEOutside begin, TFunctionValue::TEOutside end) {
+        range_setOutside_begin(begin);
+        range_setOutside_end(end);
+    }
+    void range_setOutside_begin(TFunctionValue::TEOutside begin) { mBegin = begin; }
+    void range_setOutside_end(TFunctionValue::TEOutside end) { mEnd = end; }
+    f64 range_getParameter_outside(f64 arg1) const {
+        f64 temp = arg1 - fBegin_;
+        f64 result = temp;
+        if (temp < *(f64*)&lit_652) {
+            result = TFunctionValue::toFunction(mBegin)(temp, fDifference_);
+        } else if (temp >= fDifference_) {
+            result = TFunctionValue::toFunction(mEnd)(temp, fDifference_);
+        }
+        return result + fBegin_;
+    }
+    f64 range_getParameter_progress(f64 arg1) const { return _20 + _28 * (arg1 - _20); }
+
+private:
+    /* 0x00 */ f64 fBegin_;
+    /* 0x08 */ f64 fEnd_;
+    /* 0x10 */ f64 fDifference_;
+    /* 0x18 */ s8 mProgress;
+    /* 0x19 */ s8 mAdjust;
+    /* 0x1A */ s8 _1a[2];
+    /* 0x1C */ u32 _1c;
+    /* 0x20 */ f64 _20;
+    /* 0x28 */ f64 _28;
+    /* 0x30 */ TFunctionValue::TEOutside mBegin;
+    /* 0x34 */ TFunctionValue::TEOutside mEnd;
+};
+
+class TFunctionValueAttribute_interpolate {
+public:
+    TFunctionValueAttribute_interpolate() : interpolate_(0) {}
+
+    void interpolate_initialize() { interpolate_ = 0; }
+    void interpolate_prepare() {}
+    u32 interpolate_get() const { return interpolate_; }
+    void interpolate_set(TFunctionValue::TEInterpolate interpolate) { interpolate_ = interpolate; }
+
+private:
+    /* 0x0 */ u32 interpolate_;
+};
+
+class TFunctionValue_constant : public TFunctionValue {
+public:
+    /* 8028236C */ TFunctionValue_constant();
+    /* 80283D44 */ virtual ~TFunctionValue_constant() {}
+
+    /* 802823B4 */ virtual u32 getType() const;
+    /* 802823BC */ virtual TFunctionValueAttributeSet getAttributeSet();
+    /* 802823D0 */ virtual void initialize();
+    /* 802823E0 */ virtual void prepare();
+    /* 802823E4 */ virtual f64 getValue(f64);
+
+    void data_set(f64 value) { fValue_ = value; }
+
+private:
+    f64 fValue_;
+};
+
+class TFunctionValue_composite : TFunctionValue, TFunctionValueAttribute_refer {
+public:
+    struct TData {
+        TData(void* data) : u32data((u32)data) {}
+        TData(const void* data) : rawData(data) {}
+        TData(u32 data) : u32data(data) {}
+        TData(f32 data) : f32data(data) {}
+
+        inline void operator=(const TData& rhs) { f32data = rhs.f32data; }
+
+        union {
+            const void* rawData;
+            u32 u32data;
+            f64 f32data;
+        };
+    };
+    typedef f64 (*UnkFunc)(f64, const TFunctionValueAttribute_refer*,
+                           const TFunctionValue_composite::TData*);
+    typedef f64 (*CompositeFunc)(const JGadget::TVector_pointer<TFunctionValue>&,
+                                 const TFunctionValue_composite::TData&, f64);
+
+    /* 80281D5C */ TFunctionValue_composite();
+    /* 80283DA4 */ virtual ~TFunctionValue_composite() {}
+
+    /* 80281DB8 */ virtual u32 getType() const;
+    /* 80281DC0 */ virtual TFunctionValueAttributeSet getAttributeSet();
+    /* 80281DE0 */ virtual void initialize();
+    /* 80281E24 */ virtual void prepare();
+    /* 80281E28 */ virtual f64 getValue(f64);
+    /* 80281E5C */ void composite_raw(TVector_pointer<TFunctionValue*> const&, TData const&, f64);
+    /* 80281EC8 */ void composite_index(TVector_pointer<TFunctionValue*> const&, TData const&, f64);
+    /* 8028202C */ void composite_parameter(TVector_pointer<TFunctionValue*> const&, TData const&,
+                                            f64);
+    /* 80282094 */ void composite_add(TVector_pointer<JStudio::TFunctionValue*> const&,
+                                      TData const&, f64);
+    /* 80282118 */ void composite_subtract(TVector_pointer<TFunctionValue*> const&, TData const&,
+                                           f64);
+    /* 80282200 */ void composite_multiply(TVector_pointer<TFunctionValue*> const&, TData const&,
+                                           f64);
+    /* 80282284 */ void composite_divide(TVector_pointer<TFunctionValue*> const&, TData const&,
+                                         f64);
+
+    void data_set(CompositeFunc fn, const TData& dat) {
+        pfn_ = (UnkFunc)fn;
+        data_setData(dat);
+    }
+    const TData* data_getData() const { return &data; }
+    void data_setData(const TData& dat) { data = dat; }
+
+private:
+    UnkFunc pfn_;
+    TData data;
+};
+
+class TFunctionValue_transition : TFunctionValue,
+                                  TFunctionValueAttribute_range,
+                                  TFunctionValueAttribute_interpolate {
+public:
+    /* 802823EC */ TFunctionValue_transition();
+    /* 80283CE4 */ virtual ~TFunctionValue_transition() {}
+
+    /* 8028244C */ virtual u32 getType() const;
+    /* 80282454 */ virtual TFunctionValueAttributeSet getAttributeSet();
+    /* 80282484 */ virtual void initialize();
+    /* 802824D0 */ virtual void prepare();
+    /* 802824F4 */ virtual f64 getValue(f64);
+
+    void data_set(f64 a1, f64 a2) {
+        _48 = a1;
+        _50 = a2;
+    }
+
+private:
+    /* 0x48 */ f64 _48;
+    /* 0x50 */ f64 _50;
+};
+
+class TFunctionValue_list : TFunctionValue,
+                            TFunctionValueAttribute_range,
+                            TFunctionValueAttribute_interpolate {
+public:
+    struct TIndexData_ {
+        f64 _0;
+        f64 _8;
+        u32 _10;
+    };
+    typedef f64 (*update_INTERPOLATE)(const TFunctionValue_list&, const TIndexData_&);
+
+    /* 802826BC */ TFunctionValue_list();
+    virtual ~TFunctionValue_list() {}
+
+    /* 80282720 */ virtual u32 getType() const;
+    /* 80282728 */ virtual TFunctionValueAttributeSet getAttributeSet();
+    /* 80282758 */ virtual void initialize();
+    /* 802827A8 */ virtual void prepare();
+    /* 80282858 */ virtual f64 getValue(f64);
+
+    void data_set(const f32* pf, u32 u) {
+        ASSERT((pf != NULL) || (u == 0));
+        _44 = pf;
+        _48 = u;
+    }
+
+    void data_setInterval(f64 f) {
+        ASSERT(f > TValue(0));
+        _50 = f;
+    }
+
+    /* 80282C10 */ static f64
+    update_INTERPOLATE_NONE_(JStudio::TFunctionValue_list const&,
+                             JStudio::TFunctionValue_list::TIndexData_ const&);
+    /* 80282C24 */ static f64
+    update_INTERPOLATE_LINEAR_(JStudio::TFunctionValue_list const&,
+                               JStudio::TFunctionValue_list::TIndexData_ const&);
+    /* 80282C58 */ static f64
+    update_INTERPOLATE_PLATEAU_(JStudio::TFunctionValue_list const&,
+                                JStudio::TFunctionValue_list::TIndexData_ const&);
+    /* 80282CA8 */ static f64
+    update_INTERPOLATE_BSPLINE_dataMore3_(JStudio::TFunctionValue_list const&,
+                                          JStudio::TFunctionValue_list::TIndexData_ const&);
+
+private:
+    /* 0x44 */ const f32* _44;
+    /* 0x48 */ u32 _48;
+    /* 0x50 */ f64 _50;
+    /* 0x58 */ update_INTERPOLATE _58;
+};
+
+class TFunctionValue_list_parameter : TFunctionValue,
+                                      TFunctionValueAttribute_range,
+                                      TFunctionValueAttribute_interpolate {
+public:
+    struct TIterator_data_ {
+        TIterator_data_(const f32* value) : value_(value) {}
+        TIterator_data_(const TIterator_data_& other) : value_(other.value_) {}
+
+        void operator=(const TIterator_data_& rhs) { value_ = rhs.value_; }
+
+        const f32* get() const { return value_; }
+        void set(const f32* value) { value_ = value; }
+
+        const f32* value_;
+    };
+    typedef f64 (*update_INTERPOLATE)(const TFunctionValue_list_parameter&, f64);
+
+    /* 80282D34 */ TFunctionValue_list_parameter();
+    /* 80283C24 */ virtual ~TFunctionValue_list_parameter() {}
+
+    /* 80282DA0 */ virtual u32 getType() const;
+    /* 80282DA8 */ virtual TFunctionValueAttributeSet getAttributeSet();
+    /* 80282DD8 */ void data_set(f32 const*, u32);
+    /* 80282E08 */ virtual void initialize();
+    /* 80282E60 */ virtual void prepare();
+    /* 80282F10 */ virtual f64 getValue(f64);
+
+    /* 80282FE8 */ static f64
+    update_INTERPOLATE_NONE_(JStudio::TFunctionValue_list_parameter const&, f64);
+    /* 80282FF4 */ static f64
+    update_INTERPOLATE_LINEAR_(JStudio::TFunctionValue_list_parameter const&, f64);
+    /* 80283024 */ static f64
+    update_INTERPOLATE_PLATEAU_(JStudio::TFunctionValue_list_parameter const&, f64);
+    /* 80283060 */ static f64
+    update_INTERPOLATE_BSPLINE_dataMore3_(JStudio::TFunctionValue_list_parameter const&, f64);
+
+private:
+    /* 0x44 */ const f32* _44;
+    /* 0x48 */ u32 _48;
+    /* 0x4c */ TIterator_data_ dat1;
+    /* 0x50 */ TIterator_data_ dat2;
+    /* 0x54 */ TIterator_data_ dat3;
+    /* 0x58 */ update_INTERPOLATE _58;
+};
+
+class TFunctionValue_hermite : TFunctionValue, TFunctionValueAttribute_range {
+public:
+    struct TIterator_data_ {
+        TIterator_data_(const TFunctionValue_hermite& rParent, const f32* value) {
+            value_ = value;
+            size_ = rParent.data_getSize();
+        }
+        TIterator_data_(const TIterator_data_& other) {
+            value_ = other.value_;
+            size_ = other.size_;
+        }
+
+        void set(const f32* value, u32 size) {
+            value_ = value;
+            size_ = size;
+        }
+
+        void operator=(const TIterator_data_& rhs) {
+            value_ = rhs.value_;
+            size_ = rhs.size_;
+        }
+
+        /* 0x00 */ const f32* value_;
+        /* 0x04 */ u32 size_;
+    };
+
+    /* 802832C4 */ TFunctionValue_hermite();
+    /* 80283BC4 */ virtual ~TFunctionValue_hermite() {}
+
+    /* 80283344 */ virtual u32 getType() const;
+    /* 8028334C */ virtual TFunctionValueAttributeSet getAttributeSet();
+    /* 8028336C */ void data_set(f32 const*, u32, u32);
+    /* 802833BC */ virtual void initialize();
+    /* 80283428 */ virtual void prepare();
+    /* 8028344C */ virtual f64 getValue(f64);
+
+    u32 data_getSize() const { return uSize_; }
+
+private:
+    /* 0x40 */ const f32* pf_;
+    /* 0x44 */ u32 u_;
+    /* 0x48 */ u32 uSize_;
+    /* 0x4c */ TIterator_data_ dat1;
+    /* 0x50 */ TIterator_data_ dat2;
+    /* 0x54 */ TIterator_data_ dat3;
+};
+
+}  // namespace JStudio
 
 #endif /* FUNCTIONVALUE_H */

include/JSystem/JStudio/JStudio/fvb-data-parse.h
@@ -1,6 +1,52 @@
 #ifndef FVB_DATA_PARSE_H
 #define FVB_DATA_PARSE_H
 
-#include "dolphin/types.h"
+#include "JSystem/JStudio/JStudio/fvb-data.h"
+
+namespace JStudio {
+namespace fvb {
+namespace data {
+
+class TParse_TBlock : public TParseData_aligned<4> {
+public:
+    TParse_TBlock(const void* content) : TParseData_aligned<4>(content) {}
+
+    const TBlock* get() const { return (TBlock*)getRaw(); }
+
+    u32 get_size() const { return get()->size; }
+    const void* getNext() const {
+        u32 size = get_size();
+        return (const void*)((u8*)getRaw() + size);
+    }
+    u16 get_type() const { return get()->type; }
+    u16 get_IDSize() const { return get()->id_size; }
+    const void* getBlockEnd_() const { return (u8*)getRaw() + sizeof(TBlock); }
+    const void* get_ID() const {
+        const void* ret = 0;
+        if (get_IDSize())
+            ret = getBlockEnd_();
+        return ret;
+    }
+    const void* getContent() const {
+        u32 size = align_roundUp(get_IDSize(), 4);
+        return (const void*)((int)getBlockEnd_() + size);
+    }
+};
+
+class TParse_TParagraph : public TParseData_aligned<4> {
+public:
+    struct TData {
+        /* 0x04 */ u32 u32Size;
+        /* 0x08 */ u32 u32Type;
+        /* 0x0C */ const void* pContent;
+    };
+    TParse_TParagraph(const void* content) : TParseData_aligned<4>(content) {}
+
+    /* 802850AC */ void getData(JStudio::fvb::data::TParse_TParagraph::TData*) const;
+};
+
+}  // namespace data
+}  // namespace fvb
+}  // namespace JStudio
 
 #endif /* FVB_DATA_PARSE_H */

include/JSystem/JStudio/JStudio/fvb-data.h
@@ -1,6 +1,63 @@
 #ifndef FVB_DATA_H
 #define FVB_DATA_H
 
-#include "dolphin/types.h"
+#include "JSystem/JGadget/binary.h"
+#include "JSystem/JStudio/JStudio/functionvalue.h"
+
+using namespace JGadget::binary;
+
+namespace JStudio {
+namespace fvb {
+namespace data {
+
+extern f32 ga4cSignature[1 + 1 /* padding */];
+
+const int PARAGRAPH_DATA = 1;
+
+typedef enum TEComposite {
+    /* 0x0 */ COMPOSITE_NONE,
+    /* 0x1 */ COMPOSITE_RAW,
+    /* 0x2 */ COMPOSITE_IDX,
+    /* 0x3 */ COMPOSITE_PARAM,
+    /* 0x4 */ COMPOSITE_ADD,
+    /* 0x5 */ COMPOSITE_SUB,
+    /* 0x6 */ COMPOSITE_MUL,
+    /* 0x7 */ COMPOSITE_DIV,
+    /* 0x8 */ COMPOSITE_ENUM_SIZE,
+};
+
+typedef const void* (*CompositeOperation)(TFunctionValue_composite::TData);
+
+struct TBlock {
+    /* 0x0 */ u32 size;
+    /* 0x4 */ u16 type;
+    /* 0x6 */ u16 id_size;
+    /* 0x8 */ u8 id[0];
+};
+
+struct THeader {
+    /* 0x00 */ char signature[4];
+    /* 0x04 */ u16 byte_order;  // must be 0xFEFF
+    /* 0x06 */ u16 version;     // 0-1 = obselete, 2-7 = OK
+    /* 0x08 */ u32 _8;
+    /* 0x0C */ u32 block_number;
+    /* 0x10 */ u8 content[0];
+};
+// Parses a THeader
+class TParse_THeader : public TParseData_aligned<4> {
+public:
+    TParse_THeader(const void* p) : TParseData_aligned<4>(p) {}
+
+    const THeader* get() const { return (THeader*)getRaw(); }
+    const void* getContent() const { return ((THeader*)getRaw())->content; }
+    const char* get_signature() const { return get()->signature; }
+    u16 get_byteOrder() const { return get()->byte_order; }
+    u16 get_version() const { return get()->version; }
+    u32 get_blockNumber() const { return get()->block_number; }
+};
+
+}  // namespace data
+}  // namespace fvb
+}  // namespace JStudio
 
 #endif /* FVB_DATA_H */

commit a6c80b4d02188911605e739f316cc72d4d645e3c

fixed problem building RELs caused by #160

rel/d/a/d_a_title/d_a_title.cpp
@@ -152,20 +152,6 @@ struct Z2AudioMgr {
     static u8 mAudioMgrPtr[4 + 4 /* padding */];
 };
 
-namespace JStudio {
-namespace stb {
-struct data {
-    struct TParse_TParagraph_data {
-        struct TData {};
-
-        /* 80289A80 */ void getData(JStudio::stb::data::TParse_TParagraph_data::TData*) const;
-    };
-};
-
-};  // namespace stb
-
-};  // namespace JStudio
-
 struct JKRExpHeap {
     /* 802CEE2C */ void create(u32, JKRHeap*, bool);
 };