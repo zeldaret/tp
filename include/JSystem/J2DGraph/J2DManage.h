fdd13a5797f5d9d4b36ff6517f5c0a9d8748a826
lepelog <25211966+lepelog@users.noreply.github.com>
2021-12-10 20:52:41 +0100

    j2dmanage

--- include/JSystem/J2DGraph/J2DManage.h
+++ include/JSystem/J2DGraph/J2DManage.h
@@ -5,11 +5,22 @@
 #include "dolphin/types.h"
 
 struct J2DResReference {
-    /* 8030CF10 */ void getResReference(u16) const;
-    /* 8030CF44 */ void getName(u16) const;
+    u16 mCount;
+    u16 mOffsets[1];
+    /* 8030CF10 */ s8* getResReference(u16) const;
+    /* 8030CF44 */ char* getName(u16) const;
+};
+
+struct J2DataManageLink {
+    void* mData;
+    char* mName;
+    J2DataManageLink* mNext;
 };
 
 class J2DDataManage {
+private:
+    J2DataManageLink* mList;
+
 public:
     /* 8030CE18 */ void* get(char const*);
     /* 8030CE7C */ void* get(JSUInputStream*);
