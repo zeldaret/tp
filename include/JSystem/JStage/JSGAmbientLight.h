95ffaa0eef4a708d09006eb5cc9730d6c7d6b293
TakaRikka <takarikka@outlook.com>
2022-07-19 18:51:36 -0700

    decompile JStage

--- include/JSystem/JStage/JSGAmbientLight.h
+++ include/JSystem/JStage/JSGAmbientLight.h
@@ -1,6 +1,16 @@
 #ifndef JSGAMBIENTLIGHT_H
 #define JSGAMBIENTLIGHT_H
 
-#include "dolphin/types.h"
+#include "JSystem/JStage/JSGObject.h"
+#include "dolphin/gx/GX.h"
+
+namespace JStage {
+    struct TAmbientLight : public TObject {
+        /* 80280B20 */ virtual ~TAmbientLight() = 0;
+        /* 80280B80 */ virtual s32 JSGFGetType() const;
+        /* 80280B88 */ virtual GXColor JSGGetColor() const;
+        /* 80280B9C */ virtual void JSGSetColor(GXColor);
+    };
+};
 
 #endif /* JSGAMBIENTLIGHT_H */
--- include/JSystem/JStage/JSGSystem.h
+++ include/JSystem/JStage/JSGSystem.h
@@ -2,5 +2,20 @@
 #define JSGSYSTEM_H
 
 #include "dolphin/types.h"
+#include "JSystem/JStage/JSGObject.h"
+
+namespace JStage {
+    struct TEObject {};
+
+    struct TSystem : public TObject {
+        /* 80280E90 */ virtual ~TSystem() = 0;
+        /* 80280EF0 */ virtual s32 JSGFGetType() const;
+        /* 80280EF8 */ virtual bool JSGFindObject(JStage::TObject**, char const*, JStage::TEObject) const;
+        /* 80280F00 */ virtual bool JSGCreateObject(char const*, JStage::TEObject, u32);
+        /* 80280F08 */ virtual void JSGDestroyObject(JStage::TObject*);
+        /* 80280F0C */ virtual bool JSGGetSystemData(u32);
+        /* 80280F14 */ virtual void JSGSetSystemData(u32, u32);
+    };
+};
 
 #endif /* JSGSYSTEM_H */
