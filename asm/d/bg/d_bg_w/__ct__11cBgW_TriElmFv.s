68c04bb333ebd2e89bd939a0b93dadd9f8f9628e
TakaRikka <takarikka@outlook.com>
2022-11-02 06:34:34 -0700

    remove asm

--- asm/d/bg/d_bg_w/__ct__11cBgW_TriElmFv.s
+++
@@ -1,8 +0,0 @@
-lbl_80079754:
-/* 80079754  3C 80 80 3B */	lis r4, __vt__11cBgW_TriElm@ha /* 0x803ABB9C@ha */
-/* 80079758  38 04 BB 9C */	addi r0, r4, __vt__11cBgW_TriElm@l /* 0x803ABB9C@l */
-/* 8007975C  90 03 00 14 */	stw r0, 0x14(r3)
-/* 80079760  3C 80 80 3A */	lis r4, __vt__8cM3dGPla@ha /* 0x803A37C0@ha */
-/* 80079764  38 04 37 C0 */	addi r0, r4, __vt__8cM3dGPla@l /* 0x803A37C0@l */
-/* 80079768  90 03 00 10 */	stw r0, 0x10(r3)
-/* 8007976C  4E 80 00 20 */	blr 
