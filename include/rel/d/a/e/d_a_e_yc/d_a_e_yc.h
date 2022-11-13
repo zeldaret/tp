af4e91d61bf3c2aec8b5448506a77824b965f5b8
Pheenoh <pheenoh@gmail.com>
2022-11-13 11:50:19 -0700

    initial changes to d_a_e_yc

--- include/rel/d/a/e/d_a_e_yc/d_a_e_yc.h
+++ include/rel/d/a/e/d_a_e_yc/d_a_e_yc.h
@@ -2,5 +2,47 @@
 #define D_A_E_YC_H
 
 #include "dolphin/types.h"
+#include "f_op/f_op_actor.h"
+#include "Z2AudioLib/Z2Creature.h"
+// #include "d/bg/d_bg_s_acch.h"
+// #include "d/cc/d_cc_d.h"
+
+class e_yc_class : public fopAc_ac_c {
+private:
+    /* 0x568 */ u8 field_0x568[80];
+    /* 0x5B8 */ int field_0x5B8;
+    /* 0x5BC */ Z2CreatureEnemy mEnemy;
+    /* 0x660 */ int mAnm;
+    /* 0x664 */ int field_0x664;
+    /* 0x668 */ u8 field_0x668[6];
+    /* 0x66E */ s16 mCurrentAction;
+    /* 0x670 */ s16 field_0x670;
+    /* 0x672 */ u8 field_0x672[2];
+    /* 0x674 */ f32 field_0x674;
+    /* 0x678 */ f32 field_0x678;
+    /* 0x67C */ f32 field_0x67C;
+    /* 0x680 */ s16 field_0x680;
+    /* 0x682 */ u8 field_0x682[2];
+    /* 0x684 */ f32 field_0x684;
+    /* 0x688 */ u32 field_0x688;
+    /* 0x68C */ u8 field_0x68C[4];
+    /* 0x690 */ f32 field_0x690;
+    /* 0x694 */ u8 field_0x694[8];
+    /* 0x69C */ f32 field_0x69C;
+    /* 0x6A0 */ s16 field_0x6A0;
+    /* 0x6A2 */ s16 field_0x6A2;
+    /* 0x6A4 */ s16 field_0x6A4;
+    /* 0x6A6 */ s16 field_0x6A6;
+    /* 0x6A8 */ s16 field_0x6A8;
+    /* 0x6AA */ s16 field_0x6AA;
+    /* 0x6AC */ void* field_0x6AC;
+    /* 0x6B0 */ u8 field_0x6B0[72];    
+    // /* 0x6F4 */ dBgS_Acch mBgS_Acch;
+    // /* 0x8CC */ u8 field_0x8CC[28];
+    // /* 0x8E8 */ dCcD_GStts mCcD_GStts;
+    // /* 0x908 */ dCcD_GObjInf mCcD_GObjInf;
+    // /* 0xA0C */ u8 field_0xA0C[364];
+    // /* 0xB78 */ cCcD_Obj* mpTgHitObj;
+};
 
 #endif /* D_A_E_YC_H */
--- rel/d/a/e/d_a_e_yc/d_a_e_yc.cpp
+++ rel/d/a/e/d_a_e_yc/d_a_e_yc.cpp
@@ -19,66 +19,16 @@ struct mDoMtx_stack_c {
     static u8 now[48];
 };
 
-struct mDoExt_McaMorfCallBack2_c {};
-
-struct mDoExt_McaMorfCallBack1_c {};
-
-struct J3DAnmTransform {};
-
-struct J3DModelData {};
-
-struct Z2Creature {};
-
-struct mDoExt_McaMorfSO {
-    /* 800107D0 */ mDoExt_McaMorfSO(J3DModelData*, mDoExt_McaMorfCallBack1_c*,
-                                    mDoExt_McaMorfCallBack2_c*, J3DAnmTransform*, int, f32, int,
-                                    int, Z2Creature*, u32, u32);
-    /* 80010E70 */ void setAnm(J3DAnmTransform*, int, f32, f32, f32, f32);
-    /* 800110B0 */ void play(u32, s8);
-    /* 800111C0 */ void entryDL();
-    /* 800111EC */ void modelCalc();
-    /* 80011310 */ void stopZelAnime();
-};
-
-struct fopAc_ac_c {
-    /* 80018B64 */ fopAc_ac_c();
-};
-
-struct e_yc_class {};
-
 struct daE_YC_HIO_c {
     /* 807EFECC */ daE_YC_HIO_c();
     /* 807F27BC */ ~daE_YC_HIO_c();
-};
-
-struct Vec {};
-
-struct cXyz {
-    /* 80266B34 */ void operator-(Vec const&) const;
 };
 
 struct dVibration_c {
     /* 8006FA24 */ void StartShock(int, int, cXyz);
 };
 
-struct dKy_tevstr_c {};
-
-struct dScnKy_env_light_c {
-    /* 801A37C4 */ void settingTevStruct(int, cXyz*, dKy_tevstr_c*);
-    /* 801A4DA0 */ void setLightTevColorType_MAJI(J3DModelData*, dKy_tevstr_c*);
-};
-
-struct dRes_info_c {};
-
-struct dRes_control_c {
-    /* 8003C2EC */ void getRes(char const*, s32, dRes_info_c*, int);
-};
-
 struct dPa_levelEcallBack {};
-
-struct csXyz {};
-
-struct _GXColor {};
 
 struct dPa_control_c {
     struct level_c {
@@ -93,11 +43,6 @@ struct dPa_control_c {
                             u8, dPa_levelEcallBack*, s8, _GXColor const*, _GXColor const*,
                             cXyz const*, f32);
 };
-
-struct dDlst_shadowControl_c {
-    static u8 mSimpleTexObj[32];
-};
-
 struct dCcD_Stts {
     /* 80083860 */ void Init(int, int, fopAc_ac_c*);
 };
@@ -155,30 +100,6 @@ struct cCcD_Obj {};
 
 struct cCcS {
     /* 80264BA8 */ void Set(cCcD_Obj*);
-};
-
-struct cBgS_PolyInfo {};
-
-struct _GXTexObj {};
-
-struct Z2CreatureEnemy {
-    /* 802C0F64 */ Z2CreatureEnemy();
-    /* 802C1094 */ void init(Vec*, Vec*, u8, u8);
-    /* 802C1B7C */ void setLinkSearch(bool);
-    /* 802C1B90 */ void setEnemyName(char const*);
-};
-
-struct JGeometry {
-    template <typename A1>
-    struct TVec3 {};
-    /* TVec3<f32> */
-    struct TVec3__template0 {};
-};
-
-struct J3DModel {};
-
-struct J3DFrameCtrl {
-    /* 8032842C */ void checkPass(f32);
 };
 
 //
@@ -282,11 +203,6 @@ extern "C" void setEnemyName__15Z2CreatureEnemyFPCc();
 extern "C" void* __nw__FUl();
 extern "C" void __dl__FPv();
 extern "C" void checkPass__12J3DFrameCtrlFf();
-extern "C" void PSMTXCopy();
-extern "C" void PSMTXTrans();
-extern "C" void PSMTXMultVec();
-extern "C" void PSVECAdd();
-extern "C" void PSVECSquareMag();
 extern "C" void _savegpr_24();
 extern "C" void _savegpr_25();
 extern "C" void _savegpr_27();
@@ -298,7 +214,6 @@ extern "C" void _restgpr_27();
 extern "C" void _restgpr_28();
 extern "C" void _restgpr_29();
 extern "C" extern void* g_fopAc_Method[8];
-extern "C" extern void* g_fpcLf_Method[5 + 1 /* padding */];
 extern "C" extern void* __vt__8dCcD_Sph[36];
 extern "C" extern void* __vt__9dCcD_Stts[11];
 extern "C" extern void* __vt__12cCcD_SphAttr[25];
@@ -307,10 +222,7 @@ extern "C" extern void* __vt__9cCcD_Stts[8];
 extern "C" u8 now__14mDoMtx_stack_c[48];
 extern "C" extern u8 g_dComIfG_gameInfo[122384];
 extern "C" u8 mSimpleTexObj__21dDlst_shadowControl_c[32];
-extern "C" extern u8 g_env_light[4880];
-extern "C" extern u8 j3dSys[284];
 extern "C" extern void* calc_mtx[1 + 1 /* padding */];
-extern "C" extern u32 __float_nan;
 extern "C" extern u8 pauseTimer__9dScnPly_c[4];
 extern "C" void __register_global_object();
 
