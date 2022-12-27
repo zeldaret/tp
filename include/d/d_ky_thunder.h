8244d9a6e1aed5fb3cd96a1cbbd0f72401f2baa7
Pheenoh <pheenoh@gmail.com>
2022-12-27 16:16:36 -0700

    d_ky_thunder first pass (#223)
    * dThunder_c::adjustHeap and dThunder_c::createHeap
    
    * remove asm

--- include/d/d_ky_thunder.h
+++ include/d/d_ky_thunder.h
@@ -2,5 +2,37 @@
 #define D_D_KY_THUNDER_H
 
 #include "dolphin/types.h"
+#include "JSystem/JKernel/JKRSolidHeap.h"
+#include "m_Do/m_Do_ext.h"
+
+class dThunder_c {
+private:
+    /* 0x00 */ u8 field_0x00[0xf8];
+    /* 0xF8 */ JKRSolidHeap* mpHeap;
+public:
+    /* 801ADF58 */ int createHeap();
+    /* 801ADFB4 */ void adjustHeap();
+    /* 801AE458 */ int create();
+
+    ~dThunder_c() {
+        mDoExt_destroySolidHeap(mpHeap);
+    }
+    
+};
+
+inline int draw() {
+    // finish
+};
+
+inline int execute() {
+    // finish
+};
+
+inline int thunder_delete(dThunder_c* i_dthunderP) {
+    if (i_dthunderP) {
+        delete i_dthunderP;
+    }
+    return 1;
+}
 
 #endif /* D_D_KY_THUNDER_H */
