908e8c6206939377719e01d79da3dcd4fc17482e
TakaRikka <takarikka@outlook.com>
2023-01-02 18:09:03 -0800

    remove asm

--- asm/JSystem/JAudio2/JASWaveArcLoader/getCurrentDir__16JASWaveArcLoaderFv.s
+++
@@ -1,4 +0,0 @@
-lbl_8029A130:
-/* 8029A130  3C 60 80 3C */	lis r3, sCurrentDir__16JASWaveArcLoader@ha /* 0x803C77E0@ha */
-/* 8029A134  38 63 77 E0 */	addi r3, r3, sCurrentDir__16JASWaveArcLoader@l /* 0x803C77E0@l */
-/* 8029A138  4E 80 00 20 */	blr 
