73ac0b0baf3117d0ca6b418b470c9b0837c748ab
TakaRikka <takarikka@outlook.com>
2023-01-14 05:35:32 -0800

    clean up some bg files

--- asm/d/bg/d_bg_s_sph_chk/func_80078B70.s
+++
@@ -1,3 +0,0 @@
-lbl_80078B70:
-/* 80078B70  38 63 FF EC */	addi r3, r3, -20
-/* 80078B74  4B FF FF 4C */	b __dt__11dBgS_SphChkFv
--- asm/d/bg/d_bg_s_sph_chk/func_80078B88.s
+++
@@ -1,3 +0,0 @@
-lbl_80078B88:
-/* 80078B88  38 63 FF C8 */	addi r3, r3, -56
-/* 80078B8C  4B FF FF 34 */	b __dt__11dBgS_SphChkFv
