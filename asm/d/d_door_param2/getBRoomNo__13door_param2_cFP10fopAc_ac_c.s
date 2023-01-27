799c16a1dbea6d9341ceaeb95bf04610228b59ee
TakaRikka <takarikka@outlook.com>
2023-01-27 15:13:48 -0800

    remove asm

--- asm/d/d_door_param2/getBRoomNo__13door_param2_cFP10fopAc_ac_c.s
+++
@@ -1,4 +0,0 @@
-lbl_8003A1C4:
-/* 8003A1C4  80 03 00 B0 */	lwz r0, 0xb0(r3)
-/* 8003A1C8  54 03 6E BE */	rlwinm r3, r0, 0xd, 0x1a, 0x1f
-/* 8003A1CC  4E 80 00 20 */	blr 
--- asm/d/d_door_param2/getFRoomNo__13door_param2_cFP10fopAc_ac_c.s
+++
@@ -1,4 +0,0 @@
-lbl_8003A1B8:
-/* 8003A1B8  80 03 00 B0 */	lwz r0, 0xb0(r3)
-/* 8003A1BC  54 03 9E BE */	rlwinm r3, r0, 0x13, 0x1a, 0x1f
-/* 8003A1C0  4E 80 00 20 */	blr 
