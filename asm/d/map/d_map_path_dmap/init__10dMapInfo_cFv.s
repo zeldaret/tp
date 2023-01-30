cbfdf6533c630abdcb144a2db39664a05b1f1ba0
TakaRikka <takarikka@outlook.com>
2023-01-29 21:03:20 -0800

    remove asm

--- asm/d/map/d_map_path_dmap/init__10dMapInfo_cFv.s
+++
@@ -1,7 +0,0 @@
-lbl_8003F6FC:
-/* 8003F6FC  38 00 00 00 */	li r0, 0
-/* 8003F700  98 0D 88 D8 */	stb r0, mNowStayFloorNo__10dMapInfo_c(r13)
-/* 8003F704  98 0D 88 E0 */	stb r0, mNowStayFloorNoDecisionFlg__10dMapInfo_c(r13)
-/* 8003F708  38 00 FF FF */	li r0, -1
-/* 8003F70C  90 0D 88 DC */	stw r0, mNowStayRoomNo__10dMapInfo_c(r13)
-/* 8003F710  4E 80 00 20 */	blr 
