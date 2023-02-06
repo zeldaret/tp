8b31853e1c3c18c325429168468efb9d106710b9
TakaRikka <takarikka@outlook.com>
2023-02-05 21:11:14 -0800

    d_a_tag_lv6CstaSw OK

---
+++ include/m_Do/m_Do_hostIO.h
@@ -0,0 +1,11 @@
+#ifndef M_DO_M_DO_HOSTIO_H
+#define M_DO_M_DO_HOSTIO_H
+
+#include "dolphin/types.h"
+
+class mDoHIO_entry_c {
+public:
+    virtual ~mDoHIO_entry_c() {}
+};
+
+#endif /* M_DO_M_DO_HOSTIO_H */
