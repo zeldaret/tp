fdf03ac4dbe68aac454f3b47ce3b7e2d5959b2fd
Pheenoh <pheenoh@gmail.com>
2023-02-15 17:47:45 -0700

    daTagStream_c::create

--- include/rel/d/a/tag/d_a_tag_stream/d_a_tag_stream.h
+++ include/rel/d/a/tag/d_a_tag_stream/d_a_tag_stream.h
@@ -5,22 +5,25 @@
 #include "f_op/f_op_actor_mng.h"
 #include "d/com/d_com_inf_game.h"
 
-// struct JMath {
-//     static u8 sincosTable_[65536];
-// };
-
 class daTagStream_c : public fopAc_ac_c {
 public:
+    u8 getPriority() {
+        return mPriority;
+    }
+
     /* 80D638F8 */ int create();
     /* 80D63A98 */ ~daTagStream_c();
     /* 80D63B58 */ int execute();
 
-    /* 0x568 */ u8 field_0x568;
+    /* 0x568 */ u8 mPriority;
     /* 0x569 */ u8 field_0x569;
     /* 0x56A */ u8 field_0x56a;
     /* 0x56B */ u8 field_0x56b;
+    /* 0x56C */ u32 mParameters;
+    /* 0x570 */ daTagStream_c* field_0x570;
+    /* 0x574 */ daTagStream_c* field_0x574;
 
-    static u8 m_top[4];
+    static daTagStream_c* m_top;
 };
 
 #endif /* D_A_TAG_STREAM_H */
