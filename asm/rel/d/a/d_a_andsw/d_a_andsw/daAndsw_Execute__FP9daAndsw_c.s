2e3352a3a39025243bc00eb8028c70707e6b9d7b
Pheenoh <pheenoh@gmail.com>
2023-02-16 07:30:50 -0700

    d_a_tag_stream OK, d_a_swTime OK, d_a_andsw OK (#287)

--- asm/rel/d/a/d_a_andsw/d_a_andsw/daAndsw_Execute__FP9daAndsw_c.s
+++
@@ -1,9 +0,0 @@
-lbl_80457AC4:
-/* 80457AC4  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80457AC8  7C 08 02 A6 */	mflr r0
-/* 80457ACC  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80457AD0  4B FF FF 51 */	bl execute__9daAndsw_cFv
-/* 80457AD4  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80457AD8  7C 08 03 A6 */	mtlr r0
-/* 80457ADC  38 21 00 10 */	addi r1, r1, 0x10
-/* 80457AE0  4E 80 00 20 */	blr 
--- asm/rel/d/a/d_a_swTime/d_a_swTime/daSwTime_Create__FP10daSwTime_c.s
+++
@@ -1,9 +0,0 @@
-lbl_80D4F7A0:
-/* 80D4F7A0  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80D4F7A4  7C 08 02 A6 */	mflr r0
-/* 80D4F7A8  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80D4F7AC  4B FF FE B5 */	bl create__10daSwTime_cFv
-/* 80D4F7B0  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80D4F7B4  7C 08 03 A6 */	mtlr r0
-/* 80D4F7B8  38 21 00 10 */	addi r1, r1, 0x10
-/* 80D4F7BC  4E 80 00 20 */	blr 
