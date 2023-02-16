7bbd413fdf6ea9109d21148e86f7daf16caae66d
Pheenoh <pheenoh@gmail.com>
2023-02-15 22:00:14 -0700

    s_e_ym

--- include/rel/d/a/e/d_a_e_ym_tag/d_a_e_ym_tag.h
+++ include/rel/d/a/e/d_a_e_ym_tag/d_a_e_ym_tag.h
@@ -2,5 +2,26 @@
 #define D_A_E_YM_TAG_H
 
 #include "dolphin/types.h"
+#include "f_op/f_op_actor_mng.h"
+
+// this probably needs to be moved to d_a_e_ym.h
+class daE_YM_c : public fopAc_ac_c {
+public:
+    u8 getTagNo() {
+        return mTagNo;
+    }
+
+    /* 0x568 */ u8 field_0x568[352];
+    /* 0x6C8 */ u8 mTagNo;
+};
+
+class daE_YM_TAG_c : public fopAc_ac_c {
+public:
+    /* 80815E8C */ void execute();
+    /* 80815F28 */ void create();
+
+
+};
+
 
 #endif /* D_A_E_YM_TAG_H */
