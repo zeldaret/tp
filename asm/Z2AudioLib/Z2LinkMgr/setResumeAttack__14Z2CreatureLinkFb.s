584a72041f458be4736ae1c16b8994342d844964
TakaRikka <takarikka@outlook.com>
2023-02-16 01:02:41 -0800

    remove asm

--- asm/Z2AudioLib/Z2LinkMgr/setResumeAttack__14Z2CreatureLinkFb.s
+++
@@ -1,5 +0,0 @@
-lbl_802C48D8:
-/* 802C48D8  88 03 00 CB */	lbz r0, 0xcb(r3)
-/* 802C48DC  50 80 2E B4 */	rlwimi r0, r4, 5, 0x1a, 0x1a
-/* 802C48E0  98 03 00 CB */	stb r0, 0xcb(r3)
-/* 802C48E4  4E 80 00 20 */	blr 
