7c6c3cb58c8db3cf47df45baa663b22200d7019b
Pheenoh <pheenoh@gmail.com>
2023-02-19 02:05:30 -0700

    match rest of TU + cleanup

--- include/rel/d/a/obj/d_a_obj_onsenFire/d_a_obj_onsenFire.h
+++ include/rel/d/a/obj/d_a_obj_onsenFire/d_a_obj_onsenFire.h
@@ -1,9 +1,15 @@
 #ifndef D_A_OBJ_ONSENFIRE_H
 #define D_A_OBJ_ONSENFIRE_H
 
-#include "dolphin/types.h"
 #include "f_op/f_op_actor_mng.h"
 #include "d/com/d_com_inf_game.h"
+
+inline void dComIfGp_particle_setSimple(u16 param_0,cXyz* param_1, 
+                                        u8 param_2, _GXColor& param_3,
+                                        _GXColor& param_4, int param_5, 
+                                        float param_6) {
+    g_dComIfG_gameInfo.play.getParticle()->setSimple(param_0,param_1,0,param_2,param_3,param_4,param_5,param_6);
+}
 
 class daObjOnsenFire_c : public fopAc_ac_c {
 public:    
--- rel/d/a/obj/d_a_obj_onsenFire/d_a_obj_onsenFire.cpp
+++ rel/d/a/obj/d_a_obj_onsenFire/d_a_obj_onsenFire.cpp
@@ -4,40 +4,9 @@
  */
 
 #include "rel/d/a/obj/d_a_obj_onsenFire/d_a_obj_onsenFire.h"
-#include "dol2asm.h"
-#include "dolphin/types.h"
-
-//
-// Forward References:
-//
-
-extern "C" void create__16daObjOnsenFire_cFv();
-extern "C" void execute__16daObjOnsenFire_cFv();
-extern "C" static void daObjOnsenFire_Create__FP10fopAc_ac_c();
-extern "C" static void daObjOnsenFire_Execute__FP16daObjOnsenFire_c();
-extern "C" static void daObjOnsenFire_Delete__FP16daObjOnsenFire_c();
-extern "C" extern void* g_profile_OBJ_ONSEN_FIRE[12];
-
-//
-// External References:
-//
-
-extern "C" void __ct__10fopAc_ac_cFv();
-extern "C" void __dt__10fopAc_ac_cFv();
-extern "C" void isEventBit__11dSv_event_cCFUs();
-extern "C" void setSimple__13dPa_control_cFUsPC4cXyzPC12dKy_tevstr_cUcRC8_GXColorRC8_GXColorif();
-extern "C" void seStartLevel__7Z2SeMgrF10JAISoundIDPC3VecUlScffffUc();
-extern "C" void _savegpr_29();
-extern "C" void _restgpr_29();
-extern "C" u8 saveBitLabels__16dSv_event_flag_c[1644 + 4 /* padding */];
-extern "C" u8 mAudioMgrPtr__10Z2AudioMgr[4 + 4 /* padding */];
-
-//
-// Declarations:
-//
+#include "d/d_procname.h"
 
 /* 80CA8278-80CA82F4 000078 007C+00 1/1 0/0 0/0 .text            create__16daObjOnsenFire_cFv */
-#ifndef NONMATCHING
 int daObjOnsenFire_c::create() {
     if (!fopAcM_CheckCondition(this, 8)) {
         new (this) daObjOnsenFire_c();
@@ -48,102 +17,61 @@ int daObjOnsenFire_c::create() {
     }
     return cPhs_COMPLEATE_e;
 }
-#else
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm void daObjOnsenFire_c::create() {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_onsenFire/d_a_obj_onsenFire/create__16daObjOnsenFire_cFv.s"
-}
-#pragma pop
-#endif
-
-/* ############################################################################################## */
-/* 80CA8474-80CA8478 000000 0004+00 1/1 0/0 0/0 .rodata          @3659 */
-SECTION_RODATA static u8 const lit_3659[4] = {
-    0x00,
-    0x00,
-    0x00,
-    0x00,
-};
-COMPILER_STRIP_GATE(0x80CA8474, &lit_3659);
-
-/* 80CA8478-80CA847C 000004 0004+00 0/1 0/0 0/0 .rodata          @3660 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static f32 const lit_3660 = 1.0f;
-COMPILER_STRIP_GATE(0x80CA8478, &lit_3660);
-#pragma pop
-
-/* 80CA847C-80CA8480 000008 0004+00 0/1 0/0 0/0 .rodata          @3661 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static f32 const lit_3661 = -1.0f;
-COMPILER_STRIP_GATE(0x80CA847C, &lit_3661);
-#pragma pop
 
 /* 80CA82F4-80CA83FC 0000F4 0108+00 1/1 0/0 0/0 .text            execute__16daObjOnsenFire_cFv */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm int daObjOnsenFire_c::execute() {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_onsenFire/d_a_obj_onsenFire/execute__16daObjOnsenFire_cFv.s"
+int daObjOnsenFire_c::execute() {
+    dComIfGp_particle_setSimple(0x100,&current.pos,0xff,g_whiteColor,g_whiteColor,0,0.0f);
+    dComIfGp_particle_setSimple(0x101,&current.pos,0xff,g_whiteColor,g_whiteColor,0,0.0f);
+    dComIfGp_particle_setSimple(0x103,&current.pos,0xff,g_whiteColor,g_whiteColor,0,0.0f);
+    Z2AudioMgr::getInterface()->mSeMgr.seStartLevel(JAISoundID(Z2SE_OBJ_ONSEN_WARM_FIRE),&current.pos,0,0,1.0f,1.0f,-1.0f,-1.0f,0);
+    return 1;
 }
-#pragma pop
 
 /* 80CA83FC-80CA841C 0001FC 0020+00 1/0 0/0 0/0 .text daObjOnsenFire_Create__FP10fopAc_ac_c */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-static asm void daObjOnsenFire_Create(fopAc_ac_c* param_0) {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_onsenFire/d_a_obj_onsenFire/daObjOnsenFire_Create__FP10fopAc_ac_c.s"
+static int daObjOnsenFire_Create(fopAc_ac_c* i_this) {
+    return static_cast<daObjOnsenFire_c*>(i_this)->create();
 }
-#pragma pop
 
 /* 80CA841C-80CA843C 00021C 0020+00 1/0 0/0 0/0 .text daObjOnsenFire_Execute__FP16daObjOnsenFire_c
  */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-static asm void daObjOnsenFire_Execute(daObjOnsenFire_c* param_0) {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_onsenFire/d_a_obj_onsenFire/daObjOnsenFire_Execute__FP16daObjOnsenFire_c.s"
+static int daObjOnsenFire_Execute(daObjOnsenFire_c* i_this) {
+    return i_this->execute();
 }
-#pragma pop
 
 /* 80CA843C-80CA846C 00023C 0030+00 1/0 0/0 0/0 .text daObjOnsenFire_Delete__FP16daObjOnsenFire_c
  */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-static asm void daObjOnsenFire_Delete(daObjOnsenFire_c* param_0) {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_onsenFire/d_a_obj_onsenFire/daObjOnsenFire_Delete__FP16daObjOnsenFire_c.s"
+static int daObjOnsenFire_Delete(daObjOnsenFire_c* i_this) {
+    i_this->~daObjOnsenFire_c();
+    return 1;
 }
-#pragma pop
 
 /* ############################################################################################## */
 /* 80CA8480-80CA84A0 -00001 0020+00 1/0 0/0 0/0 .data            l_daObjOnsenFire_Method */
-SECTION_DATA static void* l_daObjOnsenFire_Method[8] = {
-    (void*)daObjOnsenFire_Create__FP10fopAc_ac_c,
-    (void*)daObjOnsenFire_Delete__FP16daObjOnsenFire_c,
-    (void*)daObjOnsenFire_Execute__FP16daObjOnsenFire_c,
-    (void*)NULL,
-    (void*)NULL,
-    (void*)NULL,
-    (void*)NULL,
-    (void*)NULL,
+static actor_method_class l_daObjOnsenFire_Method = {
+    (process_method_func)daObjOnsenFire_Create,
+    (process_method_func)daObjOnsenFire_Delete,
+    (process_method_func)daObjOnsenFire_Execute
 };
 
 /* 80CA84A0-80CA84D0 -00001 0030+00 0/0 0/0 1/0 .data            g_profile_OBJ_ONSEN_FIRE */
-SECTION_DATA extern void* g_profile_OBJ_ONSEN_FIRE[12] = {
-    (void*)0xFFFFFFFD, (void*)0x0003FFFD,
-    (void*)0x008C0000, (void*)&g_fpcLf_Method,
-    (void*)0x00000568, (void*)NULL,
-    (void*)NULL,       (void*)&g_fopAc_Method,
-    (void*)0x02640000, (void*)&l_daObjOnsenFire_Method,
-    (void*)0x00044000, (void*)0x000E0000,
+extern actor_process_profile_definition g_profile_OBJ_ONSEN_FIRE = {
+    -3,                        // mLayerID         
+    3,                         // mListID       
+    -3,                        // mListPrio        
+    PROC_OBJ_ONSEN_FIRE,       // mProcName                         
+    0,                         // padding        
+    &g_fpcLf_Method.mBase,     // mSubMtd                           
+    sizeof(daObjOnsenFire_c),  // mSize                               
+    0,                         // mSizeOther       
+    0,                         // mParameters              
+    &g_fopAc_Method.base,      // mSubMtd                          
+    0x0264,                    // mPriority            
+    0,                         // padding       
+    0,                         // padding        
+    &l_daObjOnsenFire_Method,  // mSubMtd                              
+    0x00044000,                // mStatus                 
+    0,                         // mActorType       
+    0x0E,                      // mCullType          
+    0,                         // padding       
+    0,                         // padding       
 };
