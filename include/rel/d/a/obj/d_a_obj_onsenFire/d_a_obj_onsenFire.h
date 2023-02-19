f06cfafe8fe318b757c1404669cb58738f7eb1cc
Pheenoh <pheenoh@gmail.com>
2023-02-19 00:47:39 -0700

    daObjOnsenFire_c::create

--- include/rel/d/a/obj/d_a_obj_onsenFire/d_a_obj_onsenFire.h
+++ include/rel/d/a/obj/d_a_obj_onsenFire/d_a_obj_onsenFire.h
@@ -2,5 +2,13 @@
 #define D_A_OBJ_ONSENFIRE_H
 
 #include "dolphin/types.h"
+#include "f_op/f_op_actor_mng.h"
+#include "d/com/d_com_inf_game.h"
+
+class daObjOnsenFire_c : public fopAc_ac_c {
+public:    
+    /* 80CA8278 */ int create();
+    /* 80CA82F4 */ int execute();
+};
 
 #endif /* D_A_OBJ_ONSENFIRE_H */
