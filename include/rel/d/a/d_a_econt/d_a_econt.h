7523c3c77044caa62b3efaf4e46b0b8449474982
Pheenoh <pheenoh@gmail.com>
2023-02-19 02:15:14 -0700

    s_rd_sub

--- include/rel/d/a/d_a_econt/d_a_econt.h
+++ include/rel/d/a/d_a_econt/d_a_econt.h
@@ -2,5 +2,12 @@
 #define D_A_ECONT_H
 
 #include "dolphin/types.h"
+#include "f_op/f_op_actor_mng.h"
+#include "d/com/d_com_inf_game.h"
+
+class econt_class : public fopAc_ac_c {
+public:
+    /* 0x568 */ u8 field_0x568[88];
+};
 
 #endif /* D_A_ECONT_H */
