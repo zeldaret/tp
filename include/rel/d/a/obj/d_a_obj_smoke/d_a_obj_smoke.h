48de1fe5f739b593a1abce5a0cdc1c2e89a83e59
Pheenoh <pheenoh@gmail.com>
2023-02-19 16:58:28 -0700

    d_a_obj_smoke OK

--- include/rel/d/a/obj/d_a_obj_smoke/d_a_obj_smoke.h
+++ include/rel/d/a/obj/d_a_obj_smoke/d_a_obj_smoke.h
@@ -1,6 +1,23 @@
 #ifndef D_A_OBJ_SMOKE_H
 #define D_A_OBJ_SMOKE_H
 
-#include "dolphin/types.h"
+#include "f_op/f_op_actor_mng.h"
+#include "d/com/d_com_inf_game.h"
+
+class daObjSmoke_c : public fopAc_ac_c {
+public:
+    u8 getSwbit() {
+        return fopAcM_GetParamBit(this,0,8);
+    }
+
+    /* 80CDCE78 */ void initBaseMtx();
+    /* 80CDCE98 */ void setBaseMtx();
+    /* 80CDCEF0 */ int Create();
+    /* 80CDCF9C */ int create();
+    /* 80CDD004 */ int execute();
+    /* 80CDD0B8 */ int _delete();
+
+    /* 0x568 */ JPABaseEmitter* mParticle;
+};
 
 #endif /* D_A_OBJ_SMOKE_H */
