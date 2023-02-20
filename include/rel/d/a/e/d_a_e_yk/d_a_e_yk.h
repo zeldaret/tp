f2023e501301487a737d52270a9c6a342720231e
Pheenoh <pheenoh@gmail.com>
2023-02-19 22:52:18 -0700

    setup TU

--- include/rel/d/a/e/d_a_e_yk/d_a_e_yk.h
+++ include/rel/d/a/e/d_a_e_yk/d_a_e_yk.h
@@ -2,5 +2,16 @@
 #define D_A_E_YK_H
 
 #include "dolphin/types.h"
+#include "f_op/f_op_actor_mng.h"
+#include "d/com/d_com_inf_game.h"
+
+struct daE_YK_HIO_c {
+    /* 8080482C */ daE_YK_HIO_c();
+    /* 808077E0 */ ~daE_YK_HIO_c();
+};
+
+class e_yk_class : public fopEn_enemy_c {
+public:    
+};
 
 #endif /* D_A_E_YK_H */
