fa1f47f7a134cdc203207ec0b273ca7d32542d52
Pheenoh <pheenoh@gmail.com>
2023-02-20 00:53:33 -0700

    d_a_guard_mng OK, d_a_obj_smoke OK, d_a_tag_smk_emt OK, d_a_hitobj OK (#298)
    * d_a_guard_mng OK
    
    * d_a_obj_smoke OK
    
    * d_a_tag_smk_emt OK
    
    * d_a_hitobj attempt
    
    * fixes
    
    * rm comment
    
    * cc_sph_src
    
    * rename and move mGuardDataTbl

--- include/rel/d/a/d_a_guard_mng/d_a_guard_mng.h
+++ include/rel/d/a/d_a_guard_mng/d_a_guard_mng.h
@@ -1,6 +1,70 @@
 #ifndef D_A_GUARD_MNG_H
 #define D_A_GUARD_MNG_H
 
-#include "dolphin/types.h"
+#include "f_op/f_op_actor_mng.h"
+#include "rel/d/a/tag/d_a_tag_guard/d_a_tag_guard.h"
+#include "d/com/d_com_inf_game.h"
+
+class daGuardMng_c : public fopAc_ac_c {
+public:
+    void countMerchantNum() {
+        mMerchantNum++;
+    }
+
+    int execute() {
+        static u32 const mGuardDataTbl[2] = {
+            0x00000200, 0x00000201,
+        };
+
+        if (!daPy_py_c::i_checkNowWolf() || field_0x573 >= mLimitNum) {
+            return 1;
+        } else {
+            if (field_0x571 < mMerchantNum) {
+                field_0x571 = mMerchantNum;
+            }
+
+            checkMerchantNum();
+
+            if (mpTagGuard) {
+                if (field_0x571 > mMerchantNum && 3 < (field_0x571 - mMerchantNum)) {
+                    mpTagGuard->createGuard(mGuardDataTbl[0]);
+                    field_0x573++;
+                    field_0x571 = mMerchantNum;
+                    dComIfGs_onSaveDunSwitch(0x3c);
+                }
+            }
+        }
+
+        return 1;
+    }
+
+    u8 getLimitNum() {
+        return fopAcM_GetParam(this) & 0xff;
+    }
+
+    void create_init() {
+        mLimitNum = getLimitNum();
+        field_0x573 = 0;
+    }
+
+    int create() {
+        if (!fopAcM_CheckCondition(this, 8)) {
+            new (this) daGuardMng_c();
+            fopAcM_OnCondition(this, 8);
+        }
+        create_init();
+        return cPhs_COMPLEATE_e;
+    }
+
+    /* 80837B9C */ u8 checkMerchantNum();
+    /* 80837BEC */ void checkAppearTag(daTagGuard_c*);
+
+    /* 0x568 */ daTagGuard_c* mpTagGuard;
+    /* 0x56C */ f32 field_0x56c;
+    /* 0x570 */ u8 mMerchantNum;
+    /* 0x571 */ u8 field_0x571;
+    /* 0x572 */ u8 mLimitNum;
+    /* 0x573 */ u8 field_0x573;
+};
 
 #endif /* D_A_GUARD_MNG_H */
