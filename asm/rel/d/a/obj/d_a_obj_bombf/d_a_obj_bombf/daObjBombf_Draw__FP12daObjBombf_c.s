7ca763d76b1c544273d0c783fcb4543bbdc9d665
Trueffeloot <geiloking135@gmail.com>
2023-04-04 06:33:00 +0200

    d_a_obj_bombf OK

--- asm/rel/d/a/obj/d_a_obj_bombf/d_a_obj_bombf/daObjBombf_Draw__FP12daObjBombf_c.s
+++
@@ -1,9 +0,0 @@
-lbl_80BBAE5C:
-/* 80BBAE5C  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80BBAE60  7C 08 02 A6 */	mflr r0
-/* 80BBAE64  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80BBAE68  4B FF FF 91 */	bl draw__12daObjBombf_cFv
-/* 80BBAE6C  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80BBAE70  7C 08 03 A6 */	mtlr r0
-/* 80BBAE74  38 21 00 10 */	addi r1, r1, 0x10
-/* 80BBAE78  4E 80 00 20 */	blr 
