b84aca0cfdf46e3f6fe2947e757572f952bb1239
Trueffeloot <geiloking135@gmail.com>
2023-04-05 07:24:19 +0200

    d_a_tag_lantern OK

--- asm/rel/d/a/tag/d_a_tag_lantern/d_a_tag_lantern/daTag_Lantern_Create__FPv.s
+++
@@ -1,9 +0,0 @@
-lbl_8048F024:
-/* 8048F024  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 8048F028  7C 08 02 A6 */	mflr r0
-/* 8048F02C  90 01 00 14 */	stw r0, 0x14(r1)
-/* 8048F030  4B FF FC F9 */	bl create__15daTag_Lantern_cFv
-/* 8048F034  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 8048F038  7C 08 03 A6 */	mtlr r0
-/* 8048F03C  38 21 00 10 */	addi r1, r1, 0x10
-/* 8048F040  4E 80 00 20 */	blr 
