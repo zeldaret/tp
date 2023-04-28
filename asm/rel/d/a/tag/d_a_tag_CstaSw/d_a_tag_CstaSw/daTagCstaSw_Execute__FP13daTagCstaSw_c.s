9a65ed3d6e5e4c63a2e81b8f5ddc8a45b8dce62c
Trueffeloot <geiloking135@gmail.com>
2023-04-28 21:31:43 +0200

    d_a_tag_CstaSw & d_a_tag_shop_item OK

--- asm/rel/d/a/tag/d_a_tag_CstaSw/d_a_tag_CstaSw/daTagCstaSw_Execute__FP13daTagCstaSw_c.s
+++
@@ -1,9 +0,0 @@
-lbl_805A2420:
-/* 805A2420  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 805A2424  7C 08 02 A6 */	mflr r0
-/* 805A2428  90 01 00 14 */	stw r0, 0x14(r1)
-/* 805A242C  4B FF FF 15 */	bl Execute__13daTagCstaSw_cFv
-/* 805A2430  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 805A2434  7C 08 03 A6 */	mtlr r0
-/* 805A2438  38 21 00 10 */	addi r1, r1, 0x10
-/* 805A243C  4E 80 00 20 */	blr 
--- asm/rel/d/a/tag/d_a_tag_shop_item/d_a_tag_shop_item/daTag_ShopItem_Draw__FPv.s
+++
@@ -1,9 +0,0 @@
-lbl_80D61140:
-/* 80D61140  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80D61144  7C 08 02 A6 */	mflr r0
-/* 80D61148  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80D6114C  4B FF FE D9 */	bl Draw__16daTag_ShopItem_cFv
-/* 80D61150  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80D61154  7C 08 03 A6 */	mtlr r0
-/* 80D61158  38 21 00 10 */	addi r1, r1, 0x10
-/* 80D6115C  4E 80 00 20 */	blr 
