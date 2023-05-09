a83fba10477dff461b129315b677f480b2c1b3c5
Yotona <nzmcqueen1996@gmail.com>
2023-05-09 15:31:41 -0700

    d_a_e_db_leaf OK

--- include/rel/d/a/e/d_a_e_db_leaf/d_a_e_db_leaf.h
+++ include/rel/d/a/e/d_a_e_db_leaf/d_a_e_db_leaf.h
@@ -1,6 +1,16 @@
 #ifndef D_A_E_DB_LEAF_H
 #define D_A_E_DB_LEAF_H
 
+#include "f_op/f_op_actor_mng.h"
+#include "d/com/d_com_inf_game.h"
+#include "d/kankyo/d_kankyo.h"
+#include "d/d_resorce.h"
 #include "dolphin/types.h"
+
+class e_db_leaf_class : public fopEn_enemy_c {
+public:
+    /* 0x5AC */ request_of_phase_process_class mPhase;
+    /* 0x5B4 */ mDoExt_McaMorf* mpMorf;
+}; // Size: 0x5B8
 
 #endif /* D_A_E_DB_LEAF_H */
