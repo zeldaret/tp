df30e3d1959edd73343b2f0b898c6ab76434579e
Noah McQueen <67965519+Yotona@users.noreply.github.com>
2023-05-28 20:54:30 -0700

    lib/TRK_MINNOW_DOLPHIN work (#341)

--- include/amcstubs/AmcExi2Stubs.h
+++ include/amcstubs/AmcExi2Stubs.h
@@ -3,4 +3,13 @@
 
 #include "dolphin/types.h"
 
+void EXI2_Init(void);
+void EXI2_EnableInterrupts(void);
+u8 EXI2_Poll(void);
+u8 EXI2_ReadN(void*, u32);
+u8 EXI2_WriteN(void*, u32);
+void EXI2_Reserve(void);
+void EXI2_Unreserve(void);
+u8 AMC_IsStub(void);
+
 #endif /* AMCEXI2STUBS_H */
--- libs/TRK_MINNOW_DOLPHIN/utils/gc/MWCriticalSection_gc.c
+++ libs/TRK_MINNOW_DOLPHIN/utils/gc/MWCriticalSection_gc.c
@@ -12,20 +12,9 @@ void MWExitCriticalSection(u32* section) {
 }
 
 /* 80372CC8-80372CF8 36D608 0030+00 0/0 2/2 0/0 .text            MWEnterCriticalSection */
-#ifdef NONMATCHING
 void MWEnterCriticalSection(u32* section) {
     *section = OSDisableInterrupts();
 }
-#else
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm void MWEnterCriticalSection(u32* section) {
-    nofralloc
-#include "asm/TRK_MINNOW_DOLPHIN/utils/gc/MWCriticalSection_gc/MWEnterCriticalSection.s"
-}
-#pragma pop
-#endif
 
 /* 80372CF8-80372CFC 36D638 0004+00 0/0 1/1 0/0 .text            MWInitializeCriticalSection */
 void MWInitializeCriticalSection() {}
