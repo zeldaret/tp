d4ad6ec0d898245655e68981271a2d56a2ea4b9a
Tal Hayon <talhayon1@gmail.com>
2023-07-20 16:27:20 +0300

    Maybe improve JMAEulerToQuat

--- libs/JSystem/JMath/JMath.cpp
+++ libs/JSystem/JMath/JMath.cpp
@@ -38,8 +38,10 @@ void JMAEulerToQuat(s16 x, s16 y, s16 z, Quaternion* quat) {
     f32 sinY = JMASSin(y / 2);
     f32 sinZ = JMASSin(z / 2);
 
-    quat->w = cosX * (cosY * cosZ) + sinX * (sinY * sinZ);
-    quat->x = sinX * (cosY * cosZ) - cosX * (sinY * sinZ);
+    f32 cyz = cosY * cosZ;
+    f32 syz = sinY * sinZ;
+    quat->w = cosX * (cyz) + sinX * (syz);
+    quat->x = sinX * (cyz) - cosX * (syz);
     quat->y = cosZ * (cosX * sinY) + sinZ * (sinX * cosY);
     quat->z = sinZ * (cosX * cosY) - cosZ * (sinX * sinY);
 }
