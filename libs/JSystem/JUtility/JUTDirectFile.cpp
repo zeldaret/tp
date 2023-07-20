f78672c7bd039a48836e4298ac005a3a004f43b5
Tal Hayon <talhayon1@gmail.com>
2023-07-20 16:29:45 +0300

    JUTDirectFile OK

--- libs/JSystem/JUtility/JUTDirectFile.cpp
+++ libs/JSystem/JUtility/JUTDirectFile.cpp
@@ -4,96 +4,182 @@
 //
 
 #include "JSystem/JUtility/JUTDirectFile.h"
-#include "dol2asm.h"
 #include "dolphin/types.h"
+#include "dolphin/os/OSInterrupt.h"
+#include "global.h"
 
 //
 // Forward References:
 //
 
-extern "C" void fetch32byte__13JUTDirectFileFv();
-extern "C" void __ct__13JUTDirectFileFv();
-extern "C" void __dt__13JUTDirectFileFv();
-extern "C" void fopen__13JUTDirectFileFPCc();
-extern "C" void fclose__13JUTDirectFileFv();
-extern "C" void fgets__13JUTDirectFileFPvi();
-
 //
 // External References:
 //
-
-extern "C" void __dl__FPv();
-extern "C" void OSEnableInterrupts();
-extern "C" void OSRestoreInterrupts();
-extern "C" void DVDOpen();
-extern "C" void DVDClose();
-extern "C" void DVDReadAsyncPrio();
-extern "C" void DVDGetCommandBlockStatus();
-extern "C" void _savegpr_27();
-extern "C" void _savegpr_29();
-extern "C" void _restgpr_27();
-extern "C" void _restgpr_29();
 
 //
 // Declarations:
 //
 
 /* 802E8730-802E87F8 2E3070 00C8+00 1/1 0/0 0/0 .text            fetch32byte__13JUTDirectFileFv */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm void JUTDirectFile::fetch32byte() {
-    nofralloc
-#include "asm/JSystem/JUtility/JUTDirectFile/fetch32byte__13JUTDirectFileFv.s"
+int JUTDirectFile::fetch32byte() {
+	mToRead = mLength - ALIGN_PREV(mPos, DVD_MIN_TRANSFER_SIZE);
+
+	if (mToRead > JUTDF_BUFSIZE) {
+		mToRead = JUTDF_BUFSIZE;
+	}
+	int interrupts = OSEnableInterrupts();
+	int readRes    = DVDReadAsyncPrio(&mFileInfo, mSectorStart, ALIGN_NEXT(mToRead, DVD_MIN_TRANSFER_SIZE),
+                               ALIGN_PREV(mPos, DVD_MIN_TRANSFER_SIZE), NULL, 2);
+	OSRestoreInterrupts(interrupts);
+	if (!readRes) {
+		return -1;
+	} else {
+		interrupts = OSEnableInterrupts();
+		while (DVDGetCommandBlockStatus(&mFileInfo.block)) {
+			;
+		}
+		OSRestoreInterrupts(interrupts);
+		return mToRead;
+	}
 }
-#pragma pop
 
 /* 802E87F8-802E881C 2E3138 0024+00 0/0 1/1 0/0 .text            __ct__13JUTDirectFileFv */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm JUTDirectFile::JUTDirectFile() {
-    nofralloc
-#include "asm/JSystem/JUtility/JUTDirectFile/__ct__13JUTDirectFileFv.s"
+JUTDirectFile::JUTDirectFile() {
+	mLength      = 0;
+	mPos         = 0;
+	mToRead      = 0;
+	mSectorStart = (u8*)ALIGN_NEXT((u32)mBuffer, DVD_MIN_TRANSFER_SIZE);
+	mIsOpen      = false;
 }
-#pragma pop
+
 
 /* 802E881C-802E8860 2E315C 0044+00 0/0 1/1 0/0 .text            __dt__13JUTDirectFileFv */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm JUTDirectFile::~JUTDirectFile() {
-    nofralloc
-#include "asm/JSystem/JUtility/JUTDirectFile/__dt__13JUTDirectFileFv.s"
+JUTDirectFile::~JUTDirectFile() {
+    mIsOpen = false;
 }
-#pragma pop
 
 /* 802E8860-802E88FC 2E31A0 009C+00 0/0 1/1 0/0 .text            fopen__13JUTDirectFileFPCc */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm bool JUTDirectFile::fopen(char const* param_0) {
-    nofralloc
-#include "asm/JSystem/JUtility/JUTDirectFile/fopen__13JUTDirectFileFPCc.s"
+bool JUTDirectFile::fopen(const char* filename) {
+	if (!filename) {
+		return false;
+	}
+
+	int interrupts = OSEnableInterrupts();
+	int dvdRes     = DVDOpen(const_cast<char*>(filename), &mFileInfo);
+	OSRestoreInterrupts(interrupts);
+
+	if (!dvdRes) {
+		mIsOpen = false;
+		return false;
+	}
+
+	int interrupts2 = OSEnableInterrupts();
+	mLength         = mFileInfo.length;
+	OSRestoreInterrupts(interrupts2);
+
+	mPos    = 0;
+	mIsOpen = true;
+	return true;
 }
-#pragma pop
 
 /* 802E88FC-802E8958 2E323C 005C+00 0/0 1/1 0/0 .text            fclose__13JUTDirectFileFv */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm bool JUTDirectFile::fclose() {
-    nofralloc
-#include "asm/JSystem/JUtility/JUTDirectFile/fclose__13JUTDirectFileFv.s"
+void JUTDirectFile::fclose() {
+	if (mIsOpen) {
+		int interrupts = OSEnableInterrupts();
+		DVDClose(&mFileInfo);
+		OSRestoreInterrupts(interrupts);
+		mIsOpen = false;
+	}
 }
-#pragma pop
 
 /* 802E8958-802E8B08 2E3298 01B0+00 0/0 1/1 0/0 .text            fgets__13JUTDirectFileFPvi */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm int JUTDirectFile::fgets(void* param_0, int param_1) {
-    nofralloc
-#include "asm/JSystem/JUtility/JUTDirectFile/fgets__13JUTDirectFileFPvi.s"
+int JUTDirectFile::fgets(void* buf, int len) {
+	// if file isn't open, return error (-1).
+	if (!mIsOpen) {
+		return -1;
+	}
+
+	// if desired length to get is 0, get... 0 bytes.
+	if (len == 0) {
+		return 0;
+	}
+
+	// if desired length to get is 1, return 1.
+	// (final byte gotten is always 0, so len 1 is pointless).
+	if (len == 1) {
+		return 1;
+	}
+
+	// if buffer to read into doesn't exist, return error.
+	if (!buf) {
+		return -1;
+	}
+
+	// if we're already beyond the file length, return error.
+	if (mPos >= mLength) {
+		return -1;
+	}
+
+	int readMax;
+	u8* byteBuf   = (u8*)buf;
+	int readCount = 0;
+	readMax       = len - 1; // desired bytes of data to get (last value is then 0).
+
+	while (mPos < mLength) {
+		// if there's nothing left to read, return error.
+		if (mToRead == 0 && fetch32byte() < 0) {
+			return -1;
+		}
+
+		// read in each chunk.
+		u32 currPos   = mPos & (JUTDF_BUFSIZE - 1);
+		u32 chunkSize = (mToRead - currPos);
+		if (readCount + chunkSize > readMax) {
+			chunkSize = len - readCount - 1;
+		}
+
+		BOOL isAtEnd = FALSE;
+		for (int i = 0; i < chunkSize; i++) {
+			u8 byte = mSectorStart[currPos];
+			currPos++;
+			*byteBuf++ = byte;
+
+			// if we hit the end of a line, stop reading.
+			if (byte == '\n') {
+				isAtEnd   = TRUE;
+				chunkSize = i + 1;
+				break;
+			}
+		}
+
+		// if we exceed the buffer size, stop reading.
+		if (currPos >= JUTDF_BUFSIZE) {
+			mToRead = 0;
+		}
+
+		// if we hit the end of a line, set final byte to 0 and stop reading.
+		if (isAtEnd == TRUE) {
+			*byteBuf = 0;
+			mPos += chunkSize;
+			readCount += chunkSize;
+			break;
+		}
+
+		// we should have read the full chunkSize, so update count/pos.
+		mPos += chunkSize;
+		readCount += chunkSize;
+
+		// if we're at (or beyond) our desired length, set final byte to 0 and stop reading.
+		if (readCount >= readMax) {
+			*byteBuf = 0;
+			break;
+		}
+	}
+
+	// if got to the end of the data, set final byte to 0.
+	if (mPos >= mLength) {
+		*byteBuf = 0;
+	}
+
+	return readCount;
 }
-#pragma pop
