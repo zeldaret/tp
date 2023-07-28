e4d6808fa2cb4130815761212d8c4baeb28b08af
Tal Hayon <talhayon1@gmail.com>
2023-07-28 22:30:35 +0300

    JAISoundInfo OK

--- asm/JSystem/JAudio2/JAISoundInfo/__ct__12JAISoundInfoFb.s
+++
@@ -1,9 +0,0 @@
-lbl_802A2D34:
-/* 802A2D34  54 80 06 3F */	clrlwi. r0, r4, 0x18
-/* 802A2D38  41 82 00 08 */	beq lbl_802A2D40
-/* 802A2D3C  90 6D 85 DC */	stw r3, __OSReport_disable-0x3C(r13)
-lbl_802A2D40:
-/* 802A2D40  3C 80 80 3D */	lis r4, __vt__12JAISoundInfo@ha /* 0x803C99A0@ha */
-/* 802A2D44  38 04 99 A0 */	addi r0, r4, __vt__12JAISoundInfo@l /* 0x803C99A0@l */
-/* 802A2D48  90 03 00 00 */	stw r0, 0(r3)
-/* 802A2D4C  4E 80 00 20 */	blr 
