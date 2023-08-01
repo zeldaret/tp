e3ad56a2ac71afd06ef91c765246c36b5ec38fef
Tal Hayon <talhayon1@gmail.com>
2023-08-01 18:33:01 +0300

    JASRegisterParam OK

--- include/JSystem/JAudio2/JASRegisterParam.h
+++ include/JSystem/JAudio2/JASRegisterParam.h
@@ -3,4 +3,17 @@
 
 #include "dolphin/types.h"
 
+struct JASRegisterParam {
+    enum RegID {
+
+    };
+
+    /* 80293644 */ JASRegisterParam();
+    /* 80293664 */ void init();
+    /* 80293684 */ void write(JASRegisterParam::RegID, u32);
+    /* 802937B8 */ u32 read(JASRegisterParam::RegID);
+
+    u16 field_0x0[14];
+};
+
 #endif /* JASREGISTERPARAM_H */
