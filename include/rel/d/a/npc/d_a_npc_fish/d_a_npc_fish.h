473e00470d972ac91f0341db08d6291fd818018c
Noah McQueen <67965519+Yotona@users.noreply.github.com>
2023-08-13 21:59:03 -0600

    d_a_npc_fish (#1880)

--- include/rel/d/a/npc/d_a_npc_fish/d_a_npc_fish.h
+++ include/rel/d/a/npc/d_a_npc_fish/d_a_npc_fish.h
@@ -2,5 +2,16 @@
 #define D_A_NPC_FISH_H
 
 #include "dolphin/types.h"
+#include "f_op/f_op_actor_mng.h"
+
+class npc_fish_class : public fopAc_ac_c {
+    /* 0x568 */ u8 field_0x568[0x5b4 - 0x568]; // unused in TU, unsure of type 
+};
+
+struct fish_data_s {
+    /* 0x00 */ int param;
+    /* 0x04 */ Vec pos;
+    /* 0x10 */ u8 field_0x10;
+};
 
 #endif /* D_A_NPC_FISH_H */
