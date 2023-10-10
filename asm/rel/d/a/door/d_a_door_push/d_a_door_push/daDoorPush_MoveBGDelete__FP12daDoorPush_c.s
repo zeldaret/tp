c5b54dd7ec656e70ced5d1363a6862aecece6afc
Trueffel <106771418+Trueffeloot@users.noreply.github.com>
2023-10-10 22:43:24 +0200

    d_a_door_push OK (#1959)
    * work on d_a_push_door
    
    * d_a_door_push OK
    
    * remove unused asm

--- asm/rel/d/a/door/d_a_door_push/d_a_door_push/daDoorPush_MoveBGDelete__FP12daDoorPush_c.s
+++
@@ -1,9 +0,0 @@
-lbl_80678E3C:
-/* 80678E3C  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80678E40  7C 08 02 A6 */	mflr r0
-/* 80678E44  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80678E48  4B 9F FA 95 */	bl MoveBGDelete__16dBgS_MoveBgActorFv
-/* 80678E4C  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80678E50  7C 08 03 A6 */	mtlr r0
-/* 80678E54  38 21 00 10 */	addi r1, r1, 0x10
-/* 80678E58  4E 80 00 20 */	blr 
