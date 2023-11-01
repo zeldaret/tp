89f7bd9aa5be0418abcc9d3490c830b9edf57ed1
Trueffeloot <106771418+Trueffeloot@users.noreply.github.com>
2023-11-01 22:37:02 +0100

    remove unused asm

--- asm/rel/d/a/door/d_a_door_dbdoor00/d_a_door_dbdoor00/getBmdArcName__12daDbDoor00_cFv.s
+++
@@ -1,5 +0,0 @@
-lbl_8045D4A0:
-/* 8045D4A0  3C 60 80 46 */	lis r3, d_a_door_dbdoor00__stringBase0@ha /* 0x8045E56C@ha */
-/* 8045D4A4  38 63 E5 6C */	addi r3, r3, d_a_door_dbdoor00__stringBase0@l /* 0x8045E56C@l */
-/* 8045D4A8  38 63 00 45 */	addi r3, r3, 0x45
-/* 8045D4AC  4E 80 00 20 */	blr 
