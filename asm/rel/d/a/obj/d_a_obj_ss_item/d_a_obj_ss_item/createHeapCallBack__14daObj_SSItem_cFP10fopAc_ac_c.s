bbb38ed305f661bc820a16154f585f25e1c0c9d8
Jacob Patzer <37888505+jakepatzer@users.noreply.github.com>
2023-11-10 15:21:06 -0800

    d_a_obj_ss_item OK (#1983)

--- asm/rel/d/a/obj/d_a_obj_ss_item/d_a_obj_ss_item/createHeapCallBack__14daObj_SSItem_cFP10fopAc_ac_c.s
+++
@@ -1,9 +0,0 @@
-lbl_80CE7758:
-/* 80CE7758  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80CE775C  7C 08 02 A6 */	mflr r0
-/* 80CE7760  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80CE7764  4B FF FB 35 */	bl CreateHeap__14daObj_SSItem_cFv
-/* 80CE7768  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80CE776C  7C 08 03 A6 */	mtlr r0
-/* 80CE7770  38 21 00 10 */	addi r1, r1, 0x10
-/* 80CE7774  4E 80 00 20 */	blr 
--- asm/rel/d/a/obj/d_a_obj_ss_item/d_a_obj_ss_item/func_80CE7E94.s
+++
@@ -1,3 +0,0 @@
-lbl_80CE7E94:
-/* 80CE7E94  38 63 FF DC */	addi r3, r3, -36
-/* 80CE7E98  4B FF F3 90 */	b __dt__12dBgS_ObjAcchFv
