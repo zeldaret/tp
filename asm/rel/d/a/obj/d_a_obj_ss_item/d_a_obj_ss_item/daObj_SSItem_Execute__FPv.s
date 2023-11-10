78155cee91571ff6f8f854572e30ee6006775824
jakepatzer <37888505+jakepatzer@users.noreply.github.com>
2023-11-10 02:43:13 -0800

    d_a_obj_ss_item OK

--- asm/rel/d/a/obj/d_a_obj_ss_item/d_a_obj_ss_item/daObj_SSItem_Execute__FPv.s
+++
@@ -1,9 +0,0 @@
-lbl_80CE7DCC:
-/* 80CE7DCC  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80CE7DD0  7C 08 02 A6 */	mflr r0
-/* 80CE7DD4  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80CE7DD8  4B FF F5 B5 */	bl Execute__14daObj_SSItem_cFv
-/* 80CE7DDC  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80CE7DE0  7C 08 03 A6 */	mtlr r0
-/* 80CE7DE4  38 21 00 10 */	addi r1, r1, 0x10
-/* 80CE7DE8  4E 80 00 20 */	blr 
--- asm/rel/d/a/obj/d_a_obj_ss_item/d_a_obj_ss_item/getResName__14daObj_SSItem_cFv.s
+++
@@ -1,7 +0,0 @@
-lbl_80CE7838:
-/* 80CE7838  88 03 0B 0E */	lbz r0, 0xb0e(r3)
-/* 80CE783C  54 00 10 3A */	slwi r0, r0, 2
-/* 80CE7840  3C 60 80 CE */	lis r3, l_resFileName@ha /* 0x80CE7F34@ha */
-/* 80CE7844  38 63 7F 34 */	addi r3, r3, l_resFileName@l /* 0x80CE7F34@l */
-/* 80CE7848  7C 63 00 2E */	lwzx r3, r3, r0
-/* 80CE784C  4E 80 00 20 */	blr 
