37c0344daaa8e8f83dcf2bc0feea014e2547cb70
hev7 <143887945+hev7@users.noreply.github.com>
2023-11-15 14:29:30 +0000

    d_a_tag_Lv7Gate OK (#1985)
    * d_a_tag_Lv7Gate OK
    
    * Fix switch cases

--- asm/rel/d/a/tag/d_a_tag_Lv7Gate/d_a_tag_Lv7Gate/daTagLv7Gate_Create__FP10fopAc_ac_c.s
+++
@@ -1,9 +0,0 @@
-lbl_80D50C44:
-/* 80D50C44  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80D50C48  7C 08 02 A6 */	mflr r0
-/* 80D50C4C  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80D50C50  48 00 00 15 */	bl create__14daTagLv7Gate_cFv
-/* 80D50C54  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80D50C58  7C 08 03 A6 */	mtlr r0
-/* 80D50C5C  38 21 00 10 */	addi r1, r1, 0x10
-/* 80D50C60  4E 80 00 20 */	blr 
--- include/rel/d/a/tag/d_a_tag_Lv7Gate/d_a_tag_Lv7Gate.h
+++ include/rel/d/a/tag/d_a_tag_Lv7Gate/d_a_tag_Lv7Gate.h
@@ -1,6 +1,45 @@
 #ifndef D_A_TAG_LV7GATE_H
 #define D_A_TAG_LV7GATE_H
 
-#include "dolphin/types.h"
+#include "d/com/d_com_inf_game.h"
+#include "f_op/f_op_actor_mng.h"
+
+class daTagLv7Gate_c : public fopAc_ac_c {
+public:
+    inline ~daTagLv7Gate_c();
+
+    /* 80D50C64 */ int create();
+    /* 80D50D30 */ void create_init();
+    /* 80D511C8 */ int execute();
+    /* 80D51628 */ void calcFly();
+
+    inline int createHeap();
+    inline void setPath(u8 i_path_ID);
+    inline void initBaseMtx();
+    inline void setBaseMtx();
+    inline void flyAnime();
+    inline bool checkPoint(f32 i_speed);
+    inline bool setNextPoint();
+    inline int draw();
+
+    u8 getPathID() { return (fopAcM_GetParam(this) >> 0x10); }
+    u16 getFlowNodeNo() { return fopAcM_GetParam(this); }
+
+    /* 0X568 */ J3DModel* mpModel;
+    /* 0X56C */ mDoExt_bckAnm* mpBck;
+    /* 0X570 */ dPath* mRoomPath;
+    /* 0X574 */ request_of_phase_process_class mPhase;
+    /* 0X57C */ cXyz mPos2;
+    /* 0X588 */ cXyz mPos1;
+    /* 0X594 */ csXyz field_0x594;
+    /* 0X59C */ char* mEvName;
+    /* 0X5A0 */ int field_0x5a0;
+    /* 0X5A4 */ f32 mDistance;
+    /* 0X5A8 */ f32 mLastFrame;
+    /* 0X5AC */ u16 field_0x5ac;
+    /* 0X5AE */ s16 mEvtId;
+    /* 0X5B0 */ bool field_0x5b0;
+    /* 0X5B1 */ bool field_0x5b1;
+};  // Size: 0x5B4
 
 #endif /* D_A_TAG_LV7GATE_H */
