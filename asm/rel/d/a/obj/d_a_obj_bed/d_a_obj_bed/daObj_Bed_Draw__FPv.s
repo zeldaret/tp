5cc47d7212a12c842e610cdada9822623a804de9
Caroline Madsen <69010899+randomsalience@users.noreply.github.com>
2023-11-18 23:23:27 -0500

    d_a_obj_bed OK (#1989)

--- asm/rel/d/a/obj/d_a_obj_bed/d_a_obj_bed/daObj_Bed_Draw__FPv.s
+++
@@ -1,9 +0,0 @@
-lbl_80BAE094:
-/* 80BAE094  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80BAE098  7C 08 02 A6 */	mflr r0
-/* 80BAE09C  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80BAE0A0  4B FF FD 55 */	bl Draw__11daObj_Bed_cFv
-/* 80BAE0A4  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80BAE0A8  7C 08 03 A6 */	mtlr r0
-/* 80BAE0AC  38 21 00 10 */	addi r1, r1, 0x10
-/* 80BAE0B0  4E 80 00 20 */	blr 
--- asm/rel/d/a/obj/d_a_obj_bed/d_a_obj_bed/daObj_Bed_Execute__FPv.s
+++
@@ -1,9 +0,0 @@
-lbl_80BAE074:
-/* 80BAE074  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80BAE078  7C 08 02 A6 */	mflr r0
-/* 80BAE07C  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80BAE080  4B FF FC 35 */	bl Execute__11daObj_Bed_cFv
-/* 80BAE084  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80BAE088  7C 08 03 A6 */	mtlr r0
-/* 80BAE08C  38 21 00 10 */	addi r1, r1, 0x10
-/* 80BAE090  4E 80 00 20 */	blr 
