c85689c5da8b01831d39fa89d6dd6ee22c6153d9
Caroline Madsen <69010899+randomsalience@users.noreply.github.com>
2023-11-30 17:31:59 -0500

    d_a_obj_barDesk OK (#1997)

--- asm/rel/d/a/obj/d_a_obj_barDesk/d_a_obj_barDesk/daBarDesk_Create__FP10fopAc_ac_c.s
+++
@@ -1,9 +0,0 @@
-lbl_80BA9A28:
-/* 80BA9A28  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80BA9A2C  7C 08 02 A6 */	mflr r0
-/* 80BA9A30  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80BA9A34  4B FF F8 C1 */	bl create__11daBarDesk_cFv
-/* 80BA9A38  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80BA9A3C  7C 08 03 A6 */	mtlr r0
-/* 80BA9A40  38 21 00 10 */	addi r1, r1, 0x10
-/* 80BA9A44  4E 80 00 20 */	blr 
--- asm/rel/d/a/obj/d_a_obj_barDesk/d_a_obj_barDesk/modeBreak__11daBarDesk_cFv.s
+++
@@ -1,9 +0,0 @@
-lbl_80BA9824:
-/* 80BA9824  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80BA9828  7C 08 02 A6 */	mflr r0
-/* 80BA982C  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80BA9830  4B 47 04 4D */	bl fopAcM_delete__FP10fopAc_ac_c
-/* 80BA9834  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80BA9838  7C 08 03 A6 */	mtlr r0
-/* 80BA983C  38 21 00 10 */	addi r1, r1, 0x10
-/* 80BA9840  4E 80 00 20 */	blr 
