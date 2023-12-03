ae6bb0452d49dfaa0e489251626b21377d9c027a
Trueffel <106771418+Trueffeloot@users.noreply.github.com>
2023-12-03 01:20:22 +0100

    prepare some rels for work

--- asm/rel/d/a/tag/d_a_tag_attack_item/d_a_tag_attack_item/daTagAtkItem_Delete__FP14daTagAtkItem_c.s
+++
@@ -1,9 +0,0 @@
-lbl_805A3060:
-/* 805A3060  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 805A3064  7C 08 02 A6 */	mflr r0
-/* 805A3068  90 01 00 14 */	stw r0, 0x14(r1)
-/* 805A306C  4B FF FF CD */	bl _delete__14daTagAtkItem_cFv
-/* 805A3070  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 805A3074  7C 08 03 A6 */	mtlr r0
-/* 805A3078  38 21 00 10 */	addi r1, r1, 0x10
-/* 805A307C  4E 80 00 20 */	blr 
--- include/rel/d/a/obj/d_a_obj_table/d_a_obj_table.h
+++ include/rel/d/a/obj/d_a_obj_table/d_a_obj_table.h
@@ -1,6 +1,9 @@
 #ifndef D_A_OBJ_TABLE_H
 #define D_A_OBJ_TABLE_H
 
-#include "dolphin/types.h"
+#include "d/bg/d_bg_s_movebg_actor.h"
+#include "d/com/d_com_inf_game.h"
+#include "d/msg/d_msg_flow.h"
+#include "f_op/f_op_actor_mng.h"
 
 #endif /* D_A_OBJ_TABLE_H */
