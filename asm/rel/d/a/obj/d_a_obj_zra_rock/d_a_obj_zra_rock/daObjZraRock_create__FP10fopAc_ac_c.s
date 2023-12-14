8293fb7c2293c773381b7aafe8acbb688a9f4112
Caroline Madsen <69010899+randomsalience@users.noreply.github.com>
2023-12-14 18:19:19 -0500

    d_a_obj_zra_rock OK (#2003)

--- asm/rel/d/a/obj/d_a_obj_zra_rock/d_a_obj_zra_rock/daObjZraRock_create__FP10fopAc_ac_c.s
+++
@@ -1,9 +0,0 @@
-lbl_80D4558C:
-/* 80D4558C  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80D45590  7C 08 02 A6 */	mflr r0
-/* 80D45594  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80D45598  4B FF FB 49 */	bl create__14daObjZraRock_cFv
-/* 80D4559C  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80D455A0  7C 08 03 A6 */	mtlr r0
-/* 80D455A4  38 21 00 10 */	addi r1, r1, 0x10
-/* 80D455A8  4E 80 00 20 */	blr 
