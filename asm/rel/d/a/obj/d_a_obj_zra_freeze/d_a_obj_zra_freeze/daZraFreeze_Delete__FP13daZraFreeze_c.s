cd72e88764be3470813e2e1c5437b5612dcecf19
Caroline Madsen <randomsalience@gmail.com>
2023-12-19 17:36:23 -0500

    d_a_obj_zra_freeze, d_a_obj_zrTurara mostly OK, d_a_obj_zrTuraraRock OK

--- asm/rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze/daZraFreeze_Delete__FP13daZraFreeze_c.s
+++
@@ -1,9 +0,0 @@
-lbl_80D44B00:
-/* 80D44B00  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80D44B04  7C 08 02 A6 */	mflr r0
-/* 80D44B08  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80D44B0C  4B FF FF 79 */	bl Delete__13daZraFreeze_cFv
-/* 80D44B10  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80D44B14  7C 08 03 A6 */	mtlr r0
-/* 80D44B18  38 21 00 10 */	addi r1, r1, 0x10
-/* 80D44B1C  4E 80 00 20 */	blr 
--- rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze.cpp
+++ rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze.cpp
@@ -1,45 +1,17 @@
 /**
  * d_a_obj_zra_freeze.cpp
- *
+ * Object - Frozen Zora
  */
 
 #include "rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze.h"
 #include "JSystem/JKernel/JKRHeap.h"
 #include "SSystem/SComponent/c_math.h"
+#include "m_Do/m_Do_lib.h"
+#include "f_op/f_op_actor_mng.h"
+#include "d/com/d_com_inf_game.h"
 #include "d/a/d_a_player.h"
 #include "d/d_procname.h"
 #include "dol2asm.h"
-
-class daZraFreeze_c : public fopAc_ac_c {
-public:
-    /* 80D440B8 */ void setBaseMtx();
-    /* 80D44124 */ BOOL chkActorInScreen();
-    /* 80D442BC */ int Create();
-    /* 80D4443C */ int CreateHeap();
-    /* 80D444C0 */ void setHitodamaPrtcl();
-    /* 80D44698 */ int Execute();
-    /* 80D449E0 */ int Draw();
-    /* 80D44A84 */ int Delete();
-
-    /* 0x568 */ request_of_phase_process_class mPhaseReq;
-    /* 0x570 */ J3DModel* mpModel;
-    /* 0x574 */ Mtx mMtx;
-    /* 0x5A4 */ s32 field_0x5a4;
-    /* 0x5A8 */ s32 field_0x5a8;
-    /* 0x5AC */ s32 field_0x5ac;
-    /* 0x5B0 */ u8 field_0x5b0;
-    /* 0x5B1 */ u8 field_0x5b1;
-    /* 0x5B2 */ u8 field_0x5b2;
-    /* 0x5B3 */ u8 field_0x5B3[0x5B4 - 0x5B3];
-    /* 0x5B4 */ cXyz field_0x5b4;
-    /* 0x5C0 */ cXyz field_0x5c0;
-    /* 0x5CC */ s16 field_0x5cc;
-    /* 0x5CE */ u8 field_0x5ce;
-    /* 0x5CF */ u8 field_0x5cf;
-    /* 0x5D0 */ cXyz field_0x5d0;
-    /* 0x5DC */ s16 field_0x5dc;
-    /* 0x5DE */ s16 field_0x5de;
-};
 
 //
 // Forward References:
@@ -60,7 +32,6 @@ extern "C" static bool daZraFreeze_IsDelete__FP13daZraFreeze_c();
 extern "C" static void daZraFreeze_Delete__FP13daZraFreeze_c();
 extern "C" static void daZraFreeze_create__FP10fopAc_ac_c();
 extern "C" extern char const* const d_a_obj_zra_freeze__stringBase0;
-extern "C" extern void* g_profile_OBJ_ZRAFREEZE[12];
 
 //
 // External References:
@@ -103,106 +74,95 @@ extern "C" u8 sincosTable___5JMath[65536];
 // Declarations:
 //
 
-/* 80D440B8-80D44124 000078 006C+00 2/2 0/0 0/0 .text            setBaseMtx__13daZraFreeze_cFv */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm void daZraFreeze_c::setBaseMtx() {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze/setBaseMtx__13daZraFreeze_cFv.s"
-}
-#pragma pop
-
-/* ############################################################################################## */
-/* 80D44B48-80D44B50 000000 0008+00 3/3 0/0 0/0 .rodata          l_DATA */
-SECTION_RODATA static u8 const l_DATA[8] = {
-    0x44, 0x48, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-};
-COMPILER_STRIP_GATE(0x80D44B48, &l_DATA);
-
-/* 80D44B50-80D44B54 000008 0004+00 0/2 0/0 0/0 .rodata          @3763 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static u8 const lit_3763[4] = {
-    0x00,
-    0x00,
-    0x00,
-    0x00,
-};
-COMPILER_STRIP_GATE(0x80D44B50, &lit_3763);
-#pragma pop
-
-/* 80D44B54-80D44B58 00000C 0004+00 0/1 0/0 0/0 .rodata          @3764 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static f32 const lit_3764 = 608.0f;
-COMPILER_STRIP_GATE(0x80D44B54, &lit_3764);
-#pragma pop
-
-/* 80D44B58-80D44B5C 000010 0004+00 0/1 0/0 0/0 .rodata          @3765 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static f32 const lit_3765 = 448.0f;
-COMPILER_STRIP_GATE(0x80D44B58, &lit_3765);
-#pragma pop
-
-/* 80D44124-80D4429C 0000E4 0178+00 1/1 0/0 0/0 .text            chkActorInScreen__13daZraFreeze_cFv
- */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm BOOL daZraFreeze_c::chkActorInScreen() {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze/chkActorInScreen__13daZraFreeze_cFv.s"
-}
-#pragma pop
-
-/* 80D4429C-80D442BC 00025C 0020+00 1/1 0/0 0/0 .text            createSolidHeap__FP10fopAc_ac_c */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-static asm int createSolidHeap(fopAc_ac_c* i_this) {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze/createSolidHeap__FP10fopAc_ac_c.s"
-}
-#pragma pop
-
-/* ############################################################################################## */
-/* 80D44B5C-80D44B60 000014 0004+00 1/1 0/0 0/0 .rodata          @3840 */
-SECTION_RODATA static f32 const lit_3840 = 65536.0f;
-COMPILER_STRIP_GATE(0x80D44B5C, &lit_3840);
-
 /* 80D44BC4-80D44BC8 -00001 0004+00 3/3 0/0 0/0 .data            l_arcName */
 static char* l_arcName = "zrF";
 
-/* 80D442BC-80D4443C 00027C 0180+00 1/1 0/0 0/0 .text            Create__13daZraFreeze_cFv */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm int daZraFreeze_c::Create() {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze/Create__13daZraFreeze_cFv.s"
-}
-#pragma pop
+/* 80D44B48-80D44B50 000000 0008+00 3/3 0/0 0/0 .rodata          l_DATA */
+static f32 const l_DATA[2] = { 800.0f, 0.0f };
 
-/* 80D44BC8-80D44BD4 -00001 000C+00 1/1 0/0 0/0 .data            l_bmdName$3845 */
-static char* l_bmdName[3] = {
-    "zrA_a_TW.bmd",
-    "zrA_pain_TW.bmd",
-    "zrA_sol_TW.bmd",
-};
+/* 80D440B8-80D44124 000078 006C+00 2/2 0/0 0/0 .text            setBaseMtx__13daZraFreeze_cFv */
+void daZraFreeze_c::setBaseMtx() {
+    mDoMtx_stack_c::transS(current.pos.x, current.pos.y, current.pos.z);
+    mDoMtx_stack_c::XYZrotM(shape_angle);
+    mpModel->i_setBaseTRMtx(mDoMtx_stack_c::get());
+    PSMTXCopy(mDoMtx_stack_c::get(), mMtx);
+}
+
+/* 80D44124-80D4429C 0000E4 0178+00 1/1 0/0 0/0 .text            chkActorInScreen__13daZraFreeze_cFv
+ */
+BOOL daZraFreeze_c::chkActorInScreen() {
+    cXyz proj;
+    cXyz vec[8];
+    vec[0].set(mMin.x, mMax.y, mMin.z);
+    vec[1].set(mMax.x, mMax.y, mMin.z);
+    vec[2].set(mMin.x, mMax.y, mMax.y);
+    vec[3].set(mMax.x, mMax.y, mMax.y);
+    vec[4].set(mMin.x, mMin.y, mMin.z);
+    vec[5].set(mMax.x, mMin.y, mMin.z);
+    vec[6].set(mMin.x, mMin.y, mMax.y);
+    vec[7].set(mMax.x, mMin.y, mMax.y);
+    mDoMtx_stack_c::copy(mMtx);
+    mDoMtx_stack_c::transM(0.0f, 0.0f, 0.0f);
+    PSMTXMultVecArray(mDoMtx_stack_c::get(), vec, vec, 8);
+    for (int i = 0; i < 8; i++) {
+        mDoLib_project(&vec[i], &proj);
+        if (0.0f < proj.x && proj.x < 608.0f && 0.0f < proj.y && proj.y < 448.0f) {
+            continue;
+        }
+        return false;
+    }
+    return true;
+}
+
+/* 80D4429C-80D442BC 00025C 0020+00 1/1 0/0 0/0 .text            createSolidHeap__FP10fopAc_ac_c */
+static int createSolidHeap(fopAc_ac_c* i_this) {
+    return static_cast<daZraFreeze_c*>(i_this)->CreateHeap();
+}
+
+/* 80D442BC-80D4443C 00027C 0180+00 1/1 0/0 0/0 .text            Create__13daZraFreeze_cFv */
+cPhs__Step daZraFreeze_c::Create() {
+    fopAcM_SetupActor(this, daZraFreeze_c);
+    field_0x5b0 = (fopAcM_GetParam(this) >> 8) & 0xff;
+    field_0x5b1 = (fopAcM_GetParam(this) >> 0x10) & 0xff;
+    field_0x5b2 = (fopAcM_GetParam(this) >> 0x18) & 0xff;
+    mMapToolId = fopAcM_GetParam(this) & 0xff;
+    mEventId = -1;
+    cPhs__Step step = (cPhs__Step)dComIfG_resLoad(&mPhaseReq, l_arcName);
+    if (step == cPhs_COMPLEATE_e) {
+        if (!fopAcM_entrySolidHeap(this, createSolidHeap, 0x1540)) {
+            return cPhs_ERROR_e;
+        }
+        setBaseMtx();
+        fopAcM_SetMtx(this, mpModel->getBaseTRMtx());
+        J3DJoint* joint = mpModel->getModelData()->getJointNodePointer(0);
+        fopAcM_setCullSizeBox(this, joint->getMin()->x, joint->getMin()->y, joint->getMin()->z,
+                              joint->getMax()->x, joint->getMax()->y, joint->getMax()->z);
+        mMin.set(joint->getMin()->x, joint->getMin()->y, joint->getMin()->z);
+        mMax.set(joint->getMax()->x, joint->getMax()->y, joint->getMax()->z);
+        mTwilight = dKy_darkworld_check();
+        mParticleKey[0] = -1;
+        mParticleKey[1] = -1;
+        field_0x5cf = 0;
+        field_0x5de = cM_rndF(65536.0f);
+    }
+    return step;
+}
 
 /* 80D4443C-80D444C0 0003FC 0084+00 1/1 0/0 0/0 .text            CreateHeap__13daZraFreeze_cFv */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm int daZraFreeze_c::CreateHeap() {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze/CreateHeap__13daZraFreeze_cFv.s"
+int daZraFreeze_c::CreateHeap() {
+    static char* l_bmdName[3] = {
+        "zrA_a_TW.bmd",
+        "zrA_pain_TW.bmd",
+        "zrA_sol_TW.bmd",
+    };
+    J3DModelData* model_data = (J3DModelData*)dComIfG_getObjectRes(l_arcName, l_bmdName[mSubtype]);
+    mpModel = mDoExt_J3DModel__create(model_data, 0x80000, 0x11000084);
+    if (mpModel == NULL) {
+        return 0;
+    }
+    return 1;
 }
-#pragma pop
 
-/* ############################################################################################## */
 /* 80D44B60-80D44B64 000018 0004+00 0/1 0/0 0/0 .rodata          id$3856 */
 #pragma push
 #pragma force_active on
@@ -238,6 +198,30 @@ COMPILER_STRIP_GATE(0x80D44B6C, &lit_3919);
 
 /* 80D444C0-80D44698 000480 01D8+00 1/1 0/0 0/0 .text            setHitodamaPrtcl__13daZraFreeze_cFv
  */
+#ifdef NONMATCHING
+void daZraFreeze_c::setHitodamaPrtcl() {
+    cXyz pos = current.pos;
+    field_0x5dc = (u16)(field_0x5de << 1);
+    mParticleOffset.x = 8.0f * cM_ssin(field_0x5de);
+    mParticleOffset.y = 4.0f * cM_ssin(field_0x5dc);
+    mParticleOffset.z = mParticleOffset.x * -cM_ssin(shape_angle.y);
+    mParticleOffset.x *= cM_scos(shape_angle.y);
+    field_0x5de += 0x400;
+    pos.x = current.pos.x + mParticleOffset.x;
+    pos.y = current.pos.y + mParticleOffset.y;
+    pos.z = current.pos.z + mParticleOffset.z;
+    static u16 const id[2] = { 0x8497, 0x8498 };
+    for (int i = 0; i < 2; i++) {
+        mParticleKey[i] = dComIfGp_particle_set(mParticleKey[i], id[i], &pos, &shape_angle, NULL);
+        JPABaseEmitter* emitter = dComIfGp_particle_getEmitter(mParticleKey[i]);
+        if (emitter != NULL) {
+            u8 alpha = dComIfGs_wolfeye_effect_check() == false ? 0xff : 0;
+            emitter->setGlobalTranslation(pos.x, pos.y, pos.z);
+            emitter->setGlobalAlpha(alpha);
+        }
+    }
+}
+#else
 #pragma push
 #pragma optimization_level 0
 #pragma optimizewithasm off
@@ -246,91 +230,76 @@ asm void daZraFreeze_c::setHitodamaPrtcl() {
 #include "asm/rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze/setHitodamaPrtcl__13daZraFreeze_cFv.s"
 }
 #pragma pop
-
-/* ############################################################################################## */
-/* 80D44B70-80D44B78 000028 0008+00 0/1 0/0 0/0 .rodata          @4050 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static u8 const lit_4050[8] = {
-    0x3F, 0xE0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-};
-COMPILER_STRIP_GATE(0x80D44B70, &lit_4050);
-#pragma pop
-
-/* 80D44B78-80D44B80 000030 0008+00 0/1 0/0 0/0 .rodata          @4051 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static u8 const lit_4051[8] = {
-    0x40, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-};
-COMPILER_STRIP_GATE(0x80D44B78, &lit_4051);
-#pragma pop
-
-/* 80D44B80-80D44B88 000038 0008+00 0/1 0/0 0/0 .rodata          @4052 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static u8 const lit_4052[8] = {
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-};
-COMPILER_STRIP_GATE(0x80D44B80, &lit_4052);
-#pragma pop
-
-/* 80D44B88-80D44B88 000040 0000+00 0/0 0/0 0/0 .rodata          @stringBase0 */
-#pragma push
-#pragma force_active on
-SECTION_DEAD static char const* const stringBase_80D44BB8 = "zora_koori";
-#pragma pop
+#endif
 
 /* 80D44698-80D449E0 000658 0348+00 1/1 0/0 0/0 .text            Execute__13daZraFreeze_cFv */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm int daZraFreeze_c::Execute() {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze/Execute__13daZraFreeze_cFv.s"
+int daZraFreeze_c::Execute() {
+    if (field_0x5b0 == 0xff || dComIfGs_isSwitch(field_0x5b0, fopAcM_GetRoomNo(this))) {
+        setBaseMtx();
+        csXyz stored_shape_angle = shape_angle;
+        shape_angle.x = 0;
+        shape_angle.z = 0;
+        setHitodamaPrtcl();
+        shape_angle = stored_shape_angle;
+        if (field_0x5b1 != 0xff && dComIfGs_isSwitch(field_0x5b1, fopAcM_GetRoomNo(this))) {
+            if (!dComIfGs_isSwitch(field_0x5b2, fopAcM_GetRoomNo(this))) {
+                if (mTwilight != 0 && daPy_py_c::checkNowWolfEyeUp()) {
+                    cXyz delta = current.pos - daPy_getPlayerActorClass()->current.pos;
+                    if (delta.absXZ() < l_DATA[0] && chkActorInScreen()) {
+                        mEventId = i_dComIfGp_getEventManager().getEventIdx(this, "zora_koori", 0xff);
+                        fopAcM_orderOtherEventId(this, mEventId, mMapToolId, 0xffff, 4, 1);
+                        dComIfGs_onSwitch(field_0x5b2, fopAcM_GetRoomNo(this));
+                        daPy_getPlayerActorClass()->onWolfEyeKeep();
+                        dComIfGs_sense_type_change_Set(1);
+                        field_0x5cf = 1;
+                    }
+                }
+            } else {
+                if (i_dComIfGp_event_runCheck()) {
+                    if (mEvtInfo.i_checkCommandDemoAccrpt()) {
+                        daPy_getPlayerActorClass()->onWolfEyeKeep();
+                    } else if (field_0x5cf == 1) {
+                        dComIfGs_sense_type_change_Set(-1);
+                        field_0x5cf = 0;
+                    }
+                } else if (field_0x5cf == 1) {
+                    dComIfGs_sense_type_change_Set(-1);
+                    field_0x5cf = 0;
+                }
+            }
+        }
+    }
+    return 1;
 }
-#pragma pop
 
 /* 80D449E0-80D44A84 0009A0 00A4+00 1/1 0/0 0/0 .text            Draw__13daZraFreeze_cFv */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm int daZraFreeze_c::Draw() {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze/Draw__13daZraFreeze_cFv.s"
+int daZraFreeze_c::Draw() {
+    if (field_0x5b0 == 0xff || dComIfGs_isSwitch(field_0x5b0, fopAcM_GetRoomNo(this))) {
+        if (mTwilight != 0 && dComIfGs_wolfeye_effect_check() == true) {
+            g_env_light.settingTevStruct(4, &current.pos, &mTevStr);
+            g_env_light.setLightTevColorType_MAJI(mpModel->mModelData, &mTevStr);
+            mDoExt_modelUpdateDL(mpModel);
+        }
+    }
+    return 1;
 }
-#pragma pop
 
 /* 80D44A84-80D44AB8 000A44 0034+00 1/1 0/0 0/0 .text            Delete__13daZraFreeze_cFv */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm int daZraFreeze_c::Delete() {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze/Delete__13daZraFreeze_cFv.s"
+int daZraFreeze_c::Delete() {
+    dComIfG_resDelete(&mPhaseReq, l_arcName);
+    return 1;
 }
-#pragma pop
 
 /* 80D44AB8-80D44AD8 000A78 0020+00 1/0 0/0 0/0 .text            daZraFreeze_Draw__FP13daZraFreeze_c
  */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-static asm int daZraFreeze_Draw(daZraFreeze_c* i_this) {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze/daZraFreeze_Draw__FP13daZraFreeze_c.s"
+static int daZraFreeze_Draw(daZraFreeze_c* i_this) {
+    return i_this->Draw();
 }
-#pragma pop
 
 /* 80D44AD8-80D44AF8 000A98 0020+00 1/0 0/0 0/0 .text daZraFreeze_Execute__FP13daZraFreeze_c */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-static asm int daZraFreeze_Execute(daZraFreeze_c* i_this) {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze/daZraFreeze_Execute__FP13daZraFreeze_c.s"
+static int daZraFreeze_Execute(daZraFreeze_c* i_this) {
+    return i_this->Execute();
 }
-#pragma pop
 
 /* 80D44AF8-80D44B00 000AB8 0008+00 1/0 0/0 0/0 .text daZraFreeze_IsDelete__FP13daZraFreeze_c */
 static int daZraFreeze_IsDelete(daZraFreeze_c* i_this) {
@@ -338,45 +307,40 @@ static int daZraFreeze_IsDelete(daZraFreeze_c* i_this) {
 }
 
 /* 80D44B00-80D44B20 000AC0 0020+00 1/0 0/0 0/0 .text daZraFreeze_Delete__FP13daZraFreeze_c */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-static asm int daZraFreeze_Delete(daZraFreeze_c* i_this) {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze/daZraFreeze_Delete__FP13daZraFreeze_c.s"
+static int daZraFreeze_Delete(daZraFreeze_c* i_this) {
+    return i_this->Delete();
 }
-#pragma pop
 
 /* 80D44B20-80D44B40 000AE0 0020+00 1/0 0/0 0/0 .text            daZraFreeze_create__FP10fopAc_ac_c
  */
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-static asm int daZraFreeze_create(fopAc_ac_c* i_this) {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_zra_freeze/d_a_obj_zra_freeze/daZraFreeze_create__FP10fopAc_ac_c.s"
+static cPhs__Step daZraFreeze_create(fopAc_ac_c* i_this) {
+    return static_cast<daZraFreeze_c*>(i_this)->Create();
 }
-#pragma pop
 
 /* ############################################################################################## */
 /* 80D44BD4-80D44BF4 -00001 0020+00 1/0 0/0 0/0 .data            l_daZraFreeze_Method */
-SECTION_DATA static void* l_daZraFreeze_Method[8] = {
-    (void*)daZraFreeze_create__FP10fopAc_ac_c,
-    (void*)daZraFreeze_Delete__FP13daZraFreeze_c,
-    (void*)daZraFreeze_Execute__FP13daZraFreeze_c,
-    (void*)daZraFreeze_IsDelete__FP13daZraFreeze_c,
-    (void*)daZraFreeze_Draw__FP13daZraFreeze_c,
-    (void*)NULL,
-    (void*)NULL,
-    (void*)NULL,
+static actor_method_class l_daZraFreeze_Method = {
+    (process_method_func)daZraFreeze_create,
+    (process_method_func)daZraFreeze_Delete,
+    (process_method_func)daZraFreeze_Execute,
+    (process_method_func)daZraFreeze_IsDelete,
+    (process_method_func)daZraFreeze_Draw,
 };
 
 /* 80D44BF4-80D44C24 -00001 0030+00 0/0 0/0 1/0 .data            g_profile_OBJ_ZRAFREEZE */
-SECTION_DATA extern void* g_profile_OBJ_ZRAFREEZE[12] = {
-    (void*)0xFFFFFFFD, (void*)0x0007FFFD,
-    (void*)0x02820000, (void*)&g_fpcLf_Method,
-    (void*)0x000005E0, (void*)NULL,
-    (void*)NULL,       (void*)&g_fopAc_Method,
-    (void*)0x01840000, (void*)&l_daZraFreeze_Method,
-    (void*)0x00044100, (void*)0x000E0000,
+extern actor_process_profile_definition g_profile_OBJ_ZRAFREEZE = {
+    fpcLy_CURRENT_e,
+    7,
+    fpcPi_CURRENT_e,
+    PROC_OBJ_ZRAFREEZE,
+    &g_fpcLf_Method.mBase,
+    sizeof(daZraFreeze_c),
+    0,
+    0,
+    &g_fopAc_Method.base,
+    0x184,
+    &l_daZraFreeze_Method,
+    0x44100,
+    fopAc_ACTOR_e,
+    fopAc_CULLBOX_CUSTOM_e,
 };
