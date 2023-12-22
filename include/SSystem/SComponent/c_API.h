aba0717ff5b6e02cfb36097c834efcee172f9f23
TakaRikka <takarikka@outlook.com>
2023-12-22 05:12:04 -0800

    cleanup SSystem files

--- include/SSystem/SComponent/c_API.h
+++ include/SSystem/SComponent/c_API.h
@@ -1,9 +1,17 @@
 #ifndef C_API_H
 #define C_API_H
 
-
 typedef void (*cAPIGph_Mthd)(void);
 
-extern cAPIGph_Mthd g_cAPI_Interface[6];
+struct cAPI_Interface {
+    /* 0x00 */ cAPIGph_Mthd createMtd;
+    /* 0x04 */ cAPIGph_Mthd beforeOfDrawMtd;
+    /* 0x08 */ cAPIGph_Mthd afterOfDrawMtd;
+    /* 0x0C */ cAPIGph_Mthd painterMtd;
+    /* 0x10 */ cAPIGph_Mthd blankingOnMtd;
+    /* 0x14 */ cAPIGph_Mthd blankingOffMtd;
+};
+
+extern cAPI_Interface g_cAPI_Interface;
 
 #endif /* C_API_H */
