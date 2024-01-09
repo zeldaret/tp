1819966d8a4c4365ffef6ba56ab78f57c3818d46
Tal Hayon <talhayon1@gmail.com>
2024-01-09 23:15:15 +0200

    Work on std-vector

--- libs/PowerPC_EABI_Support/MSL/MSL_C++/MSL_Common/Include/algorithm.h
+++ libs/PowerPC_EABI_Support/MSL/MSL_C++/MSL_Common/Include/algorithm.h
@@ -43,6 +43,24 @@ void fill(ForwardIt first, ForwardIt last, const T& value) {
         *first = value;
     }
 }
+
+template<class InputIt, class OutputIt>
+inline OutputIt copy(InputIt first, InputIt last,
+              OutputIt d_first) {
+    for (; first < last; ++first, ++d_first) {
+        *d_first = *first;
+    }
+    return d_first;
+}
+
+template<class BidirIt1, class BidirIt2>
+inline BidirIt2 copy_backward(BidirIt1 first, BidirIt1 last, BidirIt2 d_last) {
+    while (first != last) {
+        *(--d_last) = *(--last);
+    }
+    return d_last;
+}
+
 }  // namespace std
 
 #endif
