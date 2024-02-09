e5b49388545f35b941ef32767fb848205b342b7f
TakaRikka <takarikka@outlook.com>
2024-02-09 05:27:53 -0800

    d_a_e_yd_leaf done

--- include/rel/d/a/e/d_a_e_yd_leaf/d_a_e_yd_leaf.h
+++ include/rel/d/a/e/d_a_e_yd_leaf/d_a_e_yd_leaf.h
@@ -1,6 +1,15 @@
 #ifndef D_A_E_YD_LEAF_H
 #define D_A_E_YD_LEAF_H
 
-#include "dolphin/types.h"
+#include "f_op/f_op_actor_mng.h"
+
+class e_yd_leaf_class : public fopEn_enemy_c {
+public:
+    /* 0x5AC */ request_of_phase_process_class mPhase;
+    /* 0x5B4 */ mDoExt_McaMorf* mpMorf;
+    /* 0x5B8 */ u8 field_0x5b8[0x5BA - 0x5B8];
+    /* 0x5BA */ s8 field_0x5ba;
+    /* 0x5BB */ u8 field_0x5bb[0x5C0 - 0x5BB];
+};
 
 #endif /* D_A_E_YD_LEAF_H */
