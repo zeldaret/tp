27adf107ab52f6f81808ebf366652a34fb787fbd
TakaRikka <takarikka@outlook.com>
2024-02-15 19:51:44 -0800

    remove asm

--- asm/rel/d/a/e/d_a_e_arrow/d_a_e_arrow/func_8067E858.s
+++
@@ -1,3 +0,0 @@
-lbl_8067E858:
-/* 8067E858  38 63 FF EC */	addi r3, r3, -20
-/* 8067E85C  4B FF F2 14 */	b __dt__14dBgS_ObjGndChkFv
--- asm/rel/d/a/e/d_a_e_arrow/d_a_e_arrow/func_8067E860.s
+++
@@ -1,3 +0,0 @@
-lbl_8067E860:
-/* 8067E860  38 63 FF B4 */	addi r3, r3, -76
-/* 8067E864  4B FF F2 0C */	b __dt__14dBgS_ObjGndChkFv
