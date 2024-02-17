57b25aa955d9f0dadb2ab768169fb65b733c2580
TakaRikka <takarikka@outlook.com>
2024-02-17 05:49:31 -0800

    remove asm

--- asm/d/map/d_map/getIconPosition__6dMap_cCFPQ27dTres_c15typeGroupData_c.s
+++
@@ -1,9 +0,0 @@
-lbl_80029038:
-/* 80029038  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 8002903C  7C 08 02 A6 */	mflr r0
-/* 80029040  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80029044  48 01 7A 99 */	bl getIconPosition__28renderingPlusDoorAndCursor_cCFPQ27dTres_c15typeGroupData_c
-/* 80029048  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 8002904C  7C 08 03 A6 */	mtlr r0
-/* 80029050  38 21 00 10 */	addi r1, r1, 0x10
-/* 80029054  4E 80 00 20 */	blr 
--- asm/d/map/d_map/getNextData__6dMap_cFPQ27dTres_c15typeGroupData_c.s
+++
@@ -1,9 +0,0 @@
-lbl_8002ABCC:
-/* 8002ABCC  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 8002ABD0  7C 08 02 A6 */	mflr r0
-/* 8002ABD4  90 01 00 14 */	stw r0, 0x14(r1)
-/* 8002ABD8  48 01 5E E1 */	bl getNextData__28renderingPlusDoorAndCursor_cFPQ27dTres_c15typeGroupData_c
-/* 8002ABDC  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 8002ABE0  7C 08 03 A6 */	mtlr r0
-/* 8002ABE4  38 21 00 10 */	addi r1, r1, 0x10
-/* 8002ABE8  4E 80 00 20 */	blr 
