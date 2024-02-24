8a4abf52cf8915204b2f4b68adf2207046e2c959
GinNoOokami <noreply>
2024-02-24 23:49:27 +0000

    d_a_obj_table decomp OK

--- rel/d/a/obj/d_a_obj_table/d_a_obj_table.cpp
+++ rel/d/a/obj/d_a_obj_table/d_a_obj_table.cpp
@@ -4,63 +4,12 @@
  */
 
 #include "rel/d/a/obj/d_a_obj_table/d_a_obj_table.h"
-#include "JSystem/JKernel/JKRHeap.h"
+
 #include "JSystem/J3DGraphBase/J3DMaterial.h"
-#include "d/bg/d_bg_w.h"
+#include "d/com/d_com_inf_game.h"
 #include "d/d_procname.h"
 #include "d/meter/d_meter2_info.h"
 #include "d/msg/d_msg_object.h"
-#include "dol2asm.h"
-
-class daObjTable_c : public dBgS_MoveBgActor {
-public:
-    /* 80D06560 */ int CreateHeap();
-    /* 80D065D0 */ int Create();
-    /* 80D0695C */ int Execute(Mtx**);
-    /* 80D06AD8 */ int Draw();
-    /* 80D06BC8 */ int Delete();
-
-    u32 getMessageNo() { return fopAcM_GetParam(this); }
-
-    request_of_phase_process_class& getPhase() { return mPhaseReq; }
-
-private:
-    /* 0x5A0 */ J3DModel* mpModel;
-    /* 0x5A4 */ request_of_phase_process_class mPhaseReq;
-    /* 0x5AC */ dMsgFlow_c mMsgFlow;
-    /* 0x5F8 */ u32 field_0x5f8;
-    /* 0x5FC */ s16 field_0x5fc;
-};  // Size: 0x600
-
-//
-// Forward References:
-//
-
-extern "C" extern char const* const d_a_obj_table__stringBase0;
-
-//
-// External References:
-//
-
-extern "C" void reset__14dEvt_control_cFPv();
-extern "C" void setObjectArchive__16dEvent_manager_cFPc();
-extern "C" void getEventIdx__16dEvent_manager_cFP10fopAc_ac_cPCcUc();
-extern "C" void endCheck__16dEvent_manager_cFs();
-extern "C" void getMyStaffId__16dEvent_manager_cFPCcP10fopAc_ac_ci();
-extern "C" void endFlowGroup__12dMsgObject_cFv();
-extern "C" void __ct__10dMsgFlow_cFv();
-extern "C" void init__10dMsgFlow_cFP10fopAc_ac_ciiPP10fopAc_ac_c();
-extern "C" void doFlow__10dMsgFlow_cFP10fopAc_ac_cPP10fopAc_ac_ci();
-extern "C" void _savegpr_29();
-extern "C" void _restgpr_29();
-extern "C" u8 saveBitLabels__16dSv_event_flag_c[1644 + 4 /* padding */];
-extern "C" u8 now__14mDoMtx_stack_c[48];
-
-#define NONMATCHING 0
-
-//
-// Declarations:
-//
 
 /* 80D06C1C-80D06C20 -00001 0004+00 4/4 0/0 0/0 .data            l_arcName */
 static char* l_arcName = "Table";
@@ -160,7 +109,7 @@ int daObjTable_c::Create() {
     mAttentionInfo.mPosition.y = y;
     mAttentionInfo.mPosition.z = z;
 
-    mMsgFlow.init(this, (s16)fopAcM_GetParamBit(this, 0, 16), 0, NULL);
+    mMsgFlow.init(this, GetFlowID(), 0, NULL);
     dMsgObject_endFlowGroup();
 
     mCullMtx = mpModel->mBaseTransformMtx;
@@ -171,66 +120,42 @@ int daObjTable_c::Create() {
     PSMTXCopy(mDoMtx_stack_c::get(), mBgMtx);
     PSMTXCopy(mDoMtx_stack_c::get(), mpModel->mBaseTransformMtx);
 
-    field_0x5fc = -1;
+    mEventID = -1;
 
     return cPhs_COMPLEATE_e;
 }
 
-/* ############################################################################################## */
-/* 80D06C0C-80D06C0C 000008 0000+00 0/0 0/0 0/0 .rodata          @stringBase0 */
-#if !NONMATCHING
-#pragma push
-#pragma force_active on
-SECTION_DEAD static char const* const stringBase_80D06C12 = "TABLE_MAP";
-#pragma pop
-#else
-static char* stringBase_80D06C12 = "TABLE_MAP";
-#endif
-
 /* 80D0695C-80D06AD8 00059C 017C+00 1/0 0/0 0/0 .text            Execute__12daObjTable_cFPPA3_A4_f
  */
-#if NONMATCHING
 int daObjTable_c::Execute(Mtx** i_mtx) {
     mEvtInfo.i_onCondition(1);
 
     dComIfG_inf_c& gameInfo = g_dComIfG_gameInfo;
-
-    if (gameInfo.getPlay().getEvent().mEventStatus != 0) {
+    if (gameInfo.getPlay().getEvent().runCheck()) {
         if (mEvtInfo.checkCommandTalk()) {
             if (mMsgFlow.doFlow(this, NULL, 0) != 0) {
                 gameInfo.getPlay().getEvent().reset(this);
-                mEvtInfo.setArchiveName(stringBase_80D06C12);
+                mEvtInfo.setArchiveName("Table");
                 i_dComIfGp_getEventManager().setObjectArchive(mEvtInfo.getArchiveName());
-                field_0x5fc = i_dComIfGp_getEventManager().getEventIdx(this, stringBase_80D06C12, -1);
+                mEventID = i_dComIfGp_getEventManager().getEventIdx(this, "TABLE_MAP", -1);
 
-                fopAcM_orderChangeEventId(this, field_0x5fc, 1, -1);
+                fopAcM_orderChangeEventId(this, mEventID, 1, -1);
             }
         } else if (i_dComIfGp_getEventManager().getMyStaffId(l_arcName, NULL, 0) != -1 && 
                     mEvtInfo.mCommand == 2 && 
-                    field_0x5fc != -1 && 
-                    i_dComIfGp_getEventManager().endCheck(field_0x5fc)) {
-            field_0x5fc = -1;                              
+                    mEventID != -1 && 
+                    i_dComIfGp_getEventManager().endCheck(mEventID)) {
+            mEventID = -1;                              
             g_meter2_info.setPauseStatus(7);
         }
     } else {
-        mMsgFlow.init(this, (s16)fopAcM_GetParamBit(this, 0, 16), 0, NULL);
+        mMsgFlow.init(this, GetFlowID(), 0, NULL);
         dMsgObject_endFlowGroup();
     }
 
     *i_mtx = &mBgMtx;
     return 1;
 }
-
-#else
-#pragma push
-#pragma optimization_level 0
-#pragma optimizewithasm off
-asm int daObjTable_c::Execute(Mtx** i_mtx) {
-    nofralloc
-#include "asm/rel/d/a/obj/d_a_obj_table/d_a_obj_table/Execute__12daObjTable_cFPPA3_A4_f.s"
-}
-#pragma pop
-#endif
 
 /* 80D06AD8-80D06BC8 000718 00F0+00 1/0 0/0 0/0 .text            Draw__12daObjTable_cFv */
 int daObjTable_c::Draw() {
@@ -242,7 +167,7 @@ int daObjTable_c::Draw() {
     dComIfGd_setList();
 
     cXyz v(current.pos.x, current.pos.y + 100.0f, current.pos.z);
-    field_0x5f8 = dComIfGd_setRealShadow(field_0x5f8, 1, mpModel, &v, 500.0f, current.pos.y, &mTevStr);
+    mShadowKey = dComIfGd_setRealShadow(mShadowKey, 1, mpModel, &v, 500.0f, current.pos.y, &mTevStr);
 
     return 1;
 }
