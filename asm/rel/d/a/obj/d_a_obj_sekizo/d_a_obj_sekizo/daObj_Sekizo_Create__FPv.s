86af5df12122fcd8fe05f68c85a441be0b9603ee
ErraticOocca <161252953+ErraticOocca@users.noreply.github.com>
2024-03-10 19:12:33 -0400

    d_a_obj_sekizo OK (#2092)
    * Test commit.
    
    * Match initBaseMtx and setBaseMtx
    
    * Match Create, Draw. Execute almost matched.
    
    * Match Execute and simpler functions.
    
    * Misc changes for pause.
    
    * Match CreateHeap and Delete. Start cleanup.
    
    * Match create(). Update inherited fopAc_ac_c members.
    
    * Most of sekizo matched, but .data has extra zeros (param_c underutilized?).
    
    * Minor cleanup.
    
    * Remove unused asm and update progress.
    
    * Fix progress.
    
    * Casting and cullMtx feedback.

--- asm/rel/d/a/obj/d_a_obj_sekizo/d_a_obj_sekizo/daObj_Sekizo_Create__FPv.s
+++
@@ -1,9 +0,0 @@
-lbl_80CCE044:
-/* 80CCE044  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80CCE048  7C 08 02 A6 */	mflr r0
-/* 80CCE04C  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80CCE050  4B FF FB BD */	bl create__14daObj_Sekizo_cFv
-/* 80CCE054  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80CCE058  7C 08 03 A6 */	mtlr r0
-/* 80CCE05C  38 21 00 10 */	addi r1, r1, 0x10
-/* 80CCE060  4E 80 00 20 */	blr 
