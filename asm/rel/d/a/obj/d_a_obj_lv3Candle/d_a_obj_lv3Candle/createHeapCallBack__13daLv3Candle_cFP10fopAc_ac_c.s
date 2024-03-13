567a12ac9a19ffa06a9834ef383c739c52bd9261
Jeff Padgham <jeff.ookami@gmail.com>
2024-03-13 08:23:47 -0700

    d_a_obj_lv3Candle OK (#2093)
    * All methods matching
    
    * Some cleanup, stuck on TU match
    
    * d_a_obj_heavySw OK (#2091)
    
    * match functions
    
    * Match TU
    
    * Cleanup
    
    * remove asm and update progress
    
    * d_a_obj_sekizo OK (#2092)
    
    * Test commit.
    
    * Match initBaseMtx and setBaseMtx
    
    * Match Create, Draw. Execute almost matched.
    
    * Match Execute and simpler functions.
    
    * Misc changes for pause.
    
    * Match CreateHeap and Delete. Start cleanup.
    
    * Match create(). Update inherited fopAc_ac_c members.
    
    * Most of sekizo matched, but .data has extra zeros (param_c underutilized?).
    
    * Minor cleanup.
    
    * Remove unused asm and update progress.
    
    * Fix progress.
    
    * Casting and cullMtx feedback.
    
    * Decomp OK, minor cleanup
    
    * Updating progress
    
    ---------
    
    Co-authored-by: GinNoOokami <noreply>
    Co-authored-by: Trueffel <106771418+Trueffeloot@users.noreply.github.com>
    Co-authored-by: ErraticOocca <161252953+ErraticOocca@users.noreply.github.com>

--- asm/rel/d/a/obj/d_a_obj_lv3Candle/d_a_obj_lv3Candle/createHeapCallBack__13daLv3Candle_cFP10fopAc_ac_c.s
+++
@@ -1,9 +0,0 @@
-lbl_80C57F6C:
-/* 80C57F6C  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80C57F70  7C 08 02 A6 */	mflr r0
-/* 80C57F74  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80C57F78  4B FF FC 49 */	bl CreateHeap__13daLv3Candle_cFv
-/* 80C57F7C  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80C57F80  7C 08 03 A6 */	mtlr r0
-/* 80C57F84  38 21 00 10 */	addi r1, r1, 0x10
-/* 80C57F88  4E 80 00 20 */	blr 
