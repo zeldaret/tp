5fce7e398959587e69bbd1c84666baae456b19d3
hatal175 <hatal175@users.noreply.github.com>
2024-04-01 12:06:45 +0300

    daObjWCover_c OK (#2117)

--- include/rel/d/a/obj/d_a_obj_well_cover/d_a_obj_well_cover.h
+++ include/rel/d/a/obj/d_a_obj_well_cover/d_a_obj_well_cover.h
@@ -5,4 +5,27 @@
 #include "d/com/d_com_inf_game.h"
 #include "f_op/f_op_actor_mng.h"
 
+class daObjWCover_c : public dBgS_MoveBgActor {
+public:
+    /* 80D36298 */ void initBaseMtx();
+    /* 80D362D4 */ void setBaseMtx();
+    /* 80D36348 */ int Create();
+    /* 80D36394 */ int CreateHeap();
+    /* 80D36404 */ int create();
+    /* 80D364E0 */ int Execute(Mtx**);
+    /* 80D3652C */ void action();
+    /* 80D365B8 */ void modeWait();
+    /* 80D36790 */ void init_modeBreak();
+    /* 80D3689C */ void modeBreak();
+    /* 80D368A0 */ int Draw();
+    /* 80D36944 */ int Delete();
+
+    u8 getSwNo() { return fopAcM_GetParam(this); }
+
+    /* 0x5A0 */ request_of_phase_process_class mPhaseReq;
+    /* 0x5A8 */ J3DModel* mpModel;
+    /* 0x5AC */ u8 field_0x5AC[0x5B0 - 0x5AC];
+    /* 0x5B0 */ u8 field_0x5b0;
+};  // Size: 0x5B4
+
 #endif /* D_A_OBJ_WELL_COVER_H */
