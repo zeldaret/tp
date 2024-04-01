0499c05de5172809e0f96524d7d59b4d92bb7e9e
Trueffel <106771418+Trueffeloot@users.noreply.github.com>
2024-04-01 04:52:02 +0200

    remove asm and update progress

--- asm/rel/d/a/obj/d_a_obj_pleaf/d_a_obj_pleaf/daObj_Pleaf_Delete__FPv.s
+++
@@ -1,9 +0,0 @@
-lbl_80CB1708:
-/* 80CB1708  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80CB170C  7C 08 02 A6 */	mflr r0
-/* 80CB1710  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80CB1714  4B FF FC B1 */	bl Delete__13daObj_Pleaf_cFv
-/* 80CB1718  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80CB171C  7C 08 03 A6 */	mtlr r0
-/* 80CB1720  38 21 00 10 */	addi r1, r1, 0x10
-/* 80CB1724  4E 80 00 20 */	blr 
--- asm/rel/d/a/obj/d_a_obj_pleaf/d_a_obj_pleaf/getResName__13daObj_Pleaf_cFv.s
+++
@@ -1,5 +0,0 @@
-lbl_80CB15B8:
-/* 80CB15B8  3C 60 80 CB */	lis r3, l_resName@ha /* 0x80CB189C@ha */
-/* 80CB15BC  38 63 18 9C */	addi r3, r3, l_resName@l /* 0x80CB189C@l */
-/* 80CB15C0  80 63 00 00 */	lwz r3, 0(r3)
-/* 80CB15C4  4E 80 00 20 */	blr 
