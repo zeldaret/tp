f55e98a1132a4f060577a4ac18847072e283bde0
TakaRikka <takarikka@outlook.com>
2024-04-13 03:42:29 -0700

    cleanup evtarea

--- include/rel/d/a/tag/d_a_tag_evtarea/d_a_tag_evtarea.h
+++ include/rel/d/a/tag/d_a_tag_evtarea/d_a_tag_evtarea.h
@@ -6,32 +6,43 @@
 class daTag_EvtArea_c : public fopAc_ac_c {
 public:
     /* 8048C4F8 */ cPhs__Step create();
-    /* 8048C728 */ bool Delete();
-    /* 8048C730 */ bool Execute();
-    /* 8048C894 */ bool Draw();
+    /* 8048C728 */ BOOL Delete();
+    /* 8048C730 */ BOOL Execute();
+    /* 8048C894 */ BOOL Draw();
     /* 8048C89C */ BOOL isDelete();
     /* 8048C94C */ BOOL chkPointInArea(cXyz, cXyz);
+
     /* 8048CD08 */ BOOL chkPointInArea(cXyz param_0) {
         cXyz local_28;
         local_28.set(0.0f, 0.0f, 0.0f);
         return chkPointInArea(param_0, local_28);
     }
+
     /* 8048CD68 */ virtual ~daTag_EvtArea_c() {}
 
-    inline s32 getType() {
+    s32 getType() {
         u8 type = (u8)shape_angle.z;
         return type == 0xff ? 0 : type;
     }
 
-    inline s32 getNo() {
+    s32 getNo() {
         u16 no = (shape_angle.z & 0xff00) >> 8;
         return no == 0xff ? 0 : no;
     }
 
-    inline u32 getOnEvtBit();
-    inline u32 getOffEvtBit();
-    inline u8 getBitSW();
-    inline u8 getBitSW2();
+    u32 getOnEvtBit() {
+        u32 ret = fopAcM_GetParam(this) & 0xfff;
+        return ret == 0xfff ? -1 : ret;
+    }
+
+    u32 getOffEvtBit() {
+        u32 ret = (fopAcM_GetParam(this) & 0xfff000) >> 12;
+        return ret == 0xfff ? -1 : ret;
+    }
+
+    u8 getBitSW() { return home.angle.x & 0xff; }
+
+    u8 getBitSW2() { return (home.angle.x & 0xff00) >> 8; }
 
 private:
     /* 0x568 vtable */
