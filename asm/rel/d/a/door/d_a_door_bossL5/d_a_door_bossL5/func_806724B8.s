fafbfcb833f8c649fcbb064bbb06cdb3af8b1023
hatal175 <hatal175@users.noreply.github.com>
2024-04-15 01:01:11 +0300

    daBdoorL5 OK (#2129)
    * daBdoorL5 OK
    
    * Remove dol2asm from d_a_door_dbdoor00

--- asm/rel/d/a/door/d_a_door_bossL5/d_a_door_bossL5/func_806724B8.s
+++
@@ -1,3 +0,0 @@
-lbl_806724B8:
-/* 806724B8  38 63 FF EC */	addi r3, r3, -20
-/* 806724BC  4B FF ED B0 */	b __dt__12dBgS_ObjAcchFv
--- asm/rel/d/a/door/d_a_door_bossL5/d_a_door_bossL5/getAnmArcName__11daBdoorL5_cFv.s
+++
@@ -1,5 +0,0 @@
-lbl_80670C74:
-/* 80670C74  3C 60 80 67 */	lis r3, d_a_door_bossL5__stringBase0@ha /* 0x80672548@ha */
-/* 80670C78  38 63 25 48 */	addi r3, r3, d_a_door_bossL5__stringBase0@l /* 0x80672548@l */
-/* 80670C7C  38 63 00 32 */	addi r3, r3, 0x32
-/* 80670C80  4E 80 00 20 */	blr 
