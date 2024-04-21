2b27ede0ca8fc6de2a8b1c018f8a71d5963932c5
Trueffel <106771418+Trueffeloot@users.noreply.github.com>
2024-04-21 04:24:47 +0200

    update progress & remove asm

--- asm/rel/d/a/obj/d_a_obj_chest/d_a_obj_chest/daObjChest_Create__FP10fopAc_ac_c.s
+++
@@ -1,9 +0,0 @@
-lbl_80BC9F44:
-/* 80BC9F44  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80BC9F48  7C 08 02 A6 */	mflr r0
-/* 80BC9F4C  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80BC9F50  4B FF F3 61 */	bl create__12daObjChest_cFv
-/* 80BC9F54  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80BC9F58  7C 08 03 A6 */	mtlr r0
-/* 80BC9F5C  38 21 00 10 */	addi r1, r1, 0x10
-/* 80BC9F60  4E 80 00 20 */	blr 
--- asm/rel/d/a/obj/d_a_obj_chest/d_a_obj_chest/func_80BC9FEC.s
+++
@@ -1,3 +0,0 @@
-lbl_80BC9FEC:
-/* 80BC9FEC  38 63 FF EC */	addi r3, r3, -20
-/* 80BC9FF0  4B FF F6 AC */	b __dt__12dBgS_ObjAcchFv
