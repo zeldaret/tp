4485a94639602e46ac58d725e7049299cd9074da
TakaRikka <takarikka@outlook.com>
2024-05-24 18:35:38 -0700

    remove asm

--- asm/rel/d/a/e/d_a_e_vt/d_a_e_vt/daE_VA_Create__FP8daE_VA_c.s
+++
@@ -1,9 +0,0 @@
-lbl_807CE244:
-/* 807CE244  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 807CE248  7C 08 02 A6 */	mflr r0
-/* 807CE24C  90 01 00 14 */	stw r0, 0x14(r1)
-/* 807CE250  4B FF F4 19 */	bl create__8daE_VA_cFv
-/* 807CE254  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 807CE258  7C 08 03 A6 */	mtlr r0
-/* 807CE25C  38 21 00 10 */	addi r1, r1, 0x10
-/* 807CE260  4E 80 00 20 */	blr 
