3c19263c02c7da857419df8c371457872eb30746
TakaRikka <takarikka@outlook.com>
2024-06-14 04:14:10 -0700

    remove asm

--- asm/d/menu/d_menu_fmap2D/getMapAreaSizeY__18dMenu_Fmap2DBack_cFv.s
+++
@@ -1,5 +0,0 @@
-lbl_801D2728:
-/* 801D2728  3C 60 80 43 */	lis r3, g_fmapHIO@ha /* 0x8042FC60@ha */
-/* 801D272C  38 63 FC 60 */	addi r3, r3, g_fmapHIO@l /* 0x8042FC60@l */
-/* 801D2730  C0 23 00 B4 */	lfs f1, 0xb4(r3)
-/* 801D2734  4E 80 00 20 */	blr 
--- asm/d/menu/d_menu_fmap2D/getMapScissorAreaSizeRealX__18dMenu_Fmap2DBack_cFv.s
+++
@@ -1,9 +0,0 @@
-lbl_801D2860:
-/* 801D2860  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 801D2864  7C 08 02 A6 */	mflr r0
-/* 801D2868  90 01 00 14 */	stw r0, 0x14(r1)
-/* 801D286C  4B FF FF 7D */	bl getMapScissorAreaSizeX__18dMenu_Fmap2DBack_cFv
-/* 801D2870  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 801D2874  7C 08 03 A6 */	mtlr r0
-/* 801D2878  38 21 00 10 */	addi r1, r1, 0x10
-/* 801D287C  4E 80 00 20 */	blr 
