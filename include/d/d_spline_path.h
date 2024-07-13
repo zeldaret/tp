89e14f9f9744cd2b47fe8dd28ee4fffafba0b716
hatal175 <hatal175@users.noreply.github.com>
2024-07-13 03:31:33 +0300

    d_spline_path ok (#2176)

--- include/d/d_spline_path.h
+++ include/d/d_spline_path.h
@@ -7,7 +7,7 @@ struct dPath;
 
 struct dPathCurve {
     /* 80097C5C */ void Init(dPath*);
-    /* 80097C74 */ void bSpline2(f32);
+    /* 80097C74 */ cXyz bSpline2(f32);
 
     /* 0x00 */ int field_0x00;
     /* 0x00 */ int field_0x04;
@@ -17,22 +17,18 @@ struct dPathCurve {
 class d2DBSplinePath {
 public:
     /* 80097878 */ void Init(s32, s32);
-    /* 80097904 */ void Step();
+    /* 80097904 */ bool Step();
     /* 80097B20 */ f32 Calc(f32*);
-    /* 80097A6C */ void Calc(cXyz*);
-    /* 80097B68 */ void Spot(f32*, f32);
+    /* 80097A6C */ cXyz Calc(cXyz*);
+    /* 80097B68 */ f32 Spot(f32*, f32);
 
     /* 0x00 */ s32 field_0x00;
     /* 0x04 */ s32 field_0x04;
     /* 0x08 */ s32 field_0x08;
     /* 0x0C */ s32 field_0x0c;
     /* 0x10 */ f32 field_0x10;
-    /* 0x14 */ f32 field_0x14;
-    /* 0x18 */ f32 field_0x18;
-    /* 0x1C */ f32 field_0x1c;
-    /* 0x20 */ u32 field_0x20;
-    /* 0x24 */ int field_0x24;
-    /* 0x28 */ int field_0x28;
+    /* 0x14 */ f32 mCurveWeight[3];
+    /* 0x20 */ int mCurveKey[3];
     /* 0x2C */ int field_0x2c;
     /* 0x30 */ void* field_0x30;
 };
