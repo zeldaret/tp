b8d7e46c03f9da4d9c3b8131a6fc0d2269f017d7
TakaRikka <takarikka@outlook.com>
2024-07-12 17:33:02 -0700

    remove asm

--- asm/rel/d/a/obj/d_a_obj_iceblock/d_a_obj_iceblock/func_80C242C4.s
+++
@@ -1,3 +0,0 @@
-lbl_80C242C4:
-/* 80C242C4  38 63 FF DC */	addi r3, r3, -36
-/* 80C242C8  4B FF FE D8 */	b __dt__12dBgS_ObjAcchFv
--- asm/rel/d/a/obj/d_a_obj_iceblock/d_a_obj_iceblock/func_80C242CC.s
+++
@@ -1,3 +0,0 @@
-lbl_80C242CC:
-/* 80C242CC  38 63 FF EC */	addi r3, r3, -20
-/* 80C242D0  4B FF FE D0 */	b __dt__12dBgS_ObjAcchFv
