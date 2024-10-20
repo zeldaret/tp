37ef95e4575ef16425fefd9a3d6b2644f767c9c6
Tal Hayon <talhayon1@gmail.com>
2024-10-20 07:14:27 +0300

    daSwLBall_c OK

--- include/d/actor/d_a_swLBall.h
+++ include/d/actor/d_a_swLBall.h
@@ -13,20 +13,42 @@
  */
 class daSwLBall_c : public fopAc_ac_c {
 public:
-    /* 80D4EB8C */ void checkArea_sub(fopAc_ac_c*);
-    /* 80D4ECCC */ void checkArea();
+    enum Action {
+        ACTION_INIT,
+        ACTION_RUN,
+        ACTION_STOP,
+    };
+
+    typedef void (daSwLBall_c::*actionFunc)();
+
+    /* 80D4EB8C */ int checkArea_sub(fopAc_ac_c*);
+    /* 80D4ECCC */ int checkArea();
     /* 80D4ED80 */ void search_lb();
-    /* 80D4EEAC */ void Create();
-    /* 80D4EEF8 */ void create();
-    /* 80D4EF60 */ void execute();
+    /* 80D4EEAC */ int Create();
+    /* 80D4EEF8 */ int create();
+    /* 80D4EF60 */ int execute();
     /* 80D4F008 */ void actionInit();
     /* 80D4F080 */ void actionRun();
     /* 80D4F220 */ void actionStop();
     /* 80D4F224 */ void PutCrrPos();
-    /* 80D4F444 */ bool _delete();
+    /* 80D4F444 */ int _delete();
+
+    u8 getType() { return fopAcM_GetParamBit(this, 24, 4); }
+    u8 getArg0() { return fopAcM_GetParamBit(this, 0, 8); }
+    u8 getArg1() { return fopAcM_GetParamBit(this, 8, 8); }
+    u8 getSwbit() { return fopAcM_GetParamBit(this, 16, 8); }
+    void setAction(Action action) { mAction = action; }
+    int checkPullLBall() {
+        return fopAcM_GetParamBit(this,28,2) == 0;
+    }
 
 private:
-    /* 0x568 */ u8 field_0x568[0x578 - 0x568];
+    /* 0x568 */ fpc_ProcID mProcIds[2];
+    /* 0x570 */ u8 mRunTimer;
+    /* 0x571 */ u8 mAction;
+    /* 0x572 */ u8 field_0x572;
+    /* 0x574 */ u8 mIsActorCarrying[2];
+    /* 0x575 */ u8 mIsPulled[2];
 };
 
 STATIC_ASSERT(sizeof(daSwLBall_c) == 0x578);
