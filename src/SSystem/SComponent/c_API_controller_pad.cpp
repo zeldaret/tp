d44bc7df26eb89646988cb352535f1acb647f483
TakaRikka <takarikka@outlook.com>
2024-12-01 06:28:20 -0800

    cleanup JUTGamePad

--- src/SSystem/SComponent/c_API_controller_pad.cpp
+++ src/SSystem/SComponent/c_API_controller_pad.cpp
@@ -13,5 +13,5 @@ void cAPICPad_recalibrate() {
 
 /* 802632AC-802632C8 25DBEC 001C+00 0/0 3/3 1/1 .text            cAPICPad_ANY_BUTTON__FUl */
 u32 cAPICPad_ANY_BUTTON(u32 portNo) {
-    return mDoCPd_c::getTrig(portNo) & (CButton::A | CButton::B | CButton::START);
+    return mDoCPd_c::getTrig(portNo) & (PAD_BUTTON_A | PAD_BUTTON_B | PAD_BUTTON_START);
 }
