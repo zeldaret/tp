c429ce21eb7cf4687da596c51fbc47249fbe4c13
hatal175 <hatal175@users.noreply.github.com>
2024-12-28 05:16:59 +0200

    d_a_obj_cwall almost matching (#2270)
    * d_a_obj_cwall almost matching
    
    * d_a_obj_pdoor OK

--- include/d/actor/d_a_obj_pdoor.h
+++ include/d/actor/d_a_obj_pdoor.h
@@ -1,6 +1,7 @@
 #ifndef D_A_OBJ_PDOOR_H
 #define D_A_OBJ_PDOOR_H
 
+#include "d/d_bg_s_movebg_actor.h"
 #include "f_op/f_op_actor_mng.h"
 
 /**
@@ -11,24 +12,44 @@
  * @details
  *
  */
-class daObjPDoor_c : public fopAc_ac_c {
+class daObjPDoor_c : public dBgS_MoveBgActor {
 public:
+    typedef void (daObjPDoor_c::*modeFunc)();
+
+    enum Mode {
+        MODE_WAIT,
+        MODE_ROTATE,
+    };
+
     /* 80CA9F9C */ void initBaseMtx();
     /* 80CA9FD8 */ void setBaseMtx();
-    /* 80CAA03C */ void Create();
-    /* 80CAA14C */ void CreateHeap();
-    /* 80CAA1BC */ void create1st();
-    /* 80CAA23C */ void Execute(f32 (**)[3][4]);
+    /* 80CAA03C */ int Create();
+    /* 80CAA14C */ int CreateHeap();
+    /* 80CAA1BC */ int create1st();
+    /* 80CAA23C */ int Execute(Mtx**);
     /* 80CAA290 */ void mode_proc_call();
     /* 80CAA31C */ void init_modeWait();
     /* 80CAA354 */ void modeWait();
     /* 80CAA698 */ void init_modeRotate();
     /* 80CAA6B4 */ void modeRotate();
-    /* 80CAA968 */ void Draw();
-    /* 80CAAA0C */ void Delete();
+    /* 80CAA968 */ int Draw();
+    /* 80CAAA0C */ int Delete();
 
-private:
-    /* 0x568 */ u8 field_0x568[0x5c4 - 0x568];
+    u8 getSwbit() { return fopAcM_GetParamBit(this, 0, 8); }
+    u8 getSwbit2() { return fopAcM_GetParamBit(this, 8, 8); }
+
+    /* 0x5A0 */ request_of_phase_process_class mPhase;
+    /* 0x5A8 */ J3DModel* mModel;
+    /* 0x5AC */ s8 mDoorDirection;
+    /* 0x5AD */ u8 mMode;
+    /* 0x5B0 */ int field_0x5b0;
+    /* 0x5B4 */ u8 field_0x5b4;
+    /* 0x5B5 */ u8 field_0x5b5;
+    /* 0x5B6 */ u8 field_0x5b6[2];
+    /* 0x5B8 */ s16 field_0x5b8;
+    /* 0x5BA */ s16 field_0x5ba;
+    /* 0x5BC */ s16 field_0x5bc;
+    /* 0x5C0 */ dBgW_Base::PushPullLabel mPushPullLabel;
 };
 
 STATIC_ASSERT(sizeof(daObjPDoor_c) == 0x5c4);
