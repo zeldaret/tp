7f7a6d23b7faff2c8c0c2fe6ba54d7ca2d0af02a
Tal Hayon <talhayon1@gmail.com>
2025-03-09 19:48:12 +0200

    Remove padding

--- include/d/d_cc_uty.h
+++ include/d/d_cc_uty.h
@@ -22,7 +22,6 @@ struct dCcU_AtInfo {
     /* 0x04 */ fopAc_ac_c* mpActor;
     /* 0x08 */ Z2Creature* mpSound;
     /* 0x0C */ csXyz mHitDirection;
-    /* 0x12 */ u8 field_0x12[2];
     /* 0x14 */ u32 mHitBit;
     /* 0x18 */ u32 field_0x18;
     /* 0x1C */ u16 mAttackPower;
