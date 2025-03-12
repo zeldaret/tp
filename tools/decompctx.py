ac849c94923b56b420192976bedd5ece09971107
Tal Hayon <talhayon1@gmail.com>
2025-03-12 21:53:43 +0200

    daObjCBlk_c equivalent

--- tools/decompctx.py
+++ tools/decompctx.py
@@ -20,6 +20,7 @@ root_dir = os.path.abspath(os.path.join(script_dir, ".."))
 src_dir = os.path.join(root_dir, "src")
 include_dirs = [
     os.path.join(root_dir, "include"),
+    os.path.join(root_dir, "include/dolphin"),
     os.path.join(root_dir, "src"),
     os.path.join(root_dir, "src/PowerPC_EABI_Support/MSL/MSL_C/MSL_Common/Include"),
     os.path.join(root_dir, "src/PowerPC_EABI_Support/MSL/MSL_C/MSL_Common_Embedded/Math/Include"),
