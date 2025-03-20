5858ce23edcf02c04e1155e08d365975b5e67858
Emma Plante <zerox029@gmail.com>
2025-03-20 18:09:45 -0400

    Flag 60%

--- include/d/actor/d_a_obj_flag.h
+++ include/d/actor/d_a_obj_flag.h
@@ -30,8 +30,8 @@ class daObjFlag_c : public fopAc_ac_c {
 private:
     /* 0x568 */ J3DModel* mpModel1;
     /* 0x56c */ J3DModel* mpModel2;
-    /* 0x578 */ request_of_phase_process_class mPhase;
-    /* 0x570 */ u8 field_0x56c[0x580 - 0x578];
+    /* 0x570 */ request_of_phase_process_class mPhase;
+    /* 0x578 */ request_of_phase_process_class mPhase2;
     /* 0x580 */ FlagJoint_c mFlagJoints[4];
     /* 0x5d0 */ cXyz mPos;
     /* 0x5dc */ float field_0x5dc;
@@ -47,25 +47,33 @@ public:
     /* 80BEB984 */ void getJointAngle(csXyz*, int);
     /* 80BEB9AC */ void calcJointAngle();
     /* 80BEBC58 */ void calcAngleSwingZ(FlagJoint_c*, f32);
-    /* 80BEBDAC */ void calcAngleSwingX(FlagJoint_c*, f32);
+    /* 80BEBDAC */ int calcAngleSwingX(FlagJoint_c*, f32);
     /* 80BEBE64 */ f32 getSwingY(f32);
+
+    ~daObjFlag_c() {
+        dComIfG_resDelete(&this->mPhase, "FlagObj");
+        dComIfG_resDelete(&this->mPhase2, daSetBgObj_c::getArcName(this));
+    }
 
     int create() {
         fopAcM_SetupActor(this, daObjFlag_c);
     
         int phase_state = dComIfG_resLoad(&mPhase, "create");
-        if (phase_state == cPhs_COMPLEATE_e) {
-            phase_state = dComIfG_resLoad(&mPhase, daSetBgObj_c::getArcName(this));
-            
-            if(phase_state == cPhs_COMPLEATE_e) {
-                if(!fopAcM_entrySolidHeap(this, createSolidHeap, 0x4000)) {
-                    return cPhs_ERROR_e;
-                }
-    
-                create_init();
-            }
+        if (phase_state != cPhs_COMPLEATE_e) {
+            return phase_state;
         }
-    
+
+        phase_state = dComIfG_resLoad(&mPhase2, daSetBgObj_c::getArcName(this));
+        if(phase_state != cPhs_COMPLEATE_e) {
+            return phase_state;
+        }
+
+        if(!fopAcM_entrySolidHeap(this, createSolidHeap, 0x4000)) {
+            return cPhs_ERROR_e;
+        }
+
+        create_init();
+
         return phase_state;
     }
 
@@ -94,8 +102,51 @@ public:
         return 1;
     }
 
+    int createHeap() {
+        int bvar2 = 0;
+        if(shape_angle.x < '\0' || shape_angle.x > 'c') {
+            bvar2 = 0;
+        }
+        else {
+            bvar2 = 1;
+            shape_angle.setall(0);
+            current.angle.setall(0);
 
-    static s16 const M_attr[26];
+            // if(dComIfG_getObjectRes(daSetBgObj_c::getArcName(this), "FlagObj") == 0) {
+                // Debug stuff
+            // }
+        
+            J3DModelData* modelData = (J3DModelData*)dComIfG_getObjectRes(daSetBgObj_c::getArcName(this), "FlagObj");
+            mpModel1 = mDoExt_J3DModel__create(modelData, 0x80000, 0x11000084);
+        }
+
+        return 1;
+    }
+
+    struct M_attrs {
+        /* 0x00 */ float not_sure;
+        /* 0x04 */ float field_0x04;
+        /* 0x08 */ float field_0x08;
+        /* 0x0c */ short field_0x0c;
+        /* 0x0e */ short field_0x0e;
+        /* 0x10 */ float undefined;
+        /* 0x14 */ short field_0x14;
+        /* 0x16 */ short field_0x16;
+        /* 0x18 */ short field_0x18;
+        /* 0x1a */ short field_0x1a;
+        /* 0x1c */ short field_0x1c;
+        /* 0x1e */ short field_0x1e;
+        /* 0x20 */ short field_0x20;
+        /* 0x22 */ short field_0x22;
+        /* 0x24 */ char field_0x24;
+        /* 0x25 */ char field_0x25;
+        /* 0x26 */ short maybe;
+        /* 0x28 */ float field_0x28;
+        /* 0x2c */ float field_0x2c;
+        /* 0x30 */ float field_0x30;
+    };
+
+    static M_attrs const M_attr;
 };
 
 STATIC_ASSERT(sizeof(daObjFlag_c) == 0x5ec);
--- src/d/actor/d_a_obj_flag.cpp
+++ src/d/actor/d_a_obj_flag.cpp
@@ -6,88 +6,16 @@
 #include "d/actor/d_a_obj_flag.h"
 #include "dol2asm.h"
 
-
-
-
-//
-// Forward References:
-//
-
-extern "C" void create_init__11daObjFlag_cFv();
-extern "C" void initBaseMtx__11daObjFlag_cFv();
-extern "C" void getJointAngle__11daObjFlag_cFP5csXyzi();
-extern "C" void calcJointAngle__11daObjFlag_cFv();
-extern "C" void calcAngleSwingZ__11daObjFlag_cFP11FlagJoint_cf();
-extern "C" void calcAngleSwingX__11daObjFlag_cFP11FlagJoint_cf();
-extern "C" void getSwingY__11daObjFlag_cFf();
-extern "C" static void nodeCallBack__FP8J3DJointi();
-extern "C" static void createSolidHeap__FP10fopAc_ac_c();
-extern "C" static void daObjFlag_Draw__FP11daObjFlag_c();
-extern "C" static void daObjFlag_Execute__FP11daObjFlag_c();
-extern "C" static bool daObjFlag_IsDelete__FP11daObjFlag_c();
-extern "C" static void daObjFlag_Delete__FP11daObjFlag_c();
-extern "C" void __dt__11FlagJoint_cFv();
-extern "C" static void daObjFlag_Create__FP10fopAc_ac_c();
-extern "C" void __ct__11FlagJoint_cFv();
-extern "C" u8 const M_attr__11daObjFlag_c[52];
-extern "C" extern char const* const d_a_obj_flag__stringBase0;
-
-//
-// External References:
-//
-
-extern "C" void mDoMtx_XrotM__FPA4_fs();
-extern "C" void mDoMtx_YrotM__FPA4_fs();
-extern "C" void mDoMtx_ZrotM__FPA4_fs();
-extern "C" void transS__14mDoMtx_stack_cFRC4cXyz();
-extern "C" void mDoExt_modelUpdateDL__FP8J3DModel();
-extern "C" void mDoExt_J3DModel__create__FP12J3DModelDataUlUl();
-extern "C" void __ct__10fopAc_ac_cFv();
-extern "C" void __dt__10fopAc_ac_cFv();
-extern "C" void fopAcM_entrySolidHeap__FP10fopAc_ac_cPFP10fopAc_ac_c_iUl();
-extern "C" void dComIfG_resLoad__FP30request_of_phase_process_classPCc();
-extern "C" void dComIfG_resDelete__FP30request_of_phase_process_classPCc();
-extern "C" void getArcName__12daSetBgObj_cFP10fopAc_ac_c();
-extern "C" void getRes__14dRes_control_cFPCcPCcP11dRes_info_ci();
-extern "C" void dKyw_get_AllWind_vec__FP4cXyzP4cXyzPf();
-extern "C" void settingTevStruct__18dScnKy_env_light_cFiP4cXyzP12dKy_tevstr_c();
-extern "C" void setLightTevColorType_MAJI__18dScnKy_env_light_cFP12J3DModelDataP12dKy_tevstr_c();
-extern "C" void __apl__5csXyzFR5csXyz();
-extern "C" void __mi__5csXyzFR5csXyz();
-extern "C" void __ml__5csXyzFf();
-extern "C" void cM_atan2s__Fff();
-extern "C" void cM_rnd__Fv();
-extern "C" void cLib_addCalcAngleS__FPsssss();
-extern "C" void cLib_chaseS__FPsss();
-extern "C" void seStartLevel__7Z2SeMgrF10JAISoundIDPC3VecUlScffffUc();
-extern "C" void __dl__FPv();
-extern "C" void __destroy_arr();
-extern "C" void __construct_array();
-extern "C" void __cvt_fp2unsigned();
-extern "C" void _savegpr_26();
-extern "C" void _restgpr_26();
-extern "C" u8 now__14mDoMtx_stack_c[48];
-extern "C" u8 mCurrentMtx__6J3DSys[48];
-extern "C" u8 sincosTable___5JMath[65536];
-extern "C" u8 mAudioMgrPtr__10Z2AudioMgr[4 + 4 /* padding */];
-
 //
 // Declarations:
 //
-
-/* ############################################################################################## */
-/* 80BEC4EC-80BEC520 000000 0034+00 4/4 0/0 0/0 .rodata          M_attr__11daObjFlag_c */
-s16 const daObjFlag_c::M_attr[26] = {
-    0x457A, 0x0000, 0x3F80, 0x0000, 0x3E99, 0x999A, 0x000F, 
-    0x07D0, 0x05DC, 0x0FA0, 0x0708, 0x0000, 0x0000,
-    0x03E8, 0x0190, 0x0000, 0x0000, 0x012C, 0x0101, 
-    0x0000, 0x463B, 0x8000, 0x453B, 0x8000, 0x463B, 0x8000,
+daObjFlag_c::M_attrs const daObjFlag_c::M_attr = {
+    0x457A0000, 0x3F800000, 0x3E99999A, 
+    0x000F, 0x07D0, 0x05DC0FA0, 
+    0x0708, 0, 0, 0x03E8, 0x0190, 
+    0, 0, 0x012C, 0x01, 0x01, 0, 
+    0x463B8000, 0x453B8000, 0x463B8000
 };
-
-
-/* 80BEC520-80BEC524 000034 0004+00 1/1 0/0 0/0 .rodata          @3637 */
-SECTION_RODATA static f32 const lit_3637 = 65535.0f;
-COMPILER_STRIP_GATE(0x80BEC520, &lit_3637);
 
 /* 80BEC524-80BEC52C 000038 0008+00 1/1 0/0 0/0 .rodata          @3639 */
 SECTION_RODATA static u8 const lit_3639[8] = {
@@ -95,15 +23,22 @@ SECTION_RODATA static u8 const lit_3639[8] = {
 };
 COMPILER_STRIP_GATE(0x80BEC524, &lit_3639);
 
-/* 80BEC554-80BEC554 000068 0000+00 0/0 0/0 0/0 .rodata          @stringBase0 */
-#pragma push
-#pragma force_active on
-SECTION_DEAD static char const* const stringBase_80BEC554 = "spec.dat";
-#pragma pop
 
 /* 80BEB778-80BEB8F0 000078 0178+00 1/1 0/0 0/0 .text            create_init__11daObjFlag_cFv */
 void daObjFlag_c::create_init() {
-    // NONMATCHING
+    field_0x5dc = (*(u32*)dComIfG_getObjectRes(daSetBgObj_c::getArcName(this), "spec.dat")) & 0xffff;
+    mPos = cXyz(current.pos.x, current.pos.y + field_0x5dc, current.pos.z);
+    mFlagJoints[0].rv = (short)(int)(cM_rnd() * 65535.0f);
+    mFlagJoints[1].rv = (short)(int)(cM_rnd() * 65535.0f);
+    mFlagJoints[2].rv = mFlagJoints[1].rv;
+    mFlagJoints[3].rv = mFlagJoints[2].rv;
+
+    field_0x5e2 = (short)(int)(cM_rnd() * 65535.0f);
+    field_0x5e4 = (short)(int)(cM_rnd() * 65535.0f);
+    field_0x5e6 = (short)(int)(cM_rnd() * 65535.0f);
+    field_0x5e8 = (short)(int)(cM_rnd() * 65535.0f);
+
+    initBaseMtx();
 }
 
 /* 80BEB8F0-80BEB984 0001F0 0094+00 1/1 0/0 0/0 .text            initBaseMtx__11daObjFlag_cFv */
@@ -137,27 +72,6 @@ SECTION_RODATA static u8 const lit_3759[4] = {
 COMPILER_STRIP_GATE(0x80BEC52C, &lit_3759);
 #pragma pop
 
-/* 80BEC530-80BEC534 000044 0004+00 0/1 0/0 0/0 .rodata          @3760 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static f32 const lit_3760 = 127.0f;
-COMPILER_STRIP_GATE(0x80BEC530, &lit_3760);
-#pragma pop
-
-/* 80BEC534-80BEC538 000048 0004+00 0/1 0/0 0/0 .rodata          @3761 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static f32 const lit_3761 = 1.0f;
-COMPILER_STRIP_GATE(0x80BEC534, &lit_3761);
-#pragma pop
-
-/* 80BEC538-80BEC53C 00004C 0004+00 0/1 0/0 0/0 .rodata          @3762 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static f32 const lit_3762 = -1.0f;
-COMPILER_STRIP_GATE(0x80BEC538, &lit_3762);
-#pragma pop
-
 /* 80BEC53C-80BEC544 000050 0008+00 1/4 0/0 0/0 .rodata          @3764 */
 SECTION_RODATA static u8 const lit_3764[8] = {
     0x43, 0x30, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00,
@@ -177,75 +91,64 @@ void daObjFlag_c::calcJointAngle() {
     }
 
     cLib_addCalcAngleS(&field_0x5e0, cM_atan2s(direction.x, direction.y), 4, 0x7fff, 0);
-
-    FlagJoint_c l_joint = mFlagJoints[0];
     for(int i = 0; i < 4; i++) {
         if(power != 0.0f && i != 0) {
-            calcAngleSwingZ(&l_joint, power);
+            calcAngleSwingZ(&mFlagJoints[i], power);
         }
         if(i == 0) {
-            l_joint.joint2 = l_joint.joint1;
-            l_joint.joint1.y = field_0x5e0 + (getSwingY(power), cM_ssin(l_joint.rv));
-            l_joint.rv += power * M_attr[0x28];
-            cStack_60  = cStack_60 - l_joint.joint2;
-            l_joint.joint3 = cStack_60;
+            mFlagJoints[i].joint2 = mFlagJoints[i].joint1;
+            mFlagJoints[i].joint1.y = field_0x5e0 + (getSwingY(power), cM_ssin(mFlagJoints[i].rv));
+            mFlagJoints[i].rv += power * M_attr.field_0x28;
+            cStack_60  = cStack_60 - mFlagJoints[i].joint2;
+            mFlagJoints[i].joint3 = cStack_60;
         }
         else {
-            l_joint.joint2 = l_joint.joint1;
-            cStack_60 = cStack_60 * M_attr[0x4];
-            l_joint.joint3 = cStack_60;
+            mFlagJoints[i].joint2 = mFlagJoints[i].joint1;
+            cStack_60 = cStack_60 * M_attr.field_0x04;
+            mFlagJoints[i].joint3 = cStack_60;
         }
 
-        if(M_attr[0x12] == NULL) {
-            l_joint.joint1.x = 0;
+        if(M_attr.field_0x25 == NULL) {
+            mFlagJoints[i].joint1.x = 0;
         }
         else {
-            calcAngleSwingX(&l_joint, power);
+            calcAngleSwingX(&mFlagJoints[i], power);
         }
-
-        l_joint = mFlagJoints[i];
     }
 }
-
-/* ############################################################################################## */
-/* 80BEC544-80BEC548 000058 0004+00 1/1 0/0 0/0 .rodata          @3791 */
-SECTION_RODATA static f32 const lit_3791 = 7.0f / 10.0f;
-COMPILER_STRIP_GATE(0x80BEC544, &lit_3791);
 
 /* 80BEBC58-80BEBDAC 000558 0154+00 1/1 0/0 0/0 .text
  * calcAngleSwingZ__11daObjFlag_cFP11FlagJoint_cf               */
 void daObjFlag_c::calcAngleSwingZ(FlagJoint_c* param_0, f32 param_1) {
-    // NONMATCHING
-}
+    field_0x5e8 += M_attr.field_0x0e; 
+    float swing = param_1 * -((float)(int)M_attr.field_0x0e / 0.7) + (float)(int)M_attr.field_0x0e / 0.7 + (float)(int)M_attr.field_0x1a * cM_ssin(field_0x5e8);
+    if(M_attr.field_0x2c < swing) {
+        swing = M_attr.field_0x2c;
+    }
+    if(M_attr.field_0x24 == '\0') {
+        param_0->joint1.z = (s16)(int)((float)(int)M_attr.field_0x0e * cM_ssin(param_0->rv));
+    }
+    else {
+        param_0->joint1.z = (s16)(int)(swing * cM_ssin(param_0->rv));
+    }
 
-/* ############################################################################################## */
-/* 80BEC548-80BEC54C 00005C 0004+00 0/1 0/0 0/0 .rodata          @3803 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static f32 const lit_3803 = 16384.0f;
-COMPILER_STRIP_GATE(0x80BEC548, &lit_3803);
-#pragma pop
+    param_0->rv += (short)(int)(param_1 * M_attr.field_0x30);
+}
 
 /* 80BEBDAC-80BEBE64 0006AC 00B8+00 1/1 0/0 0/0 .text
  * calcAngleSwingX__11daObjFlag_cFP11FlagJoint_cf               */
-void daObjFlag_c::calcAngleSwingX(FlagJoint_c* param_0, f32 param_1) {
-    // NONMATCHING
+int daObjFlag_c::calcAngleSwingX(FlagJoint_c* param_0, f32 param_1) {
+    float swing = ((16384.0f / M_attr.field_0x08) * param_1 - 16384.0f);
+    if(swing > 0.0f) {
+        swing = 0.0f;
+    }
+
+    if((float)(int)(param_0->joint1).x > swing) {
+        return cLib_addCalcAngleS((s16*)param_0, -swing, 0x1e, 0x7fff, -0x7fff);
+    }
+    
+    return cLib_chaseS((s16*)param_0, -swing, 500);
 }
-
-/* ############################################################################################## */
-/* 80BEC54C-80BEC550 000060 0004+00 0/1 0/0 0/0 .rodata          @3832 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static u32 const lit_3832 = 0x45D05555;
-COMPILER_STRIP_GATE(0x80BEC54C, &lit_3832);
-#pragma pop
-
-/* 80BEC550-80BEC554 000064 0004+00 0/1 0/0 0/0 .rodata          @3833 */
-#pragma push
-#pragma force_active on
-SECTION_RODATA static f32 const lit_3833 = 4000.0f;
-COMPILER_STRIP_GATE(0x80BEC550, &lit_3833);
-#pragma pop
 
 /* 80BEBE64-80BEBFC8 000764 0164+00 1/1 0/0 0/0 .text            getSwingY__11daObjFlag_cFf */
 f32 daObjFlag_c::getSwingY(f32 param_0) {
@@ -257,23 +160,23 @@ f32 daObjFlag_c::getSwingY(f32 param_0) {
         return swing;
     }
 
-    float var3 = (float)(int)M_attr[10] * cM_ssin(field_0x5e2);
-    float var4 = (float)(int)M_attr[11] * cM_ssin(field_0x5e4);
-    float var5 = (float)(int)M_attr[12] * cM_ssin(field_0x5e6);
+    float var3 = (float)(int)M_attr.field_0x14 * cM_ssin(field_0x5e2);
+    float var4 = (float)(int)M_attr.field_0x16 * cM_ssin(field_0x5e4);
+    float var5 = (float)(int)M_attr.field_0x18 * cM_ssin(field_0x5e6);
 
-    if(M_attr[14] == 0) {
+    if(M_attr.field_0x1c == 0) {
         var3 = 0.0;
     }
-    if(M_attr[15] == 0) {
+    if(M_attr.field_0x1e == 0) {
         var4 = 0.0;
     }
-    if(M_attr[16] == 0) {
+    if(M_attr.field_0x20 == 0) {
         var5 = 0.0;
     }
 
-    field_0x5e2 += M_attr[14];
-    field_0x5e4 += M_attr[15];
-    field_0x5e6 += M_attr[16];
+    field_0x5e2 += M_attr.field_0x1c;
+    field_0x5e4 += M_attr.field_0x1e;
+    field_0x5e6 += M_attr.field_0x20;
     
     return (int)(short)(int)(var5 + var4 + (swing + var3));
 }
@@ -292,18 +195,9 @@ static int nodeCallBack(J3DJoint* param_0, int param_1) {
     return 1;
 }
 
-/* ############################################################################################## */
-/* 80BEC554-80BEC554 000068 0000+00 0/0 0/0 0/0 .rodata          @stringBase0 */
-#pragma push
-#pragma force_active on
-SECTION_DEAD static char const* const stringBase_80BEC55D = "flag%02d.bmd";
-SECTION_DEAD static char const* const stringBase_80BEC56A = "FlagObj";
-SECTION_DEAD static char const* const stringBase_80BEC572 = "model0.bmd";
-#pragma pop
-
 /* 80BEC0B8-80BEC234 0009B8 017C+00 1/1 0/0 0/0 .text            createSolidHeap__FP10fopAc_ac_c */
 static int createSolidHeap(fopAc_ac_c* param_0) {
-    // NONMATCHING
+    return static_cast<daObjFlag_c*>(param_0)->createHeap();
 }
 
 /* 80BEC234-80BEC300 000B34 00CC+00 1/0 0/0 0/0 .text            daObjFlag_Draw__FP11daObjFlag_c */
@@ -325,14 +219,14 @@ static bool daObjFlag_IsDelete(daObjFlag_c* param_0) {
 
 /* 80BEC340-80BEC3BC 000C40 007C+00 1/0 0/0 0/0 .text            daObjFlag_Delete__FP11daObjFlag_c
  */
-static void daObjFlag_Delete(daObjFlag_c* param_0) {
-    // NONMATCHING
+static int daObjFlag_Delete(daObjFlag_c* param_0) {
+    param_0->~daObjFlag_c();
+
+    return 1;
 }
 
 /* 80BEC3BC-80BEC3F8 000CBC 003C+00 2/2 0/0 0/0 .text            __dt__11FlagJoint_cFv */
-FlagJoint_c::~FlagJoint_c() {
-    // NONMATCHING
-}
+FlagJoint_c::~FlagJoint_c() {}
 
 /* 80BEC3F8-80BEC4E0 000CF8 00E8+00 1/0 0/0 0/0 .text            daObjFlag_Create__FP10fopAc_ac_c */
 static int daObjFlag_Create(fopAc_ac_c* param_0) {
@@ -347,11 +241,11 @@ FlagJoint_c::FlagJoint_c() {
 /* ############################################################################################## */
 /* 80BEC580-80BEC5A0 -00001 0020+00 1/0 0/0 0/0 .data            l_daObjFlag_Method */
 static actor_method_class l_daObjFlag_Method = {
-    (process_method_func)daObjFlag_Create__FP10fopAc_ac_c,
-    (process_method_func)daObjFlag_Delete__FP11daObjFlag_c,
-    (process_method_func)daObjFlag_Execute__FP11daObjFlag_c,
-    (process_method_func)daObjFlag_IsDelete__FP11daObjFlag_c,
-    (process_method_func)daObjFlag_Draw__FP11daObjFlag_c,
+    (process_method_func)daObjFlag_Create,
+    (process_method_func)daObjFlag_Delete,
+    (process_method_func)daObjFlag_Execute,
+    (process_method_func)daObjFlag_IsDelete,
+    (process_method_func)daObjFlag_Draw,
 };
 
 /* 80BEC5A0-80BEC5D0 -00001 0030+00 0/0 0/0 1/0 .data            g_profile_Obj_Flag */
