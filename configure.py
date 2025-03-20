1a5a81976abb05a3f3bfae924a9eda03558a44f2
Emma <zerox029@gmail.com>
2025-03-20 06:11:32 -0400

    d_a_obj_metalbox matching (#2333)
    * Started work on d_a_obj_stick
    
    * Lots of green
    
    * Fixed mAcch vtable
    
    * Only missing switch/if problem in create
    
    * All green
    
    * Set stick matching
    
    * Cleanup
    
    * Addressed PR comments
    
    * Reset vscode config
    
    * d_a_ob_metalbox mostly complete
    
    * Completed d_a_obj_metalbox
    
    * Addressed PR issues

--- configure.py
+++ configure.py
@@ -1543,7 +1543,7 @@ config.libs = [
     ActorRel(MatchingFor("GZ2E01"), "d_a_obj_ladder"),
     ActorRel(MatchingFor("GZ2E01"), "d_a_obj_lv2Candle"),
     ActorRel(NonMatching, "d_a_obj_magne_arm"),
-    ActorRel(NonMatching, "d_a_obj_metalbox"),
+    ActorRel(MatchingFor("GZ2E01"), "d_a_obj_metalbox"),
     ActorRel(MatchingFor("GZ2E01"), "d_a_obj_mgate"),
     ActorRel(NonMatching, "d_a_obj_nameplate"),
     ActorRel(NonMatching, "d_a_obj_ornament_cloth"),
