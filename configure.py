315b0ed471b0b09ce321c154e01eae232c5b0ee6
LagoLunatic <LagoLunatic@users.noreply.github.com>
2025-03-21 20:05:52 -0400

    Unlink magLift for debug

--- configure.py
+++ configure.py
@@ -2002,7 +2002,7 @@ config.libs = [
     ActorRel(NonMatching, "d_a_obj_lv8OptiLift"),
     ActorRel(NonMatching, "d_a_obj_lv8UdFloor"),
     ActorRel(NonMatching, "d_a_obj_lv9SwShutter"),
-    ActorRel(Matching, "d_a_obj_magLift"),
+    ActorRel(MatchingFor("GZ2E01"), "d_a_obj_magLift"),
     ActorRel(NonMatching, "d_a_obj_magLiftRot"),
     ActorRel(NonMatching, "d_a_obj_maki"),
     ActorRel(NonMatching, "d_a_obj_master_sword"),
