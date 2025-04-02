de1f46ad6724f435ab9570ed3ccffe508dc4f32b
TetrisGG <tetrisggdev@gmail.com>
2025-04-02 23:16:38 +0200

    d_a_obj_maki matching
    d_a_obj_maki matching | * cleanup for PR

--- include/d/actor/d_a_obj_maki.h
+++ include/d/actor/d_a_obj_maki.h
@@ -23,16 +23,16 @@ public:
     /* 0x574 */ mDoExt_brkAnm* mBrk;
     /* 0x578 */ J3DModel* mModel2;
     /* 0x57c */ u8 field_0x57c;
-    /* 0x57d */ char field_0x57d; // Undefined
-    /* 0x57e */ short field_0x57e;
-    /* 0x580 */ short field_0x580[2];
+    /* 0x57d */ u8 field_0x57d; // Undefined
+    /* 0x57e */ s16 field_0x57e;
+    /* 0x580 */ s16 field_0x580[2];
     /* 0x584*/  dCcD_Stts mStts;
     /* 0x5c0 */ dCcD_Sph mSph;
     /* 0x6f8 */ u32 field_0x6f8[4];
-    /* 0x6fc */ char field_0x6fc[0x70c - 0x708]; // Undefined
+    /* 0x6fc */ u8 field_0x6fc[0x70c - 0x708]; // Undefined
     /* 0x70c */ LIGHT_INFLUENCE mLightObj;
     /* 0x72c */ u8 field_0x72c;
-    /* 0x72d */ char field_0x72d[0x730 - 0x72d]; //Undefined
+    /* 0x72d */ u8 field_0x72d[0x730 - 0x72d]; //Undefined
 };
 
 STATIC_ASSERT(sizeof(obj_maki_class) == 0x730);
--- src/d/actor/d_a_obj_maki.cpp
+++ src/d/actor/d_a_obj_maki.cpp
@@ -1,7 +1,7 @@
 /**
  * @file d_a_obj_maki.cpp
- * 
-*/
+ *
+ */
 
 #include "d/actor/d_a_obj_maki.h"
 #include "JSystem/J3DGraphAnimator/J3DAnimation.h"
@@ -17,9 +17,7 @@
 #include "f_op/f_op_actor_mng.h"
 #include "m_Do/m_Do_ext.h"
 #include "m_Do/m_Do_mtx.h"
-#include "mtx.h"
 #include "types.h"
-
 
 /* 80C8FE0C-80C8FE24 0000EC 0018+00 1/1 0/0 0/0 .text            __ct__16daObj_Maki_HIO_cFv */
 daObj_Maki_HIO_c::daObj_Maki_HIO_c() {
@@ -28,135 +26,136 @@ daObj_Maki_HIO_c::daObj_Maki_HIO_c() {
 
 /* 80C8FE24-80C8FEDC 000104 00B8+00 1/0 0/0 0/0 .text            daObj_Maki_Draw__FP14obj_maki_class
  */
-int daObj_Maki_Draw(obj_maki_class* param_0) {
-    g_env_light.settingTevStruct(0x10, &param_0->current.pos, &param_0->tevStr);
+int daObj_Maki_Draw(obj_maki_class* i_this) {
+    g_env_light.settingTevStruct(0x10, &i_this->current.pos, &i_this->tevStr);
 
-    if(param_0->field_0x57e == 0){
-        g_env_light.setLightTevColorType_MAJI(param_0->mModel1, &param_0->tevStr);
-        param_0->mBrk->entry(param_0->mModel1->getModelData());
-        mDoExt_modelUpdateDL(param_0->mModel1);
-
-    } else if(param_0->field_0x57c != 0x01){
-        g_env_light.setLightTevColorType_MAJI(param_0->mModel2, &param_0->tevStr);
-        mDoExt_modelUpdateDL(param_0->mModel2);
+    if (i_this->field_0x57e == 0) {
+        g_env_light.setLightTevColorType_MAJI(i_this->mModel1, &i_this->tevStr);
+        i_this->mBrk->entry(i_this->mModel1->getModelData());
+        mDoExt_modelUpdateDL(i_this->mModel1);
+    } else if (i_this->field_0x57c != 0x01) {
+        g_env_light.setLightTevColorType_MAJI(i_this->mModel2, &i_this->tevStr);
+        mDoExt_modelUpdateDL(i_this->mModel2);
     }
     return 1;
 }
 
 /* 80C8FEDC-80C90378 0001BC 049C+00 2/1 0/0 0/0 .text daObj_Maki_Execute__FP14obj_maki_class */
-int daObj_Maki_Execute(obj_maki_class* param_0) {
-
+int daObj_Maki_Execute(obj_maki_class* i_this) {
     for (int i = 0; i < 2; i++) {
-        if(param_0->field_0x580[i] != 0){
-            param_0->field_0x580[i]--;
+        if (i_this->field_0x580[i] != 0) {
+            i_this->field_0x580[i]--;
         }
     }
-    cXyz cxyz = param_0->current.pos;
+    cXyz cxyz = i_this->current.pos;
     J3DModel* model;
-    if(param_0->field_0x57e == 0){
-        model = param_0->mModel1;
-        param_0->mBrk->play();
+    if (i_this->field_0x57e == 0) {
+        model = i_this->mModel1;
+        i_this->mBrk->play();
 
         for (int i = 0; i < 5; i++) {
-            static u16 eff_id[] = {0x8204,0x8205,0x8206,0x8207,0x8208};
-            param_0->field_0x6f8[i] = dComIfGp_particle_set(param_0->field_0x6f8[i], eff_id[i] , &param_0->current.pos, &param_0->tevStr, &param_0->shape_angle,0,0xff,0,0xffffffff,0,0,0);
+            static u16 eff_id[] = {0x8204, 0x8205, 0x8206, 0x8207, 0x8208};
+            i_this->field_0x6f8[i] = dComIfGp_particle_set(
+                i_this->field_0x6f8[i], eff_id[i], &i_this->current.pos, &i_this->tevStr,
+                &i_this->shape_angle, NULL, 0xff, NULL, -1, NULL, NULL, NULL);
         }
 
         s8 isHit = 0;
-        if(param_0->mSph.ChkTgHit() != 0){
+        if (i_this->mSph.ChkTgHit() != 0) {
             isHit = 1;
-        } 
-        
-        if(param_0->mSph.ChkCoHit() != 0){
-            cCcD_Obj* hitObj = param_0->mSph.GetCoHitObj();
-            if(fopAcM_GetName(hitObj->GetAc()) == 0xef){
+        }
+
+        if (i_this->mSph.ChkCoHit() != 0) {
+            cCcD_Obj* hitObj = i_this->mSph.GetCoHitObj();
+            if (fopAcM_GetName(hitObj->GetAc()) == 0xef) {
                 isHit = 1;
             }
-        } 
-
-        if(param_0->field_0x580[0] == 0 && param_0->mSph.ChkAtHit() != 0){
-            param_0->field_0x580[0] = 10;
-            fopAcM_seStart(param_0, 0x800a4,0);
         }
 
-        if(isHit){
-            param_0->field_0x57e = 1;
-            u32 i_no = fopAcM_GetParam(param_0) >> 0x18;
-            if(i_no != 0xff){
-                dComIfGs_onSwitch(i_no, fopAcM_GetRoomNo(param_0));
+        if (i_this->field_0x580[0] == 0 && i_this->mSph.ChkAtHit() != 0) {
+            i_this->field_0x580[0] = 10;
+            fopAcM_seStart(i_this, 0x800a4, 0);
+        }
+
+        if (isHit) {
+            i_this->field_0x57e = 1;
+            u32 i_no = fopAcM_GetParam(i_this) >> 0x18;
+            if (i_no != 0xff) {
+                dComIfGs_onSwitch(i_no, fopAcM_GetRoomNo(i_this));
             }
 
-            for(int i = 0; i < 2; i++){
+            for (int i = 0; i < 2; i++) {
                 static u16 eff_id[] = {0x8209, 0x820A};
-                dComIfGp_particle_set(eff_id[i],&param_0->current.pos,&param_0->tevStr,&param_0->shape_angle,0);
+                dComIfGp_particle_set(eff_id[i], &i_this->current.pos, &i_this->tevStr,
+                                      &i_this->shape_angle, 0);
             }
-            fopAcM_seStart(param_0, 0x800a5, 0);
-            model = param_0->mModel2;
+            fopAcM_seStart(i_this, 0x800a5, 0);
+            model = i_this->mModel2;
         }
 
-        fopAcM_seStartLevel(param_0, 0x800a3, 0);
+        fopAcM_seStartLevel(i_this, 0x800a3, 0);
 
-        if(param_0->field_0x57c != 1){
+        if (i_this->field_0x57c != 1) {
             cxyz.x += -150.0f;
-            param_0->mSph.SetR(250.0f);
+            i_this->mSph.SetR(250.0f);
         } else {
-            param_0->mSph.SetR(50.0f);
+            i_this->mSph.SetR(50.0f);
         }
-        
-        param_0->mSph.SetC(cxyz);
-        dComIfG_Ccsp()->Set(&param_0->mSph);
 
-        if(param_0->mLightObj.mPow > 0.1f){
-            dKy_plight_cut(&param_0->mLightObj);
-            param_0->mLightObj.mPow = 0.0f;
+        i_this->mSph.SetC(cxyz);
+        dComIfG_Ccsp()->Set(&i_this->mSph);
+
+        if (i_this->mLightObj.mPow > 0.1f) {
+            dKy_plight_cut(&i_this->mLightObj);
+            i_this->mLightObj.mPow = 0.0f;
         }
     } else {
-        model = param_0->mModel2;
-        if(param_0->field_0x57c != 1){
+        model = i_this->mModel2;
+        if (i_this->field_0x57c != 1) {
             cxyz.y += 20000.0f;
-            param_0->field_0x6f8[0] = dComIfGp_particle_set(param_0->field_0x6f8[0],0x820b,&param_0->current.pos, &param_0->tevStr, &param_0->shape_angle,0,0xff,0,0xffffffff,0,0,0);
-            fopAcM_seStartLevel(param_0, 0x800a6, 0);
+            i_this->field_0x6f8[0] = dComIfGp_particle_set(
+                i_this->field_0x6f8[0], 0x820b, &i_this->current.pos, &i_this->tevStr,
+                &i_this->shape_angle, 0, 0xff, 0, 0xffffffff, 0, 0, 0);
+            fopAcM_seStartLevel(i_this, 0x800a6, 0);
         }
     }
 
-    MtxP pfVar5;
-    mDoMtx_stack_c::transS(param_0->current.pos.x, param_0->current.pos.y, param_0->current.pos.z);
-    mDoMtx_stack_c::YrotM(param_0->shape_angle.y);
-    pfVar5 = mDoMtx_stack_c::get();
-    model->setBaseTRMtx(pfVar5);
-    Mtx &mtx = model->getBaseTRMtx();
-    fopAcM_SetMtx(param_0, mtx);
-    param_0->eyePos = param_0->current.pos;
+    mDoMtx_stack_c::transS(i_this->current.pos.x, i_this->current.pos.y, i_this->current.pos.z);
+    mDoMtx_stack_c::YrotM(i_this->shape_angle.y);
+    model->setBaseTRMtx(mDoMtx_stack_c::get());
+    fopAcM_SetMtx(i_this, model->getBaseTRMtx());
+    i_this->eyePos = i_this->current.pos;
+
     return 1;
-    // NONMATCHING
 }
 
 /* 80C90378-80C90380 000658 0008+00 1/0 0/0 0/0 .text daObj_Maki_IsDelete__FP14obj_maki_class */
-bool daObj_Maki_IsDelete(obj_maki_class* param_0) {
+int daObj_Maki_IsDelete(obj_maki_class* i_this) {
     return true;
 }
 
 u8 lbl_567_bss_8;
 
 /* 80C90380-80C903F0 000660 0070+00 1/0 0/0 0/0 .text daObj_Maki_Delete__FP14obj_maki_class */
-int daObj_Maki_Delete(obj_maki_class* param_0) {
-    dComIfG_resDelete(&param_0->mPhase,"Obj_maki");
-    if (param_0->field_0x72c != 0){
+int daObj_Maki_Delete(obj_maki_class* i_this) {
+    dComIfG_resDelete(&i_this->mPhase, "Obj_maki");
+    if (i_this->field_0x72c != 0) {
         lbl_567_bss_8 = 0;
     }
 
-    if(param_0->mLightObj.mPow > 0.1f){
-        dKy_plight_cut(&param_0->mLightObj);
+    if (i_this->mLightObj.mPow > 0.1f) {
+        dKy_plight_cut(&i_this->mLightObj);
     }
+
     return 1;
 }
 
 /* 80C903F0-80C90544 0006D0 0154+00 1/1 0/0 0/0 .text            useHeapInit__FP10fopAc_ac_c */
-int useHeapInit(fopAc_ac_c* param_0) {
-    obj_maki_class* a_this = (obj_maki_class*)param_0;
+int useHeapInit(fopAc_ac_c* i_this) {
+    obj_maki_class* a_this = (obj_maki_class*)i_this;
 
-    J3DModelData* modelData = (J3DModelData*) dComIfG_getObjectRes("Obj_maki",4);
-    a_this->mModel1 = mDoExt_J3DModel__create(modelData,0x80000,0x11000084);
+    J3DModelData* modelData = (J3DModelData*)dComIfG_getObjectRes("Obj_maki", 4);
+    a_this->mModel1 = mDoExt_J3DModel__create(modelData, 0x80000, 0x11000084);
     if (a_this->mModel1 == NULL) {
         return 0;
     }
@@ -166,51 +165,55 @@ int useHeapInit(fopAc_ac_c* param_0) {
         return 0;
     }
 
-    J3DAnmTevRegKey* pbrk = (J3DAnmTevRegKey*) dComIfG_getObjectRes("Obj_maki",8);
-    if (!a_this->mBrk->init(a_this->mModel1->getModelData(),pbrk,TRUE,J3DFrameCtrl::EMode_LOOP,1.0f,0,-1)) {
+    J3DAnmTevRegKey* pbrk = (J3DAnmTevRegKey*)dComIfG_getObjectRes("Obj_maki", 8);
+    if (!a_this->mBrk->init(a_this->mModel1->getModelData(), pbrk, TRUE, J3DFrameCtrl::EMode_LOOP,
+                            1.0f, 0, -1))
+    {
         return 0;
     }
 
-    modelData = (J3DModelData*) dComIfG_getObjectRes("Obj_maki",5);
-    a_this->mModel2 = mDoExt_J3DModel__create(modelData,0x80000,0x11000084);
+    modelData = (J3DModelData*)dComIfG_getObjectRes("Obj_maki", 5);
+    a_this->mModel2 = mDoExt_J3DModel__create(modelData, 0x80000, 0x11000084);
     if (a_this->mModel2 == NULL) {
         return 0;
     }
+
     return 1;
 }
 
 /* 80C90A60-80C90A68 000018 0008+00 2/2 0/0 0/0 .bss             l_HIO */
 daObj_Maki_HIO_c l_HIO;
 
-/* 80C9058C-80C9080C 00086C 0280+00 1/0 0/0 0/0 .text            daObj_Maki_Create__FP10fopAc_ac_c */
-cPhs__Step daObj_Maki_Create(fopAc_ac_c* param_0) {
-    fopAcM_SetupActor(param_0, obj_maki_class);
-    obj_maki_class* a_this = (obj_maki_class*)param_0;
+/* 80C9058C-80C9080C 00086C 0280+00 1/0 0/0 0/0 .text            daObj_Maki_Create__FP10fopAc_ac_c
+ */
+cPhs__Step daObj_Maki_Create(fopAc_ac_c* i_this) {
+    fopAcM_SetupActor(i_this, obj_maki_class);
+    obj_maki_class* a_this = (obj_maki_class*)i_this;
     static dCcD_SrcSph cc_sph_src = {
         {
-            {0x0, {{AT_TYPE_CSTATUE_SWING, 0x1, 0xd}, {0x40000000, 0x3}, 0x75}}, // mObj
-            {dCcD_SE_NONE, 0x0, 0x0, 0x0, 0x0}, // mGObjAt
-            {dCcD_SE_METAL, 0x2, 0x0, 0x0, 0x3}, // mGObjTg
-            {0x0}, // mGObjCo
-        }, // mObjInf
+            {0x0, {{AT_TYPE_CSTATUE_SWING, 0x1, 0xd}, {0x40000000, 0x3}, 0x75}},  // mObj
+            {dCcD_SE_NONE, 0x0, 0x0, 0x0, 0x0},                                   // mGObjAt
+            {dCcD_SE_METAL, 0x2, 0x0, 0x0, 0x3},                                  // mGObjTg
+            {0x0},                                                                // mGObjCo
+        },                                                                        // mObjInf
         {
-            {{0.0f, 0.0f, 0.0f}, 40.0f} // mSph
-        } // mSphAttr
+            {{0.0f, 0.0f, 0.0f}, 40.0f}  // mSph
+        }  // mSphAttr
     };
 
-    cPhs__Step phase = (cPhs__Step) dComIfG_resLoad(&a_this->mPhase,"Obj_maki");
+    cPhs__Step phase = (cPhs__Step)dComIfG_resLoad(&a_this->mPhase, "Obj_maki");
     if (phase == cPhs_COMPLEATE_e) {
-        a_this->field_0x57c = (u8) fopAcM_GetParam(a_this);
+        a_this->field_0x57c = (u8)fopAcM_GetParam(a_this);
         u32 cVar3 = fopAcM_GetParam(a_this) >> 0x18;
-        if(cVar3 != 0xff && dComIfGs_isSwitch(cVar3, fopAcM_GetRoomNo(a_this))){
+        if (cVar3 != 0xff && dComIfGs_isSwitch(cVar3, fopAcM_GetRoomNo(a_this))) {
             a_this->field_0x57e = 1;
         }
 
-        if(!fopAcM_entrySolidHeap(a_this, useHeapInit, 0x1020)){
+        if (!fopAcM_entrySolidHeap(a_this, useHeapInit, 0x1020)) {
             return cPhs_ERROR_e;
         }
 
-        if(lbl_567_bss_8 == 0){
+        if (lbl_567_bss_8 == 0) {
             a_this->field_0x72c = 1;
             lbl_567_bss_8 = '\x01';
             l_HIO.field_0x4 = -1;
@@ -222,11 +225,11 @@ cPhs__Step daObj_Maki_Create(fopAc_ac_c* param_0) {
         a_this->mSph.Set(cc_sph_src);
         a_this->mSph.SetStts(&a_this->mStts);
 
-        if(a_this->field_0x57c == 1){
+        if (a_this->field_0x57c == 1) {
             a_this->mSph.SetTgType(0xd8fbfdff);
         }
 
-        if(strcmp(dComIfGp_getStartStageName(), "F_SP118") != 0 && a_this->field_0x57e != 1){
+        if (strcmp(dComIfGp_getStartStageName(), "F_SP118") != 0 && a_this->field_0x57e != 1) {
             a_this->mLightObj.mPosition = a_this->current.pos;
             a_this->mLightObj.mColor.r = 0xaf;
             a_this->mLightObj.mColor.g = 0x5d;
@@ -243,27 +246,25 @@ cPhs__Step daObj_Maki_Create(fopAc_ac_c* param_0) {
 
 /* 80C909C4-80C909E4 -00001 0020+00 1/0 0/0 0/0 .data            l_daObj_Maki_Method */
 actor_method_class l_daObj_Maki_Method = {
-    (process_method_func)daObj_Maki_Create,
-    (process_method_func)daObj_Maki_Delete,
-    (process_method_func)daObj_Maki_Execute,
-    (process_method_func)daObj_Maki_IsDelete,
+    (process_method_func)daObj_Maki_Create,  (process_method_func)daObj_Maki_Delete,
+    (process_method_func)daObj_Maki_Execute, (process_method_func)daObj_Maki_IsDelete,
     (process_method_func)daObj_Maki_Draw,
 };
 
 /* 80C909E4-80C90A14 -00001 0030+00 0/0 0/0 1/0 .data            g_profile_OBJ_MAKI */
 extern actor_process_profile_definition g_profile_OBJ_MAKI = {
-  fpcLy_CURRENT_e,        // mLayerID
-  3,                      // mListID
-  fpcPi_CURRENT_e,        // mListPrio
-  PROC_OBJ_MAKI,          // mProcName
-  &g_fpcLf_Method.base,  // sub_method
-  sizeof(obj_maki_class), // mSize
-  0,                      // mSizeOther
-  0,                      // mParameters
-  &g_fopAc_Method.base,   // sub_method
-  50,                     // mPriority
-  &l_daObj_Maki_Method,   // sub_method
-  0x00040100,             // mStatus
-  fopAc_ACTOR_e,          // mActorType
-  fopAc_CULLBOX_CUSTOM_e, // cullType
+    fpcLy_CURRENT_e,         // mLayerID
+    3,                       // mListID
+    fpcPi_CURRENT_e,         // mListPrio
+    PROC_OBJ_MAKI,           // mProcName
+    &g_fpcLf_Method.base,    // sub_method
+    sizeof(obj_maki_class),  // mSize
+    0,                       // mSizeOther
+    0,                       // mParameters
+    &g_fopAc_Method.base,    // sub_method
+    50,                      // mPriority
+    &l_daObj_Maki_Method,    // sub_method
+    0x00040100,              // mStatus
+    fopAc_ACTOR_e,           // mActorType
+    fopAc_CULLBOX_CUSTOM_e,  // cullType
 };
