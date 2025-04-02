d68894ecef03b10c679d2d93edb943dc5f48ce23
Carter <144170194+carter-ktb21@users.noreply.github.com>
2025-04-02 12:34:49 -0400

    d_a_npc_midp almost matching

--- include/d/actor/d_a_npc_midp.h
+++ include/d/actor/d_a_npc_midp.h
@@ -14,13 +14,19 @@
 class daNpc_midP_c : public daNpcT_c {
 public:
     typedef int (daNpc_midP_c::*cutFunc)(int);
+    typedef int (daNpc_midP_c::*actionFunc)(void*);
+
+    enum Type {
+        TYPE_0,
+        TYPE_1,
+    };
 
     /* 80A70C6C */ ~daNpc_midP_c();
     /* 80A70D90 */ int create();
     /* 80A71074 */ int CreateHeap();
-    /* 80A714BC */ void Delete();
-    /* 80A714F0 */ void Execute();
-    /* 80A71510 */ void Draw();
+    /* 80A714BC */ int Delete();
+    /* 80A714F0 */ int Execute();
+    /* 80A71510 */ int Draw();
     /* 80A715D4 */ static int createHeapCallBack(fopAc_ac_c*);
     /* 80A715F4 */ static int ctrlJointCallBack(J3DJoint*, int);
     /* 80A7164C */ u8 getType();
@@ -39,11 +45,11 @@ public:
     /* 80A72064 */ void setCollision();
     /* 80A7216C */ int drawDbgInfo();
     /* 80A72174 */ void drawGhost();
-    /* 80A721E0 */ void selectAction();
-    /* 80A72228 */ void chkAction(int (daNpc_midP_c::*)(void*));
-    /* 80A72254 */ void setAction(int (daNpc_midP_c::*)(void*));
-    /* 80A722FC */ void wait(void*);
-    /* 80A72548 */ void talk(void*);
+    /* 80A721E0 */ int selectAction();
+    /* 80A72228 */ int chkAction(int (daNpc_midP_c::*)(void*));
+    /* 80A72254 */ int setAction(int (daNpc_midP_c::*)(void*));
+    /* 80A722FC */ int wait(void*);
+    /* 80A72548 */ int talk(void*);
     /* 80A737D0 */ daNpc_midP_c(daNpcT_faceMotionAnmData_c const* param_1, daNpcT_motionAnmData_c const* param_2,
                                 daNpcT_MotionSeqMngr_c::sequenceStepData_c const* param_3, int param_4,
                                 daNpcT_MotionSeqMngr_c::sequenceStepData_c const* param_5, int param_6,
@@ -72,7 +78,10 @@ private:
     /* 0xE40 */ u8 field_0xE40[0xe44 - 0xe40];
     /* 0xE44 */ dCcD_Cyl mCyl;
     /* 0xF80 */ u8 mType;
-    /* 0xF81 */ u8 field_0xf81[0xfa0-0xf81];
+    /* 0xF81 */ u8 field_0xf81[0xf84-0xf81];
+    /* 0xF84 */ actionFunc field_0xf84;
+    /* 0xF90 */ actionFunc field_0xf90;
+    /* 0xF9C */ int field_0xf9c;
 };
 
 STATIC_ASSERT(sizeof(daNpc_midP_c) == 0xfa0);
@@ -82,44 +91,44 @@ public:
     /* 80A73954 */ ~daNpc_midP_Param_c();
 
     struct Data {
-        /* 0x00 */ f32 field_0x00;
-        /* 0x04 */ f32 field_0x04;
-        /* 0x08 */ f32 field_0x08;
-        /* 0x0C */ f32 field_0x0c;
-        /* 0x10 */ f32 field_0x10;
-        /* 0x14 */ f32 field_0x14;
-        /* 0x18 */ f32 field_0x18;
-        /* 0x1C */ f32 field_0x1c;
-        /* 0x20 */ f32 field_0x20;
-        /* 0x24 */ f32 field_0x24;
-        /* 0x28 */ f32 field_0x28;
-        /* 0x2C */ f32 field_0x2c;
-        /* 0x30 */ f32 field_0x30;
-        /* 0x34 */ f32 field_0x34;
-        /* 0x38 */ f32 field_0x38;
-        /* 0x3C */ f32 field_0x3c;
-        /* 0x40 */ f32 field_0x40;
-        /* 0x44 */ f32 field_0x44;
-        /* 0x48 */ s16 field_0x48;
-        /* 0x4A */ s16 field_0x4a;
-        /* 0x4C */ s16 field_0x4c;
-        /* 0x4E */ s16 field_0x4e;
-        /* 0x50 */ f32 field_0x50;
-        /* 0x54 */ f32 field_0x54;
-        /* 0x58 */ f32 field_0x58;
-        /* 0x5C */ f32 field_0x5c;
-        /* 0x60 */ s16 field_0x60;
-        /* 0x62 */ s16 field_0x62;
-        /* 0x64 */ int field_0x64;
-        /* 0x68 */ int field_0x68;
-        /* 0x6C */ f32 field_0x6c;
-        /* 0x70 */ f32 field_0x70;
-        /* 0x74 */ f32 field_0x74;
-        /* 0x78 */ f32 field_0x78;
-        /* 0x7C */ f32 field_0x7c;
-        /* 0x80 */ f32 field_0x80;
-        /* 0x84 */ f32 field_0x84;
-        /* 0x88 */ f32 field_0x88;
+        /* 0x00 */ f32 field_0x00;  // 210.0f
+        /* 0x04 */ f32 mGravity;  // -3.0f
+        /* 0x08 */ f32 field_0x08;  // 1.0f
+        /* 0x0C */ f32 field_0x0c;  // 500.0f
+        /* 0x10 */ f32 mWeight;  // 255.0f
+        /* 0x14 */ f32 mCylH;  // 190.0f
+        /* 0x18 */ f32 mWallH;  // 35.0f
+        /* 0x1C */ f32 mWallR;  // 30.0f
+        /* 0x20 */ f32 field_0x20;  // 0.0f
+        /* 0x24 */ f32 field_0x24;  // 0.0f
+        /* 0x28 */ f32 field_0x28;  // 10.0f
+        /* 0x2C */ f32 field_0x2c;  // -10.0f
+        /* 0x30 */ f32 field_0x30;  // 30.0f
+        /* 0x34 */ f32 field_0x34;  // 10.0f
+        /* 0x38 */ f32 field_0x38;  // 45.0f
+        /* 0x3C */ f32 field_0x3c;  // -45.0f
+        /* 0x40 */ f32 field_0x40;  // 0.6f
+        /* 0x44 */ f32 mMorfFrames;  // 12.0f
+        /* 0x48 */ s16 field_0x48;  // 3
+        /* 0x4A */ s16 field_0x4a;  // 6
+        /* 0x4C */ s16 field_0x4c;  // 5
+        /* 0x4E */ s16 field_0x4e;  // 6
+        /* 0x50 */ f32 field_0x50;  // 110.0f
+        /* 0x54 */ f32 field_0x54;  // 0.0f
+        /* 0x58 */ f32 field_0x58;  // 0.0f
+        /* 0x5C */ f32 field_0x5c;  // 0.0f
+        /* 0x60 */ s16 field_0x60;  // 60
+        /* 0x62 */ s16 field_0x62;  // 8
+        /* 0x64 */ int field_0x64;  // 0
+        /* 0x68 */ int field_0x68;  // 0
+        /* 0x6C */ f32 field_0x6c;  // 4.0f
+        /* 0x70 */ f32 field_0x70;  // 0.0f
+        /* 0x74 */ f32 field_0x74;  // 0.0f
+        /* 0x78 */ f32 field_0x78;  // 0.0f
+        /* 0x7C */ f32 field_0x7c;  // 0.0f
+        /* 0x80 */ f32 field_0x80;  // 0.0f
+        /* 0x84 */ f32 field_0x84;  // 0.0f
+        /* 0x88 */ f32 field_0x88;  // 0.0f
     };
 
     static const Data m;
