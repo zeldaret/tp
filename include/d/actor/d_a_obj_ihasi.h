6a3ab4eb9eefd471813d89fb2f2aedba506359fd
ItsNiklas <nikbau2000@gmail.com>
2025-04-03 18:51:06 +0900

    d_a_obj_ihasi cleanup

--- include/d/actor/d_a_obj_ihasi.h
+++ include/d/actor/d_a_obj_ihasi.h
@@ -3,8 +3,8 @@
 
 #include "SSystem/SComponent/c_phase.h"
 
-#include "f_op/f_op_actor.h"
 #include "d/d_bg_w.h"
+#include "f_op/f_op_actor.h"
 
 /**
  * @ingroup actors-objects
@@ -16,23 +16,20 @@
  */
 class obj_ihasi_class : public fopAc_ac_c {
 public:
-    /* 0x568 */ request_of_phase_process_class mPhase;
+    /* 0x568 */ request_of_phase_process_class mPhaseReq;
     /* 0x570 */ J3DModel* mpModel1;
     /* 0x574 */ J3DModel* mpModel2;
-    /* 0x574 */ mDoExt_btkAnm* mpBtkAnm;
-    /* 0x574 */ short mCount;
-    /* 0x574 */ short mFlag1;
-    /* 0x574 */ bool mFlag2;
-    /* 0x574 */ bool mUnk3;
-    /* 0x574 */ short mArr[2];
-    /* 0x574 */ s8 mFlag3;
-    /* 0x574 */ u8 mUnk4;
-    /* 0x574 */ Mtx mMtx;
-    /* 0x574 */ dBgW* mpBgW;
-    /* 0x574 */ u8 mUnk2[8];
+    /* 0x578 */ mDoExt_btkAnm* mpBtkAnm;
+    /* 0x57C */ s16 mShort1;
+    /* 0x57E */ s16 mFlag1;
+    /* 0x580 */ u8 field_0x580[2];
+    /* 0x582 */ s16 mArr[2];
+    /* 0x586 */ s8 mFlag2;
+    /* 0x588 */ Mtx mMtx;
+    /* 0x5B8 */ dBgW* mpBgW;
+    /* 0x5BC */ u8 field_0x5bc[8];
 };
 
 STATIC_ASSERT(sizeof(obj_ihasi_class) == 0x5c4);
-
 
 #endif /* D_A_OBJ_IHASI_H */
--- src/d/actor/d_a_obj_ihasi.cpp
+++ src/d/actor/d_a_obj_ihasi.cpp
@@ -4,29 +4,7 @@
  */
 
 #include "d/actor/d_a_obj_ihasi.h"
-#include "dol2asm.h"
-
 #include "d/d_com_inf_game.h"
-
-//
-// Forward References:
-//
-
-extern "C" static void daObj_Ihasi_Draw__FP15obj_ihasi_class();
-extern "C" static void ih_normal__FP15obj_ihasi_class();
-extern "C" static void ih_disappear__FP15obj_ihasi_class();
-extern "C" static void action__FP15obj_ihasi_class();
-extern "C" static void daObj_Ihasi_Execute__FP15obj_ihasi_class();
-extern "C" static bool daObj_Ihasi_IsDelete__FP15obj_ihasi_class();
-extern "C" static void daObj_Ihasi_Delete__FP15obj_ihasi_class();
-extern "C" static void useHeapInit__FP10fopAc_ac_c();
-extern "C" void __dt__12J3DFrameCtrlFv();
-extern "C" static void daObj_Ihasi_Create__FP10fopAc_ac_c();
-extern "C" extern char const* const d_a_obj_ihasi__stringBase0;
-
-//
-// Declarations:
-//
 
 /* 80C26018-80C2611C 000078 0104+00 1/0 0/0 0/0 .text daObj_Ihasi_Draw__FP15obj_ihasi_class */
 static bool daObj_Ihasi_Draw(obj_ihasi_class* i_this) {
@@ -49,22 +27,19 @@ static bool daObj_Ihasi_Draw(obj_ihasi_class* i_this) {
 }
 
 /* 80C2611C-80C26120 00017C 0004+00 1/1 0/0 0/0 .text            ih_normal__FP15obj_ihasi_class */
-static void ih_normal(obj_ihasi_class* param_0) {
+static void ih_normal(obj_ihasi_class* i_this) {
     return;
 }
 
 /* 80C26120-80C26124 000180 0004+00 1/1 0/0 0/0 .text            ih_disappear__FP15obj_ihasi_class
  */
-static void ih_disappear(obj_ihasi_class* param_0) {
+static void ih_disappear(obj_ihasi_class* i_this) {
     return;
 }
 
-/* ############################################################################################## */
 /* 80C26124-80C26204 000184 00E0+00 1/1 0/0 0/0 .text            action__FP15obj_ihasi_class */
 static void action(obj_ihasi_class* i_this) {
-    short var = i_this->mFlag1;
-
-    switch (var) {
+    switch (i_this->mFlag1) {
     case 0:
         ih_normal(i_this);
         break;
@@ -91,7 +66,7 @@ static void action(obj_ihasi_class* i_this) {
 
 /* 80C26204-80C262C4 000264 00C0+00 2/1 0/0 0/0 .text daObj_Ihasi_Execute__FP15obj_ihasi_class */
 static int daObj_Ihasi_Execute(obj_ihasi_class* i_this) {
-    i_this->mCount += 1;
+    i_this->mShort1 += 1;
 
     for (int i = 0; i < 2; i++) {
         if (i_this->mArr[i] != 0) {
@@ -99,9 +74,9 @@ static int daObj_Ihasi_Execute(obj_ihasi_class* i_this) {
         }
     }
 
-    if (i_this->mFlag3 != 0) {
+    if (i_this->mFlag2 != 0) {
         i_this->mFlag1 = 1;
-        i_this->mFlag3 = 0;
+        i_this->mFlag2 = 0;
 
         dComIfG_Bgsp().Release(i_this->mpBgW);
         i_this->mpBgW = NULL;
@@ -117,10 +92,9 @@ static int daObj_Ihasi_IsDelete(obj_ihasi_class* param_0) {
     return 1;
 }
 
-/* ############################################################################################## */
 /* 80C262CC-80C26324 00032C 0058+00 1/0 0/0 0/0 .text daObj_Ihasi_Delete__FP15obj_ihasi_class */
 static int daObj_Ihasi_Delete(obj_ihasi_class* _this) {
-    dComIfG_resDelete(&_this->mPhase, "Obj_ihasi");
+    dComIfG_resDelete(&_this->mPhaseReq, "Obj_ihasi");
 
     if (_this->mpBgW != NULL) {
         dComIfG_Bgsp().Release(_this->mpBgW);
@@ -129,14 +103,113 @@ static int daObj_Ihasi_Delete(obj_ihasi_class* _this) {
     return 1;
 }
 
-/* ############################################################################################## */
+/* 80C26324-80C264F8 000384 01D4+00 1/1 0/0 0/0 .text            useHeapInit__FP10fopAc_ac_c */
+static int useHeapInit(fopAc_ac_c* i_actor) {
+    obj_ihasi_class* i_this = static_cast<obj_ihasi_class*>(i_actor);
+
+    J3DModelData* modelData = (J3DModelData*)dComIfG_getObjectRes("Obj_ihasi", 5);
+    i_this->mpModel1 = mDoExt_J3DModel__create(modelData, 0x80000, 0x11000284);
+
+    if (i_this->mpModel1 == NULL) {
+        return 0;
+    }
+
+    i_this->mpBgW = new dBgW();
+    if (i_this->mpBgW == NULL) {
+        return 0;
+    }
+
+    cBgD_t* pdzb = (cBgD_t*)dComIfG_getObjectRes("Obj_ihasi", 0xc);
+    if (i_this->mpBgW->Set(pdzb, 1, &i_this->mMtx) == 1) {
+        return 0;
+    }
+
+    i_this->mpBgW->SetCrrFunc(dBgS_MoveBGProc_Typical);
+
+    J3DModelData* modelData2 = (J3DModelData*)dComIfG_getObjectRes("Obj_ihasi", 6);
+    i_this->mpModel2 = mDoExt_J3DModel__create(modelData2, 0x80000, 0x11000284);
+    if (i_this->mpModel2 == NULL) {
+        return 0;
+    }
+
+    i_this->mpBtkAnm = new mDoExt_btkAnm();
+    if (i_this->mpBtkAnm == NULL) {
+        return 0;
+    }
+
+    J3DAnmTextureSRTKey* key = (J3DAnmTextureSRTKey*)dComIfG_getObjectRes("Obj_ihasi", 9);
+    return i_this->mpBtkAnm->init(i_this->mpModel2->getModelData(), key, 1, 0, 1.0f, 0, -1) ? 1 : 0;
+}
+
+/* 80C26540-80C26880 0005A0 0340+00 1/0 0/0 0/0 .text            daObj_Ihasi_Create__FP10fopAc_ac_c
+ */
+static cPhs__Step daObj_Ihasi_Create(fopAc_ac_c* i_actor) {
+    fopAcM_SetupActor(i_actor, obj_ihasi_class);
+    obj_ihasi_class* i_this = static_cast<obj_ihasi_class*>(i_actor);
+
+    cPhs__Step step = (cPhs__Step)dComIfG_resLoad(&i_this->mPhaseReq, "Obj_ihasi");
+
+    if (step == cPhs_COMPLEATE_e) {
+        u32 param;
+        csXyz angle;
+        cXyz pos;
+
+        if (dComIfGs_isEventBit(dSv_event_flag_c::saveBitLabels[0x53]) != 0) {
+            angle.set(0x0, -0x8000, 0x0);
+            pos.set(34750.0f, -300.0f, -44000.0f);
+
+            param = fopAcM_GetParam(i_actor) & 0xff000000 | 0xfffe00;
+            fopAcM_create(PROC_E_S1, param, &pos, fopAcM_GetRoomNo(i_actor), &angle, NULL, -1);
+
+            pos.set(35250.0f, -300.0f, -43500.0f);
+            angle.y += 0x5555;
+            fopAcM_create(PROC_E_S1, param, &pos, fopAcM_GetRoomNo(i_actor), &angle, NULL, -1);
+
+            pos.set(34250.0f, -300.0f, -43500.0f);
+            angle.y += 0x5555;
+            fopAcM_create(PROC_E_S1, param, &pos, fopAcM_GetRoomNo(i_actor), &angle, NULL, -1);
+
+            return cPhs_ERROR_e;
+        } else {
+            if (!fopAcM_entrySolidHeap(i_actor, useHeapInit, 0x43b0)) {
+                return cPhs_ERROR_e;
+            }
+
+            if (dComIfG_Bgsp().Regist(i_this->mpBgW, i_this)) {
+                return cPhs_ERROR_e;
+            }
+
+            i_this->mShort1 = cM_rndF(65536.0f);
+
+            if (!strcmp(dComIfGp_getStartStageName(), "F_SP121") && !fopAcM_GetRoomNo(i_actor)) {
+                pos.set(34800.0f, 5700.0f, -26735.0f);
+                fopAcM_create(PROC_E_WAP, 0xffffff35, &pos, fopAcM_GetRoomNo(i_actor), NULL, NULL,
+                              -1);
+
+                angle.set(0x0, -0x8000, 0x0);
+                pos.set(34800.0f, -300.0f, -30340.0f);
+                param = fopAcM_GetParam(i_actor) & 0xff000000 | 0xfffe00;
+                fopAcM_create(PROC_E_S1, param, &pos, fopAcM_GetRoomNo(i_actor), &angle, NULL, -1);
+
+                pos.set(35087.0f, -300.0f, -29879.0f);
+                fopAcM_create(PROC_E_S1, param, &pos, fopAcM_GetRoomNo(i_actor), &angle, NULL, -1);
+
+                pos.set(34500.0f, -300.0f, -29879.0f);
+                fopAcM_create(PROC_E_S1, param, &pos, fopAcM_GetRoomNo(i_actor), &angle, NULL, -1);
+            }
+
+            daObj_Ihasi_Execute(i_this);
+        }
+    }
+
+    return step;
+}
+
 /* 80C268D8-80C268F8 -00001 0020+00 1/0 0/0 0/0 .data            l_daObj_Ihasi_Method */
 static actor_method_class l_daObj_Ihasi_Method = {
-    (process_method_func)daObj_Ihasi_Create__FP10fopAc_ac_c,
-    (process_method_func)daObj_Ihasi_Delete__FP15obj_ihasi_class,
-    (process_method_func)daObj_Ihasi_Execute__FP15obj_ihasi_class,
-    (process_method_func)daObj_Ihasi_IsDelete__FP15obj_ihasi_class,
-    (process_method_func)daObj_Ihasi_Draw__FP15obj_ihasi_class,
+    (process_method_func)daObj_Ihasi_Create,  (process_method_func)daObj_Ihasi_Delete,
+    (process_method_func)daObj_Ihasi_Execute, (process_method_func)daObj_Ihasi_IsDelete,
+    (process_method_func)daObj_Ihasi_Draw,
 };
 
 /* 80C268F8-80C26928 -00001 0030+00 0/0 0/0 1/0 .data            g_profile_OBJ_IHASI */
@@ -156,123 +229,3 @@ extern actor_process_profile_definition g_profile_OBJ_IHASI = {
     fopAc_ACTOR_e,            // mActorType
     fopAc_CULLBOX_CUSTOM_e,   // cullType
 };
-
-/* 80C26928-80C26934 000050 000C+00 2/2 0/0 0/0 .data            __vt__12J3DFrameCtrl */
-// SECTION_DATA extern void* __vt__12J3DFrameCtrl[3] = {
-//     (void*)NULL /* RTTI */,
-//     (void*)NULL,
-//     (void*)__dt__12J3DFrameCtrlFv,
-// };
-
-/* 80C26324-80C264F8 000384 01D4+00 1/1 0/0 0/0 .text            useHeapInit__FP10fopAc_ac_c */
-static int useHeapInit(fopAc_ac_c* actor) {
-    obj_ihasi_class* i_this = static_cast<obj_ihasi_class*>(actor);
-
-    J3DModelData* modelData = (J3DModelData*)dComIfG_getObjectRes("Obj_ihasi", 5);
-    i_this->mpModel1 = mDoExt_J3DModel__create(modelData, 0x80000, 0x11000284);
-
-    if (i_this->mpModel1 == NULL) {
-        return 0;
-    }
-
-    i_this->mpBgW = new dBgW();
-    if (i_this->mpBgW == NULL) {
-        return 0;
-    }
-
-    cBgD_t* pdzb = (cBgD_t*)dComIfG_getObjectRes("Obj_ihasi", 0xc);
-
-    if (i_this->mpBgW->Set(pdzb, 1, &i_this->mMtx) == 1) {
-        return 0;
-    }
-
-    i_this->mpBgW->SetCrrFunc(dBgS_MoveBGProc_Typical);
-
-    J3DModelData* modelData2 = (J3DModelData*)dComIfG_getObjectRes("Obj_ihasi", 6);
-    i_this->mpModel2 = mDoExt_J3DModel__create(modelData2, 0x80000, 0x11000284);
-
-    if (i_this->mpModel2 == NULL) {
-        return 0;
-    }
-
-    i_this->mpBtkAnm = new mDoExt_btkAnm();
-    if (i_this->mpBtkAnm == NULL) {
-        return 0;
-    }
-
-    J3DAnmTextureSRTKey* key = (J3DAnmTextureSRTKey*)dComIfG_getObjectRes("Obj_ihasi", 9);
-    return i_this->mpBtkAnm->init(i_this->mpModel2->getModelData(), key, 1, 0, 1.0f, 0, -1) ? 1 : 0;
-}
-
-/* 80C264F8-80C26540 000558 0048+00 1/0 0/0 0/0 .text            __dt__12J3DFrameCtrlFv */
-// J3DFrameCtrl::~J3DFrameCtrl() {
-// extern "C" void __dt__12J3DFrameCtrlFv() {
-// NONMATCHING
-// }
-
-/* ############################################################################################## */
-/* 80C26540-80C26880 0005A0 0340+00 1/0 0/0 0/0 .text            daObj_Ihasi_Create__FP10fopAc_ac_c
- */
-static cPhs__Step daObj_Ihasi_Create(fopAc_ac_c* i_this) {
-    fopAcM_SetupActor(i_this, obj_ihasi_class);
-    obj_ihasi_class* _this = static_cast<obj_ihasi_class*>(i_this);
-
-    cPhs__Step step = (cPhs__Step)dComIfG_resLoad(&_this->mPhase, "Obj_ihasi");
-
-    if (step == cPhs_COMPLEATE_e) {
-        u32 param;
-        csXyz angle;
-        cXyz pos;
-
-        if (dComIfGs_isEventBit(dSv_event_flag_c::saveBitLabels[0x53]) != 0) {
-            angle.set(0x0, -0x8000, 0x0);
-            pos.set(34750.0f, -300.0f, -44000.0f);
-
-            param = fopAcM_GetParam(i_this) & 0xff000000 | 0xfffe00;
-            fopAcM_create(PROC_E_S1, param, &pos, fopAcM_GetRoomNo(i_this), &angle, NULL, -1);
-
-            pos.set(35250.0f, -300.0f, -43500.0f);
-            angle.y += 0x5555;
-            fopAcM_create(PROC_E_S1, param, &pos, fopAcM_GetRoomNo(i_this), &angle, NULL, -1);
-
-            pos.set(34250.0f, -300.0f, -43500.0f);
-            angle.y += 0x5555;
-            fopAcM_create(PROC_E_S1, param, &pos, fopAcM_GetRoomNo(i_this), &angle, NULL, -1);
-
-            return cPhs_ERROR_e;
-        } else {
-            if (!fopAcM_entrySolidHeap(i_this, useHeapInit, 0x43b0)) {
-                return cPhs_ERROR_e;
-            }
-
-            if (dComIfG_Bgsp().Regist(_this->mpBgW, _this)) {
-                return cPhs_ERROR_e;
-            }
-
-            _this->mCount = (short)(int)cM_rndF(65536.0f);
-
-            if (!strcmp(dComIfGp_getStartStageName(), "F_SP121") && !fopAcM_GetRoomNo(i_this)) {
-                pos.set(34800.0f, 5700.0f, -26735.0f);
-                fopAcM_create(PROC_E_WAP, 0xffffff35, &pos, fopAcM_GetRoomNo(i_this), NULL, NULL,
-                              -1);
-
-                angle.set(0x0, -0x8000, 0x0);
-                pos.set(34800.0f, -300.0f, -30340.0f);
-                param = fopAcM_GetParam(i_this) & 0xff000000 | 0xfffe00;
-                fopAcM_create(PROC_E_S1, param, &pos, fopAcM_GetRoomNo(i_this), &angle, NULL, -1);
-
-                pos.set(35087.0f, -300.0f, -29879.0f);
-                fopAcM_create(PROC_E_S1, param, &pos, fopAcM_GetRoomNo(i_this), &angle, NULL, -1);
-
-                pos.set(34500.0f, -300.0f, -29879.0f);
-                fopAcM_create(PROC_E_S1, param, &pos, fopAcM_GetRoomNo(i_this), &angle, NULL, -1);
-            }
-
-            daObj_Ihasi_Execute(_this);
-        }
-    }
-
-    return step;
-}
-
-/* 80C268C4-80C268C4 00003C 0000+00 0/0 0/0 0/0 .rodata          @stringBase0 */
