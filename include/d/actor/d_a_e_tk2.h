eeb0a94f1acd29a0210c170c38b784f29ce0afa2
ItsNiklas <nikbau2000@gmail.com>
2025-04-18 11:06:28 +0900

    d_a_e_tk, d_a_e_tk2 cleanup

--- include/d/actor/d_a_e_tk2.h
+++ include/d/actor/d_a_e_tk2.h
@@ -1,65 +1,58 @@
 #ifndef D_A_E_TK2_H
 #define D_A_E_TK2_H
 
-#include "SSystem/SComponent/c_phase.h"
 #include "d/d_bg_s_acch.h"
 #include "d/d_cc_d.h"
 #include "d/d_cc_uty.h"
 #include "f_op/f_op_actor.h"
+
 /**
  * @ingroup actors-enemies
  * @class e_tk2_class
  * @brief Fire Toadpoli
  *
- * @details
- *
+ * @details Fire Toadpoli in the Goron Mines Entrance.
+ * Armored, two-legged frogs that spit Fireballs at Link.
+ * They cannot swim, but will dive under the lava if Link
+ * gets too close. They share most of the code with the
+ * Water Toadpoli e_tk.
  */
+ 
 class e_tk2_class : public fopEn_enemy_c {
 public:
-    request_of_phase_process_class mPhaseReq;
-    u8 mArg0;
-    u8 mArg1;
-    u8 mArg2;
-    u8 mArg3;
-    mDoExt_McaMorfSO *mpMorf;
-    int mAnim;
-    Z2CreatureEnemy mSound;
-    float mAnimSpeed;
-    short mLifetime;
-    short mAction;
-    short mMode;
-    u8 field14_0x66e;
-    u8 field15_0x66f;
-    cXyz mPos;
-    short mSomeAngle;
-    u8 field18_0x67e;
-    u8 field19_0x67f;
-    short mPlayerAngleY;
-    u8 field21_0x682;
-    u8 field22_0x683;
-    float mPlayerDistanceLimit;
-    u8 field24_0x688;
-    u8 field25_0x689;
-    short mActionTimer[3];
-    short mExecuteState;
-    short mInvincibilityTimer;
-    s8 mAttentionOFF;
-    s8 mTKBallSpawned;
-    u8 field31_0x696;
-    u8 field32_0x697;
-    int mpBallID;
-    dBgS_AcchCir mAcchCir;
-    dBgS_ObjAcch mAcch;
-    dCcD_Stts mStts;
-    dCcD_Sph mSph;
-    dCcU_AtInfo mAtInfo;
-    bool mInitHIO;
-    u8 field40_0xa4d;
-    u8 field41_0xa4e;
-    u8 field42_0xa4f;
+    /* 0x5AC */ request_of_phase_process_class mPhaseReq;
+    /* 0x5B4 */ u8 mArg0;
+    /* 0x5B5 */ u8 mArg1;
+    /* 0x5B6 */ u8 mArg2;
+    /* 0x5B7 */ u8 mArg3; /* Unused */
+    /* 0x5B8 */ mDoExt_McaMorfSO* mpMorf;
+    /* 0x5BC */ int mAnim;
+    /* 0x5C0 */ Z2CreatureEnemy mSound;
+    /* 0x664 */ f32 mAnimSpeed;
+    /* 0x668 */ s16 mLifetime;
+    /* 0x66A */ s16 mAction;
+    /* 0x66C */ s16 mMode;
+    /* 0x670 */ cXyz mPos;
+    /* 0x67C */ s16 mSomeAngle;
+    /* 0x67E */ u8 field18_0x67e[2];
+    /* 0x680 */ s16 mPlayerAngleY;
+    /* 0x684 */ f32 mPlayerDistanceLimit;
+    /* 0x688 */ u8 field24_0x688[2];
+    /* 0x68A */ s16 mActionTimer[3];
+    /* 0x690 */ s16 mExecuteState;
+    /* 0x692 */ s16 mInvincibilityTimer;
+    /* 0x694 */ s8 mAttentionOFF;
+    /* 0x695 */ s8 mTKBallSpawned;
+    /* 0x698 */ fpc_ProcID mBallID;
+    /* 0x69C */ dBgS_AcchCir mAcchCir;
+    /* 0x6DC */ dBgS_ObjAcch mAcch;
+    /* 0x8B4 */ dCcD_Stts mStts;
+    /* 0x8F0 */ dCcD_Sph mSph;
+    /* 0xA28 */ dCcU_AtInfo mAtInfo;
+    /* 0xA4C */ bool mInitHIO;
 };
 
-STATIC_ASSERT(sizeof(e_tk2_class) == 0xa50);
+STATIC_ASSERT(sizeof(e_tk2_class) == 0xA50);
 
 class daE_TK2_HIO_c {
 public:
@@ -70,8 +63,8 @@ public:
     /* 0x08 */ f32 mRadiusScale;
     /* 0x0C */ f32 mPlayerRange1;
     /* 0x10 */ f32 mPlayerRange2;
-    /* 0x14 */ f32 mSpeedModifier1;
-    /* 0x18 */ f32 mSpeedModifier2;
+    /* 0x14 */ f32 mSpeedModifier1; /* Unused */
+    /* 0x18 */ f32 mSpeedModifier2; /* Unused */
 };
 
 STATIC_ASSERT(sizeof(daE_TK2_HIO_c) == 0x1C);
