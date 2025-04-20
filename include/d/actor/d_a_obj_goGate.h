9194774985fda19caddaf2b3366c9adadf2e7d26
meintte <daniel_stalder@hispeed.ch>
2025-04-20 09:40:41 +0200

    d_a_obj_goGate matching (#2401)
    * d_a_obj_goGate matching
    
    * use UNK_REL_DATA, fix inline and multVec

--- include/d/actor/d_a_obj_goGate.h
+++ include/d/actor/d_a_obj_goGate.h
@@ -1,7 +1,10 @@
 #ifndef D_A_OBJ_GOGATE_H
 #define D_A_OBJ_GOGATE_H
 
+#include "SSystem/SComponent/c_phase.h"
+#include "d/d_bg_s_movebg_actor.h"
 #include "f_op/f_op_actor_mng.h"
+#include "m_Do/m_Do_hostIO.h"
 
 /**
  * @ingroup actors-objects
@@ -11,12 +14,18 @@
  * @details
  *
  */
-class daGoGate_c : public fopAc_ac_c {
+class daGoGate_c : public dBgS_MoveBgActor {
 public:
+    enum Mode {
+        MODE_WAIT = 0,
+        MODE_OPEN = 1,
+        MODE_CLOSE = 2,
+    };
+
     /* 80BFD724 */ void setBaseMtx();
-    /* 80BFD7C0 */ void CreateHeap();
-    /* 80BFD82C */ void create();
-    /* 80BFD984 */ void Execute(f32 (**)[3][4]);
+    /* 80BFD7C0 */ int CreateHeap();
+    /* 80BFD82C */ cPhs__Step create();
+    /* 80BFD984 */ int Execute(Mtx**);
     /* 80BFD9D4 */ void moveGate();
     /* 80BFDAD8 */ void init_modeWait();
     /* 80BFDAE4 */ void modeWait();
@@ -25,20 +34,36 @@ public:
     /* 80BFDC04 */ void init_modeMoveClose();
     /* 80BFDC10 */ void modeMoveClose();
     /* 80BFDD20 */ void setSe();
-    /* 80BFDDDC */ void Draw();
-    /* 80BFDE80 */ void Delete();
+    /* 80BFDDDC */ int Draw();
+    /* 80BFDE80 */ int Delete();
+
+    u32 getLR() { return fopAcM_GetParamBit(this, 8, 8); }
+    u32 getSw() { return fopAcM_GetParamBit(this, 0, 8); }
 
 private:
-    /* 0x568 */ u8 field_0x568[0x5b8 - 0x568];
+    /* 0x5A0 */ request_of_phase_process_class mPhase;
+    /* 0x5A8 */ J3DModel* mpModel;
+    /* 0x5AC */ u8 mMode;
+    /* 0x5AD */ u8 mSw;
+    /* 0x5AE */ u8 mOpen;
+    /* 0x5AF */ u8 mLR;
+    /* 0x5B0 */ f32 field_0x5b0;
+    /* 0x5B4 */ f32 field_0x5b4;
 };
 
 STATIC_ASSERT(sizeof(daGoGate_c) == 0x5b8);
 
-class daGoGate_HIO_c {
+class daGoGate_HIO_c : public mDoHIO_entry_c {
 public:
     /* 80BFD6AC */ daGoGate_HIO_c();
-    /* 80BFDF3C */ ~daGoGate_HIO_c();
+    /* 80BFDF3C */ virtual ~daGoGate_HIO_c() {}
+
+    void genMessage(JORMContext*);
+
+    /* 0x8 */ f32 mSpeed;
+    /* 0xc */ u8 mShockStrength;
 };
 
+STATIC_ASSERT(sizeof(daGoGate_HIO_c) == 12);
 
 #endif /* D_A_OBJ_GOGATE_H */
