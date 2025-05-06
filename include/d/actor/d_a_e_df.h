339ecdb7d50f080ff2f7fadef9dbc8fb95f8a871
Trueffel <106771418+Trueffeloot@users.noreply.github.com>
2025-05-06 22:48:40 +0200

    decompile a few enemy classes

--- include/d/actor/d_a_e_df.h
+++ include/d/actor/d_a_e_df.h
@@ -1,16 +1,21 @@
 #ifndef D_A_E_DF_H
 #define D_A_E_DF_H
+
+#include "Z2AudioLib/Z2Creature.h"
+#include "d/d_cc_d.h"
+#include "d/d_cc_uty.h"
 #include "f_op/f_op_actor_mng.h"
 
 /**
  * @ingroup actors-enemies
  * @class daE_DF_c
  * @brief Deku Flower
- * 
- * @details 
- * 
+ *
+ * @details
+ *
  */
 class daE_DF_c : public fopEn_enemy_c {
+public:
     /* 806A7710 */ void CreateHeap();
     /* 806A7828 */ void initCcCylinder();
     /* 806A78A0 */ void setCcCylinder();
@@ -39,16 +44,44 @@ class daE_DF_c : public fopEn_enemy_c {
     /* 806A9550 */ void Delete();
     /* 806A95B8 */ void setBaseMtx();
     /* 806A9678 */ void Create();
+
 private:
-    /* 0x5ac */ u8 field_0x5ac[0x838 - 0x5ac];
+    /* 0x5AC */ u8 field_0x5ac;
+    /* 0x5AD */ u8 field_0x5ad;
+    /* 0x5AE */ u8 field_0x5ae;
+    /* 0x5B0 */ csXyz field_0x5b0;
+    /* 0x5B6 */ u8 field_0x5B6[0x5B8 - 0x5B6];
+    /* 0x5B8 */ s32 field_0x5b8;
+    /* 0x5BC */ u8 field_0x5bc;
+    /* 0x5BE */ s16 field_0x5be;
+    /* 0x5C0 */ s32 field_0x5c0;
+    /* 0x5C4 */ s16 field_0x5c4;
+    /* 0x5C6 */ s16 field_0x5c6;
+    /* 0x5C8 */ mDoExt_McaMorfSO* mpMorfSO;
+    /* 0x5CC */ request_of_phase_process_class mPhaseReq;
+    /* 0x5D4 */ u8 field_0x5D4[0x5D8 - 0x5D4];
+    /* 0x5D8 */ Z2CreatureEnemy mCreatureSound;
+    /* 0x67C */ dCcD_Stts mStts;
+    /* 0x6B8 */ dCcD_Cyl mCylinder;
+    /* 0x7F4 */ dCcU_AtInfo mAtInfo;
+    /* 0x818 */ u8 mHIOInit;
+    /* 0x819 */ u8 field_0x819;
+    /* 0x81C */ cXyz field_0x81c;
+    /* 0x828 */ cXyz field_0x828;
+    /* 0x834 */ f32 field_0x834;
 };
 
 STATIC_ASSERT(sizeof(daE_DF_c) == 0x838);
 
-class daE_DF_HIO_c {
+class daE_DF_HIO_c : public JORReflexible {
+public:
     /* 806A76EC */ daE_DF_HIO_c();
-    /* 806A9A84 */ ~daE_DF_HIO_c();
-};
+    /* 806A9A84 */ virtual ~daE_DF_HIO_c();
 
+    void genMessage(JORMContext*);
+
+    /* 0x4 */ s8 field_0x4;
+    /* 0x8 */ f32 speed;
+};
 
 #endif /* D_A_E_DF_H */
