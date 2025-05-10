eb3296596ec30cb0cd0b2d861594b8a5a4f7bd17
ItsNiklas <nikbau2000@gmail.com>
2025-05-11 00:19:33 +0900

    d_a_npc_cd equivalent?

--- include/d/actor/d_a_npc_cd.h
+++ include/d/actor/d_a_npc_cd.h
@@ -4,106 +4,6 @@
 #include "Z2AudioLib/Z2Creature.h"
 #include "d/actor/d_a_npc.h"
 
-struct daNpcCd_HIO_Jnt_c : public JORReflexible {
-    /* 80157810 */ virtual ~daNpcCd_HIO_Jnt_c() {}
-    /* 801578B0 */ daNpcCd_HIO_Jnt_c() {}
-
-    void genMessage(JORMContext*);
-
-    /* 0x04 */ cXyz jntT;
-    /* 0x10 */ csXyz jntR;
-};
-
-struct daNpcCd_HIO_Child_c {
-    /* 801577A0 */ virtual ~daNpcCd_HIO_Child_c() {}
-    /* 80157858 */ daNpcCd_HIO_Child_c() {}
-
-    /* 0x004 */ daNpcCd_HIO_Jnt_c field_0x4[12];
-    /* 0x128 */ s16 mAtn;
-};
-
-struct daNpcCd_HIO_c : public fOpAcm_HIO_entry_c {
-    /* 80157600 */ daNpcCd_HIO_c();
-    /* 801577A0 */ virtual ~daNpcCd_HIO_c() {}
-
-    void genMessage(JORMContext*);
-
-    /* 0x0004 */ daNpcCd_HIO_Child_c field_0x0004[16];
-    /* 0x1648 */ daNpcCd_HIO_Child_c field_0x1648[14];
-};
-
-extern daNpcCd_HIO_c l_Cd_HIO;
-
-inline s16 HIO_atnOfs(int param_1) {
-    s16 rv;
-    if (param_1 < 16) {
-        rv = l_Cd_HIO.field_0x0004[param_1].mAtn;
-    } else {
-        rv = l_Cd_HIO.field_0x0004[param_1 - 16].mAtn;
-    }
-    return rv;
-}
-
-inline s16 HIO_jntRX(int param_1, int param_2) {
-    s16 rv;
-    if (param_1 < 16) {
-        rv = l_Cd_HIO.field_0x0004[param_1].field_0x4[param_2].jntR.x;
-    } else {
-        rv = l_Cd_HIO.field_0x1648[param_1 - 16].field_0x4[param_2].jntR.x;
-    }
-    return 182.04444885253906f * rv;
-}
-
-inline s16 HIO_jntRY(int param_1, int param_2) {
-    s16 rv;
-    if (param_1 < 16) {
-        rv = l_Cd_HIO.field_0x0004[param_1].field_0x4[param_2].jntR.y;
-    } else {
-        rv = l_Cd_HIO.field_0x1648[param_1 - 16].field_0x4[param_2].jntR.y;
-    }
-    return 182.04444885253906f * rv;
-}
-
-inline s16 HIO_jntRZ(int param_1, int param_2) {
-    s16 rv;
-    if (param_1 < 16) {
-        rv = l_Cd_HIO.field_0x0004[param_1].field_0x4[param_2].jntR.z;
-    } else {
-        rv = l_Cd_HIO.field_0x1648[param_1 - 16].field_0x4[param_2].jntR.z;
-    }
-    return 182.04444885253906f * rv;
-}
-
-static inline f32 HIO_jntTX(int param_1, int param_2) {
-    f32 rv;
-    if (param_1 < 16) {
-        rv = l_Cd_HIO.field_0x0004[param_1].field_0x4[param_2].jntT.x;
-    } else {
-        rv = l_Cd_HIO.field_0x1648[param_1 - 16].field_0x4[param_2].jntT.x;
-    }
-    return 10.0f * rv;
-}
-
-static inline f32 HIO_jntTY(int param_1, int param_2) {
-    f32 rv;
-    if (param_1 < 16) {
-        rv = l_Cd_HIO.field_0x0004[param_1].field_0x4[param_2].jntT.y;
-    } else {
-        rv = l_Cd_HIO.field_0x1648[param_1 - 16].field_0x4[param_2].jntT.y;
-    }
-    return 10.0f * rv;
-}
-
-static inline f32 HIO_jntTZ(int param_1, int param_2) {
-    f32 rv;
-    if (param_1 < 16) {
-        rv = l_Cd_HIO.field_0x0004[param_1].field_0x4[param_2].jntT.z;
-    } else {
-        rv = l_Cd_HIO.field_0x1648[param_1 - 16].field_0x4[param_2].jntT.z;
-    }
-    return 10.0f * rv;
-}
-
 class daNpcCd_c : public fopAc_ac_c {
 public:
     /* 8015605C */ int NpcCreate(int);
@@ -146,5 +46,126 @@ public:
     /* 0x9E6 */ s16 field_0x9e6;
     /* 0x9E8 */ bool mIsDarkWorld;
 };
+
+STATIC_ASSERT(sizeof(daNpcCd_c) == 0x9EC);
+
+
+struct daNpcCd_HIO_Jnt_c : public JORReflexible {
+    /* 80157810 */ virtual ~daNpcCd_HIO_Jnt_c() {}
+    /* 801578B0 */ daNpcCd_HIO_Jnt_c() {}
+
+#ifdef DEBUG
+    void genMessage(JORMContext*);
+#endif
+
+    /* 0x04 */ f32 jntTx;
+    /* 0x08 */ f32 jntTy;
+    /* 0x0C */ f32 jntTz;
+    /* 0x10 */ s16 jntRx;
+    /* 0x12 */ s16 jntRy;
+    /* 0x14 */ s16 jntRz;
+};
+
+struct daNpcCd_HIO_Child_c : public JORReflexible {
+    /* 801577A0 */ virtual ~daNpcCd_HIO_Child_c() {}
+    /* 80157858 */ daNpcCd_HIO_Child_c() {}
+
+#ifdef DEBUG
+    void genMessage(JORMContext*);
+#endif
+
+    /* 0x004 */ daNpcCd_HIO_Jnt_c field_0x4[12];
+    /* 0x128 */ s16 field_0x128[0x0E];
+    /* 0x136 */ s16 mAtn;
+    /* 0x156 */ s16 field_0x146[0x10];
+};
+
+STATIC_ASSERT(sizeof(daNpcCd_HIO_Child_c) == 0x164);
+
+class daNpcCd_HIO_c : public fOpAcm_HIO_entry_c {
+public:
+    /* 80157600 */ daNpcCd_HIO_c();
+    /* 801577A0 */ virtual ~daNpcCd_HIO_c() {}
+
+#ifdef DEBUG
+    void genMessage(JORMContext*);
+#endif
+
+    /* 0x0004 */ daNpcCd_HIO_Child_c field_0x0004[16];
+    /* 0x1648 */ daNpcCd_HIO_Child_c field_0x1648[14];
+};
+
+STATIC_ASSERT(sizeof(daNpcCd_HIO_c) == 0x29BC);
+
+extern daNpcCd_HIO_c l_Cd_HIO;
+inline f32 HIO_atnOfs(int param_1) {
+    s16 rv;
+    if (param_1 < 16) {
+        rv = l_Cd_HIO.field_0x0004[param_1].mAtn;
+    } else {
+        rv = l_Cd_HIO.field_0x1648[param_1 - 16].mAtn;
+    }
+    return rv;
+}
+
+inline s16 HIO_jntRX(int param_1, int param_2) {
+    s16 rv;
+    if (param_1 < 16) {
+        rv = l_Cd_HIO.field_0x0004[param_1].field_0x4[param_2].jntRx;
+    } else {
+        rv = l_Cd_HIO.field_0x1648[param_1 - 16].field_0x4[param_2].jntRx;
+    }
+    return 182.04444885253906f * rv;
+}
+
+inline s16 HIO_jntRY(int param_1, int param_2) {
+    s16 rv;
+    if (param_1 < 16) {
+        rv = l_Cd_HIO.field_0x0004[param_1].field_0x4[param_2].jntRy;
+    } else {
+        rv = l_Cd_HIO.field_0x1648[param_1 - 16].field_0x4[param_2].jntRy;
+    }
+    return 182.04444885253906f * rv;
+}
+
+inline s16 HIO_jntRZ(int param_1, int param_2) {
+    s16 rv;
+    if (param_1 < 16) {
+        rv = l_Cd_HIO.field_0x0004[param_1].field_0x4[param_2].jntRz;
+    } else {
+        rv = l_Cd_HIO.field_0x1648[param_1 - 16].field_0x4[param_2].jntRz;
+    }
+    return 182.04444885253906f * rv;
+}
+
+static inline f32 HIO_jntTX(int param_1, int param_2) {
+    f32 rv;
+    if (param_1 < 16) {
+        rv = l_Cd_HIO.field_0x0004[param_1].field_0x4[param_2].jntTx;
+    } else {
+        rv = l_Cd_HIO.field_0x1648[param_1 - 16].field_0x4[param_2].jntTx;
+    }
+    return 10.0f * rv;
+}
+
+static inline f32 HIO_jntTY(int param_1, int param_2) {
+    f32 rv;
+    if (param_1 < 16) {
+        rv = l_Cd_HIO.field_0x0004[param_1].field_0x4[param_2].jntTy;
+    } else {
+        rv = l_Cd_HIO.field_0x1648[param_1 - 16].field_0x4[param_2].jntTy;
+    }
+    return 10.0f * rv;
+}
+
+static inline f32 HIO_jntTZ(int param_1, int param_2) {
+    f32 rv;
+    if (param_1 < 16) {
+        rv = l_Cd_HIO.field_0x0004[param_1].field_0x4[param_2].jntTz;
+    } else {
+        rv = l_Cd_HIO.field_0x1648[param_1 - 16].field_0x4[param_2].jntTz;
+    }
+    return 10.0f * rv;
+}
 
 #endif /* A_NPC_D_A_NPC_CD_H */
