c9ee3d486163ce9a2dde34a7abd3a33c380c2378
meintte <daniel_stalder@hispeed.ch>
2025-05-27 11:47:15 +0200

    d_a_obj_takaraDai matching (#2457)

--- configure.py
+++ configure.py
@@ -2115,7 +2115,7 @@ config.libs = [
     ActorRel(NonMatching, "d_a_obj_szbridge"),
     ActorRel(NonMatching, "d_a_obj_taFence"),
     ActorRel(MatchingFor("GZ2E01"), "d_a_obj_table"),
-    ActorRel(NonMatching, "d_a_obj_takaraDai"),
+    ActorRel(MatchingFor("GZ2E01"), "d_a_obj_takaraDai"),
     ActorRel(NonMatching, "d_a_obj_tatigi"),
     ActorRel(MatchingFor("GZ2E01"), "d_a_obj_ten"),
     ActorRel(MatchingFor("GZ2E01"), "d_a_obj_testcube"),
