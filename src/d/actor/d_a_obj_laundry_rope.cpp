215896646d55704568c8f2272b2229ff40c32c15
Arnaut <arnautscholten1996@gmail.com>
2025-05-30 19:14:41 +0200

    Cleanup

--- src/d/actor/d_a_obj_laundry_rope.cpp
+++ src/d/actor/d_a_obj_laundry_rope.cpp
@@ -1,17 +1,18 @@
 /**
  * @file d_a_obj_laundry_rope.cpp
- * 
-*/
+ *
+ */
 
 #include "d/actor/d_a_obj_laundry_rope.h"
+#include "SSystem/SComponent/c_lib.h"
+#include "SSystem/SComponent/c_math.h"
 #include "d/d_cc_d.h"
+#include "d/d_com_inf_game.h"
+#include "d/d_kankyo_wether.h"
 #include "d/d_path.h"
 #include "dol2asm.h"
-#include "SSystem/SComponent/c_math.h"
-#include "d/d_com_inf_game.h"
-#include "SSystem/SComponent/c_lib.h"
-#include "d/d_kankyo_wether.h"
 #include "gx/GXStruct.h"
+
 
 //
 // Declarations:
@@ -25,12 +26,12 @@ daObjLndRope_Attr_c const daObjLndRope_c::mAttr = {-5.0f, 0.2f, 0.5f, 0.8f, 0.0f
 static const s8 l_laundryList[13] = {-1, -1, 3, 0, -1, 1, 1, -1, 2, 2, -1, 2, -1};
 
 u8 daObjLndRope_c::getPathId() {
-    return fopAcM_GetParamBit(this, 8, 8);;
+    return fopAcM_GetParamBit(this, 8, 8);
 }
 
 /* 80C521F8-80C52820 000078 0628+00 1/1 0/0 0/0 .text            create_init__14daObjLndRope_cFv */
 void daObjLndRope_c::create_init() {
-    gravity = mAttr.mGravity;
+    gravity = attr()->mGravity;
     dPath* roomPath = dPath_GetRoomPath(getPathId(), fopAcM_GetRoomNo(this));
 
     dPnt* pathPoints = roomPath->m_points;
@@ -39,11 +40,10 @@ void daObjLndRope_c::create_init() {
     current.pos = (startPoint + endPoint) * 0.5f;
     mPos = endPoint - startPoint;
 
-    if (fabsf(mPos.x) > fabsf(mPos.z)){
+    if (fabsf(mPos.x) > fabsf(mPos.z)) {
         mScale = 1.0f / mPos.x;
         mFlag = 1;
-    }
-    else{
+    } else {
         mScale = 1.0f / mPos.z;
         mFlag = 0;
     }
@@ -52,18 +52,18 @@ void daObjLndRope_c::create_init() {
     shape_angle.x = cM_atan2s(-mPos.y, mPos.absXZ());
     f32 cullSizeBoxZ = mPos.absXZ() * 0.5f + 3.0f;
     fopAcM_setCullSizeBox(this, -75.0f, -200.0f, -cullSizeBoxZ, 75.0f, 50.0f, cullSizeBoxZ);
-    
+
     f32 totalLength = mPos.abs();
     const f32 segmentFraction = 1.0f / 14.0f;
     mSegmentLength = totalLength * segmentFraction;
-    cXyz segmentOffset = mPos * (mSegmentLength / totalLength);    
+    cXyz segmentOffset = mPos * (mSegmentLength / totalLength);
 
     cXyz* ropePosition = mRopeMat.getPos(0);
     cXyz* loopPos = &mPositions2[0];
-    *ropePosition = startPoint;    
+    *ropePosition = startPoint;
     ropePosition++;
-    
-    for (int i = 1; i < 14; i++){
+
+    for (int i = 1; i < 14; i++) {
         *ropePosition = *(ropePosition - 1) + segmentOffset;
         *loopPos = *ropePosition;
         ropePosition++;
@@ -73,22 +73,20 @@ void daObjLndRope_c::create_init() {
     *ropePosition = endPoint;
     setNormalRopePos();
     initBaseMtx();
-    
+
     fpc_ProcID* procId = &mProcIds[0];
     ropePosition = mRopeMat.getPos(0) + 1;
     const s8* laundryEntry = &l_laundryList[0];
-    
-    for (int i = 1; i < 14; i++){
-        if (*laundryEntry == -1){
+
+    for (int i = 1; i < 14; i++) {
+        if (*laundryEntry == -1) {
             *procId = -1;
-        }
-        else{
+        } else {
             *procId = fopAcM_createChild(
-                PROC_Obj_Laundry,
-                fopAcM_GetID(this),
-                *laundryEntry,
-                ropePosition,
-                fopAcM_GetRoomNo(this), &csXyz(0, cLib_targetAngleY(ropePosition, ropePosition+1) + 0x4000, -cLib_targetAngleX(ropePosition, ropePosition+1)),
+                PROC_Obj_Laundry, fopAcM_GetID(this), *laundryEntry, ropePosition,
+                fopAcM_GetRoomNo(this),
+                &csXyz(0, cLib_targetAngleY(ropePosition, ropePosition + 1) + 0x4000,
+                       -cLib_targetAngleX(ropePosition, ropePosition + 1)),
                 NULL, -1, 0);
         }
 
@@ -114,87 +112,85 @@ void daObjLndRope_c::setBaseMtx() {
 /* 80C53364-80C533A4 000060 0040+00 0/0 0/0 0/0 .rodata          ccSphSrc$3837 */
 const static dCcD_SrcSph ccSphSrc = {
     {
-        {0x0, {{0x0, 0x0, 0x0}, {0x1607a, 0x11}, 0x149}}, // mObj
-        {dCcD_SE_NONE, 0x0, 0x0, 0x0, 0x0}, // mGObjAt
-        {dCcD_SE_NONE, 0x0, 0x0, 0x0, 0x4}, // mGObjTg
-        {0x0}, // mGObjCo
-    }, // mObjInf
+        {0x0, {{0x0, 0x0, 0x0}, {0x1607a, 0x11}, 0x149}},  // mObj
+        {dCcD_SE_NONE, 0x0, 0x0, 0x0, 0x0},                // mGObjAt
+        {dCcD_SE_NONE, 0x0, 0x0, 0x0, 0x4},                // mGObjTg
+        {0x0},                                             // mGObjCo
+    },                                                     // mObjInf
     {
-        {{0.0f, 0.0f, 0.0f}, 0.0f} // mSph
-    } // mSphAttr
+        {{0.0f, 0.0f, 0.0f}, 0.0f}  // mSph
+    }  // mSphAttr
 };
 
 cXyz* daObjLndRope_c::getRopeStartPos() {
     return mRopeMat.getPos(0);
 }
 
-float daObjLndRope_c::getStartRate(cXyz* i_ropeSegmentPos) {
-    if (mFlag != NULL){
+f32 daObjLndRope_c::getStartRate(cXyz* i_ropeSegmentPos) {
+    if (mFlag != NULL) {
         return mScale * (i_ropeSegmentPos->x - getRopeStartPos()->x);
-    }
-    else{
+    } else {
         return mScale * (i_ropeSegmentPos->z - getRopeStartPos()->z);
     }
 }
 
 /* 80C528E0-80C52CC8 000760 03E8+00 2/2 0/0 0/0 .text setNormalRopePos__14daObjLndRope_cFv */
 void daObjLndRope_c::setNormalRopePos() {
-    cXyz* ropePos = mRopeMat.getPos(0)+1;
+    cXyz* ropePos = mRopeMat.getPos(0) + 1;
     cXyz* pos1 = mPositions1;
     cXyz* pos2 = mPositions2;
 
     cXyz posModifier;
     cXyz windPow = dKyw_get_AllWind_vecpow(&current.pos);
-    
-    if (cM_rnd() < 0.1f){
+
+    if (cM_rnd() < 0.1f) {
         windPow *= mSegmentLength * attr()->field_0x4;
-    }
-    else{
+    } else {
         windPow.zero();
     }
-    
+
     int i;
-    for (i = 1; i < 14;){
+    for (i = 1; i < 14;) {
         ropePos->y = pos2->y;
         posModifier = *ropePos - *(ropePos - 1);
         posModifier.y += gravity;
         posModifier += *pos1;
-        
-        if (cM_rnd() < 0.6f){
+
+        if (cM_rnd() < 0.6f) {
             posModifier += windPow;
         }
-        
+
         posModifier.normalizeZP();
         *ropePos = *(ropePos - 1) + (posModifier * mSegmentLength);
-        
+
         i++;
         ropePos++;
-        pos2++; 
+        pos2++;
         pos1++;
-    }    
+    }
 
     ropePos = mRopeMat.getPos(0) + 13;
     pos1 = &mPositions1[12];
-    for (i= 13; i > 0;){
+    for (i = 13; i > 0;) {
         posModifier = *ropePos - *(ropePos + 1);
         posModifier.y += gravity;
         posModifier += *pos1;
         posModifier.normalizeZP();
         *ropePos = *(ropePos + 1) + (posModifier * mSegmentLength);
-        
+
         i--;
         ropePos--;
         pos1--;
     }
-    
+
     ropePos = mRopeMat.getPos(0) + 1;
     pos1 = mPositions1;
     pos2 = mPositions2;
     cXyz* current = mRopeMat.getPos(0);
-    
-    for (i = 1; i < 14;){
+
+    for (i = 1; i < 14;) {
         f32 adjustedRopeY = current->y + mPos.y * getStartRate(ropePos);
-                
+
         *pos1 = (*ropePos - *pos2) * attr()->field_0x8;
         *pos2 = *ropePos;
         ropePos->y += attr()->field_0xc * (adjustedRopeY - ropePos->y);
@@ -207,13 +203,13 @@ void daObjLndRope_c::setNormalRopePos() {
 
     ropePos = mRopeMat.getPos(0) + 1;
     fpc_ProcID* points = mProcIds;
-    for (i = 1; i < 14;){
+    for (i = 1; i < 14;) {
         if (*points != -1) {
-            fopAc_ac_c* actor = (fopAc_ac_c*) fpcM_SearchByID(*points);
-            if (actor != NULL){
+            fopAc_ac_c* actor = (fopAc_ac_c*)fpcM_SearchByID(*points);
+            if (actor != NULL) {
                 cXyz* actorPos = fopAcM_GetPosition_p(actor);
                 csXyz* actorAngle = fopAcM_GetShapeAngle_p(actor);
-                cXyz temp = *(ropePos + 1) - *ropePos;            
+                cXyz temp = *(ropePos + 1) - *ropePos;
                 *actorPos = *ropePos;
                 actorAngle->y = cLib_targetAngleY(ropePos, ropePos + 1) + 0x4000;
                 actorAngle->z = -cLib_targetAngleX(ropePos, ropePos + 1);
@@ -230,14 +226,14 @@ void daObjLndRope_c::setNormalRopePos() {
 static char* l_arcName = "Obj_SRope";
 
 int daObjLndRope_c::createHeap() {
-    ResTIMG* resTimg = (ResTIMG*) dComIfG_getObjectRes(l_arcName, "Obj_ItaRope.bti");
+    ResTIMG* resTimg = (ResTIMG*)dComIfG_getObjectRes(l_arcName, "Obj_ItaRope.bti");
     int result = mRopeMat.init(1, 15, resTimg, 1);
-    if (result == 0){
+    if (result == 0) {
         return 0;
     }
 
     f32* ropeSize = mRopeMat.getSize(0);
-    for (int i = 0; i < 15; i++){
+    for (int i = 0; i < 15; i++) {
         *ropeSize = 2.0f;
         ropeSize++;
     }
@@ -273,7 +269,7 @@ static int daObjLndRope_Draw(daObjLndRope_c* i_this) {
 }
 
 int daObjLndRope_c::execute() {
-    gravity = mAttr.mGravity;
+    gravity = attr()->mGravity;
     setNormalRopePos();
     return 1;
 }
@@ -284,8 +280,8 @@ static int daObjLndRope_Execute(daObjLndRope_c* i_this) {
 }
 
 /* 80C52E34-80C52E3C 000CB4 0008+00 1/0 0/0 0/0 .text daObjLndRope_IsDelete__FP14daObjLndRope_c */
-static bool daObjLndRope_IsDelete(daObjLndRope_c* i_this) {
-    return true;
+static int daObjLndRope_IsDelete(daObjLndRope_c* i_this) {
+    return 1;
 }
 
 daObjLndRope_c::~daObjLndRope_c() {
@@ -321,27 +317,25 @@ static int daObjLndRope_Create(fopAc_ac_c* i_this) {
 /* ############################################################################################## */
 /* 80C533D4-80C533F4 -00001 0020+00 1/0 0/0 0/0 .data            l_daObjLndRope_Method */
 static actor_method_class l_daObjLndRope_Method = {
-    (process_method_func)daObjLndRope_Create,
-    (process_method_func)daObjLndRope_Delete,
-    (process_method_func)daObjLndRope_Execute,
-    (process_method_func)daObjLndRope_IsDelete,
+    (process_method_func)daObjLndRope_Create,  (process_method_func)daObjLndRope_Delete,
+    (process_method_func)daObjLndRope_Execute, (process_method_func)daObjLndRope_IsDelete,
     (process_method_func)daObjLndRope_Draw,
 };
 
 /* 80C533F4-80C53424 -00001 0030+00 0/0 0/0 1/0 .data            g_profile_Obj_LndRope */
 extern actor_process_profile_definition g_profile_Obj_LndRope = {
-  fpcLy_CURRENT_e,        // mLayerID
-  7,                      // mListID
-  fpcPi_CURRENT_e,        // mListPrio
-  PROC_Obj_LndRope,       // mProcName
-  &g_fpcLf_Method.base,  // sub_method
-  sizeof(daObjLndRope_c), // mSize
-  0,                      // mSizeOther
-  0,                      // mParameters
-  &g_fopAc_Method.base,   // sub_method
-  34,                     // mPriority
-  &l_daObjLndRope_Method, // sub_method
-  0x00040180,             // mStatus
-  fopAc_ACTOR_e,          // mActorType
-  fopAc_CULLBOX_CUSTOM_e, // cullType
+    fpcLy_CURRENT_e,         // mLayerID
+    7,                       // mListID
+    fpcPi_CURRENT_e,         // mListPrio
+    PROC_Obj_LndRope,        // mProcName
+    &g_fpcLf_Method.base,    // sub_method
+    sizeof(daObjLndRope_c),  // mSize
+    0,                       // mSizeOther
+    0,                       // mParameters
+    &g_fopAc_Method.base,    // sub_method
+    34,                      // mPriority
+    &l_daObjLndRope_Method,  // sub_method
+    0x00040180,              // mStatus
+    fopAc_ACTOR_e,           // mActorType
+    fopAc_CULLBOX_CUSTOM_e,  // cullType
 };
