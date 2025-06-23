88f57073e5fbb2571aff2c1976e0ebf6b084ea3f
TakaRikka <takarikka@outlook.com>
2025-06-23 01:06:58 -0700

    obj_dust

--- src/d/actor/d_a_obj_dust.cpp
+++ src/d/actor/d_a_obj_dust.cpp
@@ -5,50 +5,45 @@
 
 #include "d/actor/d_a_obj_dust.h"
 
-/* 80BE2F98-80BE2FA0 000000 0004+04 4/4 0/0 0/0 .rodata          @3673 */
-static u8 const lit_3673[4+ 4 /* padding */] = {
-    0x00,
-    0x00,
-    0x00,
-    0x00,
-    /* padding */
-    0x00,
-    0x00,
-    0x00,
-    0x00,
-};
-
 /* 80BE3054-80BE3058 -00001 0004+00 3/3 0/0 0/0 .data            l_arcName */
 static const char* l_arcName = "M_Dust";
 
 namespace {
-    /* 80BE30F8-80BE30FC 000000 0004+00 2/2 0/0 0/0 .bss             e_ymb__26@unnamed@d_a_obj_dust_cpp@ */
-    static fopAc_ac_c* e_ymb;
+/* 80BE30F8-80BE30FC 000000 0004+00 2/2 0/0 0/0 .bss             e_ymb__26@unnamed@d_a_obj_dust_cpp@ */
+static fopAc_ac_c* e_ymb;
 
-    /* 80BE30FC-80BE3100 000004 0004+00 2/2 0/0 0/0 .bss e_ymb_Pos__26@unnamed@d_a_obj_dust_cpp@ */
-    static cXyz* e_ymb_Pos;
+/* 80BE30FC-80BE3100 000004 0004+00 2/2 0/0 0/0 .bss e_ymb_Pos__26@unnamed@d_a_obj_dust_cpp@ */
+static cXyz* e_ymb_Pos;
 };
 
-/* 80BE2490-80BE24F8 000250 0068+00 1/1 0/0 0/0 .text            RideOn_Angle__11daObjDust_cFRsfsf */
-void daObjDust_c::RideOn_Angle(s16& i_angle, f32 i_param1, s16 i_param2, f32 i_param3) {
-    cLib_addCalcAngleS(&i_angle, i_param2 * (i_param1 / i_param3), 1, 0x100, 1);
+static f32 dummyFloat() {
+    return 0.0f;
+}
+
+static f64 dummyFloat2() {
+    return 4503601774854144.0;
 }
 
 /* 80BE22B8-80BE2490 000078 01D8+00 1/1 0/0 0/0 .text            Search_Ymb__11daObjDust_cFv */
 void daObjDust_c::Search_Ymb() {
-    cXyz ymb_delta(e_ymb_Pos->x - current.pos.x, e_ymb_Pos->y - current.pos.y, e_ymb_Pos->z - current.pos.z);
+    f32 e_ymb_speed = 0.0f;
+    cXyz ymb_rel_dist(e_ymb_Pos->x - current.pos.x, e_ymb_Pos->y - current.pos.y, e_ymb_Pos->z - current.pos.z);
 
     mDoMtx_stack_c::YrotS(-shape_angle.y);
-    mDoMtx_stack_c::multVec(&ymb_delta, &ymb_delta);
+    mDoMtx_stack_c::multVec(&ymb_rel_dist, &ymb_rel_dist);
 
-    if (ymb_delta.x < 750.0f && ymb_delta.x > -750.0f && ymb_delta.z < 450.0f && ymb_delta.z > -450.0f && ymb_delta.y < 600.0f) {
-        f32 speed = fopAcM_GetSpeedF(e_ymb);
-        if (speed > 0) {
-            field_0x5ac = 256.0f + speed * 16.0f;
-            field_0x5a0 = speed * 31.0f;
+    if (ymb_rel_dist.x < (750.0f + BREG_F(0)) && ymb_rel_dist.x > (-750.0f - BREG_F(0)) &&
+        ymb_rel_dist.z < (450.0f + BREG_F(1)) && ymb_rel_dist.z > (-450.0f - BREG_F(1)) &&
+        ymb_rel_dist.y < (600.0f + BREG_F(7)))
+    {
+        e_ymb_speed = fopAcM_GetSpeedF(e_ymb);
+        if (e_ymb_speed > 0.0f) {
+            mRideAngleAmplitude = 256.0f + (e_ymb_speed * (16.0f + BREG_F(6)));
+            mRideOscillationSpeed = e_ymb_speed * (31.0f + BREG_F(5));
 
-            cLib_addCalc(&field_0x5d0, -100.0f, 0.5f, 100.0f, 0);
-            if (mYmbFlag == NULL && mRideOnFlag == NULL && field_0x5a0 > 300.0f && field_0x5a0 < 1900.0f) {
+            cLib_addCalc(&mOffsetY, -100.0f + BREG_F(8), 0.5f, 100.0f, 0);
+
+            if (mYmbFlag == false && mRideOnFlag == true && mRideOscillationSpeed > 300.0f && mRideOscillationSpeed < 1900.0f) {
                 fopAcM_seStart(this, Z2SE_OBJ_FLOATBOARD_SWING, 0);
                 mYmbFlag = true; 
             }
@@ -58,34 +53,44 @@ void daObjDust_c::Search_Ymb() {
     }
 }
 
+/* 80BE2490-80BE24F8 000250 0068+00 1/1 0/0 0/0 .text            RideOn_Angle__11daObjDust_cFRsfsf */
+void daObjDust_c::RideOn_Angle(s16& o_angle, f32 i_value, s16 i_scale, f32 i_max) {
+    cLib_addCalcAngleS(&o_angle, i_scale * (i_value / i_max), 1, 0x100, 1);
+}
+
 /* 80BE24F8-80BE2708 0002B8 0210+00 1/1 0/0 0/0 .text            Check_RideOn__11daObjDust_cFv */
 int daObjDust_c::Check_RideOn() {
     daPy_py_c* player = daPy_getPlayerActorClass();
-    cXyz* player_pos = fopAcM_GetPosition_p(player);
-    s16 target = 0;
+    cXyz& player_pos = fopAcM_GetPosition(player);
+    f32 player_speed = 0.0f;
+    s16 intensity_target = 0;
+
     mRideOnFlag = false;
 
-    cXyz delta_vec(player_pos->x -current.pos.x, player_pos->y - current.pos.y, player_pos->z - current.pos.z);   
+    cXyz player_rel_dist(player_pos.x - current.pos.x, player_pos.y - current.pos.y, player_pos.z - current.pos.z);   
     mDoMtx_stack_c::YrotS(-shape_angle.y);
-    mDoMtx_stack_c::multVec(&delta_vec, &delta_vec);
+    mDoMtx_stack_c::multVec(&player_rel_dist, &player_rel_dist);
 
-    if (delta_vec.x < 750.0f && delta_vec.x > -750.0f && delta_vec.z < 450.0f && delta_vec.z > -450.0f) {
-        RideOn_Angle(field_0x5c8, (s16)-delta_vec.x, 0x200, 750.0);
-        RideOn_Angle(field_0x5cc, delta_vec.z, 0x200, 450.0);
+    if (player_rel_dist.x < 750.0f && player_rel_dist.x > -750.0f && player_rel_dist.z < 450.0f && player_rel_dist.z > -450.0f) {
+        RideOn_Angle(mRideAngle.x, (s16)-player_rel_dist.x, 0x200, 750.0);
+        RideOn_Angle(mRideAngle.z, player_rel_dist.z, 0x200, 450.0);
+        f32 var_f30 = 0.0f;
         mRideOnFlag = true;
         
-        if (fopAcM_GetSpeedF(player) > 0.0f) {
-            cLib_addCalcAngleS(&field_0x5ac, 0x150, 0xb, 0x100, 0);
-            target = 0x400;            
+        player_speed = fopAcM_GetSpeedF(player);
+        if (player_speed > 0.0f) {
+            cLib_addCalcAngleS(&mRideAngleAmplitude, 0x150, 11, 0x100, 0);
+            intensity_target = 0x400;            
         } else {
-            cLib_addCalcAngleS(&field_0x5ac, 0x100, 0xb, 0x100, 0);            
+            cLib_addCalcAngleS(&mRideAngleAmplitude, 0x100, 11, 0x100, 0);            
         }
     } else {
-        cLib_addCalcAngleS(&field_0x5ac, 0x100, 0xb, 0x100, 0);
-        cLib_addCalcAngleS(&field_0x5c8, 0, 2, 0x100, 1);
-        cLib_addCalcAngleS(&field_0x5cc, 0, 2, 0x100, 1);
+        cLib_addCalcAngleS(&mRideAngleAmplitude, 0x100, 11, 0x100, 0);
+        cLib_addCalcAngleS(&mRideAngle.x, 0, 2, 0x100, 1);
+        cLib_addCalcAngleS(&mRideAngle.z, 0, 2, 0x100, 1);
     }
-    cLib_addCalc(&field_0x5a4, target, 0.05, 100.0f, 0.0f);
+
+    cLib_addCalc(&mRideIntensity, intensity_target, 0.05f + HREG_F(13), 100.0f, 0.0f);
     return 0;
 }
 
@@ -106,9 +111,12 @@ void daObjDust_c::setBaseMtx() {
 
 /* 80BE2790-80BE27BC 000550 002C+00 1/1 0/0 0/0 .text
  * rideCallBack__FP4dBgWP10fopAc_ac_cP10fopAc_ac_c              */
-static void rideCallBack(dBgW*, fopAc_ac_c* i_bgActor, fopAc_ac_c* i_rideActor) {
-    if (std::abs(i_rideActor->current.pos.y - i_rideActor->old.pos.y) > 30.0f) {
-        static_cast<daObjDust_c*>(i_bgActor)->field_0x5a8 = i_rideActor->current.pos.y - i_rideActor->old.pos.y;
+static void rideCallBack(dBgW* i_bgw, fopAc_ac_c* i_bgActor, fopAc_ac_c* i_rideActor) {
+    fopAc_ac_c* ride_actor = (fopAc_ac_c*)i_rideActor;
+    daObjDust_c* a_this = (daObjDust_c*)i_bgActor;
+
+    if (std::abs(ride_actor->current.pos.y - ride_actor->old.pos.y) > 30.0f) {
+        a_this->mRideBounce = ride_actor->current.pos.y - ride_actor->old.pos.y;
     }
 }
 
@@ -126,13 +134,13 @@ static int daObjDust_Execute(daObjDust_c* i_this) {
 /* 80BE2808-80BE2810 0005C8 0008+00 1/0 0/0 0/0 .text            daObjDust_IsDelete__FP11daObjDust_c
  */
 static int daObjDust_IsDelete(daObjDust_c* i_this) {
-    return true;
+    return 1;
 }
 
 /* 80BE2810-80BE2834 0005D0 0024+00 1/0 0/0 0/0 .text            daObjDust_Delete__FP11daObjDust_c
  */
 static int daObjDust_Delete(daObjDust_c* i_this) {
-    fopAcM_GetID(i_this);
+    fpc_ProcID id = fopAcM_GetID(i_this);
     i_this->MoveBGDelete();
     return 1;
 }
@@ -140,20 +148,19 @@ static int daObjDust_Delete(daObjDust_c* i_this) {
 int daObjDust_c::create() {
     fopAcM_SetupActor(this, daObjDust_c);
 
-    int phaseStep = dComIfG_resLoad(&mPhaseReq, l_arcName);
-    if (phaseStep == cPhs_COMPLEATE_e) {
-        phaseStep = MoveBGCreate(
-            l_arcName,
-            dComIfG_getObjctResName2Index(l_arcName, "M_FloatingDust01.dzb"),
-            dBgS_MoveBGProc_TypicalRotY,
-            0x1060,
-            NULL);
-        if (phaseStep == cPhs_ERROR_e) {
-            return phaseStep;
+    int phase_state = dComIfG_resLoad(&mPhaseReq, l_arcName);
+    if (phase_state == cPhs_COMPLEATE_e) {
+        int dzb_id = dComIfG_getObjctResName2Index(l_arcName, "M_FloatingDust01.dzb");
+        JUT_ASSERT(116, dzb_id != -1);
+
+        phase_state = MoveBGCreate(l_arcName, dzb_id, dBgS_MoveBGProc_TypicalRotY, 0x1060, NULL);
+        if (phase_state == cPhs_ERROR_e) {
+            return phase_state;
         }
     }
 
     fopAcM_setCullSizeBox(this, -200.0f, -50.0f, -200.0f, 200.0f, 50.0f, 200.0f);
+
     mAcch.Set(
         fopAcM_GetPosition_p(this),
         fopAcM_GetOldPosition_p(this),
@@ -165,25 +172,28 @@ int daObjDust_c::create() {
         NULL);
     mAcchCir.SetWall(100.0f, 200.0f);
     mAcch.CrrPos(dComIfG_Bgsp());
-    current.pos.y = -18850.0f;
-    return phaseStep;
+    current.pos.y = -18850.0f + HREG_F(0) + oREG_F(0);
+
+    return phase_state;
 }
 
 /* 80BE2834-80BE2A00 0005F4 01CC+00 1/0 0/0 0/0 .text            daObjDust_Create__FP10fopAc_ac_c */
 static int daObjDust_Create(fopAc_ac_c* i_this) {
     daObjDust_c* a_this = (daObjDust_c*)i_this;
+    fpc_ProcID id = fopAcM_GetID(i_this);
     return a_this->create();
 }
 
 /* 80BE2A70-80BE2AE8 000830 0078+00 1/0 0/0 0/0 .text            CreateHeap__11daObjDust_cFv */
 int daObjDust_c::CreateHeap() {
     J3DModelData* modelData = (J3DModelData*)dComIfG_getObjectRes(l_arcName, "M_FloatingDust01.bmd");
-    JUT_ASSERT(82, modelData != 0);
+    JUT_ASSERT(86, modelData != 0);
 
     mpModel = mDoExt_J3DModel__create(modelData, 0x80000, 0x11000084);
     if (mpModel == NULL) {
         return 0;
     }
+
     return 1;
 }
 
@@ -199,40 +209,35 @@ int daObjDust_c::Create() {
 // NONMATCHING somehow this needs to not inline TSinCosTable<13,f32>::sinShort
 int daObjDust_c::Execute(Mtx** i_mtx) {
     fopAcM_seStartLevel(this, Z2SE_OBJ_FLOATBOARD, 0);
-    daPy_py_c* pdVar = daPy_getPlayerActorClass();
-    fopAcM_GetPosition(pdVar);
-    fopAcM_GetSpeed(pdVar);
+    daPy_py_c* player = daPy_getPlayerActorClass();
+    cXyz& player_pos = fopAcM_GetPosition(player);
+    cXyz& player_speed = fopAcM_GetSpeed(player);
 
-    if (fopAcM_SearchByName(PROC_E_YMB, &e_ymb) != 0 && e_ymb != NULL) {
-        e_ymb_Pos = fopAcM_GetPosition_p(e_ymb);
+    if (fopAcM_SearchByName(PROC_E_YMB, &e_ymb) && e_ymb != NULL) {
+        e_ymb_Pos = &fopAcM_GetPosition(e_ymb);
     }
    
-    field_0x5c4 += field_0x5a4 * 2.0f + 768.0f + field_0x5a0 * 2.0f;
-    field_0x5c6 += field_0x5a4 * 2.0f + 768.0f + field_0x5a0 * 2.0f;
+    mRideOscillationPhaseX += 768.0f + TREG_F(1) + ((2.0f + HREG_F(1)) * mRideIntensity) + ((2.0f + BREG_F(3)) * mRideOscillationSpeed);
+    mRideOscillationPhaseZ += 768.0f + TREG_F(2) + ((2.0f + HREG_F(2)) * mRideIntensity) + ((2.0f + BREG_F(3)) * mRideOscillationSpeed);
     Check_RideOn();
 
     if (e_ymb != NULL) {
         Search_Ymb();
     }
 
-    cXyz water_pos(current.pos.x, current.pos.y + 300.0f, current.pos.z);
-    f32 water_f32;
-    if(fopAcM_wt_c::waterCheck(&water_pos)) {
-        water_f32 = fopAcM_wt_c::getWaterY() - 40.0f;
+    cXyz water_check_pos(current.pos.x, current.pos.y + 300.0f, current.pos.z);
+    f32 height;  //! @bug: uninitialized value. if the waterCheck fails, this causes the platform height to chase to an unintended height, usually y-0
+    if (fopAcM_wt_c::waterCheck(&water_check_pos)) {
+        height = fopAcM_wt_c::getWaterY() - 40.0f;
     }
 
-    cLib_addCalc(&current.pos.y, 
-        water_f32 + cM_ssin(field_0x5c6) * 20.0f + ((field_0x5d0 + 80.0f) + field_0x5a8), 
-        0.1f, 15.0f, 0.1f);
-    cLib_addCalcAngleS(&shape_angle.x,
-        field_0x5c8 + field_0x5ac * cM_ssin(field_0x5c4 + 0x2000), 
-        2, 0x1000, 1);
-    cLib_addCalcAngleS(&shape_angle.z,
-        field_0x5cc + field_0x5ac * cM_ssin(field_0x5c6),
-        2, 0x1000, 1);
-    cLib_chaseF(&field_0x5a8, 0.0f, 1.0f);
-    cLib_addCalc(&field_0x5a0, 0.0f, 0.01f, 100.0f, 0.0f);
-    cLib_addCalc(&field_0x5d0, 0.0f, 0.1f, 100.0f, 0.0f);
+    cLib_addCalc(&current.pos.y, mOffsetY + (HREG_F(0) + (80.0f + height + mRideBounce) + (20.0f * cM_ssin(mRideOscillationPhaseZ))), 0.1f, 15.0f, 0.1f);
+    cLib_addCalcAngleS(&shape_angle.x, mRideAngle.x + ((1.0f + nREG_F(0)) * mRideAngleAmplitude * cM_ssin(mRideOscillationPhaseX + oREG_S(1) + 0x2000)), 2, 0x1000, 1);
+    cLib_addCalcAngleS(&shape_angle.z, mRideAngle.z + ((1.0f + nREG_F(1)) * mRideAngleAmplitude * cM_ssin(mRideOscillationPhaseZ)), 2, 0x1000, 1);
+
+    cLib_chaseF(&mRideBounce, 0.0f, 1.0f);
+    cLib_addCalc(&mRideOscillationSpeed, 0.0f, 0.01f + HREG_F(13), 100.0f, 0.0f);
+    cLib_addCalc(&mOffsetY, 0.0f, 0.1f, 100.0f, 0.0f);
 
     fopAcM_posMoveF(this, mStts.GetCCMoveP());
 
