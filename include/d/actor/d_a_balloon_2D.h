541501d7811b9d54aa03e8999921175831f86e6a
Max Roncace <me@caseif.net>
2025-07-19 19:20:19 +0000

    d_a_balloon equivalent (#2542)

--- include/d/actor/d_a_balloon_2D.h
+++ include/d/actor/d_a_balloon_2D.h
@@ -13,6 +13,75 @@ class J2DPicture;
 class J2DPane;
 class J2DTextBox;
 
+class daBalloon2D_HIO_c : public mDoHIO_entry_c {
+public:
+    inline daBalloon2D_HIO_c();
+    /* 806553A8 */ virtual ~daBalloon2D_HIO_c();
+
+#if DEBUG
+    void genMessage(JORMContext*);
+
+#endif
+
+    void resetAddScoreSetFlag();
+
+    struct param {
+        /* 0x04 */ u8 mAdjustmentToggle;
+        /* 0x08 */ s32 mScore;
+        /* 0x0C */ u8 mComboCount;
+        /* 0x0D */ u8 mBalloonSize;
+        /* 0x0E */ u8 mOverheadScoreToggle;
+        /* 0x10 */ s32 mOverheadScore;
+        /* 0x14 */ s32 field_0x14;
+        /* 0x18 */ f32 field_0x1c;
+        /* 0x1C */ f32 field_0x20;
+        /* 0x20 */ f32 mScorePosX;
+        /* 0x24 */ f32 mScorePosY;
+        /* 0x28 */ f32 mScoreSize;
+        /* 0x2C */ f32 m5DNumberPosX;
+        /* 0x30 */ f32 m5DNumberPosY;
+        /* 0x34 */ f32 m5DNumberSizeX;
+        /* 0x38 */ f32 m5DNumberSizeY;
+        /* 0x3C */ f32 m5DNumberAlpha;
+        /* 0x40 */ f32 m5DNumber2PosX;
+        /* 0x44 */ f32 m5DNumber2PosY;
+        /* 0x48 */ f32 m5DNumber2Size;
+        /* 0x4C */ f32 mUnkPosX;
+        /* 0x50 */ f32 mUnkPosY;
+        /* 0x54 */ f32 mUnkSize;
+        /* 0x58 */ f32 mComboPosX;
+        /* 0x5C */ f32 mComboPosY;
+        /* 0x60 */ f32 mComboSize;
+        /* 0x64 */ f32 m2DNumberPosX;
+        /* 0x68 */ f32 m2DNumberPosY;
+        /* 0x6C */ f32 m2DNumberSizeX;
+        /* 0x70 */ f32 m2DNumberSizeY;
+        /* 0x74 */ f32 field_0x78;
+        /* 0x78 */ f32 m2DNumberComboPosX;
+        /* 0x7C */ f32 m2DNumberComboPosY;
+        /* 0x80 */ f32 m2DNumberComboSize;
+        /* 0x84 */ f32 m2DNumberComboAlpha;
+        /* 0x88 */ f32 mBalloonLargePosX;
+        /* 0x8C */ f32 mBalloonLargePosY;
+        /* 0x90 */ f32 mBalloonLargeSize;
+        /* 0x94 */ f32 mBalloonLargeAlpha;
+        /* 0x98 */ f32 mBalloonMedPosX;
+        /* 0x9C */ f32 mBalloonMedPosY;
+        /* 0xA0 */ f32 mBalloonMedSize;
+        /* 0xA4 */ f32 mBalloonMedAlpha;
+        /* 0xA8 */ f32 mBalloonSmallPosX;
+        /* 0xAC */ f32 mBalloonSmallPosY;
+        /* 0xB0 */ f32 mBalloonSmallSize;
+        /* 0xB4 */ f32 mBalloonSmallAlpha;
+        /* 0xB8 */ f32 m2DNumberCombo2PosX;
+        /* 0xBC */ f32 m2DNumberCombo2PosY;
+        /* 0xC0 */ f32 m2DNumberCombo2Size;
+        /* 0xC4 */ f32 m2DNumberCombo2PosX43;
+    };
+
+    param m;
+};
+
 /**
  * @ingroup actors-unsorted
  * @class daBalloon2D_c
@@ -29,15 +98,9 @@ public:
         void set(daBalloon2D_c* i_this) { mBalloon = i_this; }
 
         /* 806534CC */ virtual void draw();
-        /* 8065544C */ virtual ~c_list();
+        /* 8065544C */ virtual ~c_list() {}
 
         daBalloon2D_c* mBalloon;
-    };
-
-    class CHeadScore {
-    public:
-        /* 80655304 */ ~CHeadScore();
-        /* 80655340 */ CHeadScore();
     };
 
     daBalloon2D_c() {
@@ -79,6 +142,12 @@ public:
         u8 field_0xf;
     };
 
+    class CHeadScore : public ScoreCount {
+    public:
+        /* 80655304 */ ~CHeadScore();
+        /* 80655340 */ CHeadScore();
+    };
+
     /* 0x574 */ J2DScreen* mScreen;
     /* 0x578 */ CPaneMgr* field_0x578;
     /* 0x57C */ CPaneMgr* field_0x57c;
@@ -96,63 +165,13 @@ public:
     /* 0x5A4 */ J2DPicture* field_0x5c4[10];
     /* 0x5EC */ J2DPicture* field_0x5ec;
     /* 0x5F0 */ c_list field_0x5f0;
-    /* 0x5F8 */ ScoreCount field_0x5f8[20];
+    /* 0x5F8 */ CHeadScore field_0x5f8[20];
     /* 0x738 */ f32 mAllAlpha;
     /* 0x73C */ f32 mComboAlpha;
     /* 0x740 */ u32 mScoreCount;
     /* 0x744 */ u8 mComboNum;
     /* 0x745 */ u8 mBalloonSize;
     /* 0x746 */ u8 mIsVisible;
-};
-
-class daBalloon2D_HIO_c /*: public mDoHIO_entry_c */ {
-public:
-    /* 806553A8 */ /* virtual */ ~daBalloon2D_HIO_c();
-    /* 0x00 */ u32 field_0x00;
-    /* 0x04 */ u8 field_0x04;
-    /* 0x00 */ u8 field_0x05[0x20 - 5];
-    /* 0x20 */ f32 field_0x20;
-    /* 0x24 */ f32 field_0x24;
-    /* 0x28 */ f32 field_0x28;
-    /* 0x2C */ f32 field_0x2c;
-    /* 0x30 */ f32 field_0x30;
-    /* 0x34 */ f32 field_0x34;
-    /* 0x38 */ f32 field_0x38;
-    /* 0x3C */ f32 field_0x3c;
-    /* 0x40 */ f32 field_0x40;
-    /* 0x44 */ f32 field_0x44;
-    /* 0x48 */ f32 field_0x48;
-    /* 0x4C */ f32 field_0x4c;
-    /* 0x50 */ f32 field_0x50;
-    /* 0x54 */ f32 field_0x54;
-    /* 0x58 */ f32 field_0x58;
-    /* 0x5C */ f32 field_0x5c;
-    /* 0x60 */ f32 field_0x60;
-    /* 0x64 */ f32 field_0x64;
-    /* 0x68 */ f32 field_0x68;
-    /* 0x6C */ f32 field_0x6c;
-    /* 0x70 */ f32 field_0x70;
-    /* 0x74 */ f32 field_0x74;
-    /* 0x78 */ f32 field_0x78;
-    /* 0x7C */ f32 field_0x7c;
-    /* 0x80 */ f32 field_0x80;
-    /* 0x84 */ f32 field_0x84;
-    /* 0x88 */ f32 field_0x88;
-    /* 0x8C */ f32 field_0x8c;
-    /* 0x90 */ f32 field_0x90;
-    /* 0x94 */ f32 field_0x94;
-    /* 0x98 */ f32 field_0x98;
-    /* 0x9C */ f32 field_0x9c;
-    /* 0xA0 */ f32 field_0xa0;
-    /* 0xA4 */ f32 field_0xa4;
-    /* 0xA8 */ f32 field_0xa8;
-    /* 0xAC */ f32 field_0xac;
-    /* 0xB0 */ f32 field_0xb0;
-    /* 0xB4 */ f32 field_0xb4;
-    /* 0xB8 */ f32 field_0xb8;
-    /* 0xBC */ f32 field_0xbc;
-    /* 0xC0 */ f32 field_0xc0;
-    /* 0xC4 */ f32 field_0xc4;
 };
 
 #include "f_op/f_op_actor_mng.h"
