75dd29ab27c5b7f430b2ce9d07dd1c7e2d8bde48
hatal175 <hatal175@users.noreply.github.com>
2025-07-23 14:53:49 +0300

    Work on d_a_obj_toby

--- include/d/actor/d_a_obj_toby.h
+++ include/d/actor/d_a_obj_toby.h
@@ -3,6 +3,32 @@
 
 #include "f_op/f_op_actor_mng.h"
 
+struct yuka_s {
+public:
+    /* 80D154F4 */ ~yuka_s() {}
+    /* 80D15530 */ yuka_s() {}
+
+    /* 0x00 */ J3DModel* mModel1;
+    /* 0x04 */ J3DModel* mModel2;
+    /* 0x08 */ cXyz mPos;
+    /* 0x14 */ cXyz mSpeed;
+    /* 0x20 */ csXyz mAngle;
+    /* 0x26 */ csXyz mAngleSpeed;
+    /* 0x2C */ int field_0x2c;
+    /* 0x30 */ f32 mScale;
+    /* 0x34 */ s8 field_0x34;
+    /* 0x36 */ s16 field_0x36;
+    /* 0x38 */ u8 field_0x38;
+    /* 0x39 */ s8 field_0x39;
+    /* 0x3A */ s8 field_0x3a;
+    /* 0x3C */ int field_0x3c;
+    /* 0x40 */ Mtx mMtx;
+    /* 0x70 */ dBgW* mBgW;
+    /* 0x74 */ int mParticle;
+};
+
+STATIC_ASSERT(sizeof(yuka_s) == 0x78);
+
 /**
  * @ingroup actors-objects
  * @class obj_toby_class
@@ -11,23 +37,44 @@
  * @details
  *
  */
-class obj_toby_class : public fopAc_ac_c {
-private:
-    /* 0x568 */ u8 field_0x568[0x6374 - 0x568];
+class obj_toby_class {
+public:
+    /* 0x0000 */ fopAc_ac_c mBase;
+    /* 0x0568 */ request_of_phase_process_class mPhase;
+    /* 0x0570 */ char* mArcName;
+    /* 0x0574 */ u8 field_0x574;
+    /* 0x0575 */ u8 field_0x575;
+    /* 0x0576 */ u8 field_0x576;
+    /* 0x0577 */ u8 field_0x577;
+    /* 0x0578 */ u8 mSwBit;
+    /* 0x0579 */ u8 field_0x579[0x57c - 0x579];
+    /* 0x057C */ f32 field_0x57c;
+    /* 0x0580 */ s16 field_0x580;
+    /* 0x0582 */ s16 field_0x582;
+    /* 0x0584 */ s16 field_0x584;
+    /* 0x0586 */ s16 field_0x586[2];
+    /* 0x058C */ yuka_s mYukas[200];
+    /* 0x634C */ int mYukaNum;
+    /* 0x6350 */ s16 field_0x6350;
+    /* 0x6352 */ s16 field_0x6352;
+    /* 0x6354 */ cXyz field_0x6354;
+    /* 0x6360 */ cXyz field_0x6360;
+    /* 0x636C */ f32 field_0x636c;
+    /* 0x6370 */ u8 field_0x6370;
+    /* 0x6371 */ u8 field_0x6371;
 };
 
 STATIC_ASSERT(sizeof(obj_toby_class) == 0x6374);
 
-class daObj_Toby_HIO_c {
+class daObj_Toby_HIO_c : public JORReflexible{
 public:
     /* 80D1378C */ daObj_Toby_HIO_c();
-    /* 80D15534 */ ~daObj_Toby_HIO_c();
-};
+    /* 80D15534 */ virtual ~daObj_Toby_HIO_c() {}
 
-struct yuka_s {
-public:
-    /* 80D154F4 */ ~yuka_s();
-    /* 80D15530 */ yuka_s();
+    void genMessage(JORMContext*);
+
+    /* 0x4 */ s8 field_0x4;
+    /* 0x8 */ f32 field_0x8;
 };
 
 #endif /* D_A_OBJ_TOBY_H */
