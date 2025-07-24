53d9f65e0d30be9f142624f0a5d0afa464473ffc
TakaRikka <38417346+TakaRikka@users.noreply.github.com>
2025-07-24 12:45:36 -0700

    e_sm2 equivalent (#2553)
    * e_sm2 equivalent
    
    * fix

--- configure.py
+++ configure.py
@@ -1709,7 +1709,7 @@ config.libs = [
     ActorRel(MatchingFor("GZ2E01"), "d_a_e_sg"),
     ActorRel(NonMatching, "d_a_e_sh"),
     ActorRel(NonMatching, "d_a_e_sm"),
-    ActorRel(NonMatching, "d_a_e_sm2"),
+    ActorRel(Equivalent, "d_a_e_sm2"),  # weak func order
     ActorRel(NonMatching, "d_a_e_st"),
     ActorRel(MatchingFor("GZ2E01"), "d_a_e_st_line"),
     ActorRel(NonMatching, "d_a_e_sw"),
