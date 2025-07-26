79bec70b2ccc6906aaf4e5d38fe56e7d36e38d1f
TakaRikka <takarikka@outlook.com>
2025-07-25 17:42:08 -0700

    e_bs equivalent

--- configure.py
+++ configure.py
@@ -1654,7 +1654,7 @@ config.libs = [
     ActorRel(NonMatching, "d_a_e_bg"),
     ActorRel(NonMatching, "d_a_e_bi"),
     ActorRel(MatchingFor("GZ2E01"), "d_a_e_bi_leaf"),
-    ActorRel(NonMatching, "d_a_e_bs"),
+    ActorRel(Equivalent, "d_a_e_bs"), # weak func order
     ActorRel(Equivalent, "d_a_e_bu"),
     ActorRel(Equivalent, "d_a_e_bug"), # weak func order
     ActorRel(Equivalent, "d_a_e_cr"), # weak func order
