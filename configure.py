aa7eb88afe4dbb4797f51aea01e0eeaab5b7cb92
YunataSavior <yunatasavior@gmail.com>
2025-09-24 23:31:33 -0700

    npc_ne OK

--- configure.py
+++ configure.py
@@ -1867,7 +1867,7 @@ config.libs = [
     ActorRel(MatchingFor("GZ2E01", "GZ2P01", "GZ2J01"), "d_a_npc_moi"),
     ActorRel(NonMatching, "d_a_npc_moir", extra_cflags=[DANPCF_C_HACK]),
     ActorRel(NonMatching, "d_a_npc_myna2"),
-    ActorRel(NonMatching, "d_a_npc_ne"),
+    ActorRel(MatchingFor("GZ2E01"), "d_a_npc_ne"),
     ActorRel(MatchingFor("GZ2E01", "GZ2P01", "GZ2J01"), "d_a_npc_p2"),
     ActorRel(MatchingFor("GZ2E01", "GZ2P01", "GZ2J01"), "d_a_npc_pachi_besu"),
     ActorRel(NonMatching, "d_a_npc_pachi_maro"),
