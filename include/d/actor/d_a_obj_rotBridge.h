40e61bf913152aafe3f2c728102e4d519887cca2
TakaRikka <38417346+TakaRikka@users.noreply.github.com>
2025-09-28 17:40:58 -0700

    rottrap / rotbridge done (#2713)

--- include/d/actor/d_a_obj_rotBridge.h
+++ include/d/actor/d_a_obj_rotBridge.h
@@ -2,6 +2,8 @@
 #define D_A_OBJ_ROTBRIDGE_H
 
 #include "f_op/f_op_actor_mng.h"
+#include "d/d_bg_s_movebg_actor.h"
+#include "m_Do/m_Do_hostIO.h"
 
 /**
  * @ingroup actors-objects
@@ -11,34 +13,51 @@
  * @details
  *
  */
-class daRotBridge_c : public fopAc_ac_c {
+class daRotBridge_c : public dBgS_MoveBgActor {
 public:
     /* 80CBEA24 */ void setBaseMtx();
-    /* 80CBEB38 */ void CreateHeap();
-    /* 80CBECBC */ void create();
-    /* 80CBEF1C */ void rideCallBack(dBgW*, fopAc_ac_c*, fopAc_ac_c*);
-    /* 80CBEF34 */ void Execute(Mtx**);
+    /* 80CBECBC */ int create();
+    /* 80CBEF1C */ static void rideCallBack(dBgW*, fopAc_ac_c*, fopAc_ac_c*);
     /* 80CBF050 */ void playerAreaCheck();
     /* 80CBF1EC */ void moveBridge();
-    /* 80CBF404 */ void Draw();
-    /* 80CBF4BC */ void Delete();
 
-    u8 getPlayerRide() const { return mPlayerRide; }
+    /* 80CBEB38 */ virtual int CreateHeap();
+    /* 80CBEF34 */ virtual int Execute(Mtx**);
+    /* 80CBF404 */ virtual int Draw();
+    /* 80CBF4BC */ virtual int Delete();
+
+    u8 getPlayerRide() const { return mIsPlayerRide; }
     u8 getBridgeAngle() const { return mBridgeAngle; }
-private:
-    /* 0x568 */ u8 field_0x568[0x5af - 0x568];
-    /* 0x5af */ u8 mPlayerRide;
-    /* 0x5b0 */ u8 field_0x5b0[0x5b8 - 0x5b0];
-    /* 0x5b8 */ u8 mBridgeAngle;
-    /* 0x5b9 */ u8 field_0x5b9[0x610 - 0x5b9];
+
+    /* 0x5A0 */ request_of_phase_process_class mPhase;
+    /* 0x5A8 */ J3DModel* mpModel;
+    /* 0x5AC */ u8 mType;
+    /* 0x5AD */ u8 bitSw;
+    /* 0x5AE */ u8 mDoMove;
+    /* 0x5AF */ u8 mIsPlayerRide;
+    /* 0x5B0 */ u8 mIsPlayerInArea;
+    /* 0x5B2 */ csXyz mRotation;
+    /* 0x5B8 */ u8 mBridgeAngle;
+    /* 0x5BC */ mDoExt_bckAnm mBck;
+    /* 0x5D8 */ f32 mBckSpeed;
+    /* 0x5DC */ dBgW* mpBgW2;
+    /* 0x5E0 */ Mtx mMtx;
 };
 
 STATIC_ASSERT(sizeof(daRotBridge_c) == 0x610);
 
-class daRotBridge_HIO_c {
+class daRotBridge_HIO_c : public mDoHIO_entry_c {
 public:
     /* 80CBE98C */ daRotBridge_HIO_c();
-    /* 80CBF5BC */ ~daRotBridge_HIO_c();
+    /* 80CBF5BC */ virtual ~daRotBridge_HIO_c() {}
+
+    void genMessage(JORMContext*);
+
+    /* 0x04 */ u8 shake_power;
+    /* 0x05 */ u8 field_0x5;
+    /* 0x06 */ u8 field_0x6;
+    /* 0x08 */ f32 vibration_range[3];  // ?m, 15m, 9m
+    /* 0x14 */ u8 draw_wall_collision;
 };
 
 
--- include/d/actor/d_a_obj_rotTrap.h
+++ include/d/actor/d_a_obj_rotTrap.h
@@ -2,6 +2,9 @@
 #define D_A_OBJ_ROTTRAP_H
 
 #include "f_op/f_op_actor_mng.h"
+#include "d/d_bg_s_movebg_actor.h"
+#include "d/d_cc_d.h"
+#include "m_Do/m_Do_hostIO.h"
 
 /**
  * @ingroup actors-objects
@@ -11,12 +14,10 @@
  * @details
  *
  */
-class daRotTrap_c : public fopAc_ac_c {
+class daRotTrap_c : public dBgS_MoveBgActor {
 public:
     /* 80CBF940 */ void setBaseMtx();
-    /* 80CBF9D8 */ void CreateHeap();
-    /* 80CBFAC8 */ void create();
-    /* 80CBFEC4 */ void Execute(Mtx**);
+    /* 80CBFAC8 */ int create();
     /* 80CBFF1C */ void procMain();
     /* 80CC00AC */ void init_modeWait();
     /* 80CC0120 */ void modeWait();
@@ -25,22 +26,45 @@ public:
     /* 80CC034C */ void init_modeMove();
     /* 80CC0360 */ void modeMove();
     /* 80CC0440 */ void seSet();
-    /* 80CC0548 */ void Draw();
-    /* 80CC05EC */ void Delete();
 
-    static u8 const mCcDObjInfo[48];
-    static u8 mCcDCyl[68];
+    /* 80CBF9D8 */ virtual int CreateHeap();
+    /* 80CBFEC4 */ virtual int Execute(Mtx**);
+    /* 80CC0548 */ virtual int Draw();
+    /* 80CC05EC */ virtual int Delete();
+
+    int getSw() { return fopAcM_GetParamBit(this, 0, 8); }
+
+    static const dCcD_SrcGObjInf mCcDObjInfo;
+    static dCcD_SrcCyl mCcDCyl;
 
 private:
-    /* 0x568 */ u8 field_0x568[0x173c - 0x568];
+    /* 0x05A0 */ request_of_phase_process_class mPhase;
+    /* 0x05A8 */ J3DModel* mpModel;
+    /* 0x05AC */ u8 mMode;
+    /* 0x05AD */ u8 mSwbit;
+    /* 0x05AE */ u8 field_0x5ae;
+    /* 0x05B0 */ s16 field_0x5b0;
+    /* 0x05B2 */ s16 field_0x5b2;
+    /* 0x05B4 */ dCcD_Stts mCcStts;
+    /* 0x05F0 */ dCcD_Cyl mCcCyl[14];
+    /* 0x1738 */ dBgW* mpBgW2;
 };
 
 STATIC_ASSERT(sizeof(daRotTrap_c) == 0x173c);
 
-class daRotTrap_HIO_c {
+class daRotTrap_HIO_c : public mDoHIO_entry_c {
 public:
     /* 80CBF8AC */ daRotTrap_HIO_c();
-    /* 80CC0728 */ ~daRotTrap_HIO_c();
+    /* 80CC0728 */ virtual ~daRotTrap_HIO_c() {}
+
+    void genMessage(JORMContext*);
+
+    /* 0x4 */ f32 rotate_time;
+    /* 0x8 */ u8 field_0x8;
+    /* 0x9 */ u8 AT_adjust_SW;
+    /* 0xA */ u8 index;
+    /* 0xC */ s16 acc;
+    /* 0xE */ u8 vibration;
 };
 
 
