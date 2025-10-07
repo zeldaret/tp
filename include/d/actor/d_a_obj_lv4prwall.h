418d4b7c36421376032c0f2822be0568205b03ba
TakaRikka <takarikka@outlook.com>
2025-10-06 20:49:19 -0700

    lv4bridge / lv4prwall

--- include/d/actor/d_a_obj_lv4prwall.h
+++ include/d/actor/d_a_obj_lv4prwall.h
@@ -2,6 +2,7 @@
 #define D_A_OBJ_LV4PRWALL_H
 
 #include "f_op/f_op_actor_mng.h"
+#include "d/d_bg_s_movebg_actor.h"
 
 /**
  * @ingroup actors-objects
@@ -11,19 +12,37 @@
  * @details
  *
  */
-class daObjLv4PRwall_c : public fopAc_ac_c {
+class daObjLv4PRwall_c : public dBgS_MoveBgActor, request_of_phase_process_class {
 public:
-    /* 80C68C98 */ void create1st();
+    /* 80C68C98 */ int create1st();
     /* 80C68DD8 */ void setMtx();
-    /* 80C68F20 */ void CreateHeap();
-    /* 80C68FA0 */ void Create();
-    /* 80C690D8 */ void Execute(Mtx**);
-    /* 80C696BC */ void Draw();
-    /* 80C69760 */ void Delete();
-    /* 80C69870 */ ~daObjLv4PRwall_c();
 
-private:
-    /* 0x568 */ u8 field_0x568[0x62c - 0x568];
+    /* 80C68F20 */ virtual int CreateHeap();
+    /* 80C68FA0 */ virtual int Create();
+    /* 80C690D8 */ virtual int Execute(Mtx**);
+    /* 80C696BC */ virtual int Draw();
+    /* 80C69760 */ virtual int Delete();
+    /* 80C69870 */ virtual ~daObjLv4PRwall_c() {}
+    
+    int getType() { return fopAcM_GetParamBit(this, 0, 3); }
+    int getSwNo() { return fopAcM_GetParamBit(this, 3, 8); }
+    int getSw2No() { return fopAcM_GetParamBit(this, 11, 8); }
+    int getSw3No() { return fopAcM_GetParamBit(this, 19, 8); }
+
+    /* 0x5A8 */ Mtx mBgMtx2;
+    /* 0x5D8 */ Mtx mModelMtx;
+    /* 0x608 */ J3DModel* mpModel;
+    /* 0x60C */ s16 field_0x60c;
+    /* 0x60E */ s16 field_0x60e;
+    /* 0x610 */ s16 field_0x610;
+    /* 0x614 */ int mType;
+    /* 0x618 */ u8 field_0x618;
+    /* 0x619 */ u8 field_0x619;
+    /* 0x61C */ f32 field_0x61c;
+    /* 0x620 */ f32 field_0x620;
+    /* 0x624 */ int field_0x624;
+    /* 0x628 */ u8 field_0x628;
+    /* 0x629 */ u8 field_0x629;
 };
 
 STATIC_ASSERT(sizeof(daObjLv4PRwall_c) == 0x62c);
