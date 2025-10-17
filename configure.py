86895b23c2588ed8942e297fce79f1913102297a
Carco_21 <144170194+carter-ktb21@users.noreply.github.com>
2025-10-17 18:09:58 -0400

    d_a_obj_swchain matching (#2741)
    * initial
    
    * Little more work
    
    * matching
    
    * updated matching versions

--- configure.py
+++ configure.py
@@ -2181,7 +2181,7 @@ config.libs = [
     ActorRel(MatchingFor("GZ2E01", "GZ2P01", "GZ2J01"), "d_a_obj_swBallB"),
     ActorRel(MatchingFor("GZ2E01", "GZ2P01", "GZ2J01"), "d_a_obj_swBallC"),
     ActorRel(MatchingFor("GZ2E01", "GZ2P01", "GZ2J01"), "d_a_obj_swLight"),
-    ActorRel(NonMatching, "d_a_obj_swchain"),
+    ActorRel(MatchingFor("GZ2E01", "GZ2P01", "GZ2J01"), "d_a_obj_swchain"),
     ActorRel(Equivalent, "d_a_obj_swhang"), # vtable order
     ActorRel(MatchingFor("GZ2E01", "GZ2P01", "GZ2J01"), "d_a_obj_sword"),
     ActorRel(NonMatching, "d_a_obj_swpush2"),
