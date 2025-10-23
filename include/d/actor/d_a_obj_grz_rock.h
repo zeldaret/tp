d6711a18c804d90aa4b42aa4a4565c649fddfd30
Carco_21 <144170194+carter-ktb21@users.noreply.github.com>
2025-10-22 21:38:51 -0400

    d_a_obj_grz_rock and d_a_obj_h_saku matching (#2749)
    * matching
    
    * h_saku initial work
    
    * h_saku matching
    
    * PR cleanup
    
    * fixed header padding

--- include/d/actor/d_a_obj_grz_rock.h
+++ include/d/actor/d_a_obj_grz_rock.h
@@ -1,7 +1,8 @@
 #ifndef D_A_OBJ_GRZ_ROCK_H
 #define D_A_OBJ_GRZ_ROCK_H
 
-#include "f_op/f_op_actor_mng.h"
+#include "d/d_bg_s_acch.h"
+#include "d/d_bg_s_movebg_actor.h"
 
 /**
  * @ingroup actors-objects
@@ -10,26 +11,31 @@
  *
  * @details
  *
- */
-class daObjGrzRock_c : public fopAc_ac_c {
+*/
+
+class daObjGrzRock_c : public dBgS_MoveBgActor {
 public:
     /* 80C14BD8 */ void setBaseMtx();
     /* 80C14C54 */ void setEnvTevColor();
     /* 80C14CB0 */ void setRoomNo();
     /* 80C14CF0 */ void setPrtcl();
-    /* 80C14DA0 */ void Create();
-    /* 80C14EE0 */ void CreateHeap();
-    /* 80C14F50 */ void create();
-    /* 80C1514C */ void Execute(Mtx**);
-    /* 80C151A4 */ void Draw();
-    /* 80C15248 */ void Delete();
+    /* 80C14DA0 */ int Create();
+    /* 80C14EE0 */ int CreateHeap();
+    /* 80C14F50 */ cPhs__Step create();
+    /* 80C1514C */ int Execute(Mtx**);
+    /* 80C151A4 */ int Draw();
+    /* 80C15248 */ int Delete();
 
     void setSmashFlag(u8 i_smashFlag) { mSmashFlag = i_smashFlag; }
 
 private:
-    /* 0x568 */ u8 field_0x568[0x801 - 0x568];
+    /* 0x568 */ request_of_phase_process_class mPhase[1];
+    /* 0x5A0 */ J3DModel* mpModel;
+    /* 0x5AC */ dBgS_ObjAcch mBgc;
+    /* 0x784 */ dBgS_AcchCir mAcchCir;
+    /* 0x7C4 */ cBgS_GndChk mGndChk;
+    /* 0x800 */ u8 field_0x800;
     /* 0x801 */ u8 mSmashFlag;
-    /* 0x802 */ u8 field_0x802[0x804 - 0x802];
 };
 
 STATIC_ASSERT(sizeof(daObjGrzRock_c) == 0x804);
