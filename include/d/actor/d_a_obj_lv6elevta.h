adec8f8a1a93906b0ad6a9936db0782ebf05d1d5
roeming <brother64youyou@gmail.com>
2025-10-22 21:43:34 -0400

    link d_a_obj_lv6elevta

--- include/d/actor/d_a_obj_lv6elevta.h
+++ include/d/actor/d_a_obj_lv6elevta.h
@@ -1,7 +1,9 @@
 #ifndef D_A_OBJ_LV6ELEVTA_H
 #define D_A_OBJ_LV6ELEVTA_H
 
+#include "d/d_event_lib.h"
 #include "f_op/f_op_actor_mng.h"
+#include "d/d_bg_s_movebg_actor.h"
 
 /**
  * @ingroup actors-objects
@@ -11,24 +13,45 @@
  * @details
  *
  */
-class daObjLv6ElevtA_c : public fopAc_ac_c {
+class daObjLv6ElevtA_c : public dBgS_MoveBgActor,
+                         public request_of_phase_process_class,
+                         public dEvLib_callback_c {
 public:
-    /* 80C82D38 */ void create1st();
+    daObjLv6ElevtA_c() : dEvLib_callback_c(this) {}
+    virtual ~daObjLv6ElevtA_c() {};
+    /* 80C82D38 */ int create1st();
     /* 80C82DC8 */ void setMtx();
-    /* 80C82E38 */ void CreateHeap();
-    /* 80C82EA8 */ void Create();
-    /* 80C83028 */ void eventStart();
+    /* 80C82E38 */ int CreateHeap();
+    /* 80C82EA8 */ int Create();
+    /* 80C83028 */ bool eventStart();
     /* 80C830BC */ void moveAngle(void*);
-    /* 80C8321C */ void Execute(Mtx**);
-    /* 80C83538 */ void Draw();
-    /* 80C835DC */ void Delete();
-    /* 80C83764 */ ~daObjLv6ElevtA_c();
+    /* 80C8321C */ int Execute(Mtx**);
+    /* 80C83538 */ int Draw();
+    /* 80C835DC */ int Delete();
+
+    int getSwNo() { return fopAcM_GetParamBit(this, 0, 8); };
+    int getSw2No() { return fopAcM_GetParamBit(this, 8, 8); };
+    int getEvent() { return fopAcM_GetParamBit(this, 0x10, 8); };
 
 private:
-    /* 0x568 */ u8 field_0x568[0x628 - 0x568];
+    /* 0x5B8 */ Mtx mMtx1;
+    /* 0x5E8 */ Mtx mMtx2;
+    /* 0x618 */ J3DModel* mModel;
+    /* 0x61C */ int mAngle;
+    /* 0x620 */ int mMode;
+    /* 0x624 */ bool mIsMoving;
 };
 
 STATIC_ASSERT(sizeof(daObjLv6ElevtA_c) == 0x628);
 
+#if DEBUG
+class daObjLv6ElevtA_HIO_c : public mDoHIO_entry_c {
+public:
+    daObjLv6ElevtA_HIO_c();
+    void genMessage(JORMContext*);
+
+    /* 0x6 */ s16 mRightAngleTurnFrameCount;
+};
+#endif
 
 #endif /* D_A_OBJ_LV6ELEVTA_H */
