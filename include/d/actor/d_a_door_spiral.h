ebf09addcdfdfa8fcab1aa65227951d380aba60d
Carco_21 <cdray2121@gmail.com>
2025-10-26 17:44:35 -0400

    d_a_door_spiral initial work

--- include/d/actor/d_a_door_spiral.h
+++ include/d/actor/d_a_door_spiral.h
@@ -1,6 +1,75 @@
 #ifndef D_A_DOOR_SPIRAL_H
 #define D_A_DOOR_SPIRAL_H
 
+#include "SSystem/SComponent/c_phase.h"
+#include "d/d_bg_w.h"
 #include "dolphin/types.h"
+#include "f_op/f_op_actor.h"
+
+class daSpiral_c : public fopAc_ac_c {
+public:
+    class dSpiral_stop_c {
+        /* 0x00 */ J3DModel* mModel;
+        /* 0x04 */ dBgW* mpBgW;
+        /* 0x08 */ u8 field_0x8[0x38 - 0x8];
+        /* 0x38 */ Mtx mMtx;
+        /* 0x68 */ f32 field_0x68;
+        /* 0x6C */ f32 field_0x6c;
+        /* 0x70 */ u8 field_0x70;
+        /* 0x71 */ u8 field_0x71;
+        /* 0x72 */ u8 field_0x72;
+    };
+
+    /* 80825050 */ char* getAlwaysArcName();
+    /* 80825080 */ char* getSpiralDzbName(int);
+    /* 808250A0 */ char* getDzb();
+    /* 808250B0 */ char* getBmdArcName();
+    /* 808250C0 */ char* getEvArcName();
+    /* 808250D0 */ char* getBmd(int);
+    /* 808250F0 */ char* getBmd2(int);
+    /* 80825160 */ J3DModelData* getModelData();
+    /* 808251E0 */ char* getStopBmd();
+    /* 808251F0 */ char* getStopBmd2();
+    /* 80825200 */ J3DModelData* getStopModelData();
+    /* 80825280 */ char* getStopDzb();
+    /* 808252D0 */ bool debugCheckParam();
+    /* 80825380 */ cPhs__Step create();
+    /* 80825520 */ int CreateHeap();
+    /* 808257D0 */ int CreateInit();
+
+    /* 0x570 */ request_of_phase_process_class mPhase1;
+    /* 0x578 */ request_of_phase_process_class mPhase2;
+    /* 0x580 */ request_of_phase_process_class mPhase3;
+    /* 0x588 */ J3DModel* mModel;
+    /* 0x58C */ dBgW* field_0x58c;
+    /* 0x590 */ Mtx mMtx;
+    /* 0x5C0 */ dBgW* field_0x5c0;
+    /* 0x5C4 */ u8 field_0x5c4[0x5f4 - 0x5c4];
+    /* 0x5F4 */ u8 mIdx;
+    /* 0x5F5 */ u8 field_0x5f5;
+    /* 0x5F6 */ u8 field_0x5f6;
+    /* 0x5F7 */ u8 mAction;
+    /* 0x5F8 */ int field_0x5f8;
+    /* 0x5FC */ s16 field_0x5fc[8];
+    /* 0x60C */ u8 field_0x60c[8];
+    /* 0x614 */ u8 field_0x614;
+    /* 0x615 */ u8 field_0x615[0x618 - 0x615];
+    /* 0x618 */ int mStaffId;
+    /* 0x61C */ s8 mStayNo;
+    /* 0x61D */ u8 field_0x61d;
+    /* 0x61E */ u8 field_0x61e;
+    /* 0x61F */ u8 field_0x61f;
+    /* 0x620 */ cXyz field_0x620;
+    /* 0x62C */ u8 mFrontCheck;
+    /* 0x62D */ u8 field_0x62d;
+    /* 0x62E */ u8 field_0x62e[0x630 - 0x62e];
+    /* 0x630 */ cXyz field_0x630;
+    /* 0x63C */ dSpiral_stop_c mSpiralStop;
+    /* 0x6AF */ u8 field_0x6af[0x6b4 - 0x6af];
+    /* 0x6B4 */ s16 field_0x6b4;
+    /* 0x6B8 */ cXyz field_0x6b8[2];
+    /* 0x6D0 */ bool field_0x6d0;
+    // /* 0x6D1 */ 
+};
 
 #endif /* D_A_DOOR_SPIRAL_H */
--- src/d/actor/d_a_door_spiral.cpp
+++ src/d/actor/d_a_door_spiral.cpp
@@ -5,7 +5,191 @@
 #include "d/dolzel_rel.h" // IWYU pragma: keep
 
 #include "d/actor/d_a_door_spiral.h"
+#include "d/d_door_param2.h"
 #include "f_op/f_op_actor.h"
+
+static void* searchSpiralSub(void* i_actor, void* i_data) {
+    daSpiral_c* actor1 = (daSpiral_c*)i_actor;
+    daSpiral_c* actor2 = (daSpiral_c*)i_data;
+    if (actor1 != NULL && fopAcM_IsActor(actor1)) {
+        if (fopAcM_GetProfName(actor1) == PROC_SPIRAL_DOOR && actor1 != actor2) {
+            if (door_param2_c::getBRoomNo(actor1) == door_param2_c::getBRoomNo(actor2)) {
+                return actor1;
+            }
+        }
+    }
+
+    return NULL;
+}
+
+char* daSpiral_c::getAlwaysArcName() {
+    return getBmdArcName();
+}
+
+static char* dzb[2] = {
+    "door-stepD.dzb",
+    "door-stepU.dzb",
+};
+
+char* daSpiral_c::getSpiralDzbName(int idx) {
+    return dzb[idx];
+}
+
+char* daSpiral_c::getDzb() {
+    return "door-dark.dzb";
+}
+
+char* daSpiral_c::getBmdArcName() {
+    return "K_step";
+}
+
+char* daSpiral_c::getEvArcName() {
+    return "DoorEvt";
+}
+
+static char* bmd[2] = {
+    "K_stepd.bmd",
+    "K_stepu.bmd",
+};
+
+char* daSpiral_c::getBmd(int idx) {
+    return bmd[idx];
+}
+
+char* daSpiral_c::getBmd2(int idx) {
+    static char bmdName;
+    if (idx == 0) {
+        sprintf(&bmdName, "door-stairSpiralU.bmd");
+    } else {
+        sprintf(&bmdName, "door-stairSpiralD.bmd");
+    }
+
+    return &bmdName;
+}
+
+J3DModelData* daSpiral_c::getModelData() {
+    J3DModelData* modelData = (J3DModelData*)dComIfG_getStageRes(getBmd2(mIdx));
+    if (modelData == NULL) {
+        modelData = (J3DModelData*)dComIfG_getObjectRes(getBmdArcName(), getBmd(mIdx));
+    }
+
+    return modelData;
+}
+
+char* daSpiral_c::getStopBmd() {
+    return "door-spiralStop.bmd";
+}
+
+char* daSpiral_c::getStopBmd2() {
+    return "door-stop.bmd";
+}
+
+J3DModelData* daSpiral_c::getStopModelData() {
+    J3DModelData* modelData = (J3DModelData*)dComIfG_getStageRes(getStopBmd2());
+    if (modelData == NULL) {
+        modelData = (J3DModelData*)dComIfG_getObjectRes(getBmdArcName(), getStopBmd());
+    }
+
+    return modelData;
+}
+
+char* daSpiral_c::getStopDzb() {
+    return "door-spiralStop.dzb";
+}
+
+static int CheckCreateHeap(fopAc_ac_c* a_this) {
+    daSpiral_c* i_this = (daSpiral_c*)a_this;
+    return i_this->CreateHeap();
+}
+
+bool daSpiral_c::debugCheckParam() {
+    bool rv = false;
+
+    if (!field_0x6d0) {
+        u8 fRoomNo = door_param2_c::getFRoomNo(this);
+        if (fRoomNo == 63) {
+            OS_REPORT_ERROR("螺旋階段：部屋番号指定がありません！\n"); // Spiral staircase: No room number specified!
+            rv = true;
+        }
+
+        u8 frontOption = door_param2_c::getFrontOption(this);
+        if (frontOption == 2) {
+            OS_REPORT_ERROR("螺旋階段：鍵オプションは対応していません！\n"); // Spiral staircase: Key option not supported!
+            rv = true;
+        }
+
+        field_0x6d0 = true;
+    }
+
+    return rv;
+}
+
+cPhs__Step daSpiral_c::create() {
+    if (debugCheckParam()) {
+        return cPhs_ERROR_e;
+    }
+
+    cPhs__Step phase = (cPhs__Step)dComIfG_resLoad(&mPhase1, getAlwaysArcName());
+    if (phase != cPhs_COMPLEATE_e) {
+        return phase;
+    }
+
+    phase = (cPhs__Step)dComIfG_resLoad(&mPhase3, getEvArcName());
+    if (phase != cPhs_COMPLEATE_e) {
+        return phase;
+    }
+
+    phase = (cPhs__Step)dComIfG_resLoad(&mPhase2, getBmdArcName());
+    if (phase != cPhs_COMPLEATE_e) {
+        return phase;
+    }
+
+    fopAcM_SetRoomNo(this, door_param2_c::getFRoomNo(this));
+
+    u8 backOption = door_param2_c::getBackOption(this);
+    switch (backOption) {
+        case 4:
+            mIdx = 0;
+            break;
+        
+        case 5:
+            mIdx = 1;
+            break;
+
+        default:
+            OS_REPORT_ERROR("螺旋階段の裏オプション設定が正しくありません！<%d>\n", backOption); // Spiral staircase back option setting is incorrect! <%d>
+            JUT_ASSERT(300, FALSE);
+            break;
+    }
+
+    if (!fopAcM_entrySolidHeap(this, CheckCreateHeap, 0x7000)) {
+        return cPhs_ERROR_e;
+    }
+
+    CreateInit();
+
+    return cPhs_COMPLEATE_e;
+}
+
+int daSpiral_c::CreateHeap() {
+    J3DModelData* modelData = getModelData();
+    JUT_ASSERT(338, modelData != NULL);
+    mModel = mDoExt_J3DModel__create(modelData, 0x80000, 0x11000084);
+    if (mModel == NULL) {
+        return 0;
+    }
+
+    field_0x58c = new dBgW();
+    if (field_0x58c == NULL) {
+        return 0;
+    }
+
+    
+}
+
+int daSpiral_c::CreateInit() {
+
+};
 
 extern actor_process_profile_definition g_profile_SPIRAL_DOOR = {
     fpcLy_CURRENT_e,        // mLayerID
