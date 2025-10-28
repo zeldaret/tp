004186680f422d5588e9791a314db39a13d49ee8
Carco_21 <cdray2121@gmail.com>
2025-10-28 10:02:06 -0400

    d_a_hitobj work

--- src/d/actor/d_a_hitobj.cpp
+++ src/d/actor/d_a_hitobj.cpp
@@ -1,7 +1,7 @@
 /**
  * d_a_hitobj.cpp
  * 
- */
+*/
 
 #include "d/dolzel_rel.h" // IWYU pragma: keep
 
@@ -13,13 +13,14 @@ static int daHitobj_Draw(hitobj_class* i_this) {
     return 1;
 }
 
-/* 8046B360-8046B3C8 000080 0068+00 1/0 0/0 0/0 .text            daHitobj_Execute__FP12hitobj_class
- */
+/* 8046B360-8046B3C8 000080 0068+00 1/0 0/0 0/0 .text            daHitobj_Execute__FP12hitobj_class */
 static int daHitobj_Execute(hitobj_class* i_this) {
-    if (i_this->field_0x572 != 0) {
-        i_this->field_0x572--;
-        i_this->field_0x5b0.SetC(i_this->current.pos);
-        dComIfG_Ccsp()->Set(&i_this->field_0x5b0);
+    fopAc_ac_c* a_this = (fopAc_ac_c*)i_this;
+    
+    if (i_this->mTimer != 0) {
+        i_this->mTimer--;
+        i_this->mSph.SetC(a_this->current.pos);
+        dComIfG_Ccsp()->Set(&i_this->mSph);
     } else {
         fopKyM_Delete(i_this);
     }
@@ -27,53 +28,61 @@ static int daHitobj_Execute(hitobj_class* i_this) {
     return 1;
 }
 
-/* 8046B3C8-8046B3D0 0000E8 0008+00 1/0 0/0 0/0 .text            daHitobj_IsDelete__FP12hitobj_class
- */
+/* 8046B3C8-8046B3D0 0000E8 0008+00 1/0 0/0 0/0 .text            daHitobj_IsDelete__FP12hitobj_class */
 static int daHitobj_IsDelete(hitobj_class* i_this) {
     return 1;
 }
 
-/* 8046B3D0-8046B400 0000F0 0030+00 1/0 0/0 0/0 .text            daHitobj_Delete__FP12hitobj_class
- */
+#if DEBUG
+static char* dat_114287 = "Delete -> Hitobj(id=%d)\n";
+#endif
+
+/* 8046B3D0-8046B400 0000F0 0030+00 1/0 0/0 0/0 .text            daHitobj_Delete__FP12hitobj_class */
 static int daHitobj_Delete(hitobj_class* i_this) {
+    fpc_ProcID id = fopAcM_GetID(i_this);
     dComIfG_resDelete(&i_this->mPhase,"Hitobj");
     return 1;
 }
 
-/* ############################################################################################## */
-/* 8046B5DC-8046B61C 000000 0040+00 1/1 0/0 0/0 .data            cc_sph_src$3657 */
-static dCcD_SrcSph cc_sph_src = {
-    {
-        {0, {{0, 0, 0x13}, {0, 3}, 0}},
-        {dCcD_SE_NONE, 0, 0, 0, {0}},
-        {dCcD_SE_NONE, 0, 0, 0, {0}},
-        {0},
-    },
-    {
-        {
-            {0.0f, 0.0f, 0.0f},
-            100.0f,
-        },
-    }
-};
-
 /* 8046B400-8046B53C 000120 013C+00 1/0 0/0 0/0 .text            daHitobj_Create__FP10fopAc_ac_c */
-static int daHitobj_Create(fopAc_ac_c* i_this) {
-    hitobj_class* hitobj = (hitobj_class*)i_this;
-    fopAcM_ct(i_this, hitobj_class);
+static int daHitobj_Create(fopAc_ac_c* a_this) {
+    fopAc_ac_c* player = dComIfGp_getPlayer(0);
+    fpc_ProcID id = fopAcM_GetID(a_this);
+    fopAcM_ct(a_this, hitobj_class);
+    hitobj_class* i_this = (hitobj_class*)a_this;
 
-    int ret = dComIfG_resLoad(&hitobj->mPhase,"Hitobj");
+    int ret = dComIfG_resLoad(&i_this->mPhase,"Hitobj");
 
     if (ret == cPhs_COMPLEATE_e) {
-        hitobj->field_0x570 = fopAcM_GetParam(hitobj);
-        hitobj->field_0x574.Init(0xff,0xff,hitobj);
-        hitobj->field_0x5b0.Set(cc_sph_src);
-        hitobj->field_0x5b0.SetStts(&hitobj->field_0x574);
-        hitobj->field_0x572 = 3;
+        i_this->arg0 = fopAcM_GetParam(a_this);
+        i_this->mStts.Init(0xFF, 0xFF, a_this);
+
+        static dCcD_SrcSph cc_sph_src = {
+            {
+                {0, {{0, 0, 0x13}, {0, 3}, 0}},
+                {dCcD_SE_NONE, 0, 0, 0, {0}},
+                {dCcD_SE_NONE, 0, 0, 0, {0}},
+                {0},
+            },
+            {
+                {
+                    {0.0f, 0.0f, 0.0f},
+                    100.0f,
+                },
+            }
+        };
+
+        i_this->mSph.Set(cc_sph_src);
+        i_this->mSph.SetStts(&i_this->mStts);
+        i_this->mTimer = 3;
     }
 
     return ret;
 }
+
+#if DEBUG
+static char* dat_114317 = "Create -> Hitobj(id=%d)\n";
+#endif
 
 /* 8046B61C-8046B63C -00001 0020+00 1/0 0/0 0/0 .data            l_daHitobj_Method */
 static actor_method_class l_daHitobj_Method = {
