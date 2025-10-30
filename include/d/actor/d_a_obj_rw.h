194282d2c268947588c1b39108ce7e9b0100de91
TakaRikka <38417346+TakaRikka@users.noreply.github.com>
2025-10-29 22:56:22 -0700

    obj_pdwall / obj_rw / obj_stopper2 done (#2760)
    * obj_pdwall / obj_rw / obj_stopper2 done
    
    * fix jp/pal
    
    * Use NULL instead of 0
    
    * Use dEvtCnd_CANDEMO_e
    
    ---------
    
    Co-authored-by: hatal175 <hatal175@users.noreply.github.com>

--- include/d/actor/d_a_obj_rw.h
+++ include/d/actor/d_a_obj_rw.h
@@ -3,6 +3,7 @@
 
 #include "f_op/f_op_actor_mng.h"
 #include "d/d_cc_d.h"
+#include "d/d_cc_uty.h"
 
 /**
  * @ingroup actors-objects
@@ -12,23 +13,30 @@
  * @details
  *
  */
-class obj_rw_class : public fopAc_ac_c {
-private:
-    /* 0x568 */ u8 field_0x568[0xe3c - 0x568];
+class obj_rw_class {
+public:
+    /* 0x000 */ fopEn_enemy_c enemy;
+    /* 0x5AC */ request_of_phase_process_class phase;
+    /* 0x5B4 */ J3DModel* model;
+    /* 0x5B8 */ Z2CreatureEnemy sound;
+    /* 0x65C */ u32 shadowId;
+    /* 0x660 */ s16 field_0x660;
+    /* 0x662 */ s16 action;
+    /* 0x664 */ s16 mode;
+    /* 0x666 */ s16 field_0x666[2];
+    /* 0x66A */ s16 invulnerability_timer;
+    /* 0x66C */ f32 field_0x66c;
+    /* 0x670 */ s16 field_0x670;
+    /* 0x672 */ s16 field_0x672;
+    /* 0x674 */ s16 field_0x674;
+    /* 0x678 */ dCcD_Stts ccStts;
+    /* 0x6B4 */ dCcD_Sph ccSph[2];
+    /* 0x924 */ dCcD_Cyl ccCyl[4];
+    /* 0xE14 */ dCcU_AtInfo atInfo;
+    /* 0xE38 */ s8 field_0xe38;
+    /* 0xE39 */ u8 HIOInit;
 };
 
 STATIC_ASSERT(sizeof(obj_rw_class) == 0xe3c);
-
-struct daObj {
-public:
-    /* 80037900 */ void HitSeStart(cXyz const*, int, dCcD_GObjInf const*, u32);
-};
-
-class daOBJ_RW_HIO_c {
-public:
-    /* 80CC298C */ daOBJ_RW_HIO_c();
-    /* 80CC3A5C */ ~daOBJ_RW_HIO_c();
-};
-
 
 #endif /* D_A_OBJ_RW_H */
