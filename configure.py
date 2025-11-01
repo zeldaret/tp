1be8e6c6c893ae563ec63043ba570b75fefe6639
Max Roncace <me@caseif.net>
2025-11-01 01:04:10 -0400

    d_a_e_hm matching (#2764)

--- configure.py
+++ configure.py
@@ -1755,7 +1755,7 @@ config.libs = [
     ActorRel(MatchingFor("GZ2E01", "GZ2P01", "GZ2J01"), "d_a_e_gob"),
     ActorRel(MatchingFor("GZ2E01", "GZ2P01", "GZ2J01"), "d_a_e_gs"),
     ActorRel(MatchingFor("GZ2E01", "GZ2P01", "GZ2J01"), "d_a_e_hb_leaf"),
-    ActorRel(NonMatching, "d_a_e_hm"),
+    ActorRel(MatchingFor("GZ2E01", "GZ2P01", "GZ2J01"), "d_a_e_hm"),
     ActorRel(NonMatching, "d_a_e_hp"),
     ActorRel(MatchingFor("GZ2E01", "GZ2P01", "GZ2J01"), "d_a_e_hz"),
     ActorRel(MatchingFor("GZ2E01", "GZ2P01", "GZ2J01"), "d_a_e_hzelda"),
