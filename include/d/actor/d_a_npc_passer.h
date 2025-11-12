63093fea4a96821e9432e3d05d2c780e628a48b4
Carco_21 <144170194+carter-ktb21@users.noreply.github.com>
2025-11-11 22:58:27 -0500

    d_a_npc_passer and d_a_npc_passer2 matching (#2796)
    * passer matching and passer2 init work
    
    * passer2 matching

--- include/d/actor/d_a_npc_passer.h
+++ include/d/actor/d_a_npc_passer.h
@@ -2,24 +2,36 @@
 #define D_A_NPC_PASSER_H
 
 #include "d/actor/d_a_npc.h"
+#include "d/actor/d_a_npc_cd2.h"
 
 /**
  * @ingroup actors-npcs
  * @class daNpcPasser_c
- * @brief Hylian Passerby (Castle Town NPC?)
+ * @brief Hylian Passerby
  *
  * @details
  *
- */
-class daNpcPasser_c : public fopAc_ac_c {
+*/
+
+class daNpcPasser_c : public daNpcCd2_c {
 public:
-    struct Mode_e {};
+    typedef void (daNpcPasser_c::*actionFunc)();
+    typedef BOOL (daNpcPasser_c::*seqFunc)(void*);
+
+    enum Mode_e {
+        /* 0x0 */ MODE_0,
+        /* 0x1 */ MODE_1,
+        /* 0x2 */ MODE_2,
+        /* 0x3 */ MODE_3,
+        /* 0x4 */ MODE_4,
+        /* 0x5 */ MODE_MAX_e,
+    };
 
     /* 80AA2C18 */ ~daNpcPasser_c();
-    /* 80AA2C9C */ void createHeap();
-    /* 80AA2D40 */ void create();
-    /* 80AA2FD4 */ void execute();
-    /* 80AA315C */ void draw();
+    /* 80AA2C9C */ int createHeap();
+    /* 80AA2D40 */ cPhs__Step create();
+    /* 80AA2FD4 */ int execute();
+    /* 80AA315C */ int draw();
     /* 80AA31EC */ void setAction(daNpcPasser_c::Mode_e);
     /* 80AA3230 */ void callInit();
     /* 80AA3258 */ void callExecute();
@@ -40,66 +52,96 @@ public:
     /* 80AA4780 */ void setBaseMtx();
     /* 80AA47FC */ void initCollision();
     /* 80AA48D8 */ void setCollision();
-    /* 80AA492C */ void waita(void*);
-    /* 80AA49D4 */ void talka(void*);
-    /* 80AA4A7C */ void waitwall(void*);
-    /* 80AA4B24 */ void talkwall(void*);
-    /* 80AA4BCC */ void talkbwall(void*);
-    /* 80AA4C74 */ void talkb(void*);
-    /* 80AA4D1C */ void talkc(void*);
-    /* 80AA4DC4 */ void waitb(void*);
-    /* 80AA4E6C */ void sitwaita(void*);
-    /* 80AA4F14 */ void sittalka(void*);
-    /* 80AA4FBC */ void sittalkab(void*);
-    /* 80AA5064 */ void sitwaitb(void*);
-    /* 80AA510C */ void sittalkb(void*);
-    /* 80AA51B4 */ void sittalkbb(void*);
-    /* 80AA525C */ void looka(void*);
-    /* 80AA5304 */ void lookb(void*);
-    /* 80AA53AC */ void playm(void*);
-    /* 80AA5454 */ void turnr(void*);
-    /* 80AA5510 */ void turnl(void*);
-    /* 80AA55CC */ void walka(void*);
-    /* 80AA5634 */ void runa(void*);
-    /* 80AA569C */ void runb(void*);
-    /* 80AA5704 */ void walkb(void*);
-    /* 80AA576C */ void browsea(void*);
-    /* 80AA5814 */ void browseb(void*);
-    /* 80AA58BC */ void waitTime(void*);
+    /* 80AA492C */ BOOL waita(void*);
+    /* 80AA49D4 */ BOOL talka(void*);
+    /* 80AA4A7C */ BOOL waitwall(void*);
+    /* 80AA4B24 */ BOOL talkwall(void*);
+    /* 80AA4BCC */ BOOL talkbwall(void*);
+    /* 80AA4C74 */ BOOL talkb(void*);
+    /* 80AA4D1C */ BOOL talkc(void*);
+    /* 80AA4DC4 */ BOOL waitb(void*);
+    /* 80AA4E6C */ BOOL sitwaita(void*);
+    /* 80AA4F14 */ BOOL sittalka(void*);
+    /* 80AA4FBC */ BOOL sittalkab(void*);
+    /* 80AA5064 */ BOOL sitwaitb(void*);
+    /* 80AA510C */ BOOL sittalkb(void*);
+    /* 80AA51B4 */ BOOL sittalkbb(void*);
+    /* 80AA525C */ BOOL looka(void*);
+    /* 80AA5304 */ BOOL lookb(void*);
+    /* 80AA53AC */ BOOL playm(void*);
+    /* 80AA5454 */ BOOL turnr(void*);
+    /* 80AA5510 */ BOOL turnl(void*);
+    /* 80AA55CC */ BOOL walka(void*);
+    /* 80AA5634 */ BOOL runa(void*);
+    /* 80AA569C */ BOOL runb(void*);
+    /* 80AA5704 */ BOOL walkb(void*);
+    /* 80AA576C */ BOOL browsea(void*);
+    /* 80AA5814 */ BOOL browseb(void*);
+    /* 80AA58BC */ BOOL waitTime(void*);
 
-    static u8 ActionTable[120];
-    static void* m_funcTbl[28];
-    static u8 m_seq00_funcTbl[24];
-    static u8 m_seq01_funcTbl[24];
-    static u8 m_seq02_funcTbl[24];
-    static u8 m_seq03_funcTbl[24];
-    static u8 m_seq04_funcTbl[24];
-    static u8 m_seq05_funcTbl[48];
-    static u8 m_seq06_funcTbl[48];
-    static u8 m_seq07_funcTbl[24];
-    static u8 m_seq08_funcTbl[84];
-    static u8 m_seq09_funcTbl[24];
-    static u8 m_seq10_funcTbl[24];
-    static u8 m_seq11_funcTbl[72];
-    static u8 m_seq12_funcTbl[24];
-    static u8 m_seq13_funcTbl[72];
-    static u8 m_seq14_funcTbl[24];
-    static u8 m_seq15_funcTbl[24];
-    static u8 m_seq16_funcTbl[84];
-    static u8 m_seq17_funcTbl[24];
-    static u8 m_seq18_funcTbl[24];
-    static u8 m_seq19_funcTbl[84];
-    static u8 m_seq20_funcTbl[24];
-    static u8 m_seq21_funcTbl[24];
-    static u8 m_seq22_funcTbl[48];
-    static u8 m_seq23_funcTbl[84];
-    static u8 m_seq24_funcTbl[60];
-    static u8 m_seq25_funcTbl[84];
-    static u8 m_seq26_funcTbl[36];
-    static u8 m_seq27_funcTbl[12];
+    int getObjNum() { return (fopAcM_GetParam(this) >> 8) & 0xFF; }
+    int getTimeHour() { return dKy_darkworld_check() ? dKy_getDarktime_hour() : dKy_getdaytime_hour(); }
+    int getTimeMinute() { return dKy_darkworld_check() ? dKy_getDarktime_minute() : dKy_getdaytime_minute(); }
+    int getTime() {return (getTimeHour() * 60) + getTimeMinute(); }
+    int getDayOfWeek() { return dKy_darkworld_check() ? dKy_getDarktime_week() : dKy_get_dayofweek(); }
+    int getEndTime() { return current.angle.x; }
+    int getType() { return fopAcM_GetParam(this) & 0xFF; }
+    u8 getMoveType() { return (fopAcM_GetParam(this) >> 24) & 0xF; }
+    u8 getWalkMotionType() { return (fopAcM_GetParam(this) >> 28) & 3; }
+    u8 getRunMotionType() { return fopAcM_GetParam(this) >> 30; }
+    u8 getPathID() { return (fopAcM_GetParam(this) >> 16) & 0xFF; }
+    BOOL isStop() { return TRUE; }
+
+    static actionFunc ActionTable[5][2];
+    static seqFunc* m_funcTbl[28];
+    static seqFunc m_seq00_funcTbl[2];
+    static seqFunc m_seq01_funcTbl[2];
+    static seqFunc m_seq02_funcTbl[2];
+    static seqFunc m_seq03_funcTbl[2];
+    static seqFunc m_seq04_funcTbl[2];
+    static seqFunc m_seq05_funcTbl[4];
+    static seqFunc m_seq06_funcTbl[4];
+    static seqFunc m_seq07_funcTbl[2];
+    static seqFunc m_seq08_funcTbl[7];
+    static seqFunc m_seq09_funcTbl[2];
+    static seqFunc m_seq10_funcTbl[2];
+    static seqFunc m_seq11_funcTbl[6];
+    static seqFunc m_seq12_funcTbl[2];
+    static seqFunc m_seq13_funcTbl[6];
+    static seqFunc m_seq14_funcTbl[2];
+    static seqFunc m_seq15_funcTbl[2];
+    static seqFunc m_seq16_funcTbl[7];
+    static seqFunc m_seq17_funcTbl[2];
+    static seqFunc m_seq18_funcTbl[2];
+    static seqFunc m_seq19_funcTbl[7];
+    static seqFunc m_seq20_funcTbl[2];
+    static seqFunc m_seq21_funcTbl[2];
+    static seqFunc m_seq22_funcTbl[4];
+    static seqFunc m_seq23_funcTbl[7];
+    static seqFunc m_seq24_funcTbl[5];
+    static seqFunc m_seq25_funcTbl[7];
+    static seqFunc m_seq26_funcTbl[3];
+    static seqFunc m_seq27_funcTbl[1];
 
 private:
-    /* 0x568 */ u8 field_0x568[0xb28 - 0x568];
+    /* 0xAC8 */ J3DModel* mpModel;
+    /* 0xACC */ PathTrace_c m_path;
+    /* 0xAF4 */ daNpcPasser_c::actionFunc* mAction;
+    /* 0xAF8 */ int mActionIdx;
+    /* 0xAFC */ int mPrevActionIdx;
+    /* 0xB00 */ int m_objNum;
+    /* 0xB04 */ int arg0;
+    /* 0xB08 */ int m_dayOfWeek;
+    /* 0xB0C */ int m_endTime;
+    /* 0xB10 */ f32 field_0xb10;
+    /* 0xB14 */ f32 field_0xb14;
+    /* 0xB18 */ f32 mPosY;
+    /* 0xB1C */ u32 field_0xb1c;
+    /* 0xB20 */ s16 mTargetAngleY;
+    /* 0xB22 */ u8 field_0xb22;
+    /* 0xB23 */ u8 field_0xb23;
+    /* 0xB24 */ u8 mPathMode;
+    /* 0xB25 */ u8 field_0xb25;
 };
 
 STATIC_ASSERT(sizeof(daNpcPasser_c) == 0xb28);
