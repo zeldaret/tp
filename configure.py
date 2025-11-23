33c30d94949fbe0adf3e76db91345867bf7a7dd2
LagoLunatic <LagoLunatic@users.noreply.github.com>
2025-11-23 17:38:49 -0500

    Match daNpcCd2_c::checkFearSituation and daNpcCd2_c::getAnmP

--- configure.py
+++ configure.py
@@ -684,7 +684,7 @@ config.libs = [
             Object(MatchingFor(ALL_GCN), "d/actor/d_a_no_chg_room.cpp"),
             Object(NonMatching, "d/actor/d_a_npc.cpp"),
             Object(MatchingFor(ALL_GCN), "d/actor/d_a_npc_cd.cpp"),
-            Object(NonMatching, "d/actor/d_a_npc_cd2.cpp"),
+            Object(NonMatching, "d/actor/d_a_npc_cd2.cpp"), # stripped vtable order
             Object(MatchingFor(ALL_GCN), "d/actor/d_a_obj_item.cpp"),
             Object(MatchingFor(ALL_GCN), "d/d_insect.cpp"),
             Object(MatchingFor(ALL_GCN), "d/actor/d_a_obj_ss_base.cpp"),
