e619b704a8069f4d0b663fcb42e9dfc77d222b92
kipcode66 <kipcode66@gmail.com>
2025-12-14 03:07:25 -0500

    fix compilation issue

--- configure.py
+++ configure.py
@@ -756,7 +756,7 @@ config.libs = [
             Object(MatchingFor(ALL_GCN), "d/actor/d_a_no_chg_room.cpp"),
             Object(NonMatching, "d/actor/d_a_npc.cpp"),
             Object(MatchingFor(ALL_GCN), "d/actor/d_a_npc_cd.cpp"),
-            Object(MatchingFor(ALL_GCN), "d/actor/d_a_npc_cd2.cpp"), # stripped vtable order
+            Object(NonMatching, "d/actor/d_a_npc_cd2.cpp"), # stripped vtable order
             Object(MatchingFor(ALL_GCN), "d/actor/d_a_obj_item.cpp"),
             Object(MatchingFor(ALL_GCN), "d/d_insect.cpp"),
             Object(MatchingFor(ALL_GCN), "d/actor/d_a_obj_ss_base.cpp"),
