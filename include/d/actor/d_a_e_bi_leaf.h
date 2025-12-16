618a3783f8224e586d0aefdd57dc191ef9998492
Luzagar <40547781+Luzagar@users.noreply.github.com>
2025-12-16 02:04:20 +0100

    d_a_e_b +d_a_e_bi_leaf  documentation (#2952)
    * some e_bi documentation
    
    * fix match issue
    
    * fix error
    
    * e_bi_leaf documentation

--- include/d/actor/d_a_e_bi_leaf.h
+++ include/d/actor/d_a_e_bi_leaf.h
@@ -13,11 +13,11 @@
  */
 class e_bi_leaf_class : public fopEn_enemy_c {
 public:
-    /* 0x5AC */ request_of_phase_process_class mPhase;
-    /* 0x5B4 */ u8 field_0x5b4;
-    /* 0x5B8 */ J3DModel* mpModel;
-    /* 0x5BC */ s16 mAction;
-    /* 0x5BE */ s16 mTimer;
+    /* 0x5AC */ request_of_phase_process_class phase;
+    /* 0x5B4 */ u8 type;
+    /* 0x5B8 */ J3DModel* model;
+    /* 0x5BC */ s16 action;
+    /* 0x5BE */ s16 timer;
     /* 0x5C0 */ u8 field_0x5c0[0x5D0 - 0x5C0];
 };
 
