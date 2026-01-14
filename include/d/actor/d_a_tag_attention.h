9d73ad6b3d43b8a50c8450577f094f9b5acf3a31
Max Roncace <me@caseif.net>
2026-01-14 02:27:03 -0500

    d_a_tag_attention almost matching

--- include/d/actor/d_a_tag_attention.h
+++ include/d/actor/d_a_tag_attention.h
@@ -4,6 +4,22 @@
 #include "d/d_com_inf_game.h"
 #include "f_op/f_op_actor_mng.h"
 
+class daAttp_HIO_c : public mDoHIO_entry_c {
+public:
+    daAttp_HIO_c();
+
+    void genMessage(JORMContext*);
+
+    /* 0x06 */ u8 enable_type_switching;
+    /* 0x07 */ u8 type_spec;
+    /* 0x08 */ u8 enable_distance_display;
+    /* 0x0C */ f32 dist_max;
+    /* 0x10 */ f32 dist_max_release;
+    /* 0x14 */ f32 dist_adjust;
+    /* 0x18 */ f32 upper_y;
+    /* 0x1C */ f32 lower_y;
+};
+
 class daAttp_c : public fopAc_ac_c {
 public:
     int Create();
@@ -11,28 +27,33 @@ public:
     int execute();
     int draw();
     int _delete();
+
+#if DEBUG
+    Mtx field_0x568;
+    u8 field_0x598;
+#endif
 };  // Size: 0x568
 
 namespace daAttp_prm {
-static inline u32 getArg0(daAttp_c* pActor) {
-    return fopAcM_GetParam(pActor) & 3;
+static inline u8 getArg0(daAttp_c* pActor) {
+    return fopAcM_GetParam(pActor) & 0x3;
 }
-static inline u32 getArg1(daAttp_c* pActor) {
-    return (fopAcM_GetParam(pActor) >> 2) & 3;
+static inline u8 getArg1(daAttp_c* pActor) {
+    return (fopAcM_GetParam(pActor) >> 2) & 0x3;
 }
-static inline u32 getArg2(daAttp_c* pActor) {
-    return (fopAcM_GetParam(pActor) >> 4) & 7;
+static inline u8 getArg2(daAttp_c* pActor) {
+    return (fopAcM_GetParam(pActor) >> 4) & 0x7;
 }
-static inline u32 getArg3(daAttp_c* pActor) {
-    return (fopAcM_GetParam(pActor) >> 7) & 1;
+static inline u8 getArg3(daAttp_c* pActor) {
+    return (fopAcM_GetParam(pActor) >> 7) & 0x1;
 }
-static inline u32 getArg4(daAttp_c* pActor) {
-    return (fopAcM_GetParam(pActor) >> 8) & 1;
+static inline u8 getArg4(daAttp_c* pActor) {
+    return (fopAcM_GetParam(pActor) >> 8) & 0x1;
 }
-static inline u32 getSw1(daAttp_c* pActor) {
+static inline u8 getSw1(daAttp_c* pActor) {
     return (fopAcM_GetParam(pActor) >> 0x10) & 0xff;
 }
-static inline u32 getSw2(daAttp_c* pActor) {
+static inline u8 getSw2(daAttp_c* pActor) {
     return (fopAcM_GetParam(pActor) >> 0x18);
 }
 }  // namespace daAttp_prm
--- src/d/actor/d_a_tag_attention.cpp
+++ src/d/actor/d_a_tag_attention.cpp
@@ -6,12 +6,98 @@
 #include "d/dolzel_rel.h" // IWYU pragma: keep
 
 #include "d/actor/d_a_tag_attention.h"
-#include "d/actor/d_a_player.h"
 
-static const u8 l_dist_tbl[] = { 0x23, 0x24 };
+#include "d/actor/d_a_player.h"
+#include "d/d_debug_viewer.h"
+
+#if DEBUG
+daAttp_HIO_c l_HIO;
+
+void daAttp_HIO_c::genMessage(JORMContext* ctx) {
+    static const char* l_type_name[37] = {
+        "注目なし - ダミー",       // No focus - dummy
+        "注目だけ - 標準",        // Focus only - standard
+        "会話     - 標準",      // Conversation - standard
+        "戦闘     - 標準",      // Combat - standard
+        "戦闘Ｂ   - 標準",       // Combat B - standard
+        "会話Ｂ   - 標準",       // Conversation B - standard
+        "持上げ   - 標準",       // Lifting - standard
+        "持上げ   - タル",       // Lifting - barrel
+        "持上げ   - 壷大",       // Lifting - large jar
+        "持上げ   - 水壷",       // Lifting - water jar
+        "持上げ   - 椅子",       // Lifting - chair
+        "持上げ   - 小石",       // Lifting - pebble
+        "持上げ   - 岩",        // Lifting - rock
+        "馬",                // Horse
+        "ドア、宝箱",            // Door, treasure chest
+        "持上げボコの棒",          // Lifting stick
+        "敵氷",               // Enemy ice
+        "引き連れられ石像",         // Dragged stone statue
+        "引き連れられ石像",         // Dragged stone statue
+        "ジャガ－",             // Jaguar
+        "接近会話",             // Close-range conversation
+        "長距離戦闘",            // Long-range combat
+        "短距離戦闘",            // Short-range combat
+        "予備戦闘",             // Reserve combat
+        "予備戦闘",             // Reserve combat
+        "予備戦闘",             // Reserve combat
+        "床レバー用",            // Floor lever
+        "狼鎖",               // Wolf chain
+        "ＮＰＣナビィ － ハイラル城、呼びかけデモ用（会話）",   // NPC Navii - Hyrule Castle, demo call (conversation)
+        "ＮＰＣナビィ － ハイラル城、呼びかけデモ用（注目）",   // NPC Navii - Hyrule Castle, demo call (focus)
+        "狼掘る",              // Wolf digging
+        "カヌー",              // Canoe
+        "覗きタグ",             // Peek tag
+        "馬戦闘",              // Horse combat
+        "注目点　通常長さ",         // Focus point - normal length
+        "注目点　長注目版",         // Focus point - long focus
+        "調整用カスタム",          // Adjustment custom
+    };
+    static const u8 l_type[37] = {
+        0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 14, 15, 16,
+        17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+        33, 34, 35, 36, 37,
+    };
+    static const int num = 37;
+
+    // "Focus point"
+    ctx->genLabel("注目点", 0, 0, NULL, -1, -1, 0x200, 0x18);
+    // "Type switching ON"
+    ctx->genCheckBox("タイプ切り替えＯＮ", &enable_type_switching, 1, 0, NULL, -1, -1, 0x200, 0x18);
+
+    // "Type specification"
+    ctx->startComboBox("タイプ指定", &type_spec, 0, NULL, -1, -1, 0x100, 0x1a);
+    for (int i = 0; i < num; i++) {
+        ctx->genComboBoxItem(l_type_name[i], l_type[i]);
+    }
+    ctx->endComboBox();
+
+    // "Distance display ON"
+    ctx->genCheckBox("距離表示ＯＮ", &enable_distance_display, 1, 0, NULL, -1, -1, 0x200, 0x18);
+
+    // "For adjustment"
+    ctx->genLabel("調整用", 0, 0, NULL, -1, -1, 0x200, 0x18);
+    ctx->genSlider("on", &dist_max, 0.0f, 10000.0f, 0, NULL, -1, -1, 0x200, 0x18);
+    ctx->genSlider("off", &dist_max_release, 0.0f, 10000.0f, 0, NULL, -1, -1, 0x200, 0x18);
+    ctx->genSlider("angle adjust", &dist_adjust, 0.0f, 10000.0f, 0, NULL, -1, -1, 0x200, 0x18);
+    ctx->genSlider("hi", &upper_y, -10000.0f, 10000.0f, 0, NULL, -1, -1, 0x200, 0x18);
+    ctx->genSlider("low", &lower_y, -10000.0f, 10000.0f, 0, NULL, -1, -1, 0x200, 0x18);
+}
+#endif
 
 int daAttp_c::Create() {
-    attention_info.distances[fopAc_attn_LOCK_e] = l_dist_tbl[daAttp_prm::getArg4(this)];
+    static const u8 l_dist_tbl[] = { 35, 36 };
+
+    u8 arg4 = daAttp_prm::getArg4(this);
+    attention_info.distances[fopAc_attn_LOCK_e] = l_dist_tbl[arg4];
+
+#if DEBUG
+    field_0x598 = l_dist_tbl[arg4];
+
+    mDoMtx_stack_c::transS(current.pos.x, current.pos.y, current.pos.z);
+    cMtx_copy(mDoMtx_stack_c::get(), field_0x568);
+    fopAcM_SetMtx(this, field_0x568);
+#endif
 
     execute();
     return 1;
@@ -20,10 +106,31 @@ int daAttp_c::Create() {
 int daAttp_c::create() {
     fopAcM_ct(this, daAttp_c);
     Create();
+
+#if DEBUG
+    // "Key points"
+    l_HIO.entryHIO("注目点");
+#endif
+
     return cPhs_COMPLEATE_e;
 }
 
 int daAttp_c::execute() {
+#if DEBUG
+    if (l_HIO.enable_type_switching) {
+        attention_info.distances[fopAc_attn_LOCK_e] = l_HIO.type_spec;
+    } else {
+        attention_info.distances[fopAc_attn_LOCK_e] = field_0x598;
+    }
+
+    dist_entry& distTable = dAttention_c::getDistTable(37);
+    distTable.mDistMax = l_HIO.dist_max;
+    distTable.mDistMaxRelease = l_HIO.dist_max_release;
+    distTable.mDistanceAdjust = l_HIO.dist_adjust;
+    distTable.mUpperY = l_HIO.upper_y;
+    distTable.mLowerY = l_HIO.lower_y;
+#endif
+
     bool bVar1 = true;
     fopAc_ac_c& link = *daPy_getPlayerActorClass();
 
@@ -58,6 +165,7 @@ int daAttp_c::execute() {
         }
         break;
     default:
+        JUT_ASSERT(432, FALSE);
         break;
     case 7:
         break;
@@ -80,6 +188,8 @@ int daAttp_c::execute() {
         case 3:
             bVar1 = false;
             break;
+        default:
+            JUT_ASSERT(461, FALSE);
         }
     } else {
         switch (daAttp_prm::getArg0(this)) {
@@ -97,6 +207,9 @@ int daAttp_c::execute() {
             break;
         case 3:
             bVar1 = false;
+            break;
+        default:
+            JUT_ASSERT(485, FALSE);
         }
 
         if (daAttp_prm::getSw2(this) != 0xff) {
@@ -120,11 +233,34 @@ int daAttp_c::execute() {
 }
 
 int daAttp_c::draw() {
+#if DEBUG
+    if (l_HIO.enable_distance_display) {
+        GXColor color1 = {0xff, 0, 0, 0x80};
+        GXColor color2 = {0, 0, 0xff, 0x80};
+        cXyz sp10(current.pos);
+        sp10.y += 5.0f;
+        dist_entry& distTable =
+            dAttention_c::getDistTable(attention_info.distances[fopAc_attn_LOCK_e]);
+
+        dDbVw_drawCircleXlu(current.pos, distTable.mDistMax, color2, 1, 12);
+        dDbVw_drawCircleXlu(current.pos, distTable.mDistMaxRelease, color1, 1, 12);
+    }
+#endif
+
     return 1;
 }
 
 int daAttp_c::_delete() {
+#if DEBUG
+    l_HIO.removeHIO();
+#endif
+
     return 1;
+}
+
+daAttp_HIO_c::daAttp_HIO_c() {
+    type_spec = 0;
+    dist_max = dist_max_release = dist_adjust = upper_y = lower_y = 0.0f;
 }
 
 static int daAttp_Draw(daAttp_c* i_this) {
@@ -137,11 +273,14 @@ static int daAttp_Execute(daAttp_c* i_this) {
 }
 
 static int daAttp_Delete(daAttp_c* i_this) {
+    fpc_ProcID id = fopAcM_GetID(i_this);
     return i_this->_delete();
 }
 
 static int daAttp_Create(fopAc_ac_c* i_this) {
-    return static_cast<daAttp_c*>(i_this)->create();
+    daAttp_c* attp = static_cast<daAttp_c*>(i_this);
+    fpc_ProcID id = fopAcM_GetID(i_this);
+    return attp->create();
 }
 
 static actor_method_class l_daAttp_Method = {
