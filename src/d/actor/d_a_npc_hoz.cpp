a5b7f2d1c473728203fe206e79abbc43ff8ade07
Jeffrey Crowell <github@crowell.biz>
2026-01-17 19:15:44 -0800

    fix build in daNpc_Hoz_c::test (#3050)
    mMotionSeqMngr.setNo and mFaceMotionSeqMngr.setNo were called with int instead of the proper pointer.

--- src/d/actor/d_a_npc_hoz.cpp
+++ src/d/actor/d_a_npc_hoz.cpp
@@ -854,8 +854,8 @@ int daNpc_Hoz_c::test(void* i_this) {
         mMode = 2;
         // fall-through
     case 2:
-        mFaceMotionSeqMngr.setNo((int)mpHIO, -1.0f, 0, 0);
-        mMotionSeqMngr.setNo((int)mpHIO, -1.0f, 0, 0);
+        mFaceMotionSeqMngr.setNo(mpHIO->m.common.face_expression, -1.0f, 0, 0);
+        mMotionSeqMngr.setNo(mpHIO->m.common.motion, -1.0f, 0, 0);
         mJntAnm.lookNone(0);
         attention_info.flags = 0;
     case 3:
