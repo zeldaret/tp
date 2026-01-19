bc13ea6ecfa6ee112aca5c390c3a91f288c80814
Max Roncace <me@caseif.net>
2026-01-18 20:53:46 -0500

    d_a_tag_river_back debug almost matching (weak func order)

--- configure.py
+++ configure.py
@@ -2838,7 +2838,7 @@ config.libs = [
     ActorRel(MatchingFor(ALL_GCN), "d_a_tag_poFire"), # debug weak func order
     ActorRel(MatchingFor(ALL_GCN), "d_a_tag_qs"),
     ActorRel(MatchingFor(ALL_GCN), "d_a_tag_ret_room"), # debug weak func order
-    ActorRel(MatchingFor(ALL_GCN), "d_a_tag_river_back"),
+    ActorRel(MatchingFor(ALL_GCN), "d_a_tag_river_back"), # debug weak func order
     ActorRel(MatchingFor(ALL_GCN), "d_a_tag_rmbit_sw"),
     ActorRel(MatchingFor(ALL_GCN, "Shield"), "d_a_tag_schedule"),
     ActorRel(MatchingFor(ALL_GCN, "Shield"), "d_a_tag_setBall"),
--- src/d/actor/d_a_tag_river_back.cpp
+++ src/d/actor/d_a_tag_river_back.cpp
@@ -12,10 +12,14 @@ static char* l_arcName = "RvBack";
 
 static char* l_evName = "RIVER_BACK";
 
+static char* l_staffName = "rvback";
+
 int daTagRiverBack_c::Create() {
     mMapToolID = -1;
     eventInfo.setArchiveName(l_arcName);
-    mEventID = dComIfGp_getEventManager().getEventIdx(this, l_evName, -1);
+    mEventID = (s16)dComIfGp_getEventManager().getEventIdx(this, l_evName, -1);
+    // "River flow reversal tag: evid<%d>tool<%d>"
+    OS_REPORT("川流れ戻しタグ：evid<%d>tool<%d>\n", mEventID, mMapToolID);
     return 1;
 }
 
@@ -23,8 +27,7 @@ int daTagRiverBack_c::create() {
     fopAcM_ct(this, daTagRiverBack_c);
     int phase = dComIfG_resLoad(&mPhase, l_arcName);
     if (phase == cPhs_COMPLEATE_e) {
-        int result = Create();
-        if (!result) {
+        if (!Create()) {
             return cPhs_ERROR_e;
         }
     }
@@ -32,13 +35,11 @@ int daTagRiverBack_c::create() {
 }
 
 int daTagRiverBack_c::execute() {
-    u8 swBit = getSwBit();
-    if (swBit != 0xFF && fopAcM_isSwitch(this, swBit) != 0) {
+    if (getSwbit() != 0xFF && fopAcM_isSwitch(this, getSwbit()) != 0) {
         return 1;
     }
 
-    u8 swBit2 = getSwBit2();
-    if (swBit2 != 0xFF && fopAcM_isSwitch(this, swBit2) == 0) {
+    if (getSwbit2() != 0xFF && fopAcM_isSwitch(this, getSwbit2()) == 0) {
         return 1;
     }
 
@@ -46,8 +47,6 @@ int daTagRiverBack_c::execute() {
 
     return 1;
 }
-
-static char* l_staffName = "rvback";
 
 void daTagRiverBack_c::event_proc_call() {
     typedef void (daTagRiverBack_c::*actionFunc)();
@@ -59,7 +58,8 @@ void daTagRiverBack_c::event_proc_call() {
 }
 
 void daTagRiverBack_c::actionWait() {
-    if (dComIfGp_checkPlayerStatus0(0, 0x100000) != 0) {
+    u32 result = dComIfGp_checkPlayerStatus0(0, 0x100000);
+    if (result != 0) {
         mCount = 0;
         setAction(ACTION_ORDER_EVENT_e);
         fopAcM_orderOtherEventId(this, mEventID, mMapToolID, 0xFFFF, 0, 1);
@@ -72,7 +72,7 @@ void daTagRiverBack_c::actionOrderEvent() {
     if (eventInfo.checkCommandDemoAccrpt()) {
         setAction(ACTION_EVENT_e);
         mStaffID = dComIfGp_evmng_getMyStaffId(l_staffName, NULL, 0);
-        player->mNoResetFlg0 |= 0x10000;
+        player->onDemoStreamAccept();
         demoProc();
     } else {
         fopAcM_orderOtherEventId(this, mEventID, mMapToolID, 0xFFFF, 0, 1);
@@ -82,7 +82,7 @@ void daTagRiverBack_c::actionOrderEvent() {
 
 void daTagRiverBack_c::actionEvent() {
     daPy_py_c* player = daPy_getPlayerActorClass();
-    mCount += 1;
+    mCount += (s16)1;
     if (dComIfGp_evmng_endCheck(mEventID)) {
         setAction(ACTION_DEAD_e);
         dComIfGp_event_reset();
@@ -96,34 +96,35 @@ void daTagRiverBack_c::actionEvent() {
 
 void daTagRiverBack_c::actionDead() {}
 
-static char* action_table[] = {"WAIT", "SCENE_CHG"};
-
 int daTagRiverBack_c::demoProc() {
+    static char* action_table[] = {"WAIT", "SCENE_CHG"};
+
+    daPy_py_c* player = daPy_getPlayerActorClass();
     int act_id = dComIfGp_evmng_getMyActIdx(mStaffID, action_table, ARRAY_SIZEU(action_table), 0, 0);
 
+    int* timer_p;
     if (dComIfGp_evmng_getIsAddvance(mStaffID)) {
         switch (act_id) {
-        case WAIT_e: {
-            int* timer_p = dComIfGp_evmng_getMyIntegerP(mStaffID, "Timer");
+        case WAIT_e:
+            timer_p = dComIfGp_evmng_getMyIntegerP(mStaffID, "Timer");
             if (timer_p == NULL) {
                 mTimer = 1;
             } else {
                 mTimer = *timer_p;
             }
             break;
-        }
         case SCENE_CHG_e:
             mDoAud_seStart(Z2SE_FORCE_BACK, 0, 0, 0);
         }
     }
     switch (act_id) {
     case WAIT_e:
-        if ((cLib_calcTimer(&mTimer) & 0xFF) == 0) {
+        if (cLib_calcTimer(&mTimer) == 0) {
             dComIfGp_evmng_cutEnd(mStaffID);
         }
         break;
     case SCENE_CHG_e:
-        dStage_changeScene(fopAcM_GetParamBit(this, 0, 8) & 0xFF, 0.0f, 0, fopAcM_GetRoomNo(this),
+        dStage_changeScene(getNextSceneNo(), 0.0f, 0, fopAcM_GetRoomNo(this),
                            0, -1);
         break;
     default:
@@ -143,10 +144,12 @@ static int daTagRiverBack_Execute(daTagRiverBack_c* i_this) {
 }
 
 static int daTagRiverBack_Delete(daTagRiverBack_c* i_this) {
+    fopAcM_RegisterDeleteID(i_this, "RiverBack");
     return i_this->_delete();
 }
 
 static int daTagRiverBack_Create(daTagRiverBack_c* i_this) {
+    fopAcM_RegisterCreateID(i_this, "RiverBack");
     return i_this->create();
 }
 
