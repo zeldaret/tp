5d4fd1b771a9f12bee08ea598da751674ac545e1
TakaRikka <takarikka@outlook.com>
2026-01-21 23:23:46 -0800

    d_debug_pad mostly done

--- include/d/d_debug_pad.h
+++ include/d/d_debug_pad.h
@@ -1,11 +1,19 @@
 #ifndef D_DEBUG_PAD_H
 #define D_DEBUG_PAD_H
 
-#include "m_Do/m_Do_controller_pad.h"
 #include "JSystem/JUtility/TColor.h"
 
 class dDebugPad_c {
 public:
+    enum Mode_e {
+        MODE_CAMERA_e,
+        MODE_LIGHT_e,
+        MODE_WINDTEST_e,
+        MODE_SDCARD_e,
+
+        MODE_MAX_e,
+    };
+
     dDebugPad_c();
 
     bool Active() { return mIsActive; }
@@ -13,6 +21,8 @@ public:
     bool Report(int, int, JUtility::TColor, const char*, ...);
     bool Enable(s32);
     bool Trigger();
+
+    static const char mode_name[MODE_MAX_e][9];
 
     /* 0x0 */ bool mIsActive;
     /* 0x1 */ u8 mTrigger;
